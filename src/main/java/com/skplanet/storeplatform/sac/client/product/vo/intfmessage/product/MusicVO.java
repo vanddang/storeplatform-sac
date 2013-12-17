cdb5506adaf2a61635db1a3a3788ea319d207b20
이태희 <zabara337@gmail.com>
2013-12-17 08:29:01 +0000

    SAC VO작업_이태희
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@2127 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/product/vo/intfmessage/product/MusicVO.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/product/vo/intfmessage/product/MusicVO.java
@@ -14,29 +14,26 @@ import java.util.List;
 
 import com.skplanet.storeplatform.framework.core.annotation.ProtobufMapping;
 import com.skplanet.storeplatform.framework.core.common.vo.CommonVO;
-import com.skplanet.storeplatform.sac.client.intfmessage.common.vo.SourceProto.Source;
 import com.skplanet.storeplatform.sac.client.intfmessage.product.vo.MusicProto;
-import com.skplanet.storeplatform.sac.client.intfmessage.product.vo.ServiceProto.Service;
-import com.skplanet.storeplatform.sac.client.intfmessage.user.vo.BellProto.Bell;
 import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.IdentifierVO;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.SourceVO;
 
 /**
- * Interface Message Identifier Value Object.
+ * Interface Message Music Value Object.
  * 
- * Updated on : 2013. 12. 17. Updated by : 오승민, Incross.
+ * Updated on : 2013. 12. 17. Updated by : 이태희, SK 플래닛.
  */
-@SuppressWarnings("serial")
 @ProtobufMapping(MusicProto.Music.class)
 public class MusicVO extends CommonVO implements Serializable {
-	private IdentifierVO identifier;
-	/**
-	 * 제공 음질 및 사이즈 정보 제공 음악의 경우 type을 audio/mp3-192, audio/mp3-128로 구분한다. 파일 size 정보 Remote 정보
+	private static final long serialVersionUID = 1L;
+
+	private IdentifierVO identifier; // 다운로드 ID
+	/*
+	 * 제공 음질 및 사이즈 정보 제공(음악의 경우 type을 audio/mp3-192, audio/mp3-128로 구분한다.)
 	 */
-	private List<Source> sourceList;
-
-	private List<Service> serviceList;
-
-	private Bell bell;
+	private List<SourceVO> sourceList;
+	private List<ServiceVO> serviceList;
+	private BellVO bell; // 컬러링/벨소리 부가 정보
 
 	public IdentifierVO getIdentifier() {
 		return this.identifier;
@@ -46,28 +43,27 @@ public class MusicVO extends CommonVO implements Serializable {
 		this.identifier = identifier;
 	}
 
-	public List<Source> getSourceList() {
+	public List<SourceVO> getSourceList() {
 		return this.sourceList;
 	}
 
-	public void setSourceList(List<Source> sourceList) {
+	public void setSourceList(List<SourceVO> sourceList) {
 		this.sourceList = sourceList;
 	}
 
-	public List<Service> getServiceList() {
+	public List<ServiceVO> getServiceList() {
 		return this.serviceList;
 	}
 
-	public void setServiceList(List<Service> serviceList) {
+	public void setServiceList(List<ServiceVO> serviceList) {
 		this.serviceList = serviceList;
 	}
 
-	public Bell getBell() {
+	public BellVO getBell() {
 		return this.bell;
 	}
 
-	public void setBell(Bell bell) {
+	public void setBell(BellVO bell) {
 		this.bell = bell;
 	}
-
 }
