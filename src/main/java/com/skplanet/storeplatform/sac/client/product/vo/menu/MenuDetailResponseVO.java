bfa79d482c46cfdcc90e875dc14e7c3691b59448
윤주영 <jy1124yun@gmail.com>
2013-12-19 09:13:02 +0000

    전시 메뉴 API
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@2398 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/product/vo/menu/MenuDetailResponseVO.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.product.vo.menu;
+
+import java.io.Serializable;
+
+import com.skplanet.storeplatform.framework.core.annotation.ProtobufMapping;
+import com.skplanet.storeplatform.framework.core.common.vo.CommonVO;
+import com.skplanet.storeplatform.sac.client.product.vo.MenuDetailResProto;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.CommonResponse;
+
+/**
+ * Interface Message Menu List Value Object.
+ * 
+ * Updated on : 2013. 12. 19 Updated by : 윤주영, SK 플래닛.
+ */
+@ProtobufMapping(MenuDetailResProto.MenuDetailResult.class)
+public class MenuDetailResponseVO extends CommonVO implements Serializable {
+
+	private static final long serialVersionUID = 11123123125L;
+
+	private CommonResponse commonRes;
+	private MenuDetail menu;
+
+	public CommonResponse getCommonRes() {
+		return this.commonRes;
+	}
+
+	public void setCommonRes(CommonResponse commonRes) {
+		this.commonRes = commonRes;
+	}
+
+	public MenuDetail getMenu() {
+		return this.menu;
+	}
+
+	public void setMenu(MenuDetail menu) {
+		this.menu = menu;
+	}
+}
