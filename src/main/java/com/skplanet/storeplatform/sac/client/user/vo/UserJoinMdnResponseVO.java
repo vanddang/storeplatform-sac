7ebc3728f486a09eb5f627867ef0030e07952d30
심대진 <tlaeo00@gmail.com>
2013-12-20 06:49:44 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@2456 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/user/vo/UserJoinMdnResponseVO.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.user.vo;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import com.skplanet.storeplatform.framework.core.annotation.ProtobufMapping;
+
+/**
+ * MDN 회원 가입 응답
+ * 
+ * Updated on : 2013. 12. 19. Updated by : 심대진, 다모아 솔루션.
+ */
+@ProtobufMapping(UserJoinMdnResponseProto.UserJoinMdnResponse.class)
+public class UserJoinMdnResponseVO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 결과 코드
+	 */
+	private String resultCode;
+
+	/**
+	 * 결과 메시지
+	 */
+	private String resultMessage;
+
+	/**
+	 * 사용자 Key 값
+	 */
+	private String userKey;
+
+	public String getResultCode() {
+		return this.resultCode;
+	}
+
+	public void setResultCode(String resultCode) {
+		this.resultCode = resultCode;
+	}
+
+	public String getResultMessage() {
+		return this.resultMessage;
+	}
+
+	public void setResultMessage(String resultMessage) {
+		this.resultMessage = resultMessage;
+	}
+
+	public String getUserKey() {
+		return this.userKey;
+	}
+
+	public void setUserKey(String userKey) {
+		this.userKey = userKey;
+	}
+
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this, ToStringStyle.MULTI_LINE_STYLE);
+	}
+}
