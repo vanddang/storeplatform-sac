014892b622db8c8f320bf4f81c39204da662f392
윤주영 <jy1124yun@gmail.com>
2013-12-23 10:53:30 +0000

    전시 메뉴 API - CATEGORY DETAIL 조회 및 개발 규칙 적용 1. VO 객체 - 객체명에서 VO 제거 2. RQUEST VO 객체 생성
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@2595 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/product/vo/menu/MenuListResponse.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.product.vo.menu;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.skplanet.storeplatform.framework.core.annotation.ProtobufMapping;
+import com.skplanet.storeplatform.framework.core.common.vo.CommonVO;
+import com.skplanet.storeplatform.sac.client.product.vo.MenuListResProto;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.CommonResponse;
+
+/**
+ * Interface Message Menu List Value Object.
+ * 
+ * Updated on : 2013. 12. 19 Updated by : 윤주영, SK 플래닛.
+ */
+@ProtobufMapping(MenuListResProto.MenuListResult.class)
+public class MenuListResponse extends CommonVO implements Serializable {
+
+	private static final long serialVersionUID = 11123123126L;
+
+	private CommonResponse commonRes;
+	private List<MenuDetail> menuList;
+
+	public CommonResponse getCommonRes() {
+		return this.commonRes;
+	}
+
+	public void setCommonRes(CommonResponse commonRes) {
+		this.commonRes = commonRes;
+	}
+
+	public List<MenuDetail> getMenuList() {
+		return this.menuList;
+	}
+
+	public void setMenuList(List<MenuDetail> menuList) {
+		this.menuList = menuList;
+	}
+}
