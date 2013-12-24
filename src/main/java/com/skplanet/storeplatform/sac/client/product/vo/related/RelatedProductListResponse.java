27f7c5245123e8d628da05b783b6b0d7b8dad7d8
윤주영 <jy1124yun@gmail.com>
2013-12-24 09:09:51 +0000

    전시 API - 관련상품 조회 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@2656 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/product/vo/related/RelatedProductListResponse.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.product.vo.related;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonVO;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.CommonResponse;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Product;
+
+/**
+ * Interface Message Menu List Value Object.
+ * 
+ * Updated on : 2013. 12. 19 Updated by : 윤주영, SK 플래닛.
+ */
+public class RelatedProductListResponse extends CommonVO implements Serializable {
+
+	private static final long serialVersionUID = 11123123142L;
+
+	private CommonResponse commonRes;
+
+	private List<Product> productList;
+
+	public CommonResponse getCommonRes() {
+		return this.commonRes;
+	}
+
+	public void setCommonRes(CommonResponse commonRes) {
+		this.commonRes = commonRes;
+	}
+
+	public List<Product> getProductList() {
+		return this.productList;
+	}
+
+	public void setProductList(List<Product> productList) {
+		this.productList = productList;
+	}
+
+}
