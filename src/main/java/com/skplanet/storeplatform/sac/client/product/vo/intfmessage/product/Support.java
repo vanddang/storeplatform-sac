cba1333e2b771756b02ab27b7f6224351cbfd82d
이태희 <zabara337@gmail.com>
2013-12-24 05:14:55 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@2644 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/product/vo/intfmessage/product/Support.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product;
+
+import java.io.Serializable;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonVO;
+
+/**
+ * Interface Message Support Value Object.
+ * 
+ * Updated on : 2013. 12. 24. Updated by : 이태희, SK 플래닛.
+ */
+public class Support extends CommonVO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	/*
+	 * drm : DRM 지원여부, inApp : inApp 부모 상품 구분
+	 */
+	private String type;
+
+	private String text;
+
+	public String getType() {
+		return this.type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getText() {
+		return this.text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+}
