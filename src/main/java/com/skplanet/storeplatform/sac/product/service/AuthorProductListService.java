e747e0730ab7db5d0e70ddd70d9c7011ccb755b8
윤주영 <jy1124yun@gmail.com>
2013-12-24 10:00:31 +0000

    전시 API - 관련상품 조회 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@2661 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/product/service/AuthorProductListService.java
@@ -0,0 +1,30 @@
+package com.skplanet.storeplatform.sac.product.service;
+
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+
+import com.skplanet.storeplatform.sac.client.product.vo.related.AuthorProductListResponse;
+import com.skplanet.storeplatform.sac.client.product.vo.related.RelatedProductRequest;
+
+/**
+ * 특정 작가별 Product List Service 인터페이스(CoreStoreBusiness)
+ * 
+ * Updated on : 2013. 12. 24. Updated by : 윤주영, SK 플래닛.
+ */
+public interface AuthorProductListService {
+
+	/**
+	 * <pre>
+	 * 특정 작가별 상품 리스트 조회.
+	 * </pre>
+	 * 
+	 * @param RelatedProductRequest
+	 *            requestVO
+	 * @return AuthorProductListResponse
+	 */
+	public AuthorProductListResponse searchAuthorProductList(RelatedProductRequest requestVO)
+			throws JsonGenerationException, JsonMappingException, IOException, Exception;
+
+}
