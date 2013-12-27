173062d6aa89b49523bd02b920f87bc05062fda5
반범진 <vanddang@gtsoft.co.kr>
2013-12-27 09:10:02 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@2881 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/user/vo/SellerSecedeResonListRequestVO.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/user/vo/SellerSecedeResonListRequestVO.java
@@ -2,14 +2,11 @@ package com.skplanet.storeplatform.sac.client.user.vo;
 
 import java.io.Serializable;
 
-import com.skplanet.storeplatform.framework.core.annotation.ProtobufMapping;
-
 /**
  * 판매자 회원 탈퇴사유 리스트 request VO
  * 
  * Updated on : 2013-12-20 Updated by : 반범진, 지티소프트.
  */
-@ProtobufMapping(SellerSecedeInfoProto.SellerSecedeResonListRequest.class)
 public class SellerSecedeResonListRequestVO implements Serializable {
 	/**
 	 * 
--- src/main/java/com/skplanet/storeplatform/sac/client/user/vo/SellerSecedeResonListResponseVO.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/user/vo/SellerSecedeResonListResponseVO.java
@@ -3,14 +3,11 @@ package com.skplanet.storeplatform.sac.client.user.vo;
 import java.io.Serializable;
 import java.util.List;
 
-import com.skplanet.storeplatform.framework.core.annotation.ProtobufMapping;
-
 /**
  * 판매자 회원 탈퇴사유 리스트 response VO
  * 
  * Updated on : 2013-12-20 Updated by : 반범진, 지티소프트.
  */
-@ProtobufMapping(SellerSecedeInfoProto.SellerSecedeResonListResponse.class)
 public class SellerSecedeResonListResponseVO implements Serializable {
 
 	/**
