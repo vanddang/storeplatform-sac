6b84c3600852c0900d9ade0c10d795415f186b9a
서영배 <ra2coon@gmail.com>
2013-12-30 04:16:11 +0000

    운영자추런/Feature App/Feature 멀티미디어 상품 조회 API용 Service
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@2927 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/product/feature/recommend/service/EbookComicListService.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.product.feature.recommend.service;
+
+import com.skplanet.storeplatform.sac.client.product.vo.feature.RecommendResponseVO;
+import com.skplanet.storeplatform.sac.client.product.vo.feature.RecommendProductRequest;
+
+/**
+ * 
+ * 
+ * Updated on : 2013. 12. 19. Updated by : 서영배, GTSOFT.
+ */
+public interface EbookComicListService {
+
+	/**
+	 * <pre>
+	 * 운영자 추천 전체 카테고리 상품 리스트 조회
+	 * </pre>
+	 * 
+	 * @param
+	 * 
+	 * @return 운영자 추천 전체 카테고리 상품 리스트
+	 */
+	RecommendResponseVO searchEbookComicList(RecommendProductRequest requestVO);
+}
