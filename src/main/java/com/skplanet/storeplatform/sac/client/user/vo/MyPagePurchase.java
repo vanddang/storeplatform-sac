6c6245024c79e893bc42f32e2a5cd4f2a758efba
김현일 <nahiyak@incross.com>
2013-12-30 01:50:40 +0000

    valueObject refacting : suffix 'VO' 제거
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@2891 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/user/vo/MyPagePurchase.java
@@ -0,0 +1,183 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.user.vo;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * 
+ * 마이페이지 구매
+ * 
+ * Updated on : 2013. 11. 28. Updated by : 김현일, 인크로스.
+ */
+public class MyPagePurchase implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private int id;
+
+	private String pid;
+
+	private String payId;
+
+	private String userId;
+
+	private String purchaseId;
+
+	private double num;
+
+	/**
+	 * @return the num
+	 */
+	public double getNum() {
+		return this.num;
+	}
+
+	/**
+	 * @param num
+	 *            the num to set
+	 */
+	public void setNum(double num) {
+		this.num = num;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * method 설명.
+	 * </pre>
+	 * 
+	 * @return int
+	 */
+	public int getId() {
+		return this.id;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * method 설명.
+	 * </pre>
+	 * 
+	 * @param id
+	 *            id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * method 설명.
+	 * </pre>
+	 * 
+	 * @return String
+	 */
+	public String getPid() {
+		return this.pid;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * method 설명.
+	 * </pre>
+	 * 
+	 * @param pid
+	 *            pid
+	 */
+	public void setPid(String pid) {
+		this.pid = pid;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * method 설명.
+	 * </pre>
+	 * 
+	 * @return String
+	 */
+	public String getPayId() {
+		return this.payId;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * method 설명.
+	 * </pre>
+	 * 
+	 * @param payId
+	 *            payId
+	 */
+	public void setPayId(String payId) {
+		this.payId = payId;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * method 설명.
+	 * </pre>
+	 * 
+	 * @return String
+	 */
+	public String getUserId() {
+		return this.userId;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * method 설명.
+	 * </pre>
+	 * 
+	 * @param userId
+	 *            userId
+	 */
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * method 설명.
+	 * </pre>
+	 * 
+	 * @return String
+	 */
+	public String getPurchaseId() {
+		return this.purchaseId;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * method 설명.
+	 * </pre>
+	 * 
+	 * @param purchaseId
+	 *            purchaseId
+	 */
+	public void setPurchaseId(String purchaseId) {
+		this.purchaseId = purchaseId;
+	}
+
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this, ToStringStyle.MULTI_LINE_STYLE);
+	}
+}
