4492d160e9b682f02440c53449f02351cd500f87
오승민 <osm1021@sptek.co.kr>
2013-12-30 04:59:37 +0000

    상수 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@2935 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/api/conts/DisplayConstants.java
+++ src/main/java/com/skplanet/storeplatform/sac/api/conts/DisplayConstants.java
@@ -16,4 +16,57 @@ package com.skplanet.storeplatform.sac.api.conts;
  */
 public class DisplayConstants {
 
+	// 상품 서비스 그룹 코드
+
+	/**
+	 * 멀티미디어
+	 */
+	public static final String DP_MULTIMEDIA_PROD_SVC_GRP_CD = "DP000203";
+	/**
+	 * Tstore 쇼핑
+	 */
+	public static final String DP_TSTORE_SHOPPING_PROD_SVC_GRP_CD = "DP000206";
+	/**
+	 * 소셜쇼핑
+	 */
+	public static final String DP_SOCIAL_SHOPPING_PROD_SVC_GRP_CD = "DP000205";
+	/**
+	 * 폰꾸미기
+	 */
+	public static final String DP_PHONE_DECO_PROD_SVC_GRP_CD = "DP000204";
+	/**
+	 * 애플리캐이션
+	 */
+	public static final String DP_APP_PROD_SVC_GRP_CD = "DP000201";
+
+	// 상품 서비스 타입 코드
+	/**
+	 * 제너럴 Type
+	 */
+	public static final String DP_GENERAL_TYPE_SVC_TYPE_CD = "DP001113";
+	/**
+	 * 뮤직 Type
+	 */
+	public static final String DP_MUSIC_TYPE_SVC_TYPE_CD = "DP001111";
+	/**
+	 * APP Type
+	 */
+	public static final String DP_APP_TYPE_SVC_TYPE_CD = "DP001114";
+	/**
+	 * 고화질 동영상 Type
+	 */
+	public static final String DP_HVOD_TYPE_SVC_TYPE_CD = "DP001115";
+	/**
+	 * 동영상 Type
+	 */
+	public static final String DP_VOD_TYPE_SVC_TYPE_CD = "DP001112";
+	/**
+	 * 이북(eBook)/툰도시(Comic) Type
+	 */
+	public static final String DP_EBOOK_COMIC_TYPE_SVC_TYPE_CD = "DP001116";
+	/**
+	 * Tstore 쇼핑 Type
+	 */
+	public static final String DP_TSOTRE_SHOPPING_TYPE_SVC_TYPE_CD = "DP001117";
+
 }
