a227fdf411aea224d23e5ab388aeeb715320b88b
오승민 <osm1021@sptek.co.kr>
2013-12-30 04:48:15 +0000

    전시 상수 class 작성
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@2931 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/api/conts/DisplayConstants.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.api.conts;
+
+/**
+ * 전시용 상수 Class
+ * 
+ * Updated on : 2013. 12. 30. Updated by : 오승민, Incross.
+ */
+public class DisplayConstants {
+
+}
