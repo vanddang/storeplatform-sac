8619fad0e8378668c89e2115ee9628d28f31d573
이태희 <zabara337@gmail.com>
2014-01-03 03:59:31 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@3185 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/display/category/controller/CategoryController.java
@@ -0,0 +1,24 @@
+package com.skplanet.storeplatform.sac.display.category.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.skplanet.storeplatform.sac.client.display.vo.category.CategoryAppReq;
+import com.skplanet.storeplatform.sac.client.display.vo.category.CategoryAppRes;
+import com.skplanet.storeplatform.sac.display.category.service.CategoryAppService;
+
+@Controller
+@RequestMapping("/display/category")
+public class CategoryController {
+	@Autowired
+	private CategoryAppService categoryAppService;
+
+	@RequestMapping(value = "/app/list/v1", method = RequestMethod.GET)
+	@ResponseBody
+	public CategoryAppRes searchCategoryAppList(CategoryAppReq req) {
+		return this.categoryAppService.searchCategoryAppList(req);
+	}
+}
