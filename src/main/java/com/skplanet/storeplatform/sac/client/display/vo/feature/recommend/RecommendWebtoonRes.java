b428676db69fdc9bee8bbbef8c3ea5c79f756538
김형식 <jade.kimhs@gmail.com>
2014-01-03 02:52:04 +0000

    운영자 추천 웹툰 상품
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@3169 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/feature/recommend/RecommendWebtoonRes.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ import java.util.List;
+
+import com.skplanet.storeplatform.framework.core.annotation.ProtobufMapping;
+lose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.display.vo.feature.recommend;
+
+import java.util.List;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.CommonResponse;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Product;
+
+/**
+ * 웹툰 리스트 조회 List Value Object.
+ * 
+ * Updated on : 2013. 11. 28. Updated by : 김형식, SK 플래닛.
+ */
+public class RecommendWebtoonRes extends CommonInfo {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private List<Product> productList;
+	private CommonResponse commonResponse;
+
+	public List<Product> getProductList() {
+		return this.productList;
+	}
+
+	public void setProductList(List<Product> productList) {
+		this.productList = productList;
+	}
+
+	public CommonResponse getCommonResponse() {
+		return this.commonResponse;
+	}
+
+	public void setCommonResponse(CommonResponse commonResponse) {
+		this.commonResponse = commonResponse;
+	}
+
+}
