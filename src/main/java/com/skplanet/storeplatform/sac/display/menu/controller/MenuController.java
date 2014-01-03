621c34c73ba50fd72944d82565367b39d50d6e2b
윤주영 <jy1124yun@gmail.com>
2014-01-03 09:45:31 +0000

    전시 menu controller 적용 - Request VO
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@3251 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/display/menu/controller/MenuController.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/menu/controller/MenuController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.skplanet.storeplatform.sac.client.display.vo.menu.CategoryDetailListResponse;
@@ -33,59 +32,40 @@ public class MenuController {
 
 	@RequestMapping(value = "/list/v1", method = RequestMethod.GET)
 	@ResponseBody
-	public MenuListResponse searchMenuList(@RequestParam String tenantId, @RequestParam String systemId)
-			throws JsonGenerationException, JsonMappingException, IOException, Exception {
+	public MenuListResponse searchMenuList(MenuRequest requestVO) throws JsonGenerationException, JsonMappingException,
+			IOException, Exception {
 
 		this.logger.debug("MenuController.searchMenuList start !!");
-
-		MenuRequest requestVO = new MenuRequest();
-		requestVO.setTenantId(tenantId);
-		requestVO.setSystemId(systemId);
 
 		return this.menuListService.searchMenuList(requestVO);
 	}
 
 	@RequestMapping(value = "/specific/list/v1", method = RequestMethod.GET)
 	@ResponseBody
-	public MenuListResponse searchMenu(@RequestParam String tenantId, @RequestParam String systemId,
-			@RequestParam String menuId) throws JsonGenerationException, JsonMappingException, IOException, Exception {
+	public MenuListResponse searchMenu(MenuRequest requestVO) throws JsonGenerationException, JsonMappingException,
+			IOException, Exception {
 
 		this.logger.debug("MenuController.searchMenu start !!");
-
-		MenuRequest requestVO = new MenuRequest();
-		requestVO.setTenantId(tenantId);
-		requestVO.setSystemId(systemId);
-		requestVO.setMenuId(menuId);
 
 		return this.menuListService.searchMenuList(requestVO);
 	}
 
 	@RequestMapping(value = "/category/list/v1", method = RequestMethod.GET)
 	@ResponseBody
-	public CategoryListResponse searchTopCategoryList(@RequestParam String tenantId, @RequestParam String systemId)
-			throws JsonGenerationException, JsonMappingException, IOException, Exception {
+	public CategoryListResponse searchTopCategoryList(MenuRequest requestVO) throws JsonGenerationException,
+			JsonMappingException, IOException, Exception {
 
 		this.logger.debug("MenuController.searchTopCategoryList start !!");
-
-		MenuRequest requestVO = new MenuRequest();
-		requestVO.setTenantId(tenantId);
-		requestVO.setSystemId(systemId);
 
 		return this.categoryService.searchTopCategoryList(requestVO);
 	}
 
 	@RequestMapping(value = "/category/subCategory/list/v1", method = RequestMethod.GET)
 	@ResponseBody
-	public CategoryDetailListResponse searchDetailCategoryList(@RequestParam String tenantId,
-			@RequestParam String systemId, @RequestParam String menuId) throws JsonGenerationException,
+	public CategoryDetailListResponse searchDetailCategoryList(MenuRequest requestVO) throws JsonGenerationException,
 			JsonMappingException, IOException, Exception {
 
 		this.logger.debug("MenuController.searchDetailCategoryList start !!");
-
-		MenuRequest requestVO = new MenuRequest();
-		requestVO.setTenantId(tenantId);
-		requestVO.setSystemId(systemId);
-		requestVO.setMenuId(menuId);
 
 		return this.categoryService.searchDetailCategoryList(requestVO);
 	}
