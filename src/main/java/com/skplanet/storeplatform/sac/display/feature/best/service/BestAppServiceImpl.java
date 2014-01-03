f0f2483f5ed03086ab47bcf486ffac0950980dcf
이석희 <winterer81@naver.com>
2014-01-03 04:57:54 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@3201 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/display/feature/best/service/BestAppServiceImpl.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.display.feature.best.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.skplanet.storeplatform.framework.core.persistence.dao.CommonDAO;
+import com.skplanet.storeplatform.sac.client.display.feature.best.BestAppReq;
+import com.skplanet.storeplatform.sac.client.display.feature.best.BestAppRes;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.CommonResponse;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.Identifier;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.Menu;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.Price;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.Source;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.Title;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Accrual;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.App;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Product;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Rights;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Support;
+
+/**
+ * ProductCategory Service 인터페이스(CoreStoreBusiness) 구현체
+ * 
+ * Updated on : 2013. 12. 19. Updated by : 이석희, SK 플래닛.
+ */
+@Service
+public class BestAppServiceImpl implements BestAppService {
+	@Autowired
+	@Qualifier("sac")
+	private CommonDAO commonDAO;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.skplanet.storeplatform.sac.biz.product.service.BestAppService#BestAppService(com.skplanet
+	 * .storeplatform.sac.client.product.vo.BestAppReqVO)
+	 */
+	@Override
+	public BestAppRes searchBestAppList(BestAppReq bestAppReq) {
+		BestAppRes response = new BestAppRes();
+
+		List<Product> productList = new ArrayList<Product>();
+		List<Menu> menuList = new ArrayList<Menu>();
+		List<Source> sourceList = new ArrayList<Source>();
+		List<Support> supportList = new ArrayList<Support>();
+
+		CommonResponse commonResponse = new CommonResponse();
+		commonResponse.setTotalCount(10);
+
+		for (int i = 1; i <= 1; i++) {
+			Product product = new Product();
+			Identifier identifier = new Identifier();
+			App app = new App();
+			Accrual accrual = new Accrual();
+			Rights rights = new Rights();
+			Source source = new Source();
+			Price price = new Price();
+			Title title = new Title();
+			Support support = new Support();
+
+			// 상품ID
+			identifier = new Identifier();
+			identifier.setType("product" + i);
+			identifier.setText("H090101222_" + i);
+
+			support.setType("Y");
+			support.setText("iab");
+			supportList.add(support);
+
+			/*
+			 * Menu(메뉴정보) Id, Name, Type
+			 */
+			Menu menu = new Menu();
+			menu.setId("dummyMenuId0");
+			menu.setName("dummyMenuName0");
+			menu.setType("dummyMenuType0");
+			menuList.add(menu);
+			menu = new Menu();
+			menu.setId("dummyMenuId1");
+			menu.setName("dummyMenuName1");
+			menu.setType("dummyMenuType1");
+			menuList.add(menu);
+
+			/*
+			 * App aid, packagename, versioncode, version
+			 */
+			app.setAid("A090101222_" + i);
+			app.setPackageName("app dummy package" + i);
+			app.setVersionCode("dummy version Code" + i);
+			app.setVersion("1.1");
+
+			/*
+			 * Accrual voterCount (참여자수) DownloadCount (다운로드 수) score(평점)
+			 */
+			accrual.setVoterCount("1234");
+			accrual.setDownloadCount("800");
+			accrual.setScore(3.3);
+
+			/*
+			 * Rights grade
+			 */
+			rights.setGrade("1");
+
+			title.setText("베스트 앱_" + i);
+
+			/*
+			 * source mediaType, size, type, url
+			 */
+			source.setMediaType("media_" + i);
+			source.setSize("1024_" + i);
+			source.setType("thumbNail");
+			source.setUrl("http://www.naver.com");
+			sourceList.add(source);
+
+			/*
+			 * Price text
+			 */
+			price.setText(0);
+
+			product = new Product();
+			product.setIdentifier(identifier);
+			// product.setSupport("y|iab");
+			product.setSupportList(supportList);
+			product.setMenuList(menuList);
+			product.setApp(app);
+			product.setAccrual(accrual);
+			product.setRights(rights);
+			product.setTitle(title);
+			product.setSourceList(sourceList);
+			product.setProductExplain("앱 설명_" + i);
+			product.setPrice(price);
+
+			productList.add(product);
+
+		}
+		response.setCommonResponse(commonResponse);
+		response.setProductList(productList);
+
+		return response;
+	}
+}
