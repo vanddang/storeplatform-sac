f302128f997d6a8af9c8b72e8b8d6ec62e35e4dd
서영배 <ra2coon@gmail.com>
2014-01-06 06:18:55 +0000

    운영자 추천 Req/Res
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@3367 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/feature/category/CategoryEpubRes.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.display.vo.feature.category;
+
+import java.util.List;
+
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.CommonResponse;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Product;
+
+/**
+ * Feature 상품  리스트 조회 List Value Object.
+ * 
+ * Updated on : 2013. 12. 20. Updated by : 서영배, GTSOFT.
+ */
+public class CategoryEpubRes {
+	private CommonResponse commonResponse;
+	private List<Product> featureProductList;
+
+	public List<Product> getFeatureProductList() {
+		return featureProductList;
+	}
+
+	public void setFeatureProductList(List<Product> featureProductList) {
+		this.featureProductList = featureProductList;
+	}
+
+	public CommonResponse getCommonResponse() {
+		return commonResponse;
+	}
+
+	public void setCommonResponse(CommonResponse commonResponse) {
+		this.commonResponse = commonResponse;
+	}
+}
