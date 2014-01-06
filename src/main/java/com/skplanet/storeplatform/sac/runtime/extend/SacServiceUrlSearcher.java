40bc79dddddb88cb45683657fb09ca7931c7af01
김현일 <nahiyak@incross.com>
2014-01-06 11:57:15 +0000

    URL Searcher 내부 포트 불러오도록 수정. 개발서버 /sp_sac 기준으로 설정
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@3453 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/runtime/extend/SacServiceUrlSearcher.java
+++ src/main/java/com/skplanet/storeplatform/sac/runtime/extend/SacServiceUrlSearcher.java
@@ -9,10 +9,13 @@
  */
 package com.skplanet.storeplatform.sac.runtime.extend;
 
+import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -24,10 +27,12 @@ public class SacServiceUrlSearcher implements ServiceUrlSearcher {
 
 	@Value("#{propertiesForSac['skp.common.controller.host']}")
 	private String localHost;
-	@Value("#{propertiesForSac['skp.common.controller.rootPath']}")
-	private String rootPath;
+	@Value("#{propertiesForSac['skp.common.controller.servletPath']}")
+	private String servletPath;
 	@Value("#{propertiesForSac['component.external.baseUrl']}")
 	private String externalBaseUrl;
+	@Value("#{propertiesForSac['skp.common.server.rootPath']}")
+	private String rootPath;
 
 	@Override
 	public String search(Map<String, Object> headerMap) {
@@ -35,14 +40,38 @@ public class SacServiceUrlSearcher implements ServiceUrlSearcher {
 		String requestUrl = (String) headerMap.get("http_requestUrl");
 		String prefix = interfaceId.substring(1, 3);
 		UriComponents from = UriComponentsBuilder.fromHttpUrl(requestUrl).build();
-		String httpUrl = "";
+		UriComponentsBuilder to;
 		if (prefix.equals("05")) {
-			httpUrl = this.externalBaseUrl;
+			to = UriComponentsBuilder.fromHttpUrl(this.externalBaseUrl).query(from.getQuery());
 		} else {
-			httpUrl = new StringBuilder().append(this.localHost).append(this.rootPath).append(from.getPath())
-					.toString();
+			to = UriComponentsBuilder.fromHttpUrl(this.localHost).port(from.getPort());
+			List<String> rootPathList = UriComponentsBuilder.newInstance().path(this.rootPath).build()
+					.getPathSegments();
+			List<String> pathSegments = from.getPathSegments();
+
+			if (!CollectionUtils.isEmpty(rootPathList) && !CollectionUtils.isEmpty(pathSegments)) {
+				if (rootPathList.size() <= pathSegments.size()) {
+					for (int i = 0; i < rootPathList.size(); i++) {
+						if (!StringUtils.equals(rootPathList.get(i), pathSegments.get(i))) {
+							throw new RuntimeException("root path not match");
+						}
+					}
+					for (String path : rootPathList) {
+						to.path("/" + path);
+					}
+					to.path("/" + this.servletPath);
+					if (rootPathList.size() < pathSegments.size()) {
+						for (int i = rootPathList.size(); i < pathSegments.size(); i++) {
+							to.path("/" + pathSegments.get(i));
+						}
+					}
+				} else {
+					throw new RuntimeException("root path not match");
+				}
+			} else {
+				to.path("/" + this.servletPath + from.getPath());
+			}
 		}
-		UriComponents to = UriComponentsBuilder.fromHttpUrl(httpUrl).query(from.getQuery()).build();
-		return to.toUriString();
+		return to.build().toUriString();
 	}
 }
