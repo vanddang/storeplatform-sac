dc9888e635c284cca9c9c938b0b5a5cc476a1c08
반범진 <vanddang@gtsoft.co.kr>
2014-01-06 02:43:56 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@3313 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/member/user/dummy/DummyDataController.java
+++ src/main/java/com/skplanet/storeplatform/sac/member/user/dummy/DummyDataController.java
@@ -9,6 +9,9 @@
  */
 package com.skplanet.storeplatform.sac.member.user.dummy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +20,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.skplanet.storeplatform.sac.client.member.vo.common.DeviceExtraInfo;
+import com.skplanet.storeplatform.sac.client.member.vo.common.DeviceInfo;
+import com.skplanet.storeplatform.sac.client.member.vo.user.AuthorizeByIdRes;
+import com.skplanet.storeplatform.sac.client.member.vo.user.AuthorizeByMdnRes;
 import com.skplanet.storeplatform.sac.client.member.vo.user.CreateBySimpleRes;
+import com.skplanet.storeplatform.sac.client.member.vo.user.CreateDeviceRes;
 import com.skplanet.storeplatform.sac.client.member.vo.user.CreateOcbInformationRes;
 import com.skplanet.storeplatform.sac.client.member.vo.user.CreateRealNameRes;
 import com.skplanet.storeplatform.sac.client.member.vo.user.CreateTermsAgreementRes;
 import com.skplanet.storeplatform.sac.client.member.vo.user.GetOcbInformationRes;
+import com.skplanet.storeplatform.sac.client.member.vo.user.ListDeviceRes;
 import com.skplanet.storeplatform.sac.client.member.vo.user.ModifyEmailRes;
 import com.skplanet.storeplatform.sac.client.member.vo.user.ModifyPasswordRes;
 import com.skplanet.storeplatform.sac.client.member.vo.user.ModifyRes;
@@ -39,7 +48,8 @@ import com.skplanet.storeplatform.sac.member.user.controller.UserJoinController;
 @Controller
 public class DummyDataController {
 
-	private static final Logger logger = LoggerFactory.getLogger(UserJoinController.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(UserJoinController.class);
 
 	@Autowired
 	private HeaderInfo headerInfo;
@@ -58,7 +68,8 @@ public class DummyDataController {
 	// return res;
 	// }
 	//
-	// @RequestMapping(value = "/createByAgreement/v1", method = RequestMethod.POST)
+	// @RequestMapping(value = "/createByAgreement/v1", method =
+	// RequestMethod.POST)
 	// @ResponseBody
 	// public CreateByAgreementRes createByAgreement() {
 	//
@@ -82,6 +93,36 @@ public class DummyDataController {
 
 		CreateBySimpleRes res = new CreateBySimpleRes();
 		res.setUserKey("IW102158844420091030165015");
+
+		return res;
+	}
+
+	@RequestMapping(value = "/authorizeByMdn/v1", method = RequestMethod.GET)
+	@ResponseBody
+	public AuthorizeByMdnRes authorizeByMdn() {
+
+		logger.info("####################################################");
+		logger.info("##### 5.1.4. 모바일 전용 회원 인증 (MDN 인증) #####");
+		logger.info("####################################################");
+
+		AuthorizeByMdnRes res = new AuthorizeByMdnRes();
+		res.setUserKey("IW102158844420091030165015");
+		res.setUserAuthKey("1234567890");
+
+		return res;
+	}
+
+	@RequestMapping(value = "/authorizeById/v1", method = RequestMethod.POST)
+	@ResponseBody
+	public AuthorizeByIdRes authorizeById() {
+
+		logger.info("####################################################");
+		logger.info("##### 5.1.5. ID 기반 회원 인증 (One ID, IDP 회원) #####");
+		logger.info("####################################################");
+
+		AuthorizeByIdRes res = new AuthorizeByIdRes();
+		res.setUserKey("IW102158844420091030165015");
+		res.setUserAuthKey("1234567890");
 
 		return res;
 	}
@@ -137,6 +178,67 @@ public class DummyDataController {
 		logger.info("######################################");
 
 		CreateTermsAgreementRes res = new CreateTermsAgreementRes();
+		res.setUserKey("IW102158844420091030165015");
+
+		return res;
+	}
+
+	@RequestMapping(value = "/listDevice/v1", method = RequestMethod.GET)
+	@ResponseBody
+	public ListDeviceRes listDevice() {
+
+		logger.info("####################################################");
+		logger.info("##### 5.1.17.	휴대기기 목록 조회 #####");
+		logger.info("####################################################");
+
+		ListDeviceRes res = new ListDeviceRes();
+
+		/*
+		 * 휴대기기정보 리스트
+		 */
+		List<DeviceInfo> deviceInfoList = new ArrayList<DeviceInfo>();
+
+		/*
+		 * 휴대기기 부가정보 리스트
+		 */
+		List<DeviceExtraInfo> deviceExtraInfoList = new ArrayList<DeviceExtraInfo>();
+		DeviceExtraInfo deviceExtraInfo = new DeviceExtraInfo();
+		deviceExtraInfo.setExtraProfile("US011404");
+		deviceExtraInfo.setExtraProfileValue("LGFL");
+
+		deviceExtraInfoList.add(deviceExtraInfo);
+
+		deviceInfoList.get(0).setDeviceExtraInfoList(deviceExtraInfoList);
+		deviceInfoList.get(0).setDeviceKey("");
+		deviceInfoList.get(0).setDeviceId("01011112222");
+		deviceInfoList.get(0).setDeviceType("");
+		deviceInfoList.get(0).setDeviceModelNo("LG-SH810");
+		deviceInfoList.get(0).setDeviceTelecom("SKT");
+		deviceInfoList.get(0).setSvcMgmtNum("");
+		deviceInfoList.get(0).setDeviceNickName("LG-SH810");
+		deviceInfoList.get(0).setIsPrimary("Y");
+		deviceInfoList.get(0).setIsAuthenticated("Y");
+		deviceInfoList.get(0).setAuthenticationDate("20140106");
+		deviceInfoList.get(0).setOsVer("1.0");
+		deviceInfoList.get(0).setMakeComp("SAMSUNG");
+		deviceInfoList.get(0).setUserId("hkd");
+
+		res.setDeviceInfoList(deviceInfoList);
+
+		return res;
+	}
+
+	@RequestMapping(value = "/createDevice/v1", method = RequestMethod.GET)
+	@ResponseBody
+	public CreateDeviceRes createDevice() {
+
+		logger.info("####################################################");
+		logger.info("##### 5.1.18.	휴대기기 등록 #####");
+		logger.info("####################################################");
+
+		CreateDeviceRes res = new CreateDeviceRes();
+		res.setDeviceId("01011112222");
+		res.setDeviceKey("01011112222");
 		res.setUserKey("IW102158844420091030165015");
 
 		return res;
