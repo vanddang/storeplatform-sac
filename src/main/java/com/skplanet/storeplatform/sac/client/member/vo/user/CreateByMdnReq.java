306271172c231ef10aac7d351280a8dac4396a99
심대진 <tlaeo00@gmail.com>
2014-01-06 05:48:21 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@3346 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/member/vo/user/CreateByMdnReq.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/user/CreateByMdnReq.java
@@ -2,8 +2,6 @@ package com.skplanet.storeplatform.sac.client.member.vo.user;
 
 import java.util.List;
 
-import javax.validation.constraints.NotNull;
-
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
@@ -21,10 +19,10 @@ public class CreateByMdnReq extends CommonInfo {
 	/**
 	 * 기기 ID (mdn, uuid)
 	 */
-	@NotNull(message = "아놔짬뽕나...")
+	@NotEmpty(message = "필수 파라미터 입니다.")
 	private String deviceId;
 
-	@NotEmpty(message = "아놔 짬뽕나...")
+	@NotEmpty(message = "필수 파라미터 입니다.")
 	private String deviceTelecom;
 
 	private String joinId;
