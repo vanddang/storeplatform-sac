8e7252b34e57a877221bb9092c42fe343bef3ac8
심대진 <tlaeo00@gmail.com>
2014-01-06 00:47:27 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@3276 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/member/user/dummy/DummyDataController.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.member.user.dummy;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.skplanet.storeplatform.sac.client.member.vo.user.CreateByAgreementReq;
+import com.skplanet.storeplatform.sac.client.member.vo.user.CreateByAgreementRes;
+import com.skplanet.storeplatform.sac.client.member.vo.user.CreateByMdnReq;
+import com.skplanet.storeplatform.sac.client.member.vo.user.CreateByMdnRes;
+import com.skplanet.storeplatform.sac.member.user.common.HeaderInfo;
+import com.skplanet.storeplatform.sac.member.user.controller.UserJoinController;
+
+/**
+ * 회원 가입 서비스 Controller
+ * 
+ * Updated on : 2014. 1. 2. Updated by : 심대진, 다모아 솔루션.
+ */
+@RequestMapping(value = "/member/user/dummy")
+@Controller
+public class DummyDataController {
+
+	private static final Logger logger = LoggerFactory.getLogger(UserJoinController.class);
+
+	@Autowired
+	private HeaderInfo headerInfo;
+
+	@RequestMapping(value = "/createByMdn/v1", method = RequestMethod.POST)
+	@ResponseBody
+	public CreateByMdnRes dummyData1(@RequestBody CreateByMdnReq req, @RequestHeader Map<String, Object> headers) {
+
+		logger.info("####################################################");
+		logger.info("##### 5.1.1. 모바일 전용 회원 가입 (MDN 회원 가입) #####");
+		logger.info("####################################################");
+
+		CreateByMdnRes res = new CreateByMdnRes();
+		res.setUserKey("IW102158844420091030165015");
+
+		return res;
+	}
+
+	@RequestMapping(value = "/createByAgreement/v1", method = RequestMethod.POST)
+	@ResponseBody
+	public CreateByAgreementRes dummyData2(@RequestBody CreateByAgreementReq req, @RequestHeader Map<String, Object> headers) {
+
+		logger.info("####################################################");
+		logger.info("##### 5.1.2. ID 회원 약관 동의 가입 (One ID 회원) #####");
+		logger.info("####################################################");
+
+		CreateByAgreementRes res = new CreateByAgreementRes();
+		res.setUserKey("IW102158844420091030165015");
+
+		return res;
+	}
+}
