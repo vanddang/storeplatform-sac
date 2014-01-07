3eda7f8298dd480afcf9b162d1d0aeca1e04d350
한서구 <seoguman@nate.com>
2014-01-07 05:37:44 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@3505 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/seller/DetailInformationReq.java
@@ -0,0 +1,37 @@
+package com.skplanet.storeplatform.sac.client.member.vo.seller;
+
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+/**
+ * 판매자회원 기본 정보 조회
+ * 
+ * Updated on : 2014. 1. 7. Updated by : 한서구, 부르칸.
+ */
+@JsonSerialize(include = Inclusion.NON_NULL)
+public class DetailInformationReq extends CommonInfo {
+
+	private static final long serialVersionUID = 1L;
+
+	private String sellerKey;
+	private String aid;
+
+	public String getSellerKey() {
+		return this.sellerKey;
+	}
+
+	public void setSellerKey(String sellerKey) {
+		this.sellerKey = sellerKey;
+	}
+
+	public String getAid() {
+		return this.aid;
+	}
+
+	public void setAid(String aid) {
+		this.aid = aid;
+	}
+
+}
