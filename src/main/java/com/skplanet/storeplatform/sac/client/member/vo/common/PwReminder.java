695023633e058d117d9f6b969efbe784624ec390
한서구 <seoguman@nate.com>
2014-01-07 04:25:07 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@3486 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/member/vo/common/PwReminder.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/common/PwReminder.java
@@ -5,6 +5,11 @@ import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
 
 import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
 
+/**
+ * 사용자 부가정보
+ * 
+ * Updated on : 2014. 1. 7. Updated by : 한서구, 부르칸.
+ */
 @JsonSerialize(include = Inclusion.NON_NULL)
 public class PwReminder extends CommonInfo {
 
