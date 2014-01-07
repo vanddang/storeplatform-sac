6765c1ee10eb2f3815a139dc2eefe32b72b8d3ff
강신완 <@gmail.com>
2014-01-07 09:26:03 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@3527 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/member/user/controller/UserSelectController.java
+++ src/main/java/com/skplanet/storeplatform/sac/member/user/controller/UserSelectController.java
@@ -9,23 +9,12 @@
  */
 package com.skplanet.storeplatform.sac.member.user.controller;
 
-import java.util.Map;
-
-import javax.validation.Valid;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.skplanet.storeplatform.sac.client.member.vo.common.HeaderVo;
-import com.skplanet.storeplatform.sac.client.member.vo.user.ExistReq;
-import com.skplanet.storeplatform.sac.client.member.vo.user.ExistRes;
 import com.skplanet.storeplatform.sac.member.common.HeaderInfo;
 import com.skplanet.storeplatform.sac.member.common.ParameterExceptionHandling;
 import com.skplanet.storeplatform.sac.member.user.service.UserSelectService;
@@ -47,30 +36,30 @@ public class UserSelectController extends ParameterExceptionHandling {
 	@Autowired
 	private HeaderInfo headerInfo;
 
-	@RequestMapping(value = "/exist/v1", method = RequestMethod.POST)
-	@ResponseBody
-	public ExistRes exist(@Valid @RequestBody ExistReq req, @RequestHeader Map<String, Object> headers) {
-		logger.info("####################################################");
-		logger.info("##### 5.1.6. 회원 가입 여부 조회 (ID/MDN 기반) #####");
-		logger.info("####################################################");
-
-		/**
-		 * Header 정보 세팅
-		 */
-		HeaderVo headerVo = this.headerInfo.getHeader(headers);
-
-		/**
-		 * 파라미터 유효성 체크 todo
-		 */
-
-		/**
-		 * 회원기본정보 조회 Biz
-		 */
-		ExistRes res = this.svc.exist(headerVo, req);
-
-		logger.info("Response : {}", res.toString());
-
-		return res;
-	}
+	// @RequestMapping(value = "/exist/v1", method = RequestMethod.POST)
+	// @ResponseBody
+	// public ExistRes exist(@Valid @RequestBody ExistReq req, @RequestHeader Map<String, Object> headers) {
+	// logger.info("####################################################");
+	// logger.info("##### 5.1.6. 회원 가입 여부 조회 (ID/MDN 기반) #####");
+	// logger.info("####################################################");
+	//
+	// /**
+	// * Header 정보 세팅
+	// */
+	// HeaderVo headerVo = this.headerInfo.getHeader(headers);
+	//
+	// /**
+	// * 파라미터 유효성 체크 todo
+	// */
+	//
+	// /**
+	// * 회원기본정보 조회 Biz
+	// */
+	// ExistRes res = this.svc.exist(headerVo, req);
+	//
+	// logger.info("Response : {}", res.toString());
+	//
+	// return res;
+	// }
 
 }
