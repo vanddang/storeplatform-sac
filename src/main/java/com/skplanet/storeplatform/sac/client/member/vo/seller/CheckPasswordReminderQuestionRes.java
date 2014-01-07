a72be5645a23fb6ffc871211cc4b08cfb98e195e
한서구 <seoguman@nate.com>
2014-01-07 00:33:10 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@3460 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/member/vo/seller/CheckPasswordReminderQuestionRes.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/seller/CheckPasswordReminderQuestionRes.java
@@ -1,6 +1,14 @@
 package com.skplanet.storeplatform.sac.client.member.vo.seller;
 
-public class CheckPasswordReminderQuestionRes {
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+@JsonSerialize(include = Inclusion.NON_NULL)
+public class CheckPasswordReminderQuestionRes extends CommonInfo {
+
+	private static final long serialVersionUID = 1L;
 
 	private String isCorrect;
 
