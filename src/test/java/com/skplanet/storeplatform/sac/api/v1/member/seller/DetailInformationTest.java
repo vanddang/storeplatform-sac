b0ac4a03afaf535ae4aafddfceb9dd1ef8d88e3c
한서구 <seoguman@nate.com>
2014-01-08 06:34:03 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@3599 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/test/java/com/skplanet/storeplatform/sac/api/v1/member/seller/DetailInformationTest.java
@@ -0,0 +1,96 @@
+package com.skplanet.storeplatform.sac.api.v1.member.seller;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.skplanet.storeplatform.framework.test.RequestBodySetter;
+import com.skplanet.storeplatform.framework.test.SuccessCallback;
+import com.skplanet.storeplatform.framework.test.TestCaseTemplate;
+import com.skplanet.storeplatform.framework.test.TestCaseTemplate.RunMode;
+import com.skplanet.storeplatform.sac.client.member.vo.common.KeySearch;
+import com.skplanet.storeplatform.sac.client.member.vo.seller.DetailInformationReq;
+import com.skplanet.storeplatform.sac.client.member.vo.seller.LockAccountRes;
+
+@ActiveProfiles(value = "local")
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({ "classpath*:/spring-test/context-test.xml" })
+public class DetailInformationTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(LockAccountTest.class);
+
+	@Autowired
+	private WebApplicationContext wac;
+
+	private MockMvc mockMvc;
+
+	/**
+	 * 
+	 * <pre>
+	 * method 설명.
+	 * </pre>
+	 */
+	@Before
+	public void before() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+	}
+
+	/**
+	 * <pre>
+	 * 판매자 기본정보조회
+	 * </pre>
+	 */
+	@Test
+	public void lockAccount() {
+
+		new TestCaseTemplate(this.mockMvc).url("/member/seller/detailInformation/v1").httpMethod(HttpMethod.POST)
+				.addHeaders("x-store-auth-info", "authKey=114127c7ef42667669819dad5df8d820c;ist=N")
+				.requestBody(new RequestBodySetter() {
+					@Override
+					public Object requestBody() {
+						DetailInformationReq reqVO = new DetailInformationReq();
+						KeySearch keySearchList = null;
+
+						List<KeySearch> myList = new ArrayList<KeySearch>();
+						for (int i = 0; i < 1; i++) {
+							keySearchList = new KeySearch();
+							keySearchList.setKeyString("IF1023501437920130904104346");
+							keySearchList.setKeyType("INSD_SELLERMBR_NO");
+							myList.add(keySearchList);
+						}
+						reqVO.setKeySearchList(myList);
+						return reqVO;
+					}
+				}).success(LockAccountRes.class, new SuccessCallback() {
+					@Override
+					public void success(Object result, HttpStatus httpStatus, RunMode runMode) {
+						LockAccountRes res = (LockAccountRes) result;
+						assertThat(res.getSellerKey(), notNullValue());
+						logger.info("{}", res.toString());
+					}
+				}, HttpStatus.OK, HttpStatus.ACCEPTED).run(RunMode.JSON);
+
+	}
+}
