2f10100913af72c06bb5a9742cdf8fca501e8b99
이석희 <winterer81@naver.com>
2014-01-08 08:52:23 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@3637 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/display/feature/best/vo/BestAppDTO.java
@@ -0,0 +1,244 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.display.feature.best.vo;
+
+/**
+ * 웹툰 DTO Default Value Object.
+ * 
+ * Updated on : 2013. 12. 22. Updated by : 김형식, SK 플래닛.
+ */
+public class BestAppDTO {
+	private int totalCount;
+	private String prodId;
+	private String prodNm;
+	private String menuId;
+	private String upMenuid;
+	private String upMenuNm;
+	private String topMenuId;
+	private String prodGrdCd;
+	private String prodBaseDesc;
+	private String prodDtlDesc;
+	private String partParentClsfCd;
+	private String drmYn;
+	private String apkVer;
+	private String aid;
+	private String apkPkgNm;
+	private String menuNm;
+	private String menuNesc;
+	private int prodAmt;
+	private String apkVerCd;
+	private int avgEvluScore;
+	private String dwldCnt;
+	private String paticpersCnt;
+	private String imgPath;
+	private String imgNm;
+	private String imgSize;
+
+	public int getTotalCount() {
+		return this.totalCount;
+	}
+
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+	}
+
+	public String getProdId() {
+		return this.prodId;
+	}
+
+	public void setProdId(String prodId) {
+		this.prodId = prodId;
+	}
+
+	public String getProdNm() {
+		return this.prodNm;
+	}
+
+	public void setProdNm(String prodNm) {
+		this.prodNm = prodNm;
+	}
+
+	public String getMenuId() {
+		return this.menuId;
+	}
+
+	public void setMenuId(String menuId) {
+		this.menuId = menuId;
+	}
+
+	public String getUpMenuid() {
+		return this.upMenuid;
+	}
+
+	public void setUpMenuid(String upMenuid) {
+		this.upMenuid = upMenuid;
+	}
+
+	public String getUpMenuNm() {
+		return this.upMenuNm;
+	}
+
+	public void setUpMenuNm(String upMenuNm) {
+		this.upMenuNm = upMenuNm;
+	}
+
+	public String getTopMenuId() {
+		return this.topMenuId;
+	}
+
+	public void setTopMenuId(String topMenuId) {
+		this.topMenuId = topMenuId;
+	}
+
+	public String getProdGrdCd() {
+		return this.prodGrdCd;
+	}
+
+	public void setProdGrdCd(String prodGrdCd) {
+		this.prodGrdCd = prodGrdCd;
+	}
+
+	public String getProdBaseDesc() {
+		return this.prodBaseDesc;
+	}
+
+	public void setProdBaseDesc(String prodBaseDesc) {
+		this.prodBaseDesc = prodBaseDesc;
+	}
+
+	public String getProdDtlDesc() {
+		return this.prodDtlDesc;
+	}
+
+	public void setProdDtlDesc(String prodDtlDesc) {
+		this.prodDtlDesc = prodDtlDesc;
+	}
+
+	public String getPartParentClsfCd() {
+		return this.partParentClsfCd;
+	}
+
+	public void setPartParentClsfCd(String partParentClsfCd) {
+		this.partParentClsfCd = partParentClsfCd;
+	}
+
+	public String getDrmYn() {
+		return this.drmYn;
+	}
+
+	public void setDrmYn(String drmYn) {
+		this.drmYn = drmYn;
+	}
+
+	public String getApkVer() {
+		return this.apkVer;
+	}
+
+	public void setApkVer(String apkVer) {
+		this.apkVer = apkVer;
+	}
+
+	public String getAid() {
+		return this.aid;
+	}
+
+	public void setAid(String aid) {
+		this.aid = aid;
+	}
+
+	public String getApkPkgNm() {
+		return this.apkPkgNm;
+	}
+
+	public void setApkPkgNm(String apkPkgNm) {
+		this.apkPkgNm = apkPkgNm;
+	}
+
+	public String getMenuNm() {
+		return this.menuNm;
+	}
+
+	public void setMenuNm(String menuNm) {
+		this.menuNm = menuNm;
+	}
+
+	public String getMenuNesc() {
+		return this.menuNesc;
+	}
+
+	public void setMenuNesc(String menuNesc) {
+		this.menuNesc = menuNesc;
+	}
+
+	public int getProdAmt() {
+		return this.prodAmt;
+	}
+
+	public void setProdAmt(int prodAmt) {
+		this.prodAmt = prodAmt;
+	}
+
+	public String getApkVerCd() {
+		return this.apkVerCd;
+	}
+
+	public void setApkVerCd(String apkVerCd) {
+		this.apkVerCd = apkVerCd;
+	}
+
+	public int getAvgEvluScore() {
+		return this.avgEvluScore;
+	}
+
+	public void setAvgEvluScore(int avgEvluScore) {
+		this.avgEvluScore = avgEvluScore;
+	}
+
+	public String getDwldCnt() {
+		return this.dwldCnt;
+	}
+
+	public void setDwldCnt(String dwldCnt) {
+		this.dwldCnt = dwldCnt;
+	}
+
+	public String getPaticpersCnt() {
+		return this.paticpersCnt;
+	}
+
+	public void setPaticpersCnt(String paticpersCnt) {
+		this.paticpersCnt = paticpersCnt;
+	}
+
+	public String getImgPath() {
+		return this.imgPath;
+	}
+
+	public void setImgPath(String imgPath) {
+		this.imgPath = imgPath;
+	}
+
+	public String getImgNm() {
+		return this.imgNm;
+	}
+
+	public void setImgNm(String imgNm) {
+		this.imgNm = imgNm;
+	}
+
+	public String getImgSize() {
+		return this.imgSize;
+	}
+
+	public void setImgSize(String imgSize) {
+		this.imgSize = imgSize;
+	}
+
+}
