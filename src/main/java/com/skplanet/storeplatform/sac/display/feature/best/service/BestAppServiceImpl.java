64ca962d6dee285c973e197219117b2b55dda227
이석희 <winterer81@naver.com>
2014-01-08 08:47:41 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@3630 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/display/feature/best/service/BestAppServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/feature/best/service/BestAppServiceImpl.java
@@ -10,8 +10,11 @@
 package com.skplanet.storeplatform.sac.display.feature.best.service;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -30,6 +33,8 @@ import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.App;
 import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Product;
 import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Rights;
 import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Support;
+import com.skplanet.storeplatform.sac.display.common.service.DisplayCommonService;
+import com.skplanet.storeplatform.sac.display.feature.best.vo.BestAppDTO;
 
 /**
  * ProductCategory Service 인터페이스(CoreStoreBusiness) 구현체
@@ -38,9 +43,14 @@ import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Supp
  */
 @Service
 public class BestAppServiceImpl implements BestAppService {
+
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+
 	@Autowired
 	@Qualifier("sac")
 	private CommonDAO commonDAO;
+	@Autowired
+	private DisplayCommonService commonService;
 
 	/*
 	 * (non-Javadoc)
@@ -51,101 +61,129 @@ public class BestAppServiceImpl implements BestAppService {
 	@Override
 	public BestAppRes searchBestAppList(BestAppReq bestAppReq) {
 		BestAppRes response = new BestAppRes();
+		int totalCount = 0;
 
 		List<Product> productList = new ArrayList<Product>();
-		List<Menu> menuList = new ArrayList<Menu>();
-		List<Source> sourceList = new ArrayList<Source>();
-		List<Support> supportList = new ArrayList<Support>();
+		List<Menu> menuList = null;
+		List<Support> supportList = null;
+		List<Source> sourceList = null;
+
+		String stdDt = this.commonService.getBatchStandardDateString("S01", bestAppReq.getListId());
+
+		bestAppReq.setStdDt(stdDt);
+
+		// BEST 앱 상품 조회
+		List<BestAppDTO> appList = null;
+
+		if (!"ADM000000001".equals(bestAppReq.getListId())) {
+			// 추천, 인기(매출), 인기신규 상품 조회
+			appList = this.commonDAO.queryForList("BestApp.selectBestAppList", bestAppReq, BestAppDTO.class);
+		} else {
+			// 신규 상품조회
+			appList = this.commonDAO.queryForList("BestApp.selectNewBestAppList", bestAppReq, BestAppDTO.class);
+		}
 
 		CommonResponse commonResponse = new CommonResponse();
-		commonResponse.setTotalCount(10);
 
-		for (int i = 1; i <= 1; i++) {
-			Product product = new Product();
-			Identifier identifier = new Identifier();
-			App app = new App();
-			Accrual accrual = new Accrual();
-			Rights rights = new Rights();
-			Source source = new Source();
-			Price price = new Price();
-			Title title = new Title();
-			Support support = new Support();
+		Product product = null;
+		Identifier identifier = null;
+		App app = null;
+		Accrual accrual = null;
+		Rights rights = null;
+		Source source = null;
+		Price price = null;
+		Title title = null;
+		Support support = null;
+		Menu menu = null;
 
-			// 상품ID
-			identifier = new Identifier();
-			identifier.setType("episodeId");
-			identifier.setText("0000643818");
+		if (appList != null) {
+			Iterator<BestAppDTO> iterator = appList.iterator();
+			while (iterator.hasNext()) {
+				product = new Product();
+				identifier = new Identifier();
+				app = new App();
+				accrual = new Accrual();
+				rights = new Rights();
+				source = new Source();
+				price = new Price();
+				title = new Title();
+				support = new Support();
 
-			support.setType("iab");
-			support.setText("PD012301");
-			supportList.add(support);
+				BestAppDTO mapperVO = iterator.next();
 
-			/*
-			 * Menu(메뉴정보) Id, Name, Type
-			 */
-			Menu menu = new Menu();
-			menu.setId("DP000501");
-			menu.setName("게임");
-			menu.setType("topClass");
-			menuList.add(menu);
-			menu = new Menu();
-			menu.setId("DP01004");
-			menu.setName("RPG");
-			menuList.add(menu);
+				totalCount = mapperVO.getTotalCount();
+				commonResponse.setTotalCount(totalCount);
 
-			/*
-			 * App aid, packagename, versioncode, version
-			 */
-			app.setAid("OA00643818");
-			app.setPackageName("proj.syjt.tstore");
-			app.setVersionCode("11000");
-			app.setVersion("1.1");
+				identifier.setType("episodeId");
+				identifier.setText(mapperVO.getProdId());
 
-			/*
-			 * Accrual voterCount (참여자수) DownloadCount (다운로드 수) score(평점)
-			 */
-			accrual.setVoterCount("14305");
-			accrual.setDownloadCount("513434");
-			accrual.setScore(4.8);
+				supportList = new ArrayList<Support>();
+				support = new Support();
+				support.setType("drm");
+				support.setText(mapperVO.getDrmYn());
+				supportList.add(support);
+				support = new Support();
+				support.setType("iab");
+				if (mapperVO.getPartParentClsfCd() == null || "".equals(mapperVO.getPartParentClsfCd())) {
+					support.setText("");
+				} else {
+					support.setText(mapperVO.getPartParentClsfCd());
+				}
+				supportList.add(support);
 
-			/*
-			 * Rights grade
-			 */
-			rights.setGrade("0");
+				menuList = new ArrayList<Menu>();
 
-			title.setText("워밸리 온라인");
+				menu = new Menu();
+				menu.setId(mapperVO.getUpMenuid());
+				menu.setName(mapperVO.getUpMenuNm());
+				menu.setType("topClass");
+				menuList.add(menu);
+				menu = new Menu();
+				menu.setId(mapperVO.getMenuId());
+				menu.setName(mapperVO.getMenuNm());
+				menuList.add(menu);
 
-			/*
-			 * source mediaType, size, type, url
-			 */
-			source.setType("thumbnail");
-			source.setUrl("http://wap.tstore.co.kr/android6/201311/22/IF1423067129420100319114239/0000643818/img/thumbnail/0000643818_130_130_0_91_20131122120310.PNG");
-			sourceList.add(source);
+				app.setAid(mapperVO.getAid());
+				app.setPackageName(mapperVO.getApkPkgNm());
+				app.setVersionCode(mapperVO.getApkVerCd());
+				app.setVersion(mapperVO.getApkVer());
 
-			/*
-			 * Price text
-			 */
-			price.setText(0);
+				accrual.setVoterCount(mapperVO.getPaticpersCnt());
+				accrual.setDownloadCount(mapperVO.getDwldCnt());
+				accrual.setScore(mapperVO.getAvgEvluScore());
 
-			product = new Product();
-			product.setIdentifier(identifier);
-			// product.setSupport("y|iab");
-			product.setSupportList(supportList);
-			product.setMenuList(menuList);
-			product.setApp(app);
-			product.setAccrual(accrual);
-			product.setRights(rights);
-			product.setTitle(title);
-			product.setSourceList(sourceList);
-			product.setProductExplain("★이벤트★세상에 없던 모바일 MMORPG!");
-			product.setPrice(price);
+				rights.setGrade(mapperVO.getProdGrdCd());
 
-			productList.add(product);
+				title.setText(mapperVO.getProdNm());
 
+				sourceList = new ArrayList<Source>();
+				source.setType("thumbnail");
+				source.setUrl(mapperVO.getImgPath());
+				sourceList.add(source);
+
+				price.setText(mapperVO.getProdAmt());
+
+				product = new Product();
+				product.setIdentifier(identifier);
+				product.setSupportList(supportList);
+				product.setMenuList(menuList);
+				product.setApp(app);
+				product.setAccrual(accrual);
+				product.setRights(rights);
+				product.setTitle(title);
+				product.setSourceList(sourceList);
+				product.setProductExplain(mapperVO.getProdBaseDesc());
+				product.setPrice(price);
+
+				productList.add(product);
+			}
+			response.setCommonResponse(commonResponse);
+			response.setProductList(productList);
+		} else {
+			// 조회 결과 없음
+			commonResponse.setTotalCount(0);
+			response.setCommonResponse(commonResponse);
 		}
-		response.setCommonResponse(commonResponse);
-		response.setProductList(productList);
-
 		return response;
 	}
 }
