7c2263292d4815170b6d403d99463f315d19b304
김현일 <nahiyak@incross.com>
2014-01-08 12:02:37 +0000

    # 임시 주석(프로젝트 에러) : storeplatform-sac-client에 VO 올리신거 확인후 Commit
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@3660 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/purchase/history/controller/GiftController.java
+++ src/main/java/com/skplanet/storeplatform/sac/purchase/history/controller/GiftController.java
@@ -1,101 +1,101 @@
-/*
- * Copyright (c) 2013 SK planet.
- * All right reserved.
- *
- * This software is the confidential and proprietary information of SK planet.
- * You shall not disclose such Confidential Information and
- * shall use it only in accordance with the terms of the license agreement
- * you entered into with SK planet.
- */
-package com.skplanet.storeplatform.sac.purchase.history.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.skplanet.storeplatform.purchase.client.history.vo.GiftConfirmRequest;
-import com.skplanet.storeplatform.purchase.client.history.vo.GiftConfirmResponse;
-import com.skplanet.storeplatform.purchase.client.history.vo.GiftReceiveRequest;
-import com.skplanet.storeplatform.purchase.client.history.vo.GiftReceiveResponse;
-import com.skplanet.storeplatform.sac.client.purchase.vo.history.GiftConfirmReq;
-import com.skplanet.storeplatform.sac.client.purchase.vo.history.GiftConfirmRes;
-import com.skplanet.storeplatform.sac.client.purchase.vo.history.GiftReceiveReq;
-import com.skplanet.storeplatform.sac.client.purchase.vo.history.GiftReceiveRes;
-import com.skplanet.storeplatform.sac.purchase.history.service.GiftService;
-
-@Controller
-@RequestMapping(value = "/purchase")
-public class GiftController {
-
-	@Autowired
-	private GiftService giftService;
-
-	@RequestMapping(value = "/history/gift/get/v1", method = RequestMethod.POST)
-	@ResponseBody
-	public GiftReceiveRes searchGiftReceive(@RequestBody GiftReceiveReq giftReceiveReq) {
-
-		GiftReceiveRequest req = this.reqConvert(giftReceiveReq);
-		GiftReceiveResponse giftReceiveResponse = new GiftReceiveResponse();
-		giftReceiveResponse = this.giftService.searchGiftReceive(req);
-		GiftReceiveRes res = this.resConvert(giftReceiveResponse);
-
-		return res;
-	}
-
-	@RequestMapping(value = "/history/gift/modify/v1", method = RequestMethod.POST)
-	@ResponseBody
-	public GiftConfirmRes modifyGiftConfirm(@RequestBody GiftConfirmReq giftConfirmReq) {
-
-		GiftConfirmRequest req = this.reqConvert(giftConfirmReq);
-		GiftConfirmResponse giftConfirmResponse = new GiftConfirmResponse();
-		giftConfirmResponse = this.giftService.modifyGiftConfirm(req);
-		GiftConfirmRes res = this.resConvert(giftConfirmResponse);
-
-		return res;
-	}
-
-	private GiftConfirmRequest reqConvert(GiftConfirmReq giftConfirmReq) {
-		GiftConfirmRequest req = new GiftConfirmRequest();
-
-		req.setTenantId(giftConfirmReq.getTenantId());
-		req.setSendMbrNo(giftConfirmReq.getSendMbrNo());
-		req.setSendDeviceNo(giftConfirmReq.getSendDeviceNo());
-		req.setRecvMbrNo(giftConfirmReq.getRecvMbrNo());
-		req.setRecvDeviceNo(giftConfirmReq.getRecvDeviceNo());
-		req.setPrchsId(giftConfirmReq.getPrchsId());
-		req.setProdId(giftConfirmReq.getProdId());
-
-		return req;
-	}
-
-	private GiftConfirmRes resConvert(GiftConfirmResponse giftComfirmResponse) {
-		GiftConfirmRes res = new GiftConfirmRes();
-		res.setCount(giftComfirmResponse.getCount());
-
-		return res;
-	}
-
-	private GiftReceiveRequest reqConvert(GiftReceiveReq giftReceiveReq) {
-		GiftReceiveRequest req = new GiftReceiveRequest();
-
-		req.setTenantId(giftReceiveReq.getTenantId());
-		req.setSendMbrNo(giftReceiveReq.getSendMbrNo());
-		req.setSendDeviceNo(giftReceiveReq.getSendDeviceNo());
-		req.setRecvMbrNo(giftReceiveReq.getRecvMbrNo());
-		req.setRecvDeviceNo(giftReceiveReq.getRecvDeviceNo());
-		req.setPrchsId(giftReceiveReq.getPrchsId());
-		req.setProdId(giftReceiveReq.getProdId());
-
-		return req;
-	}
-
-	private GiftReceiveRes resConvert(GiftReceiveResponse giftReceiveResponse) {
-		GiftReceiveRes res = new GiftReceiveRes();
-		res.setRecvDt(giftReceiveResponse.getRecvDt());
-
-		return res;
-	}
-}
+///*
+// * Copyright (c) 2013 SK planet.
+// * All right reserved.
+// *
+// * This software is the confidential and proprietary information of SK planet.
+// * You shall not disclose such Confidential Information and
+// * shall use it only in accordance with the terms of the license agreement
+// * you entered into with SK planet.
+// */
+//package com.skplanet.storeplatform.sac.purchase.history.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import com.skplanet.storeplatform.purchase.client.history.vo.GiftConfirmRequest;
+//import com.skplanet.storeplatform.purchase.client.history.vo.GiftConfirmResponse;
+//import com.skplanet.storeplatform.purchase.client.history.vo.GiftReceiveRequest;
+//import com.skplanet.storeplatform.purchase.client.history.vo.GiftReceiveResponse;
+//import com.skplanet.storeplatform.sac.client.purchase.vo.history.GiftConfirmReq;
+//import com.skplanet.storeplatform.sac.client.purchase.vo.history.GiftConfirmRes;
+//import com.skplanet.storeplatform.sac.client.purchase.vo.history.GiftReceiveReq;
+//import com.skplanet.storeplatform.sac.client.purchase.vo.history.GiftReceiveRes;
+//import com.skplanet.storeplatform.sac.purchase.history.service.GiftService;
+//
+//@Controller
+//@RequestMapping(value = "/purchase")
+//public class GiftController {
+//
+//	@Autowired
+//	private GiftService giftService;
+//
+//	@RequestMapping(value = "/history/gift/get/v1", method = RequestMethod.POST)
+//	@ResponseBody
+//	public GiftReceiveRes searchGiftReceive(@RequestBody GiftReceiveReq giftReceiveReq) {
+//
+//		GiftReceiveRequest req = this.reqConvert(giftReceiveReq);
+//		GiftReceiveResponse giftReceiveResponse = new GiftReceiveResponse();
+//		giftReceiveResponse = this.giftService.searchGiftReceive(req);
+//		GiftReceiveRes res = this.resConvert(giftReceiveResponse);
+//
+//		return res;
+//	}
+//
+//	@RequestMapping(value = "/history/gift/modify/v1", method = RequestMethod.POST)
+//	@ResponseBody
+//	public GiftConfirmRes modifyGiftConfirm(@RequestBody GiftConfirmReq giftConfirmReq) {
+//
+//		GiftConfirmRequest req = this.reqConvert(giftConfirmReq);
+//		GiftConfirmResponse giftConfirmResponse = new GiftConfirmResponse();
+//		giftConfirmResponse = this.giftService.modifyGiftConfirm(req);
+//		GiftConfirmRes res = this.resConvert(giftConfirmResponse);
+//
+//		return res;
+//	}
+//
+//	private GiftConfirmRequest reqConvert(GiftConfirmReq giftConfirmReq) {
+//		GiftConfirmRequest req = new GiftConfirmRequest();
+//
+//		req.setTenantId(giftConfirmReq.getTenantId());
+//		req.setSendMbrNo(giftConfirmReq.getSendMbrNo());
+//		req.setSendDeviceNo(giftConfirmReq.getSendDeviceNo());
+//		req.setRecvMbrNo(giftConfirmReq.getRecvMbrNo());
+//		req.setRecvDeviceNo(giftConfirmReq.getRecvDeviceNo());
+//		req.setPrchsId(giftConfirmReq.getPrchsId());
+//		req.setProdId(giftConfirmReq.getProdId());
+//
+//		return req;
+//	}
+//
+//	private GiftConfirmRes resConvert(GiftConfirmResponse giftComfirmResponse) {
+//		GiftConfirmRes res = new GiftConfirmRes();
+//		res.setCount(giftComfirmResponse.getCount());
+//
+//		return res;
+//	}
+//
+//	private GiftReceiveRequest reqConvert(GiftReceiveReq giftReceiveReq) {
+//		GiftReceiveRequest req = new GiftReceiveRequest();
+//
+//		req.setTenantId(giftReceiveReq.getTenantId());
+//		req.setSendMbrNo(giftReceiveReq.getSendMbrNo());
+//		req.setSendDeviceNo(giftReceiveReq.getSendDeviceNo());
+//		req.setRecvMbrNo(giftReceiveReq.getRecvMbrNo());
+//		req.setRecvDeviceNo(giftReceiveReq.getRecvDeviceNo());
+//		req.setPrchsId(giftReceiveReq.getPrchsId());
+//		req.setProdId(giftReceiveReq.getProdId());
+//
+//		return req;
+//	}
+//
+//	private GiftReceiveRes resConvert(GiftReceiveResponse giftReceiveResponse) {
+//		GiftReceiveRes res = new GiftReceiveRes();
+//		res.setRecvDt(giftReceiveResponse.getRecvDt());
+//
+//		return res;
+//	}
+// }
--- src/main/java/com/skplanet/storeplatform/sac/purchase/history/service/GiftServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/purchase/history/service/GiftServiceImpl.java
@@ -1,66 +1,66 @@
-/*
- * Copyright (c) 2013 SK planet.
- * All right reserved.
- *
- * This software is the confidential and proprietary information of SK planet.
- * You shall not disclose such Confidential Information and
- * shall use it only in accordance with the terms of the license agreement
- * you entered into with SK planet.
- */
-package com.skplanet.storeplatform.sac.purchase.history.service;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.skplanet.storeplatform.purchase.client.history.sci.GiftSCI;
-import com.skplanet.storeplatform.purchase.client.history.vo.GiftConfirmRequest;
-import com.skplanet.storeplatform.purchase.client.history.vo.GiftConfirmResponse;
-import com.skplanet.storeplatform.purchase.client.history.vo.GiftReceiveRequest;
-import com.skplanet.storeplatform.purchase.client.history.vo.GiftReceiveResponse;
-
-/**
- * Member Service 인터페이스(CoreStoreBusiness) 구현체
- * 
- * Updated on : 2013-09-01 Updated by : $$Id$$.
- */
-@Service
-public class GiftServiceImpl implements GiftService {
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-	@Autowired
-	private GiftSCI giftSCI;
-
-	/**
-	 * 선물수신확인 체크.
-	 * 
-	 * @param GiftReceiveRequest
-	 *            선물수신확인 체크
-	 * @return GiftReceiveResponse
-	 */
-	@Override
-	public GiftReceiveResponse searchGiftReceive(GiftReceiveRequest giftReceiveRequest) {
-
-		GiftReceiveResponse giftReceiveResponse = new GiftReceiveResponse();
-		giftReceiveResponse = this.giftSCI.searchGiftReceive(giftReceiveRequest);
-
-		return giftReceiveResponse;
-	}
-
-	/**
-	 * 선물수신.
-	 * 
-	 * @param GiftConfirmRequest
-	 *            선물수신
-	 * @return GiftConfirmResponse
-	 */
-	@Override
-	public GiftConfirmResponse modifyGiftConfirm(GiftConfirmRequest giftConfirmRequest) {
-		GiftConfirmResponse giftConfirmResponse = new GiftConfirmResponse();
-		giftConfirmResponse = this.giftSCI.modifyGiftConfirm(giftConfirmRequest);
-
-		return giftConfirmResponse;
-	}
-
-}
+///*
+// * Copyright (c) 2013 SK planet.
+// * All right reserved.
+// *
+// * This software is the confidential and proprietary information of SK planet.
+// * You shall not disclose such Confidential Information and
+// * shall use it only in accordance with the terms of the license agreement
+// * you entered into with SK planet.
+// */
+//package com.skplanet.storeplatform.sac.purchase.history.service;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import com.skplanet.storeplatform.purchase.client.history.sci.GiftSCI;
+//import com.skplanet.storeplatform.purchase.client.history.vo.GiftConfirmRequest;
+//import com.skplanet.storeplatform.purchase.client.history.vo.GiftConfirmResponse;
+//import com.skplanet.storeplatform.purchase.client.history.vo.GiftReceiveRequest;
+//import com.skplanet.storeplatform.purchase.client.history.vo.GiftReceiveResponse;
+//
+///**
+// * Member Service 인터페이스(CoreStoreBusiness) 구현체
+// * 
+// * Updated on : 2013-09-01 Updated by : $$Id$$.
+// */
+//@Service
+//public class GiftServiceImpl implements GiftService {
+//	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+//
+//	@Autowired
+//	private GiftSCI giftSCI;
+//
+//	/**
+//	 * 선물수신확인 체크.
+//	 * 
+//	 * @param GiftReceiveRequest
+//	 *            선물수신확인 체크
+//	 * @return GiftReceiveResponse
+//	 */
+//	@Override
+//	public GiftReceiveResponse searchGiftReceive(GiftReceiveRequest giftReceiveRequest) {
+//
+//		GiftReceiveResponse giftReceiveResponse = new GiftReceiveResponse();
+//		giftReceiveResponse = this.giftSCI.searchGiftReceive(giftReceiveRequest);
+//
+//		return giftReceiveResponse;
+//	}
+//
+//	/**
+//	 * 선물수신.
+//	 * 
+//	 * @param GiftConfirmRequest
+//	 *            선물수신
+//	 * @return GiftConfirmResponse
+//	 */
+//	@Override
+//	public GiftConfirmResponse modifyGiftConfirm(GiftConfirmRequest giftConfirmRequest) {
+//		GiftConfirmResponse giftConfirmResponse = new GiftConfirmResponse();
+//		giftConfirmResponse = this.giftSCI.modifyGiftConfirm(giftConfirmRequest);
+//
+//		return giftConfirmResponse;
+//	}
+//
+// }
