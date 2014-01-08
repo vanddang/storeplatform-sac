eac9cb1a58a56f562b2d1127017ba1869a4dde50
전병율 <Jeon.ByungYoul@gmail.com>
2014-01-08 14:38:03 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@3671 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/test/java/com/skplanet/storeplatform/sac/external/idp/service/IdpSacServiceImplTest.java
@@ -0,0 +1,41 @@
+package com.skplanet.storeplatform.sac.external.idp.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+@ActiveProfiles(value = "local")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "classpath*:/spring-test/context-test.xml" })
+@TransactionConfiguration
+@Transactional
+public class IdpSacServiceImplTest {
+
+	// @Mock
+	// private IdpSacServiceImpl repository;
+
+	@Autowired
+	private IdpSacServiceImpl service;
+
+	@Test
+	public final void testAlredyJoinCheckByEmail() {
+		try {
+			this.service.alredyJoinCheckByEmail("aaaaa@gmail.com");
+			this.service.alredyJoinCheckBySn("1111111111111");
+			this.service.alredyJoinCheck("1", "1");
+			this.service.checkDupID("ididididid");
+			this.service.realNameAuthForNative("name", "1111111111111");
+			this.service.join4Wap("0101112222");
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
