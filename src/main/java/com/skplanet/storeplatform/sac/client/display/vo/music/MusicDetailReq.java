f378f3ee55eca506fe1be197dd42ff75f14ad344
정희원 <joyspring@sk.com>
2014-01-09 07:07:36 +0000

    ref #5498 음악 상품 상세 조회 API 개발 (Dummy 데이터)
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@3735 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/music/MusicDetailReq.java
@@ -0,0 +1,19 @@
+package com.skplanet.storeplatform.sac.client.display.vo.music;
+
+/**
+ * 음악 상세보기 요청 VO
+ *
+ * Updated on : 2014. 01. 07 Updated by : 정희원, SK 플래닛.
+ */
+public class MusicDetailReq {
+
+    private String episodeId;
+
+    public String getEpisodeId() {
+        return episodeId;
+    }
+
+    public void setEpisodeId(String episodeId) {
+        this.episodeId = episodeId;
+    }
+}
---
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/music/MusicDetailRes.java
@@ -0,0 +1,20 @@
+package com.skplanet.storeplatform.sac.client.display.vo.music;
+
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Product;
+
+/**
+ * 음악상세 Response
+ * Updated on : 2014. 01. 06. Updated by : 정희원, SK 플래닛.
+ */
+public class MusicDetailRes {
+
+    private Product product;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
