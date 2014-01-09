2614b84eba40f21ecf71e00ad7c636b844b9737b
심대진 <tlaeo00@gmail.com>
2014-01-09 05:36:40 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@3713 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/member/user/service/UserJoinServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/member/user/service/UserJoinServiceImpl.java
@@ -9,6 +9,7 @@
  */
 package com.skplanet.storeplatform.sac.member.user.service;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 
@@ -18,7 +19,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skplanet.storeplatform.framework.core.util.StringUtil;
+import com.skplanet.storeplatform.member.client.common.vo.CommonRequest;
+import com.skplanet.storeplatform.member.client.common.vo.MbrClauseAgree;
 import com.skplanet.storeplatform.member.client.user.sci.UserSCI;
+import com.skplanet.storeplatform.member.client.user.sci.vo.CreateUserRequest;
+import com.skplanet.storeplatform.member.client.user.sci.vo.CreateUserResponse;
+import com.skplanet.storeplatform.member.client.user.sci.vo.UserMbr;
 import com.skplanet.storeplatform.sac.client.member.vo.common.AgreementInfo;
 import com.skplanet.storeplatform.sac.client.member.vo.common.HeaderVo;
 import com.skplanet.storeplatform.sac.client.member.vo.user.CreateByAgreementReq;
@@ -50,6 +57,8 @@ public class UserJoinServiceImpl implements UserJoinService {
 
 	@Override
 	public CreateByMdnRes createByMdn(HeaderVo headerVo, CreateByMdnReq req) throws Exception {
+
+		CreateByMdnRes response = new CreateByMdnRes();
 
 		/**
 		 * 모번호 조회 (989 일 경우만)
@@ -97,32 +106,91 @@ public class UserJoinServiceImpl implements UserJoinService {
 		// IDPReceiverM idpReceiverM = this.idpService.join4Wap(req.getDeviceId());
 		// IDPReceiverM idpReceiverM = new IDPReceiverM();
 		// idpReceiverM.getResponseHeader().getResult();
-		// if (StringUtils.equals(idpReceiverM.getResponseHeader().getResult(), "1000")) {
-		//
-		// }
 
-		/**
-		 * TODO IDP 회원 기가입 상태인 경우 (IDP 연동) 무선회원 해지
-		 */
+		// 정상 가입이면................
+		String resultCode = "1000";
+		if (StringUtils.equals(resultCode, "1000")) {
+			logger.info("## IDP 연동 성공!!!!");
 
-		/**
-		 * TODO (SC 연동) 회원 정보 등록
-		 */
+			/**
+			 * TODO (SC 연동) 회원 정보 등록
+			 */
+			// SC 공통정보 setting
+			CommonRequest commonRequest = new CommonRequest();
+			commonRequest.setSystemID("S001");
+			commonRequest.setTenantID("S01");
 
-		/**
-		 * TODO (SC 연동) 휴대기기 정보 등록
-		 */
-		/**
-		 * 폰정보 조회
-		 */
-		// Device device = this.mcc.getPhoneInfo(req.getDeviceModelNo());
-		// logger.info("device : {}", device.getModelNm());
-		// logger.info("device : {}", device.getEngModelNm());
+			// SC 사용자 기본정보 setting
+			UserMbr userMbr = new UserMbr();
+			userMbr.setImMbrNo("IDP MBRNO2");
+			userMbr.setUserType("US011501");// 모바일회원
+			userMbr.setUserMainStatus("US010201");// 모바일전용회원
+			userMbr.setUserSubStatus("US010301");// 신청
+			userMbr.setImRegDate("19001212");
+			userMbr.setUserID(req.getDeviceId());// mdn, uuid 받는데로 넣는다. (SC 확인함.)
 
-		CreateByMdnRes result = new CreateByMdnRes();
-		result.setUserKey("12321423543464567457");
+			// // if() {
+			// // SC 법정대리인 정보 setting
+			// MbrLglAgent mbrLglAgent = new MbrLglAgent();
+			// mbrLglAgent.setParentBirthDay(req.getParentBirth());
+			// mbrLglAgent.setParentEmail(req.getParentEmail());
+			// mbrLglAgent.setParentMDN(req.getParentMdn());
+			// // }
 
-		return result;
+			// SC 이용약관 정보 setting
+			List<MbrClauseAgree> mbrClauseAgreeList = new ArrayList<MbrClauseAgree>();
+
+			// SC 사용자 가입요청 setting
+			CreateUserRequest createUserRequest = new CreateUserRequest();
+			createUserRequest.setCommonRequest(commonRequest);
+			createUserRequest.setUserMbr(userMbr);
+			CreateUserResponse createUserResponse = this.userSCI.create(createUserRequest);
+
+			logger.info("#### ResponseCode : " + createUserResponse.getCommonResponse().getResultCode());
+			logger.info("#### ResponseMsg  : " + createUserResponse.getCommonResponse().getResultMessage());
+
+			logger.info("userKey" + createUserResponse.getUserKey());
+			logger.info("UserMainStatus" + createUserResponse.getUserMainStatus());
+			logger.info("UserSubStatus" + createUserResponse.getUserSubStatus());
+
+			/**
+			 * 폰정보 조회
+			 */
+			// Device device = this.mcc.getPhoneInfo(req.getDeviceModelNo());
+			// logger.info("device : {}", device.getModelNm());
+			// logger.info("device : {}", device.getEngModelNm());
+			/**
+			 * TODO (SC 연동) 휴대기기 정보 등록
+			 */
+
+			/**
+			 * TODO 구매이력 이관 여부 [기존회원Key, 신규회원 Key]
+			 */
+			String key = "Y";
+			if (StringUtil.equals(key, "Y")) {
+				/**
+				 * 구매이력 이관 호출
+				 */
+			}
+
+			response.setUserKey(createUserResponse.getUserKey());
+
+		} else if (StringUtils.equals(resultCode, "2100")) {
+			/**
+			 * TODO IDP 회원 기가입 상태인 경우 (IDP 연동) 무선회원 해지
+			 */
+			logger.info("## (기가입 상태) 이미 서비스에 등록한 MDN");
+			throw new RuntimeException("IDP 무선회원 가입 실패");
+		} else {
+			/**
+			 * 2000 정의되지 않은 요청 2001 잘못된 요청 데이터 2002 필수 파라메타를 전달 받지 못함 2016 SP IP 검증 실패 2100 이미 서비스에 등록한 MDN 2216 UAPS로부터
+			 * MDN의 서비스 관리번호를 얻지 못함. 4200 UAPS 연동 오류 4201 UAPS 연동 간 SKT 사용자 인증 실패 5500 IDP에서 요청수행중 오류 발생(기타오류)
+			 */
+			logger.info("## IDP 무선회원 가입 연동 실패");
+			throw new RuntimeException("IDP 무선회원 가입 실패");
+		}
+
+		return response;
 
 	}
 
