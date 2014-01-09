4772cbf5871b4d711e06786ab3d1de524a61a961
김다슬 <daseul428@spteck.co.kr>
2014-01-09 02:03:53 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@3695 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/member/miscellaneous/service/MiscellaneousService.java
@@ -0,0 +1,40 @@
+package com.skplanet.storeplatform.sac.member.miscellaneous.service;
+
+import com.skplanet.storeplatform.sac.client.member.vo.miscellaneous.GetOpmdReq;
+import com.skplanet.storeplatform.sac.client.member.vo.miscellaneous.GetOpmdRes;
+import com.skplanet.storeplatform.sac.client.member.vo.miscellaneous.GetUaCodeReq;
+import com.skplanet.storeplatform.sac.client.member.vo.miscellaneous.GetUaCodeRes;
+
+/**
+ * 
+ * 기타 기능 관련 인터페이스
+ * 
+ * Updated on : 2014. 1. 7. Updated by : 김다슬, 인크로스.
+ */
+public interface MiscellaneousService {
+
+	/**
+	 * <pre>
+	 * UA 코드 정보 조회.
+	 * </pre>
+	 * 
+	 * @param request
+	 * @return
+	 * @throws Exception
+	 */
+	public GetUaCodeRes getUaCode(GetUaCodeReq request) throws Exception;
+
+	/**
+	 * <pre>
+	 * OPMD 모회선 번호 조회.
+	 * 989로 시작하는 MSISDN이 들어오면 모번호를 조회해서 반환하고, 
+	 * 989로 시작하지 않는경우 파라미터로 받은 MSISDN을 그대로 반환.
+	 * </pre>
+	 * 
+	 * @param request
+	 * @return
+	 * @throws Exception
+	 */
+	public GetOpmdRes getOpmd(GetOpmdReq req) throws Exception;
+
+}
