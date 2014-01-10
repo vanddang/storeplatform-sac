04d473eeb0793b6ed7e951407fd8386367b4c4a3
반범진 <vanddang@gtsoft.co.kr>
2014-01-10 08:11:09 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@3913 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/test/java/com/skplanet/storeplatform/sac/api/v1/member/user/AuthorizeByMdnTest.java
+++ src/test/java/com/skplanet/storeplatform/sac/api/v1/member/user/AuthorizeByMdnTest.java
@@ -1,5 +1,6 @@
 package com.skplanet.storeplatform.sac.api.v1.member.user;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -14,14 +15,16 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.skplanet.storeplatform.framework.test.RequestBodySetter;
 import com.skplanet.storeplatform.framework.test.SuccessCallback;
 import com.skplanet.storeplatform.framework.test.TestCaseTemplate;
 import com.skplanet.storeplatform.framework.test.TestCaseTemplate.RunMode;
-import com.skplanet.storeplatform.sac.api.v1.member.seller.LockAccountTest;
-import com.skplanet.storeplatform.sac.client.member.vo.seller.LockAccountRes;
+import com.skplanet.storeplatform.sac.client.member.vo.user.AuthorizeByMdnReq;
+import com.skplanet.storeplatform.sac.client.member.vo.user.AuthorizeByMdnRes;
+import com.skplanet.storeplatform.sac.member.user.service.LoginService;
 
 /**
  * 모바일 전용 회원 인증 (MDN 인증) Class Test
@@ -35,31 +38,69 @@ import com.skplanet.storeplatform.sac.client.member.vo.seller.LockAccountRes;
 @ContextConfiguration({ "classpath*:/spring-test/context-test.xml" })
 public class AuthorizeByMdnTest {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(LockAccountTest.class);
+	private static final Logger logger = LoggerFactory.getLogger(AuthorizeByMdnTest.class);
 
+	@Autowired
+	private LoginService loginService;
+	
 	@Autowired
 	private WebApplicationContext wac;
 
 	private MockMvc mockMvc;
 
+	@Before
+	public void before() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+	}
+	
 	@Test
 	public void shouldAuthorizeByMdn() {
-		new TestCaseTemplate(this.mockMvc).url("/member/seller/lockAccount/v1")
+		AuthorizeByMdnReq req = new AuthorizeByMdnReq();
+		req.setDeviceAccount("vanddang@gmail.com");
+		req.setDeviceId("0162088230");
+		req.setDeviceModelNo("SHW-M250S");
+		req.setDeviceTelecom("KT");
+		req.setIsAutoUpdate("Y");
+		req.setNativeId("358362045580844");
+		req.setOsVerOrg("1.0");
+		req.setScVer("1.0");
+		req.setRooting("Y");
+		
+		try {
+			this.loginService.authorizeByMdn(null, req);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+		/*new TestCaseTemplate(this.mockMvc).url("/dev/member/user/authorizeByMdn/v1")
 				.httpMethod(HttpMethod.POST)
 				.requestBody(new RequestBodySetter() {
 					@Override
 					public Object requestBody() {
-
-						return null;
+						AuthorizeByMdnReq req = new AuthorizeByMdnReq();
+						req.setDeviceAccount("vanddang@gmail.com");
+						req.setDeviceId("0101231234");
+						req.setDeviceModelNo("SHP-112");
+						req.setDeviceTelecom("SKT");
+						req.setIsAutoUpdate("Y");
+						req.setNativeId("358362045580844");
+						req.setOsVerOrg("1.0");
+						req.setScVer("1.0");
+						req.setRooting("Y");
+						
+						logger.info("request param : {}", req.toString());
+						
+						return req;
 					}
-				}).success(LockAccountRes.class, new SuccessCallback() {
+				}).success(AuthorizeByMdnRes.class, new SuccessCallback() {
 					@Override
 					public void success(Object result, HttpStatus httpStatus,
 							RunMode runMode) {
-
+						AuthorizeByMdnRes res = (AuthorizeByMdnRes) result;
+						logger.info("response param : {}", res.toString());
 					}
 				}, HttpStatus.OK, HttpStatus.ACCEPTED).run(RunMode.JSON);
 
-	}
+	}*/
 }
