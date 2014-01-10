3aeaf87d28c9fb829622a2192c0517a34100d007
서대영 <dale.seo@sk.com>
2014-01-10 09:42:11 +0000

    refs #7463: ICAS Dummy Data 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@3946 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/test/java/com/skplanet/storeplatform/sac/other/interpark/controller/InterparkControllerTest.java
@@ -0,0 +1,52 @@
+package com.skplanet.storeplatform.sac.other.interpark.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@ActiveProfiles(value = "local")
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({ "classpath*:/spring-test/context-test.xml" })
+public class InterparkControllerTest {
+
+	@Autowired
+	private WebApplicationContext wac;
+
+	private MockMvc mvc;
+
+	@Before
+	public void before() {
+		this.mvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+	}
+
+	@Test
+	public void testGetAuthkey() throws Exception {
+		this.mvc.perform(
+				get("/other/interpark/getAuthkey/v1")
+				.accept(MediaType.parseMediaType("application/json;charset=UTF-8"))
+				.param("type", "order")
+				.param("revOrdNo", "00000000000000000001")
+				.param("ebFileNo", "100656")
+		)
+		.andDo(print())
+		.andExpect(status().isOk())
+		.andExpect(content().contentType("application/json;charset=UTF-8"));
+		// .andExpect(jsonPath("$.id").value("#17"));
+	}
+
+}
