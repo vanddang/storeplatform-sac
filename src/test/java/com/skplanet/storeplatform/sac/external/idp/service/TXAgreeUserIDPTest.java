12d06d3b8ec1510536c164beef3ef0788509d174
김경복 <kim.rejoice@gmail.com>
2014-01-14 01:17:51 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@4140 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/test/java/com/skplanet/storeplatform/sac/external/idp/service/TXAgreeUserIDPTest.java
+++ src/test/java/com/skplanet/storeplatform/sac/external/idp/service/TXAgreeUserIDPTest.java
@@ -42,31 +42,27 @@ public class TXAgreeUserIDPTest {
 	public void TXAgreeUserIDP() {
 		try {
 			Map<String, Object> param = new HashMap<String, Object>();
-			param.put("key", "IW0012300123");
-			param.put("key_type", "1");
-			// param.put("user_mdn", "01088870008");
-			// param.put("user_mdn_auth_key", "");
-			/** 0:부, 1:모, 2:기타 */
-			// param.put("parent_type", "0");
-			/**
-			 * 법정대리인실명인증수단코드 1:휴대폰 본인인증, 3:IPIN, 6: 이메일 (외국인 법정대리인 인증)
-			 */
-			// param.put("parent_rname_auth_type", "");
-			/** 법정대리인실명인증값(해외는 미획득), CI 값(88bytes) */
-			// param.put("parent_rname_auth_key", "");
-			/** 법정대리인 이름 */
-			// param.put("parent_name", "");
-			/** 법정대리인 이메일 */
-			// param.put("parent_email", "");
-			/** 법정대리인 동의일자 (YYYYMMDD) */
-			// param.put("parent_approve_date", "");
-			/** 법정대리인동의여부, Y=동의, N=미동의 */
-			param.put("is_parent_approve", "N");
 
-			/** 항목 삭제됨 */
-			// param.put("parent_mdn", "");
-			/** 항목 삭제 */
-			// param.put("parent_approve_sst_code", "");
+			/** param Key. */
+			// operator_id
+			// key_type
+			// key
+			// user_mdn
+			// user_ci
+			// user_di
+			// join_sst_list
+			// ocb_join_code
+			// os_code
+			// browser_code
+			// user_mdn_auth_key
+			// modify_req_date
+			// modify_req_time
+			// service_profiles
+
+			// TODO 임시 테스트용
+			param.put("key", "01088870008");
+			param.put("key_type", "1");
+			param.put("is_parent_approve", "N");
 
 			ImIDPReceiverM receiverM = this.imIDPService.agreeUser(param);
 			assertThat(receiverM.getResponseHeader().getResult(), notNullValue());
