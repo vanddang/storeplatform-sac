1abfecf8cc30605caadc2f1d3d417b80037b8b6e
오승민 <osm1021@sptek.co.kr>
2014-01-14 05:27:35 +0000

    dummy parameter 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@4196 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/display/vo/banner/BannerReq.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/banner/BannerReq.java
@@ -1,5 +1,14 @@
 package com.skplanet.storeplatform.sac.client.display.vo.banner;
 
 public class BannerReq {
+	private String dummy;
+
+	public String getDummy() {
+		return this.dummy;
+	}
+
+	public void setDummy(String dummy) {
+		this.dummy = dummy;
+	}
 
 }
--- src/main/java/com/skplanet/storeplatform/sac/client/display/vo/category/CategorySpecificReq.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/category/CategorySpecificReq.java
@@ -4,6 +4,8 @@ import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
 
 public class CategorySpecificReq extends CommonInfo {
 	private String list;
+	// TODO osm1021 dummy data가 필요없어지면 삭제할것
+	private String dummy;
 
 	public String getList() {
 		return this.list;
@@ -12,4 +14,13 @@ public class CategorySpecificReq extends CommonInfo {
 	public void setList(String list) {
 		this.list = list;
 	}
+
+	public String getDummy() {
+		return this.dummy;
+	}
+
+	public void setDummy(String dummy) {
+		this.dummy = dummy;
+	}
+
 }
