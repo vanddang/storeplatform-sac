53c1ddcaa6062d9b6476de16e47a43a057c91ebb
조용진 <kama624@naver.com>
2014-01-14 05:37:25 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@4201 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/purchase/vo/history/ExistenceRes.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/purchase/vo/history/ExistenceRes.java
@@ -27,11 +27,6 @@ public class ExistenceRes extends CommonInfo implements Serializable {
 
 	private String prodId; // 상품ID
 	private String prchsId; // 구매ID
-	private String resvCol01; // 예비1
-	private String resvCol02; // 예비2
-	private String resvCol03; // 예비3
-	private String resvCol04; // 예비4
-	private String resvCol05; // 예비5
 
 	/**
 	 * @return the prodId
@@ -61,81 +56,6 @@ public class ExistenceRes extends CommonInfo implements Serializable {
 	 */
 	public void setPrchsId(String prchsId) {
 		this.prchsId = prchsId;
-	}
-
-	/**
-	 * @return the resvCol01
-	 */
-	public String getResvCol01() {
-		return this.resvCol01;
-	}
-
-	/**
-	 * @param resvCol01
-	 *            the resvCol01 to set
-	 */
-	public void setResvCol01(String resvCol01) {
-		this.resvCol01 = resvCol01;
-	}
-
-	/**
-	 * @return the resvCol02
-	 */
-	public String getResvCol02() {
-		return this.resvCol02;
-	}
-
-	/**
-	 * @param resvCol02
-	 *            the resvCol02 to set
-	 */
-	public void setResvCol02(String resvCol02) {
-		this.resvCol02 = resvCol02;
-	}
-
-	/**
-	 * @return the resvCol03
-	 */
-	public String getResvCol03() {
-		return this.resvCol03;
-	}
-
-	/**
-	 * @param resvCol03
-	 *            the resvCol03 to set
-	 */
-	public void setResvCol03(String resvCol03) {
-		this.resvCol03 = resvCol03;
-	}
-
-	/**
-	 * @return the resvCol04
-	 */
-	public String getResvCol04() {
-		return this.resvCol04;
-	}
-
-	/**
-	 * @param resvCol04
-	 *            the resvCol04 to set
-	 */
-	public void setResvCol04(String resvCol04) {
-		this.resvCol04 = resvCol04;
-	}
-
-	/**
-	 * @return the resvCol05
-	 */
-	public String getResvCol05() {
-		return this.resvCol05;
-	}
-
-	/**
-	 * @param resvCol05
-	 *            the resvCol05 to set
-	 */
-	public void setResvCol05(String resvCol05) {
-		this.resvCol05 = resvCol05;
 	}
 
 	@Override
