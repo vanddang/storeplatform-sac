4ac2461a4d25d488733ecc2803bcf58f488a646a
김다슬 <daseul428@spteck.co.kr>
2014-01-15 11:56:43 +0000

    External Comp.에서 필수파라미터로 요청한 request 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@4415 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/member/vo/miscellaneous/GetPhoneAuthorizationCodeReq.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/miscellaneous/GetPhoneAuthorizationCodeReq.java
@@ -13,8 +13,10 @@ import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
 public class GetPhoneAuthorizationCodeReq {
 	private static final long serialVersionUID = 1L;
 
-	private String userPhone;
-	private String userTelecom;
+	private String userPhone; // 휴대폰 번호
+	private String userTelecom; // 통신사 코드
+	private String srcId; // 요청측ID
+	private String teleSvcId; // 텔레서비스ID
 
 	public String getUserPhone() {
 		return this.userPhone;
@@ -31,4 +33,21 @@ public class GetPhoneAuthorizationCodeReq {
 	public void setUserTelecom(String userTelecom) {
 		this.userTelecom = userTelecom;
 	}
+
+	public String getSrcId() {
+		return this.srcId;
+	}
+
+	public void setSrcId(String srcId) {
+		this.srcId = srcId;
+	}
+
+	public String getTeleSvcId() {
+		return this.teleSvcId;
+	}
+
+	public void setTeleSvcId(String teleSvcId) {
+		this.teleSvcId = teleSvcId;
+	}
+
 }
