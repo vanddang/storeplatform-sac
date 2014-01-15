a0122e487c1d7ee7d88c8e5e0c6ee080653ce97a
한서구 <seoguman@nate.com>
2014-01-15 10:15:46 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@4385 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/seller/SearchIdReq.java
@@ -0,0 +1,55 @@
+package com.skplanet.storeplatform.sac.client.member.vo.seller;
+
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+/**
+ * 판매자회원 ID 찾기
+ * 
+ * Updated on : 2014. 1. 7. Updated by : 한서구, 부르칸.
+ */
+@JsonSerialize(include = Inclusion.NON_NULL)
+public class SearchIdReq extends CommonInfo {
+
+	private static final long serialVersionUID = 1L;
+
+	private String sellerEmail;
+	private String sellerCompany;
+	private String sellerBizNumber;
+	private String sellerPhone;
+
+	public String getSellerEmail() {
+		return this.sellerEmail;
+	}
+
+	public void setSellerEmail(String sellerEmail) {
+		this.sellerEmail = sellerEmail;
+	}
+
+	public String getSellerCompany() {
+		return this.sellerCompany;
+	}
+
+	public void setSellerCompany(String sellerCompany) {
+		this.sellerCompany = sellerCompany;
+	}
+
+	public String getSellerBizNumber() {
+		return this.sellerBizNumber;
+	}
+
+	public void setSellerBizNumber(String sellerBizNumber) {
+		this.sellerBizNumber = sellerBizNumber;
+	}
+
+	public String getSellerPhone() {
+		return this.sellerPhone;
+	}
+
+	public void setSellerPhone(String sellerPhone) {
+		this.sellerPhone = sellerPhone;
+	}
+
+}
