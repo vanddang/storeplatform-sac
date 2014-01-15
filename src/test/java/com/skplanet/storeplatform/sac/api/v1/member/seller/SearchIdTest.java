a49bbeb6b07b6f90811ab021cfd4a19ba4b5ecfb
한서구 <seoguman@nate.com>
2014-01-15 11:22:35 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@4400 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/test/java/com/skplanet/storeplatform/sac/api/v1/member/seller/SearchIdTest.java
@@ -0,0 +1,75 @@
+package com.skplanet.storeplatform.sac.api.v1.member.seller;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.skplanet.storeplatform.framework.test.SuccessCallback;
+import com.skplanet.storeplatform.framework.test.TestCaseTemplate;
+import com.skplanet.storeplatform.framework.test.TestCaseTemplate.RunMode;
+import com.skplanet.storeplatform.sac.client.member.vo.seller.SearchIdRes;
+
+@ActiveProfiles(value = "local")
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({ "classpath*:/spring-test/context-test.xml" })
+public class SearchIdTest {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(DetailAccountInformationTest.class);
+
+	@Autowired
+	private WebApplicationContext wac;
+
+	private MockMvc mockMvc;
+
+	/**
+	 * 
+	 * <pre>
+	 * method 설명.
+	 * </pre>
+	 */
+	@Before
+	public void before() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+	}
+
+	/**
+	 * <pre>
+	 * 판매자 ID찾기.
+	 * </pre>
+	 */
+	@Test
+	public void searchId() {
+
+		new TestCaseTemplate(this.mockMvc)
+				.url("/dev/member/seller/searchId/v1?sellerEmail=&sellerCompany=&sellerBizNumber=&sellerPhone")
+				.httpMethod(HttpMethod.GET).success(SearchIdRes.class, new SuccessCallback() {
+					@Override
+					public void success(Object result, HttpStatus httpStatus, RunMode runMode) {
+						SearchIdRes res = (SearchIdRes) result;
+						assertThat(res.getSellerMbr(), notNullValue());
+						LOGGER.debug("response param : {}", res.toString());
+					}
+				}, HttpStatus.OK, HttpStatus.ACCEPTED).run(RunMode.JSON);
+
+	}
+
+}
