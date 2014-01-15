6ddbb2830ba3b193457ce27eecbbf245f357043f
김현일 <nahiyak@incross.com>
2014-01-15 01:19:32 +0000

    refs #4789 검색 연동
    검색 BYPASS 으로 변경.
    
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@4302 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/test/java/com/skplanet/storeplatform/sac/other/search/controller/SearchControllerTest.java
+++ src/test/java/com/skplanet/storeplatform/sac/other/search/controller/SearchControllerTest.java
@@ -26,8 +26,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.skplanet.storeplatform.sac.client.other.vo.search.SearchReq;
-
 /**
  * 
  * 검색 컨트롤러 클래스 테스트
@@ -68,17 +66,6 @@ public class SearchControllerTest {
 	 */
 	@Test
 	public void shouldSearch() throws Exception {
-
-		SearchReq searchReq = new SearchReq();
-
-		searchReq.setKeywd("pc매니저");
-		searchReq.setAdultYN("Y");
-		searchReq.setRelSearchYN("Y");
-		searchReq.setDeviceId("38a942103867f40b7f421c9b3c3b4443");
-		searchReq.setDeviceModelNo("SHV-E160L");
-		searchReq.setOrderedBy("recent");
-		searchReq.setOffset(0);
-		searchReq.setCount(10);
 
 		this.mvc.perform(
 				get("/other/search/v1").param("keywd", "pc매니저").param("adultYN", "Y").param("relSearchYN", "Y")
