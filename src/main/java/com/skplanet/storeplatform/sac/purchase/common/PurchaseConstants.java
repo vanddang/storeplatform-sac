813f183fa962faf04ba28cc6ee70e2a6e6f3acd6
양주원 <sden2720@gmail.com>
2014-01-15 10:45:57 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@4393 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/purchase/common/PurchaseConstants.java
@@ -0,0 +1,48 @@
+package com.skplanet.storeplatform.sac.purchase.common;
+
+/**
+ * 구매 관련 상수
+ * 
+ * Updated on : 2014. 1. 9. Updated by : 양주원, 엔텔스.
+ */
+public final class PurchaseConstants {
+
+	/** 사용 여부 Y N. **/
+	public static final String USE_Y = "Y"; // Y
+	public static final String USE_N = "N"; // N
+
+	/** 구매상태코드. */
+	public static final String PRCHS_STATUS_COMPT = "OR000301"; // 구매완료
+	public static final String PRCHS_STATUS_CANCEL = "OR000302"; // 구매취소
+	public static final String PRCHS_STATUS_FAIL = "OR000304"; // 구매실패
+	public static final String PRCHS_STATUS_TEST_CANCEL = "OR000303"; // 시험폰구매취소
+	public static final String PRCHS_STATUS_DRBK = "OR000305"; // 환불
+
+	/** 구매요청경로코드. */
+	public static final String PRCHS_REQ_PATH_WEB = "OR000401"; // WEB
+	public static final String PRCHS_REQ_PATH_WAP = "OR000402"; // WAP
+	public static final String PRCHS_REQ_PATH_PC = "OR000403"; // PC Suite
+	public static final String PRCHS_REQ_PATH_MOBILE_WEB = "OR000404"; // Mobile WEB
+	public static final String PRCHS_REQ_PATH_MOBILE_CLIENT = "OR000405"; // Mobile Client
+
+	/** 수신확인경로코드. */
+	public static final String RECV_CONF_PATH_WEB_POP = "OR003901"; // 팝업확인(Web)
+	public static final String RECV_CONF_PATH_WEB_MY = "OR003902"; // 보관함확인(Web)
+	public static final String RECV_CONF_PATH_MOBILE_POP = "OR003903"; // 팝업확인(Mobile)
+	public static final String RECV_CONF_PATH_MOBILE_MY = "OR003904"; // 보관함확인(Mobile)
+
+	/** 구매유형코드. */
+	public static final String PRCHS_CASE_SEND = "OR003801"; // 선물발신
+	public static final String PRCHS_CASE_RECV = "OR003802"; // 선물수신
+
+	/** 네트워크타입코드. */
+	public static final String NETWORK_TYPE_ALL = "DP004401"; // 모두지원
+	public static final String NETWORK_TYPE_WIFI = "DP004402"; // WI-FI
+	public static final String NETWORK_TYPE_3G = "DP004403"; // 3G
+
+	/** 결제상태코드. */
+	public static final String PAYMENT_STATUS_PLAM = "OR000701";
+	public static final String PAYMENT_STATUS_CANCEL = "OR000702";
+	public static final String PAYMENT_STATUS_COMPT = "OR000703";
+
+}
