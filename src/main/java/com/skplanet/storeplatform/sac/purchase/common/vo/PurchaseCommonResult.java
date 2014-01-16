77c97686330b3582320baf0dfa442c546ee4ad71
최성우 <cswoo8101@gmail.com>
2014-01-16 07:23:52 +0000

    SAC 구매 공통 VO
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@4595 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/purchase/common/vo/PurchaseCommonResult.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.purchase.common.vo;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+/**
+ * 구매 공통 Result VO.
+ * 
+ * Updated on : 2014. 1. 16. Updated by : nTels_cswoo81, nTels.
+ */
+public class PurchaseCommonResult extends CommonInfo {
+
+	private static final long serialVersionUID = 1L;
+
+}
