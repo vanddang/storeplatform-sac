67813c8df84b72b151f43e50bfd7184f6dae71b0
최성우 <cswoo8101@gmail.com>
2014-01-16 06:17:19 +0000

    구매 공통 VO
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@4573 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/purchase/common/vo/PurchaseCommonReq.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.purchase.common.vo;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+/**
+ * 구매 공통 요청 VO.
+ * 
+ * Updated on : 2014. 1. 16. Updated by : nTels_cswoo81, nTels.
+ */
+public class PurchaseCommonReq extends CommonInfo {
+
+	private static final long serialVersionUID = 1L;
+
+	private String userKey;
+	private String deviceKey;
+
+	/**
+	 * @return the userKey
+	 */
+	public String getUserKey() {
+		return this.userKey;
+	}
+
+	/**
+	 * @param userKey
+	 *            the userKey to set
+	 */
+	public void setUserKey(String userKey) {
+		this.userKey = userKey;
+	}
+
+	/**
+	 * @return the deviceKey
+	 */
+	public String getDeviceKey() {
+		return this.deviceKey;
+	}
+
+	/**
+	 * @param deviceKey
+	 *            the deviceKey to set
+	 */
+	public void setDeviceKey(String deviceKey) {
+		this.deviceKey = deviceKey;
+	}
+
+}
---
+++ src/main/java/com/skplanet/storeplatform/sac/client/purchase/common/vo/PurchaseCommonRes.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.purchase.common.vo;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+/**
+ * Class 설명
+ * 
+ * Updated on : 2014. 1. 16. Updated by : nTels_cswoo81, nTels.
+ */
+public class PurchaseCommonRes extends CommonInfo {
+
+	private static final long serialVersionUID = 1L;
+
+}
