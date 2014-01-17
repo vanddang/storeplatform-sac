9af47e9e30f5ad3572a9012d3ee298fab4fbd478
한서구 <seoguman@nate.com>
2014-01-17 09:23:21 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@4860 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/common/SellerMbrPwdHint.java
@@ -0,0 +1,128 @@
+package com.skplanet.storeplatform.sac.client.member.vo.common;
+
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+/**
+ * 비밀번호 힌트 Value Object
+ * 
+ * Updated on : 2014. 1. 7. Updated by : 한서구, 부르칸.
+ */
+@JsonSerialize(include = Inclusion.NON_NULL)
+public class SellerMbrPwdHint extends CommonInfo {
+
+	private static final long serialVersionUID = 1L;
+
+	/** 언어 코드. */
+	private String languageCode;
+
+	/** 보안질문 ID. */
+	private String questionID;
+
+	/** 보안질문 명. */
+	private String questionName;
+
+	/** 보안질문 설명. */
+	private String questionDescription;
+
+	/** 전시 여부. */
+	private String isDisplay;
+
+	/** 전시 순서. */
+	private int displayOrder;
+
+	/** 등록 ID. */
+	private String regID; // REG_ID
+
+	/** 등록일시. */
+	private String regDate; // REG_DT
+
+	/** 수정 ID. */
+	private String updateID; // UPD_ID
+
+	/** 수정일시. */
+	private String updateDate; // UPD_DT
+
+	public String getLanguageCode() {
+		return this.languageCode;
+	}
+
+	public void setLanguageCode(String languageCode) {
+		this.languageCode = languageCode;
+	}
+
+	public String getQuestionID() {
+		return this.questionID;
+	}
+
+	public void setQuestionID(String questionID) {
+		this.questionID = questionID;
+	}
+
+	public String getQuestionName() {
+		return this.questionName;
+	}
+
+	public void setQuestionName(String questionName) {
+		this.questionName = questionName;
+	}
+
+	public String getQuestionDescription() {
+		return this.questionDescription;
+	}
+
+	public void setQuestionDescription(String questionDescription) {
+		this.questionDescription = questionDescription;
+	}
+
+	public String getIsDisplay() {
+		return this.isDisplay;
+	}
+
+	public void setIsDisplay(String isDisplay) {
+		this.isDisplay = isDisplay;
+	}
+
+	public int getDisplayOrder() {
+		return this.displayOrder;
+	}
+
+	public void setDisplayOrder(int displayOrder) {
+		this.displayOrder = displayOrder;
+	}
+
+	public String getRegID() {
+		return this.regID;
+	}
+
+	public void setRegID(String regID) {
+		this.regID = regID;
+	}
+
+	public String getRegDate() {
+		return this.regDate;
+	}
+
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+
+	public String getUpdateID() {
+		return this.updateID;
+	}
+
+	public void setUpdateID(String updateID) {
+		this.updateID = updateID;
+	}
+
+	public String getUpdateDate() {
+		return this.updateDate;
+	}
+
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
+
+}
