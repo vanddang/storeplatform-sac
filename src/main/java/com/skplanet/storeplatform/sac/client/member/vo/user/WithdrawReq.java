0be75d732d89df20fe9e09acc9766d9762cbdc05
강신완 <@gmail.com>
2014-01-17 12:03:31 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@4896 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/member/vo/user/WithdrawReq.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/user/WithdrawReq.java
@@ -1,8 +1,5 @@
 package com.skplanet.storeplatform.sac.client.member.vo.user;
 
-import org.codehaus.jackson.map.annotate.JsonSerialize;
-import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
-
 import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
 
 /**
@@ -10,7 +7,6 @@ import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
  * 
  * Updated on : 2014. 1. 6. Updated by : 강신완, 부르칸.
  */
-@JsonSerialize(include = Inclusion.NON_NULL)
 public class WithdrawReq extends CommonInfo {
 
 	private static final long serialVersionUID = 1L;
