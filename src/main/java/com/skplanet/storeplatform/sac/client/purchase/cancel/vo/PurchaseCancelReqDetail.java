b59b3fd3cd9fd4d1f01526cf3993a4c4a16a8465
최성우 <cswoo8101@gmail.com>
2014-01-20 07:08:27 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@4984 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/purchase/cancel/vo/PurchaseCancelReqDetail.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/purchase/cancel/vo/PurchaseCancelReqDetail.java
@@ -9,14 +9,14 @@
  */
 package com.skplanet.storeplatform.sac.client.purchase.cancel.vo;
 
-import com.skplanet.storeplatform.sac.client.purchase.common.vo.PurchaseCommonReq;
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
 
 /**
  * 구매 취소 요청 상세 VO.
  * 
  * Updated on : 2014. 1. 16. Updated by : nTels_cswoo81, nTels.
  */
-public class PurchaseCancelReqDetail extends PurchaseCommonReq {
+public class PurchaseCancelReqDetail extends CommonInfo {
 
 	private static final long serialVersionUID = 1L;
 
