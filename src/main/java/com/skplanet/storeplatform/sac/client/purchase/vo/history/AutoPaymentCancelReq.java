3e387c904fb5536ec545ecca8206ae1ad1ff7b31
조용진 <kama624@naver.com>
2014-01-20 10:42:13 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@5057 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/purchase/vo/history/AutoPaymentCancelReq.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.purchase.vo.history;
+
+import java.io.Serializable;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+/**
+ * 
+ * 자동결제해치예약 요청.
+ * 
+ * Updated on : 2014. 01. 15. Updated by : 조용진, 엔텔스.
+ */
+public class AutoPaymentCancelReq extends CommonInfo implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String tenantId; // Tenant ID
+	private String insdUsermbrNo; // 회원번호
+	private String insdDeviceId; // 디바이스 번호
+	private String prchsId; // 구매ID
+	private String closedCd; // 해지예약코드
+	private String closedReasonCd; // 해지사유 코드
+	private String closedReqPathCd; // 해지요청경로 코드
+
+	/**
+	 * @return the tenantId
+	 */
+	public String getTenantId() {
+		return this.tenantId;
+	}
+
+	/**
+	 * @param tenantId
+	 *            the tenantId to set
+	 */
+	public void setTenantId(String tenantId) {
+		this.tenantId = tenantId;
+	}
+
+	/**
+	 * @return the insdUsermbrNo
+	 */
+	public String getInsdUsermbrNo() {
+		return this.insdUsermbrNo;
+	}
+
+	/**
+	 * @param insdUsermbrNo
+	 *            the insdUsermbrNo to set
+	 */
+	public void setInsdUsermbrNo(String insdUsermbrNo) {
+		this.insdUsermbrNo = insdUsermbrNo;
+	}
+
+	/**
+	 * @return the insdDeviceId
+	 */
+	public String getInsdDeviceId() {
+		return this.insdDeviceId;
+	}
+
+	/**
+	 * @param insdDeviceId
+	 *            the insdDeviceId to set
+	 */
+	public void setInsdDeviceId(String insdDeviceId) {
+		this.insdDeviceId = insdDeviceId;
+	}
+
+	/**
+	 * @return the prchsId
+	 */
+	public String getPrchsId() {
+		return this.prchsId;
+	}
+
+	/**
+	 * @param prchsId
+	 *            the prchsId to set
+	 */
+	public void setPrchsId(String prchsId) {
+		this.prchsId = prchsId;
+	}
+
+	/**
+	 * @return the closedCd
+	 */
+	public String getClosedCd() {
+		return this.closedCd;
+	}
+
+	/**
+	 * @param closedCd
+	 *            the closedCd to set
+	 */
+	public void setClosedCd(String closedCd) {
+		this.closedCd = closedCd;
+	}
+
+	/**
+	 * @return the closedReasonCd
+	 */
+	public String getClosedReasonCd() {
+		return this.closedReasonCd;
+	}
+
+	/**
+	 * @param closedReasonCd
+	 *            the closedReasonCd to set
+	 */
+	public void setClosedReasonCd(String closedReasonCd) {
+		this.closedReasonCd = closedReasonCd;
+	}
+
+	/**
+	 * @return the closedReqPathCd
+	 */
+	public String getClosedReqPathCd() {
+		return this.closedReqPathCd;
+	}
+
+	/**
+	 * @param closedReqPathCd
+	 *            the closedReqPathCd to set
+	 */
+	public void setClosedReqPathCd(String closedReqPathCd) {
+		this.closedReqPathCd = closedReqPathCd;
+	}
+
+}
---
+++ src/main/java/com/skplanet/storeplatform/sac/client/purchase/vo/history/AutoPaymentCancelRes.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.purchase.vo.history;
+
+import java.io.Serializable;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+/**
+ * 
+ * 자동결제해치예약 요청.
+ * 
+ * Updated on : 2014. 01. 15. Updated by : 조용진, 엔텔스.
+ */
+public class AutoPaymentCancelRes extends CommonInfo implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String prchsId; // 구매ID
+	private String resultYn; // 업데이트 성공여부 및 이메일 발송성공여부(Y,N)
+
+	/**
+	 * @return the prchsId
+	 */
+	public String getPrchsId() {
+		return this.prchsId;
+	}
+
+	/**
+	 * @param prchsId
+	 *            the prchsId to set
+	 */
+	public void setPrchsId(String prchsId) {
+		this.prchsId = prchsId;
+	}
+
+	/**
+	 * @return the resultYn
+	 */
+	public String getResultYn() {
+		return this.resultYn;
+	}
+
+	/**
+	 * @param resultYn
+	 *            the resultYn to set
+	 */
+	public void setResultYn(String resultYn) {
+		this.resultYn = resultYn;
+	}
+
+}
