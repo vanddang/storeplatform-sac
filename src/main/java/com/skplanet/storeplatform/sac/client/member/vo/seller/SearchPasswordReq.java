68b362096879b313eca65e66a1de894398a1ba84
한서구 <seoguman@nate.com>
2014-01-20 05:45:33 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@4958 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/seller/SearchPasswordReq.java
@@ -0,0 +1,28 @@
+package com.skplanet.storeplatform.sac.client.member.vo.seller;
+
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+/**
+ * 판매자회원 Password 찾기
+ * 
+ * Updated on : 2014. 1. 20. Updated by : 한서구, 부르칸.
+ */
+@JsonSerialize(include = Inclusion.NON_NULL)
+public class SearchPasswordReq extends CommonInfo {
+
+	private static final long serialVersionUID = 1L;
+
+	private String sellerId;
+
+	public String getSellerId() {
+		return this.sellerId;
+	}
+
+	public void setSellerId(String sellerId) {
+		this.sellerId = sellerId;
+	}
+
+}
