1cf26192c68d6c84d371fc90984f53c95164de9a
김현일 <nahiyak@incross.com>
2014-01-20 12:17:56 +0000

    check Style 적용.
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@5100 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/other/vo/interpark/CreateOrderRes.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/other/vo/interpark/CreateOrderRes.java
@@ -2,23 +2,45 @@ package com.skplanet.storeplatform.sac.client.other.vo.interpark;
 
 import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
 
+/**
+ * 
+ * CreateOrderRes Value Object
+ * 
+ * Updated on : 2014. 1. 20. Updated by : 김현일, 인크로스.
+ */
 public class CreateOrderRes extends CommonInfo {
 
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * 생성자.
+	 */
 	public CreateOrderRes() {
 	}
 
+	/**
+	 * 생성자.
+	 * 
+	 * @param resultStatus
+	 *            resultStatus
+	 */
 	public CreateOrderRes(String resultStatus) {
 		this.resultStatus = resultStatus;
 	}
 
 	private String resultStatus;
 
+	/**
+	 * @return String
+	 */
 	public String getResultStatus() {
 		return this.resultStatus;
 	}
 
+	/**
+	 * @param resultStatus
+	 *            resultStatus
+	 */
 	public void setResultStatus(String resultStatus) {
 		this.resultStatus = resultStatus;
 	}
--- src/main/java/com/skplanet/storeplatform/sac/client/other/vo/interpark/GetAuthKeyRes.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/other/vo/interpark/GetAuthKeyRes.java
@@ -2,16 +2,29 @@ package com.skplanet.storeplatform.sac.client.other.vo.interpark;
 
 import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
 
+/**
+ * 
+ * GetAuthKeyRes Value Object
+ * 
+ * Updated on : 2014. 1. 20. Updated by : 김현일, 인크로스.
+ */
 public class GetAuthKeyRes extends CommonInfo {
 
 	private static final long serialVersionUID = 1L;
 
 	private String certKey;
 
+	/**
+	 * @return String
+	 */
 	public String getCertKey() {
 		return this.certKey;
 	}
 
+	/**
+	 * @param certKey
+	 *            certKey
+	 */
 	public void setCertKey(String certKey) {
 		this.certKey = certKey;
 	}
