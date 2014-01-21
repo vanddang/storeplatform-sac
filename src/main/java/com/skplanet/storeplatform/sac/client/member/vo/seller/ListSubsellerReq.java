160ee7c69b5e89dca5f26158bf199c7b35ad01df
한서구 <seoguman@nate.com>
2014-01-21 01:57:57 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@5146 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/seller/ListSubsellerReq.java
@@ -0,0 +1,28 @@
+package com.skplanet.storeplatform.sac.client.member.vo.seller;
+
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+/**
+ * 서브계정 목록 조회
+ * 
+ * Updated on : 2014. 1. 7. Updated by : 한서구, 부르칸.
+ */
+@JsonSerialize(include = Inclusion.NON_NULL)
+public class ListSubsellerReq extends CommonInfo {
+
+	private static final long serialVersionUID = 1L;
+
+	private String sellerKey;
+
+	public String getSellerKey() {
+		return this.sellerKey;
+	}
+
+	public void setSellerKey(String sellerKey) {
+		this.sellerKey = sellerKey;
+	}
+
+}
