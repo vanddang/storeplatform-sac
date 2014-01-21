d3dac1d3b9fbd348bb5ef687bf8fa334cdbcb1a1
한서구 <seoguman@nate.com>
2014-01-21 05:16:53 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@5191 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/test/java/com/skplanet/storeplatform/sac/api/v1/member/seller/ListSubsellerTest.java
@@ -0,0 +1,77 @@
+package com.skplanet.storeplatform.sac.api.v1.member.seller;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.skplanet.storeplatform.framework.test.SuccessCallback;
+import com.skplanet.storeplatform.framework.test.TestCaseTemplate;
+import com.skplanet.storeplatform.framework.test.TestCaseTemplate.RunMode;
+import com.skplanet.storeplatform.sac.api.v1.member.constant.MemberTestConstant;
+import com.skplanet.storeplatform.sac.client.member.vo.seller.ListSubsellerRes;
+
+@ActiveProfiles(value = "local")
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({ "classpath*:/spring-test/context-test.xml" })
+public class ListSubsellerTest {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ListSubsellerTest.class);
+
+	@Autowired
+	private WebApplicationContext wac;
+
+	private MockMvc mockMvc;
+
+	/**
+	 * 
+	 * <pre>
+	 * method 설명.
+	 * </pre>
+	 */
+	@Before
+	public void before() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+	}
+
+	/**
+	 * <pre>
+	 * 서브계정 목록 조회.
+	 * </pre>
+	 */
+	@Test
+	public void listSubseller() {
+
+		new TestCaseTemplate(this.mockMvc)
+				.url(MemberTestConstant.PREFIX_SELLER_LOCAL_PATH
+						+ "/listSubseller/v1?sellerKey=IF1023501663520130915113745").httpMethod(HttpMethod.GET)
+				.success(ListSubsellerRes.class, new SuccessCallback() {
+					@Override
+					public void success(Object result, HttpStatus httpStatus, RunMode runMode) {
+						ListSubsellerRes res = (ListSubsellerRes) result;
+						assertThat(res.getSellerKey(), notNullValue());
+						LOGGER.debug("response param : {}", res.toString());
+					}
+				}, HttpStatus.OK, HttpStatus.ACCEPTED).run(RunMode.JSON);
+
+	}
+
+}
