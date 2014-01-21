d363039a735d76ca900859205e5fa0101e105c8b
심대진 <tlaeo00@gmail.com>
2014-01-21 06:42:28 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@5224 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/test/java/com/skplanet/storeplatform/sac/api/v1/member/user/UserWithdrawTest.java
+++ src/test/java/com/skplanet/storeplatform/sac/api/v1/member/user/UserWithdrawTest.java
@@ -3,9 +3,14 @@ package com.skplanet.storeplatform.sac.api.v1.member.user;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +30,11 @@ import com.skplanet.storeplatform.framework.test.RequestBodySetter;
 import com.skplanet.storeplatform.framework.test.SuccessCallback;
 import com.skplanet.storeplatform.framework.test.TestCaseTemplate;
 import com.skplanet.storeplatform.framework.test.TestCaseTemplate.RunMode;
+import com.skplanet.storeplatform.sac.api.util.DateUtil;
+import com.skplanet.storeplatform.sac.api.v1.member.constant.MemberTestConstant;
+import com.skplanet.storeplatform.sac.client.member.vo.common.AgreementInfo;
+import com.skplanet.storeplatform.sac.client.member.vo.user.CreateByMdnReq;
+import com.skplanet.storeplatform.sac.client.member.vo.user.CreateByMdnRes;
 import com.skplanet.storeplatform.sac.client.member.vo.user.ExistRes;
 import com.skplanet.storeplatform.sac.client.member.vo.user.WithdrawReq;
 import com.skplanet.storeplatform.sac.client.member.vo.user.WithdrawRes;
@@ -34,6 +44,7 @@ import com.skplanet.storeplatform.sac.client.member.vo.user.WithdrawRes;
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration({ "classpath*:/spring-test/context-test.xml" })
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class UserWithdrawTest {
 	private static final Logger logger = LoggerFactory.getLogger(UserWithdrawTest.class);
 
@@ -55,18 +66,94 @@ public class UserWithdrawTest {
 
 	/**
 	 * <pre>
+	 * MDN 회원 가입 테스트.
+	 * </pre>
+	 * 
+	 * @throws Exception
+	 *             Exception
+	 */
+	@Test
+	public void acreateByMdn() throws Exception {
+
+		new TestCaseTemplate(this.mockMvc).url(MemberTestConstant.PREFIX_USER_PATH_REAL + "/createByMdn/v1").httpMethod(HttpMethod.POST)
+				.addHeaders("x-store-auth-info", "authKey=114127c7ef42667669819dad5df8d820c;ist=N")
+				.addHeaders("Accept", "application/json")
+				.addHeaders("x-planet-device-info", "model=\"SHW-M190S\",fwVersion=\"2.1.3_20101005f\",pkgVersion=\"com.skplanet.tstore.mobile/38\",rootDetection=\"no\"")
+				.requestBody(new RequestBodySetter() {
+					@Override
+					public Object requestBody() {
+
+						CreateByMdnReq reqJson = new CreateByMdnReq();
+						reqJson.setDeviceId("01088870008");
+						reqJson.setDeviceIdType("msisdn");
+						reqJson.setDeviceTelecom("US001201");
+						reqJson.setImei("A0000031648EE9");
+						reqJson.setJoinId("US002903");
+						reqJson.setIsRecvSms("Y");
+						reqJson.setOwnBirth("20020328");
+
+						reqJson.setIsParent("Y"); // 법정대리인정보 등록 여부.
+
+						// 법정 대리인 정보 (isParent 값이 Y 일경우 등록 된다.)
+						reqJson.setParentRealNameMethod("US011101");
+						reqJson.setParentName("홍길동");
+						reqJson.setParentType("F");
+						reqJson.setParentDate(DateUtil.getToday());
+						reqJson.setParentEmail("hkd@aaaa.com");
+						reqJson.setParentBirthDay("19700331");
+						reqJson.setParentTelecom("US001201");
+						reqJson.setParentPhone("01088889999");
+						reqJson.setParentCi("skpone0000132653GWyh3WsEm0FutitO5oSgC2/SgSrLKv5XohA8mxTNLitpB1 B9A3z5zrVHettHzKa5dpJA==");
+						reqJson.setParentRealNameDate(DateUtil.getToday());
+						reqJson.setParentRealNameSite("US011203"); // shop client 3.0
+
+						// 동의 정보
+						List<AgreementInfo> agreementList = new ArrayList<AgreementInfo>();
+						AgreementInfo agreement1 = new AgreementInfo();
+						agreement1.setExtraAgreementId("US010607");
+						agreement1.setExtraAgreementVersion("0.1");
+						agreement1.setIsExtraAgreement("Y");
+						AgreementInfo agreement2 = new AgreementInfo();
+						agreement2.setExtraAgreementId("US010608");
+						agreement2.setExtraAgreementVersion("0.1");
+						agreement2.setIsExtraAgreement("Y");
+						AgreementInfo agreement3 = new AgreementInfo();
+						agreement3.setExtraAgreementId("US010609");
+						agreement3.setExtraAgreementVersion("0.1");
+						agreement3.setIsExtraAgreement("Y");
+
+						agreementList.add(agreement1);
+						agreementList.add(agreement2);
+						agreementList.add(agreement3);
+						reqJson.setAgreementList(agreementList);
+
+						return reqJson;
+					}
+				}).success(CreateByMdnRes.class, new SuccessCallback() {
+					@Override
+					public void success(Object result, HttpStatus httpStatus, RunMode runMode) {
+						CreateByMdnRes res = (CreateByMdnRes) result;
+						assertThat(res.getUserKey(), notNullValue());
+						System.out.println(res.toString());
+					}
+				}, HttpStatus.OK, HttpStatus.ACCEPTED).run(RunMode.JSON);
+
+	}
+
+	/**
+	 * <pre>
 	 * 회원탈퇴 : MDN/UUID 만 가능
 	 * </pre>
 	 */
 	@Test
-	public void userWithdraw() {
+	public void buserWithdraw() {
 
 		new TestCaseTemplate(this.mockMvc).url("/dev/member/user/withdraw/v1").httpMethod(HttpMethod.POST)
 				.requestBody(new RequestBodySetter() {
 					@Override
 					public Object requestBody() {
 						WithdrawReq req = new WithdrawReq();
-						req.setDeviceId("생성한 deviceId");
+						req.setDeviceId("01088870008");
 						logger.debug("request param : {}", req.toString());
 						return req;
 					}
