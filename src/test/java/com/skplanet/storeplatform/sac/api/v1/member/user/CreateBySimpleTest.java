d9e9d34046997ff3dde02fcfc2dfa2c0d0cd194a
심대진 <tlaeo00@gmail.com>
2014-01-22 04:04:10 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@5444 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/test/java/com/skplanet/storeplatform/sac/api/v1/member/user/CreateBySimpleTest.java
+++ src/test/java/com/skplanet/storeplatform/sac/api/v1/member/user/CreateBySimpleTest.java
@@ -12,8 +12,12 @@ package com.skplanet.storeplatform.sac.api.v1.member.user;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.FixMethodOrder;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
@@ -33,6 +37,7 @@ import com.skplanet.storeplatform.framework.test.SuccessCallback;
 import com.skplanet.storeplatform.framework.test.TestCaseTemplate;
 import com.skplanet.storeplatform.framework.test.TestCaseTemplate.RunMode;
 import com.skplanet.storeplatform.sac.api.v1.member.constant.MemberTestConstant;
+import com.skplanet.storeplatform.sac.client.member.vo.common.DeviceExtraInfo;
 import com.skplanet.storeplatform.sac.client.member.vo.user.CreateByAgreementRes;
 import com.skplanet.storeplatform.sac.client.member.vo.user.CreateBySimpleReq;
 
@@ -65,32 +70,37 @@ public class CreateBySimpleTest {
 
 	/**
 	 * <pre>
-	 * ID 회원 간편 가입 (IDP 회원) [[ 단말정보 포함 ]].
+	 * ID 회원 간편 가입 (IDP 회원) [[ 단말정보 미포함 ]].
 	 * </pre>
 	 * 
 	 * @throws Exception
 	 *             Exception
 	 */
 	@Test
-	public void createBySimpleDevice() throws Exception {
+	public void createBySimpleId() throws Exception {
 
 		new TestCaseTemplate(this.mvc).url(MemberTestConstant.PREFIX_USER_PATH_DEV + "/createBySimple/v1").httpMethod(HttpMethod.POST)
 				.addHeaders("x-store-auth-info", "authKey=114127c7ef42667669819dad5df8d820c;ist=N")
 				.addHeaders("Accept", "application/json")
-				.addHeaders("x-planet-device-info", "model=\"SHW-M190S\",fwVersion=\"2.1.3_20101005f\",pkgVersion=\"com.skplanet.tstore.mobile/38\",rootDetection=\"no\"")
 				.requestBody(new RequestBodySetter() {
 					@Override
 					public Object requestBody() {
 
 						CreateBySimpleReq reqJson = new CreateBySimpleReq();
-						reqJson.setDeviceId("01076771470");
-						reqJson.setDeviceIdType("msisdn");
-						reqJson.setUserId("watermin0927");
-						reqJson.setUserPw("123qwe");
-						reqJson.setUserEmail("tlaeo00@naver.com");
-						reqJson.setDeviceTelecom("US001201");
-						reqJson.setJoinId("US012304");
-						reqJson.setIsRecvSms("Y");
+
+						// 사용자 아이디
+						reqJson.setUserId("idpsimplemember2"); // 대문자 ID는 가입 불가 IDP 정책.
+						reqJson.setUserPw("abcd1234");
+						reqJson.setUserEmail("idpsimplemember2@naver.com");
+
+						// 단말 정보
+						reqJson.setDeviceId(""); // 기기 ID
+						reqJson.setDeviceIdType(""); // 기기 ID 타입
+						reqJson.setDeviceTelecom(""); // 통신사
+						reqJson.setNativeId(""); // 기기 고유 ID (IMEI)
+						reqJson.setDeviceAccount(""); // 기기 계정 (Gmail)
+						reqJson.setJoinId(""); // 가입채널코드
+						reqJson.setIsRecvSms(""); // SMS 수신 여부
 
 						return reqJson;
 					}
@@ -107,31 +117,48 @@ public class CreateBySimpleTest {
 
 	/**
 	 * <pre>
-	 * ID 회원 간편 가입 (IDP 회원) [[ 단말정보 미포함 ]].
+	 * ID 회원 간편 가입 (IDP 회원) [[ 단말정보 포함 ]].
 	 * </pre>
 	 * 
 	 * @throws Exception
 	 *             Exception
 	 */
-	// @Test
-	public void createBySimpleId() throws Exception {
+	@Ignore
+	@Test
+	public void createBySimpleDevice() throws Exception {
 
 		new TestCaseTemplate(this.mvc).url(MemberTestConstant.PREFIX_USER_PATH_DEV + "/createBySimple/v1").httpMethod(HttpMethod.POST)
 				.addHeaders("x-store-auth-info", "authKey=114127c7ef42667669819dad5df8d820c;ist=N")
 				.addHeaders("Accept", "application/json")
+				.addHeaders("x-planet-device-info", "model=\"SHW-M190S\",fwVersion=\"2.1.3_20101005f\",pkgVersion=\"com.skplanet.tstore.mobile/38\",rootDetection=\"no\"")
 				.requestBody(new RequestBodySetter() {
 					@Override
 					public Object requestBody() {
 
 						CreateBySimpleReq reqJson = new CreateBySimpleReq();
-						reqJson.setDeviceId("");
-						reqJson.setDeviceIdType("");
-						reqJson.setUserId("planetoneuser256000");
-						reqJson.setUserPw("123456");
-						reqJson.setUserEmail("tlaeo00@naver.com");
-						reqJson.setDeviceTelecom("");
-						reqJson.setJoinId("");
-						reqJson.setIsRecvSms("");
+
+						// 사용자 아이디
+						reqJson.setUserId("idpsimplemember"); // 대문자 ID는 가입 불가 IDP 정책.
+						reqJson.setUserPw("abcd1234");
+						reqJson.setUserEmail("idpsimplemember@naver.com");
+
+						// 단말 정보
+						reqJson.setDeviceId("01048088874"); // 기기 ID
+						reqJson.setDeviceIdType("msisdn"); // 기기 ID 타입
+						reqJson.setDeviceTelecom("US012103"); // 통신사
+						reqJson.setNativeId("A0000031648EE9"); // 기기 고유 ID (IMEI)
+						reqJson.setDeviceAccount("sacuser01@yopmail.com"); // 기기 계정 (Gmail)
+						reqJson.setJoinId("US002903"); // 가입채널코드
+						reqJson.setIsRecvSms("Y"); // SMS 수신 여부
+
+						// 단말 부가 정보 리스트
+						List<DeviceExtraInfo> deviceExtraList = new ArrayList<DeviceExtraInfo>();
+						DeviceExtraInfo deviceExtraInfo = new DeviceExtraInfo();
+						deviceExtraInfo.setExtraProfile("US011407");
+						deviceExtraInfo.setExtraProfileValue("3.0");
+
+						deviceExtraList.add(deviceExtraInfo);
+						reqJson.setDeviceExtraInfoList(deviceExtraList);
 
 						return reqJson;
 					}
