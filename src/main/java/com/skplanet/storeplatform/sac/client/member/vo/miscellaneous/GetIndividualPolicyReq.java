2f35b7175908a4d6bb5d762621dd9ecb92e2b804
김경복 <kim.rejoice@gmail.com>
2014-01-22 03:29:29 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@5438 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/member/vo/miscellaneous/GetIndividualPolicyReq.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/miscellaneous/GetIndividualPolicyReq.java
@@ -1,6 +1,8 @@
 package com.skplanet.storeplatform.sac.client.member.vo.miscellaneous;
 
-import org.hibernate.validator.constraints.NotEmpty;
+import java.util.List;
+
+import org.hibernate.validator.constraints.NotBlank;
 
 import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
 
@@ -15,23 +17,12 @@ public class GetIndividualPolicyReq extends CommonInfo {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * 정책 코드.
-	 */
-	@NotEmpty(message = "policyCode should not be empty")
-	private String policyCode;
-	/**
 	 * 확인할 정책의 key값.
 	 */
-	@NotEmpty(message = "key should not be empty")
+	@NotBlank(message = "key should not be empty")
 	private String key;
 
-	public String getPolicyCode() {
-		return this.policyCode;
-	}
-
-	public void setPolicyCode(String policyCode) {
-		this.policyCode = policyCode;
-	}
+	private List<PolicyCode> policyCodeList;
 
 	public String getKey() {
 		return this.key;
@@ -39,5 +30,26 @@ public class GetIndividualPolicyReq extends CommonInfo {
 
 	public void setKey(String key) {
 		this.key = key;
+	}
+
+	public List<PolicyCode> getPolicyCodeList() {
+		return this.policyCodeList;
+	}
+
+	public void setPolicyCodeList(List<PolicyCode> policyCodeList) {
+		this.policyCodeList = policyCodeList;
+	}
+
+	public static class PolicyCode {
+		@NotBlank(message = "key should not be empty")
+		public String policyCode;
+
+		public String getPolicyCode() {
+			return this.policyCode;
+		}
+
+		public void setPolicyCode(String policyCode) {
+			this.policyCode = policyCode;
+		}
 	}
 }
