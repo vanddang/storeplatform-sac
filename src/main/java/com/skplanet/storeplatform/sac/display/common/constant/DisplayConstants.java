50e2dcbd375c87eb8ee3a734156fe9e04b28f6a0
서대영 <dale.seo@sk.com>
2014-01-22 08:41:40 +0000

    refs #8376: 공통 상수 클래스 구현
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@5491 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/display/common/constant/DisplayConstants.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.display.common.constant;
+
+/**
+*
+* SAC 전시 상수 클래스
+* 전시 파트 내에서 사용되는 상수는 여기에 정의한다.
+*
+* Updated on : 2014. 1. 22.
+* Updated by : 서대영, SK 플래닛.
+*/
+public class DisplayConstants {
+
+}
