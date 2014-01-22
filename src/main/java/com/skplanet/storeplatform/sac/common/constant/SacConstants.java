da23312ac94438d71eebb09719e36f8e98277e71
서대영 <dale.seo@sk.com>
2014-01-22 08:13:51 +0000

    refs #8376: 공통 상수 클래스 구현
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@5483 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/common/constant/SacConstants.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.common.constant;
+
+/**
+ *
+ * SAC 공통 상수 클래스
+ * 여러 파트에 걸쳐 사용되는 상수는 여기에 정의한다.
+ *
+ * Updated on : 2014. 1. 22.
+ * Updated by : 서대영, SK 플래닛.
+ */
+public class SacConstants {
+
+	/**
+	 * 요청 헤더 상수 - 인증키
+	 */
+	public static final String HEADER_AUTH_KEY = "x-sac-auth-key";
+
+	/**
+	 * 요청 헤더 상수 - 서명
+	 */
+	public static final String HEADER_AUTH_SIGNATURE = "x-sac-auth-signature";
+
+	/**
+	 * 요청 헤더 상수 - 타임스탬프
+	 */
+	public static final String HEADER_AUTH_TIMESTAMP = "x-sac-auth-timestamp";
+
+	/**
+	 * 요청 헤더 상수 - 난수
+	 */
+	public static final String HEADER_AUTH_NONCE = "x-sac-auth-nonce";
+
+
+
+}
