25173988e45582f13e7aca734db205b3ae3ea2e6
한서구 <seoguman@nate.com>
2014-01-23 01:00:28 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@5600 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/member/vo/seller/DetailSubsellerReq.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/seller/DetailSubsellerReq.java
@@ -4,27 +4,25 @@ import org.codehaus.jackson.map.annotate.JsonSerialize;
 import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
 
 import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
-import com.skplanet.storeplatform.sac.client.member.vo.common.SellerMbr;
 
 /**
  * 서브계정 상세 조회
  * 
- * Updated on : 2014. 1. 7. Updated by : 한서구, 부르칸.
+ * Updated on : 2014. 1. 23. Updated by : 한서구, 부르칸.
  */
 @JsonSerialize(include = Inclusion.NON_NULL)
-public class DetailSubsellerRes extends CommonInfo {
+public class DetailSubsellerReq extends CommonInfo {
 
 	private static final long serialVersionUID = 1L;
 
-	/** 판매자정보 Value Object. */
-	private SellerMbr sellerMbr;
+	private String sellerKey;
 
-	public SellerMbr getSellerMbr() {
-		return this.sellerMbr;
+	public String getSellerKey() {
+		return this.sellerKey;
 	}
 
-	public void setSellerMbr(SellerMbr sellerMbr) {
-		this.sellerMbr = sellerMbr;
+	public void setSellerKey(String sellerKey) {
+		this.sellerKey = sellerKey;
 	}
 
 }
