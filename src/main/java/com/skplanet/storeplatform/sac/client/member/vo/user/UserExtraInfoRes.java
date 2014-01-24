fbcf36967464a5bfa61bc6f2a1c8d85455544b9e
강신완 <@gmail.com>
2014-01-24 08:35:43 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@5996 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/member/vo/user/UserExtraInfoRes.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/user/UserExtraInfoRes.java
@@ -1,9 +1,12 @@
 package com.skplanet.storeplatform.sac.client.member.vo.user;
 
+import java.util.List;
+
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
 
 import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+import com.skplanet.storeplatform.sac.client.member.vo.common.UserExtraInfo;
 
 /**
  * [RESPONSE] 회원 부가 정보 등록/수정
@@ -18,12 +21,23 @@ public class UserExtraInfoRes extends CommonInfo {
 	/* 사용자 고유 Key */
 	private String userKey;
 
+	/* 부가정보 리스트 */
+	private List<UserExtraInfo> addInfoList;
+
 	public String getUserKey() {
 		return this.userKey;
 	}
 
 	public void setUserKey(String userKey) {
 		this.userKey = userKey;
+	}
+
+	public List<UserExtraInfo> getAddInfoList() {
+		return this.addInfoList;
+	}
+
+	public void setAddInfoList(List<UserExtraInfo> addInfoList) {
+		this.addInfoList = addInfoList;
 	}
 
 	public static long getSerialversionuid() {
