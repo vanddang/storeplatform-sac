9019fc186387d477521b4d1eaff982fd9df0246a
반범진 <vanddang@gtsoft.co.kr>
2014-01-24 04:38:24 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@5906 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/test/java/com/skplanet/storeplatform/sac/api/v1/member/user/AuthorizeByMdnTest.java
+++ src/test/java/com/skplanet/storeplatform/sac/api/v1/member/user/AuthorizeByMdnTest.java
@@ -94,7 +94,7 @@ public class AuthorizeByMdnTest {
 						public Object requestBody() {
 
 							AuthorizeByMdnReq req = new AuthorizeByMdnReq();
-							req.setDeviceId("01090556567");
+							req.setDeviceId("01020284280");
 							req.setDeviceIdType("msisdn");
 							req.setDeviceTelecom(MemberConstants.DEVICE_TELECOM_SKT);
 							req.setNativeId("358362045580842");
@@ -153,7 +153,7 @@ public class AuthorizeByMdnTest {
 		header.setTenantHeader(tenantHeader);
 
 		AuthorizeByMdnReq req = new AuthorizeByMdnReq();
-		req.setDeviceId("01090556567");
+		req.setDeviceId("01020284280");
 		req.setDeviceIdType("msisdn");
 		req.setDeviceTelecom(MemberConstants.DEVICE_TELECOM_SKT);
 		req.setNativeId("358362045580842");
@@ -162,10 +162,6 @@ public class AuthorizeByMdnTest {
 
 		List<DeviceExtraInfo> deviceExtraInfoList = new ArrayList<DeviceExtraInfo>();
 		DeviceExtraInfo deviceExtraInfo = new DeviceExtraInfo();
-		deviceExtraInfo.setExtraProfile(MemberConstants.DEVICE_EXTRA_SCVERSION);
-		deviceExtraInfo.setExtraProfileValue("1.0");
-		deviceExtraInfoList.add(deviceExtraInfo);
-
 		deviceExtraInfo = new DeviceExtraInfo();
 		deviceExtraInfo.setExtraProfile(MemberConstants.DEVICE_EXTRA_ROOTING_YN);
 		deviceExtraInfo.setExtraProfileValue("N");
