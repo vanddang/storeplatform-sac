fc78521fbfa5468401f7b95ceab1f53a4e155604
정희원 <joyspring@sk.com>
2014-01-29 04:52:12 +0000

    음악 상세보기 API 리팩토링
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@6640 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/display/vo/music/MusicDetailRes.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/music/MusicDetailRes.java
@@ -1,12 +1,24 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
 package com.skplanet.storeplatform.sac.client.display.vo.music;
 
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
 import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Product;
 
 /**
  * 음악상세 Response
  * Updated on : 2014. 01. 06. Updated by : 정희원, SK 플래닛.
  */
-public class MusicDetailRes {
+public class MusicDetailRes extends CommonInfo {
+
+    private static final long serialVersionUID = 1L;
 
     private Product product;
 
