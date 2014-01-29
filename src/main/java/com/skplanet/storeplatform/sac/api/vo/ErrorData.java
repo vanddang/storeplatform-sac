b7925b5350ad5d0d57d0ed9bee3645402b94b6e3
김형식 <jade.kimhs@gmail.com>
2014-01-29 04:38:22 +0000

    쇼핑 상품 전처리 기능
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@6631 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/api/vo/ErrorData.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+
+package com.skplanet.storeplatform.sac.api.vo;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+/**
+ * <pre>
+ * 전처리 에러데이터 Value Object
+ * </pre>
+ * 
+ * Created on : 2014-01-02 Created by : 김형식, SK플래닛 Last Updated on : 2014-01-02 Last Updated by : 김형식, SK플래닛
+ */
+public class ErrorData extends CommonInfo {
+
+	/**
+	 *
+	 */
+	// private static final long serialVersionUID = 2864668372166277348L;
+
+	// VARCHAR(4) 에러 코드
+	private String errorCode;
+
+	// VARCHAR(300)　 에러 메시지
+
+	private String errorMsg;
+
+	public String getErrorCode() {
+		return this.errorCode;
+	}
+
+	public void setErrorCode(String errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	public String getErrorMsg() {
+		return this.errorMsg;
+	}
+
+	public void setErrorMsg(String errorMsg) {
+		this.errorMsg = errorMsg;
+	}
+
+}
