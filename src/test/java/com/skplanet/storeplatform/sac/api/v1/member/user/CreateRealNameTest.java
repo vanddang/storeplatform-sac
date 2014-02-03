af835d2016fb2ea2319bd741ca45404fbebc97e6
심대진 <tlaeo00@gmail.com>
2014-02-03 07:05:35 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@6797 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/test/java/com/skplanet/storeplatform/sac/api/v1/member/user/CreateRealNameTest.java
+++ src/test/java/com/skplanet/storeplatform/sac/api/v1/member/user/CreateRealNameTest.java
@@ -33,7 +33,7 @@ import com.skplanet.storeplatform.framework.test.SuccessCallback;
 import com.skplanet.storeplatform.framework.test.TestCaseTemplate;
 import com.skplanet.storeplatform.framework.test.TestCaseTemplate.RunMode;
 import com.skplanet.storeplatform.sac.api.v1.member.constant.MemberTestConstant;
-import com.skplanet.storeplatform.sac.client.member.vo.user.ModifyReq;
+import com.skplanet.storeplatform.sac.client.member.vo.user.CreateRealNameReq;
 import com.skplanet.storeplatform.sac.client.member.vo.user.ModifyRes;
 
 /**
@@ -80,23 +80,13 @@ public class CreateRealNameTest {
 					@Override
 					public Object requestBody() {
 
-						ModifyReq reqJson = new ModifyReq();
+						CreateRealNameReq reqJson = new CreateRealNameReq();
 
 						/**
 						 * 기존 IDP 회원
 						 */
 						reqJson.setUserKey("US201401231555153430000447");
 						reqJson.setUserAuthKey("b29ef7ad8e279c67bdf4ce7cba019a0e3e9a6375");
-
-						reqJson.setUserPhone("01011111112"); // 사용자 연락처 (Sync 대상 - IDP)
-						reqJson.setIsRecvSms("N"); // SMS 수신 여부
-						reqJson.setIsRecvEmail("N"); // 이메일 수신여부
-						reqJson.setUserSex("M"); // 성별 (Sync 대상 - IDP)
-						reqJson.setUserBirthDay("19820328"); // 사용자 생년월일 (Sync 대상 - IDP)
-						reqJson.setUserZip("123123"); // 우편번호 (Sync 대상 - IDP|통합)
-						reqJson.setUserAddress("경기도 성남시 분당구"); // 주소 (Sync 대상 - IDP|통합)
-						reqJson.setUserDetailAddress("H스퀘어"); // 상세주소 (Sync 대상 - IDP|통합)
-						reqJson.setUserCalendar("2"); // 생년월일 (1 : 양력, 2 : 음력)
 
 						return reqJson;
 					}
