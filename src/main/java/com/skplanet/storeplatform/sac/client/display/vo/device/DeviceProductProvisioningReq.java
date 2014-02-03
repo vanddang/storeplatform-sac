016497759e8eb40ed9743435fb0a4bbcc8e799f4
오승민 <osm1021@sptek.co.kr>
2014-02-03 09:25:24 +0000

    상품 ID에 따른 Provisioning API VO 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@6845 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/device/DeviceProductProvisioningReq.java
@@ -0,0 +1,71 @@
+package com.skplanet.storeplatform.sac.client.display.vo.device;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+/**
+ * 상품 ID에 대한 단말 Provisioning Request Value Object.
+ * 
+ * Updated on : 2014. 2. 3. Updated by : 오승민, 인크로스.
+ */
+public class DeviceProductProvisioningReq extends CommonInfo {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 상품 유형.
+	 */
+	private String productType;
+	/**
+	 * 상품 Id list.
+	 */
+	private String list;
+	/**
+	 * 단말 모델명.
+	 */
+	private String deviceModelNo;
+
+	/**
+	 * @return the productType
+	 */
+	public String getProductType() {
+		return this.productType;
+	}
+
+	/**
+	 * @param productType
+	 *            the productType to set
+	 */
+	public void setProductType(String productType) {
+		this.productType = productType;
+	}
+
+	/**
+	 * @return the list
+	 */
+	public String getList() {
+		return this.list;
+	}
+
+	/**
+	 * @param list
+	 *            the list to set
+	 */
+	public void setList(String list) {
+		this.list = list;
+	}
+
+	/**
+	 * @return the deviceModelNo
+	 */
+	public String getDeviceModelNo() {
+		return this.deviceModelNo;
+	}
+
+	/**
+	 * @param deviceModelNo
+	 *            the deviceModelNo to set
+	 */
+	public void setDeviceModelNo(String deviceModelNo) {
+		this.deviceModelNo = deviceModelNo;
+	}
+
+}
---
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/device/DeviceProductProvisioningRes.java
@@ -0,0 +1,19 @@
+package com.skplanet.storeplatform.sac.client.display.vo.device;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.CommonResponse;
+
+/**
+ * 상품 ID에 대한 단말 Provisioning Response Value Object.
+ * 
+ * Updated on : 2014. 2. 3. Updated by : 오승민, 인크로스.
+ */
+public class DeviceProductProvisioningRes extends CommonInfo {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Common Response.
+	 */
+	private CommonResponse commonResponse;
+
+}
