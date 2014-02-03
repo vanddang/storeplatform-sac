26852630ba3f994f68e38a3b94a1b3bc43fbb5f4
홍지호 <twomine@gmail.com>
2014-02-03 08:01:21 +0000

    @Transactional 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@6813 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/display/category/service/CategoryWebtoonServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/category/service/CategoryWebtoonServiceImpl.java
@@ -17,6 +17,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.skplanet.storeplatform.framework.core.persistence.dao.CommonDAO;
 import com.skplanet.storeplatform.sac.client.display.vo.category.CategoryWebtoonReq;
@@ -46,6 +47,7 @@ import com.skplanet.storeplatform.sac.display.common.constant.DisplayConstants;
  * Updated on : 2013. 12. 23. Updated by : 김형식, SK 플래닛.
  */
 @Service
+@Transactional
 public class CategoryWebtoonServiceImpl implements CategoryWebtoonService {
 
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
