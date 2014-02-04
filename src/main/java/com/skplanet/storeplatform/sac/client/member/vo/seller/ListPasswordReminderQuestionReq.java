fffd2beefd6efc1cdcbfe9b02a3411708abd78cb
한서구 <seoguman@nate.com>
2014-02-04 01:29:55 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@6948 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/seller/ListPasswordReminderQuestionReq.java
@@ -0,0 +1,29 @@
+package com.skplanet.storeplatform.sac.client.member.vo.seller;
+
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+/**
+ * Password 보안 질문 조회
+ * 
+ * Updated on : 2014. 2. 4. Updated by : 한서구, 부르칸.
+ */
+@JsonSerialize(include = Inclusion.NON_NULL)
+public class ListPasswordReminderQuestionReq extends CommonInfo {
+
+	private static final long serialVersionUID = 1L;
+
+	/** 판매자 키. */
+	private String sellerKey;
+
+	public String getSellerKey() {
+		return this.sellerKey;
+	}
+
+	public void setSellerKey(String sellerKey) {
+		this.sellerKey = sellerKey;
+	}
+
+}
