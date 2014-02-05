aae97cb7d3af89539f4885a669dce8815ec6d2aa
반범진 <vanddang@gtsoft.co.kr>
2014-02-05 06:21:18 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@7384 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/member/user/service/LoginServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/member/user/service/LoginServiceImpl.java
@@ -20,7 +20,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.skplanet.storeplatform.external.client.idp.vo.IDPReceiverM;
 import com.skplanet.storeplatform.external.client.idp.vo.ImIDPReceiverM;
@@ -58,7 +57,6 @@ import com.skplanet.storeplatform.sac.member.common.idp.service.ImIDPService;
  * Updated on : 2014. 1. 6. Updated by : 반범진, 지티소프트.
  */
 @Service
-@Transactional
 public class LoginServiceImpl implements LoginService {
 
 	private static final Logger logger = LoggerFactory.getLogger(LoginServiceImpl.class);
