948500e657af1017c6deee78293b48007f522885
윤주영 <jy1124yun@gmail.com>
2014-02-05 07:30:18 +0000

    테마 추천, 앱 코디 추천 목록 dummy api
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@7435 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/feature/recommend/ThemeRecommendReq.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.display.vo.feature.recommend;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+/**
+ * 테마추천 Request Value Object.
+ * 
+ * Updated on : 2014. 02. 05. Updated by : 윤주영, SK 플래닛.
+ */
+public class ThemeRecommendReq extends CommonInfo {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4984188877183740905L;
+
+	private String memberNo; // 회원번호
+
+	private String msisdn; // 전화번호
+
+	private String filteredBy; // 서비스 구분 (short|long)
+
+	private int offset = 1; // 시작점 ROW
+
+	private int count = 20; // 페이지당 노출 ROW 수
+
+	public String getMemberNo() {
+		return this.memberNo;
+	}
+
+	public void setMemberNo(String memberNo) {
+		this.memberNo = memberNo;
+	}
+
+	public String getMsisdn() {
+		return this.msisdn;
+	}
+
+	public void setMsisdn(String msisdn) {
+		this.msisdn = msisdn;
+	}
+
+	public String getFilteredBy() {
+		return this.filteredBy;
+	}
+
+	public void setFilteredBy(String filteredBy) {
+		this.filteredBy = filteredBy;
+	}
+
+	public int getOffset() {
+		return this.offset;
+	}
+
+	public void setOffset(int offset) {
+		this.offset = offset;
+	}
+
+	public int getCount() {
+		return this.count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+}
---
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/feature/recommend/ThemeRecommendRes.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.display.vo.feature.recommend;
+
+import java.util.List;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.CommonResponse;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Layout;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Product;
+
+/**
+ * 테마추천 Result Value Object.
+ * 
+ * Updated on : 2014. 02. 05. Updated by : 윤주영, SK 플래닛.
+ */
+public class ThemeRecommendRes extends CommonInfo {
+
+	private static final long serialVersionUID = 11123123126L;
+
+	private CommonResponse commonResponse;
+
+	private Layout layout;
+
+	private List<Product> productList;
+
+	public CommonResponse getCommonResponse() {
+		return this.commonResponse;
+	}
+
+	public void setCommonRes(CommonResponse commonResponse) {
+		this.commonResponse = commonResponse;
+	}
+
+	public Layout getLayout() {
+		return this.layout;
+	}
+
+	public void setLayout(Layout layout) {
+		this.layout = layout;
+	}
+
+	public List<Product> getProductList() {
+		return this.productList;
+	}
+
+	public void setProductList(List<Product> productList) {
+		this.productList = productList;
+	}
+
+}
