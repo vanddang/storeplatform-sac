6029e4c2281b4035209cf8127b8e159b2b2cccf3
김형식 <jade.kimhs@gmail.com>
2014-02-06 05:26:39 +0000

    쇼핑 상품 API
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@7601 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/product/vo/intfmessage/product/Product.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/product/vo/intfmessage/product/Product.java
@@ -167,6 +167,12 @@ public class Product extends CommonInfo implements Serializable {
 	 * 상품 선택 옵션 정보.
 	 */
 	private SelectOption selectOption;
+
+	/**
+	 * 상품 선택 옵션 정보 List
+	 */
+	private List<SelectOption> selectOptionList;
+
 	/**
 	 * Restrict 유형 > ota : 네트워크 제한 > sales : 판매 중지.
 	 */
@@ -717,6 +723,21 @@ public class Product extends CommonInfo implements Serializable {
 	 */
 	public void setSelectOption(SelectOption selectOption) {
 		this.selectOption = selectOption;
+	}
+
+	/**
+	 * @return the selectOptionList
+	 */
+	public List<SelectOption> getSelectOptionList() {
+		return this.selectOptionList;
+	}
+
+	/**
+	 * @param selectOptionList
+	 *            the selectOptionList to set
+	 */
+	public void setSelectOptionList(List<SelectOption> selectOptionList) {
+		this.selectOptionList = selectOptionList;
 	}
 
 	/**
