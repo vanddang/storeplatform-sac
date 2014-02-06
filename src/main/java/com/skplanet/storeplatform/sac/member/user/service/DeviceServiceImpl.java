35b22093eba227af364b49a107f21a0372ca2fd1
반범진 <vanddang@gtsoft.co.kr>
2014-02-06 05:53:38 +0000

    DeviceInfo imMngNum변수 삭제 처리
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@7605 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/member/user/service/DeviceServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/member/user/service/DeviceServiceImpl.java
@@ -846,7 +846,7 @@ public class DeviceServiceImpl implements DeviceService {
 			addData.setDeviceKey(StringUtil.setTrim(info.getDeviceKey()));
 			addData.setDeviceId(StringUtil.setTrim(info.getDeviceId()));
 			addData.setDeviceModelNo(StringUtil.setTrim(info.getDeviceModelNo()));
-			addData.setImMngNum(StringUtil.setTrim(info.getImMngNum()));
+			//addData.setImMngNum(StringUtil.setTrim(info.getImMngNum()));
 			addData.setDeviceTelecom(StringUtil.setTrim(info.getDeviceTelecom()));
 			addData.setDeviceNickName(StringUtil.setTrim(info.getDeviceNickName()));
 			addData.setIsPrimary(StringUtil.setTrim(info.getIsPrimary()));
