7a42eb0cc07e303814f34b3c7217892ecc7728bf
이승훈 <naly3512@naver.com>
2014-02-07 01:15:32 +0000

    웹툰 회차별 목록 조회 초기 작업
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@7770 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/display/category/service/CategoryWebtoonSeriesService.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.display.category.service;
+
+import com.skplanet.storeplatform.sac.client.display.vo.category.CategoryWebtoonSeriesSacReq;
+import com.skplanet.storeplatform.sac.client.display.vo.category.CategoryWebtoonSeriesSacRes;
+import com.skplanet.storeplatform.sac.common.header.vo.SacRequestHeader;
+
+/**
+ * 특정 상품 vod 조회 Service
+ * 
+ * Updated on : 2014. 2. 4. Updated by : 이승훈, 엔텔스.
+ */
+public interface CategoryWebtoonSeriesService {
+	/**
+	 * <pre>
+	 * 특정 상품 vod 조회.
+	 * </pre>
+	 * 
+	 * @param req
+	 *            req
+	 * @param header
+	 *            header
+	 * @return CategoryWebtoonSeriesSacRes
+	 */
+	public CategoryWebtoonSeriesSacRes getCategoryWebtoonSeriesList(CategoryWebtoonSeriesSacReq req,
+			SacRequestHeader header);
+}
