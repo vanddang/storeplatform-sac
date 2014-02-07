b7eafbae3eadbc80ddd404ce39ac650f5db48ec2
이현 <teributter@gmail.com>
2014-02-07 00:24:22 +0000

    modify
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@7755 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/test/java/com/skplanet/storeplatform/sac/api/v1/member/seller/ModifyInformationTest.java
+++ src/test/java/com/skplanet/storeplatform/sac/api/v1/member/seller/ModifyInformationTest.java
@@ -7,11 +7,18 @@ import static org.junit.Assert.assertThat;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -31,6 +38,11 @@ import com.skplanet.storeplatform.sac.client.member.vo.seller.ModifyInformationR
  * 
  * Updated on : 2014. 2. 3. Updated by : 김경복, 부르칸
  */
+@ActiveProfiles(value = "local")
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({ "classpath*:/spring-test/context-test.xml" })
 public class ModifyInformationTest {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ModifyInformationTest.class);
@@ -87,29 +99,48 @@ public class ModifyInformationTest {
 						// },
 						// "loginFailCount" : "0",
 						// "isLoginSuccess" : "Y",
-						// "sessionKey" : "SE201402051026319660000583_Y6dOQGJLmr",
-						// "expireDate" : "20140206162408",
-						// "isSubSeller" : "N"
+						// "sessionKey" : "9af2454c073b4c46a73fe41c57f8ff4f",
+						// "expireDate" : "20140207050104",
+						// "isSubSeller" : "N",
+						// "loginStatusCode" : "10"
 						// }
-						req.setSessionKey("SE201402051026319660000583_Y6dOQGJLmr");
+						req.setSessionKey("9af2454c073b4c46a73fe41c57f8ff4f");
 						req.setSellerKey("SE201402051026319660000583");
+						req.setSessionKey("9af2454c073b4c46a73fe41c57f8ff4f");
 						req.setSellerClass("US010101");
 						req.setSellerCategory("US011301");
 						req.setSellerMainStatus("US010201");
 						req.setSellerSubStatus("US010301");
 						req.setSellerId("qatestqwe");
+						req.setSellerTelecom("US001201");
+						req.setSellerEmail("test@testgmail.com");
 						req.setSellerCountry("ko");
 						req.setSellerLanguage("ko");
+						req.setSellerBirthDay("19900325");
 						req.setIsDomestic("Y");
-						// 법정대리인 동의 여부
-						req.setIsParent("N");
-						// req.setIsParent("Y");
-						req.setParentRealNameMethod("US011101"); // 휴대폰 인증
+						req.setIsParent("Y");
+						req.setIsRealName("Y");
 						req.setParentType("abc");
-						// 실명인증여부
-						req.setIsRealName("N");
-						// req.setIsRealName("Y");
-						req.setSellerCI("");
+						req.setSellerCI("XXXXXXXGGXXXXX");
+						req.setSellerDI("GGGGGGXXXXXGGG");
+						req.setRealNameMethod("US011101");
+						req.setSellerPhone("010XXXX2345");
+						req.setSellerSex("M");
+						req.setSellerName("국내개인무료사용자2");
+						req.setRealNameSystemId("S01");
+						req.setParentRealNameMethod("US011101");
+						req.setParentCI("skpone0000132653GWyh3WsEm0FutitO5oSgC2/SgSrL Kv5XohA8mxTNLitpB1");
+						req.setParentBirthDay("19750325");
+						req.setParentRealNameSystemId("S01");
+						req.setParentTelecom("US001202");
+						req.setParentEmail("parent_test@test.com");
+						req.setParentRealNameDate("20140206000000");
+						req.setParentMDN("0102904XXXX");
+						req.setParentName("딘윈체스터");
+						req.setParentDate("20140205230000");
+						req.setSellerCompany("Supernatural");
+						req.setIsRecvSMS("Y");
+						req.setIsRecvEmail("Y");
 
 						LOGGER.debug("[REQUEST (SAC)-회원기본정보 수정] : \n{}", ConvertMapperUtils.convertObjectToJson(req));
 						return req;
