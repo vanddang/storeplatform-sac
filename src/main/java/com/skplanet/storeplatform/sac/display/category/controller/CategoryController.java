53c57e990742c17043580cecc4bfcbed233131ea
이승훈 <naly3512@naver.com>
2014-02-07 01:14:35 +0000

    특정 상품 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@7768 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/display/category/controller/CategoryController.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/category/controller/CategoryController.java
@@ -17,7 +17,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,10 +25,12 @@ import com.skplanet.storeplatform.sac.client.display.vo.category.CategoryAppReq;
 import com.skplanet.storeplatform.sac.client.display.vo.category.CategoryAppRes;
 import com.skplanet.storeplatform.sac.client.display.vo.category.CategoryEbookComicReq;
 import com.skplanet.storeplatform.sac.client.display.vo.category.CategoryEbookComicRes;
-import com.skplanet.storeplatform.sac.client.display.vo.category.CategorySpecificReq;
-import com.skplanet.storeplatform.sac.client.display.vo.category.CategorySpecificRes;
+import com.skplanet.storeplatform.sac.client.display.vo.category.CategorySpecificSacReq;
+import com.skplanet.storeplatform.sac.client.display.vo.category.CategorySpecificSacRes;
 import com.skplanet.storeplatform.sac.client.display.vo.category.CategoryWebtoonReq;
 import com.skplanet.storeplatform.sac.client.display.vo.category.CategoryWebtoonRes;
+import com.skplanet.storeplatform.sac.client.display.vo.category.CategoryWebtoonSeriesSacReq;
+import com.skplanet.storeplatform.sac.client.display.vo.category.CategoryWebtoonSeriesSacRes;
 import com.skplanet.storeplatform.sac.client.display.vo.music.MusicContentsListRes;
 import com.skplanet.storeplatform.sac.client.display.vo.music.MusicContentsReq;
 import com.skplanet.storeplatform.sac.client.display.vo.vod.VodBoxListRes;
@@ -38,8 +39,14 @@ import com.skplanet.storeplatform.sac.common.header.vo.SacRequestHeader;
 import com.skplanet.storeplatform.sac.display.category.service.CategoryAppService;
 import com.skplanet.storeplatform.sac.display.category.service.CategoryEbookComicService;
 import com.skplanet.storeplatform.sac.display.category.service.CategoryMusicContentsService;
+import com.skplanet.storeplatform.sac.display.category.service.CategorySpecificAppService;
+import com.skplanet.storeplatform.sac.display.category.service.CategorySpecificEbookService;
+import com.skplanet.storeplatform.sac.display.category.service.CategorySpecificMusicService;
 import com.skplanet.storeplatform.sac.display.category.service.CategorySpecificProductService;
+import com.skplanet.storeplatform.sac.display.category.service.CategorySpecificVodService;
+import com.skplanet.storeplatform.sac.display.category.service.CategorySpecificWebtoonService;
 import com.skplanet.storeplatform.sac.display.category.service.CategoryVodBoxService;
+import com.skplanet.storeplatform.sac.display.category.service.CategoryWebtoonSeriesService;
 import com.skplanet.storeplatform.sac.display.category.service.CategoryWebtoonService;
 
 /**
@@ -69,6 +76,24 @@ public class CategoryController {
 
 	@Autowired
 	private CategorySpecificProductService categorySpecificService;
+
+	@Autowired
+	private CategorySpecificAppService categorySpecificAppService;
+
+	@Autowired
+	private CategorySpecificEbookService categorySpecificEbookService;
+
+	@Autowired
+	private CategorySpecificVodService categorySpecificVodService;
+
+	@Autowired
+	private CategorySpecificMusicService categorySpecificMusicService;
+
+	@Autowired
+	private CategorySpecificWebtoonService categorySpecificWebtoonService;
+
+	@Autowired
+	private CategoryWebtoonSeriesService categoryWebtoonSeriesService;
 
 	/**
 	 * <pre>
@@ -194,16 +219,149 @@ public class CategoryController {
 	 *            req
 	 * @param header
 	 *            header
-	 * @return CategorySpecificRes
+	 * @return CategorySpecificSacRes
 	 */
 	@RequestMapping(value = "/specific/product/list/v1", method = RequestMethod.GET)
 	@ResponseBody
-	public CategorySpecificRes searchSpecificProductList(@Validated CategorySpecificReq req, SacRequestHeader header) {
+	public CategorySpecificSacRes searchSpecificProductList(CategorySpecificSacReq req, SacRequestHeader header) {
+
 		this.logger.debug("----------------------------------------------------------------");
 		this.logger.debug("searchSpecificProductList Controller started!!");
 		this.logger.debug("----------------------------------------------------------------");
 
 		return this.categorySpecificService.getSpecificProductList(req, header);
+
+	}
+
+	/**
+	 * <pre>
+	 * 특정 상품 APP조회.
+	 * </pre>
+	 * 
+	 * @param req
+	 *            req
+	 * @param header
+	 *            header
+	 * @return CategorySpecificSacRes
+	 */
+	@RequestMapping(value = "/specific/app/list/v1", method = RequestMethod.GET)
+	@ResponseBody
+	public CategorySpecificSacRes searchSpecificAppList(CategorySpecificSacReq req, SacRequestHeader header) {
+		this.logger.debug("----------------------------------------------------------------");
+		this.logger.debug("searchSpecificAppList Controller started!!");
+		this.logger.debug("----------------------------------------------------------------");
+
+		return this.categorySpecificAppService.getSpecificAppList(req, header);
+
+	}
+
+	/**
+	 * <pre>
+	 * 특정 상품 Ebook 조회.
+	 * </pre>
+	 * 
+	 * @param req
+	 *            req
+	 * @param header
+	 *            header
+	 * @return CategorySpecificSacRes
+	 */
+	@RequestMapping(value = "/specific/ebook/list/v1", method = RequestMethod.GET)
+	@ResponseBody
+	public CategorySpecificSacRes searchSpecificEbookList(CategorySpecificSacReq req, SacRequestHeader header) {
+		this.logger.debug("----------------------------------------------------------------");
+		this.logger.debug("searchSpecificEbookList Controller started!!");
+		this.logger.debug("----------------------------------------------------------------");
+
+		return this.categorySpecificEbookService.getSpecificEbookList(req, header);
+
+	}
+
+	/**
+	 * <pre>
+	 * 특정 상품 Vod 조회.
+	 * </pre>
+	 * 
+	 * @param req
+	 *            req
+	 * @param header
+	 *            header
+	 * @return CategorySpecificSacRes
+	 */
+	@RequestMapping(value = "/specific/vod/list/v1", method = RequestMethod.GET)
+	@ResponseBody
+	public CategorySpecificSacRes searchSpecificVodList(CategorySpecificSacReq req, SacRequestHeader header) {
+		this.logger.debug("----------------------------------------------------------------");
+		this.logger.debug("searchSpecificVodList Controller started!!");
+		this.logger.debug("----------------------------------------------------------------");
+
+		return this.categorySpecificVodService.getSpecificVodList(req, header);
+
+	}
+
+	/**
+	 * <pre>
+	 * 특정 상품 music 조회.
+	 * </pre>
+	 * 
+	 * @param req
+	 *            req
+	 * @param header
+	 *            header
+	 * @return CategorySpecificSacRes
+	 */
+	@RequestMapping(value = "/specific/music/list/v1", method = RequestMethod.GET)
+	@ResponseBody
+	public CategorySpecificSacRes searchSpecificMusicList(CategorySpecificSacReq req, SacRequestHeader header) {
+		this.logger.debug("----------------------------------------------------------------");
+		this.logger.debug("searchSpecificMusicList Controller started!!");
+		this.logger.debug("----------------------------------------------------------------");
+
+		return this.categorySpecificMusicService.getSpecificMusicList(req, header);
+
+	}
+
+	/**
+	 * <pre>
+	 * 특정 상품 webtoon 조회.
+	 * </pre>
+	 * 
+	 * @param req
+	 *            req
+	 * @param header
+	 *            header
+	 * @return CategorySpecificSacRes
+	 */
+	@RequestMapping(value = "/specific/webtoon/list/v1", method = RequestMethod.GET)
+	@ResponseBody
+	public CategorySpecificSacRes searchSpecificWebtoonList(CategorySpecificSacReq req, SacRequestHeader header) {
+		this.logger.debug("----------------------------------------------------------------");
+		this.logger.debug("searchSpecificWebtoonList Controller started!!");
+		this.logger.debug("----------------------------------------------------------------");
+
+		return this.categorySpecificWebtoonService.getSpecificWebtoonList(req, header);
+
+	}
+
+	/**
+	 * <pre>
+	 * 웹툰 회차별 목록 조회.
+	 * </pre>
+	 * 
+	 * @param req
+	 *            req
+	 * @param header
+	 *            header
+	 * @return CategorySpecificSacRes
+	 */
+	@RequestMapping(value = "/webtoon/series/list/v1", method = RequestMethod.GET)
+	@ResponseBody
+	public CategoryWebtoonSeriesSacRes searchWebtoonSeriesList(CategoryWebtoonSeriesSacReq req, SacRequestHeader header) {
+		this.logger.debug("----------------------------------------------------------------");
+		this.logger.debug("searchSpecificWebtoonList Controller started!!");
+		this.logger.debug("----------------------------------------------------------------");
+
+		return this.categoryWebtoonSeriesService.getCategoryWebtoonSeriesList(req, header);
 
 	}
 }
