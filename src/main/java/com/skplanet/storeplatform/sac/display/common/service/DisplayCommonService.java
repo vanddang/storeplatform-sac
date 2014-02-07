2937a2d5457e79312a20e457e4946184c5dd3ac5
이석희 <winterer81@naver.com>
2014-02-07 07:20:32 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@7895 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/display/common/service/DisplayCommonService.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/common/service/DisplayCommonService.java
@@ -1,30 +1,45 @@
 package com.skplanet.storeplatform.sac.display.common.service;
 
-import com.skplanet.storeplatform.sac.display.common.vo.MenuItem;
-
 import java.util.List;
+
+import com.skplanet.storeplatform.sac.display.common.vo.MenuItem;
 
 /**
  * 전시 공통 서비스
- *
+ * 
  * Updated on : 2014. 01. 07 Updated by : 정희원, SK 플래닛.
  */
 public interface DisplayCommonService {
 
-    /**
-     * 배치 완료 기준일시를 리턴.
-     * @param tenantId 테넌트ID
-     * @param listId 목록ID(혹은 배치ID)
-     * @return 기준일시를 yyyyMMddHHmmss 형태의 문자열로 리턴. 만족하는 데이터가 없는 경우 null
-     */
-    public String getBatchStandardDateString(String tenantId, String listId);
+	/**
+	 * 배치 완료 기준일시를 리턴.
+	 * 
+	 * @param tenantId
+	 *            테넌트ID
+	 * @param listId
+	 *            목록ID(혹은 배치ID)
+	 * @return 기준일시를 yyyyMMddHHmmss 형태의 문자열로 리턴. 만족하는 데이터가 없는 경우 null
+	 */
+	public String getBatchStandardDateString(String tenantId, String listId);
 
-    /**
-     * 메뉴 목록을 조회한다.
-     * @param prodId 상품ID
-     * @param langCd 언어코드
-     * @return
-     */
-    public List<MenuItem> getMenuItemList(String prodId, String langCd);
+	/**
+	 * 메뉴 목록을 조회한다.
+	 * 
+	 * @param prodId
+	 *            상품ID
+	 * @param langCd
+	 *            언어코드
+	 * @return
+	 */
+	public List<MenuItem> getMenuItemList(String prodId, String langCd);
+
+	/**
+	 * 단말 해상도의 코드값을 조회한다.
+	 * 
+	 * @param resolution
+	 *            해상도 값(480*720)
+	 * @return
+	 */
+	public String getResolutionCode(String resolution);
 
 }
--- src/main/java/com/skplanet/storeplatform/sac/display/common/service/DisplayCommonServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/common/service/DisplayCommonServiceImpl.java
@@ -1,38 +1,55 @@
 package com.skplanet.storeplatform.sac.display.common.service;
 
-import com.skplanet.storeplatform.framework.core.persistence.dao.CommonDAO;
-import com.skplanet.storeplatform.sac.display.common.vo.MenuItem;
-import com.skplanet.storeplatform.sac.display.common.vo.BatchStandardDateRequest;
-import com.skplanet.storeplatform.sac.display.common.vo.MenuItemReq;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import com.skplanet.storeplatform.framework.core.persistence.dao.CommonDAO;
+import com.skplanet.storeplatform.sac.display.common.vo.BatchStandardDateRequest;
+import com.skplanet.storeplatform.sac.display.common.vo.MenuItem;
+import com.skplanet.storeplatform.sac.display.common.vo.MenuItemReq;
 
 /**
  * 전시 공통 서비스
- *
+ * 
  * Updated on : 2014. 01. 07 Updated by : 정희원, SK 플래닛.
  */
 @Service
 @Transactional
 public class DisplayCommonServiceImpl implements DisplayCommonService {
 
-    @Autowired
-    @Qualifier("sac")
-    private CommonDAO commonDAO;
+	@Autowired
+	@Qualifier("sac")
+	private CommonDAO commonDAO;
 
-    @Override
-    public String getBatchStandardDateString(String tenantId, String listId) {
+	@Override
+	public String getBatchStandardDateString(String tenantId, String listId) {
 
-        return (String)commonDAO.queryForObject("Common.getBatchStandardDate", new BatchStandardDateRequest(tenantId, listId));
-    }
+		return (String) this.commonDAO.queryForObject("Common.getBatchStandardDate", new BatchStandardDateRequest(
+				tenantId, listId));
+	}
 
-    @Override
-    public List<MenuItem> getMenuItemList(String prodId, String langCd) {
+	@Override
+	public List<MenuItem> getMenuItemList(String prodId, String langCd) {
 
-        return commonDAO.queryForList("Common.getMenuItemList", new MenuItemReq(prodId, langCd), MenuItem.class);
-    }
+		return this.commonDAO.queryForList("Common.getMenuItemList", new MenuItemReq(prodId, langCd), MenuItem.class);
+	}
+
+	@Override
+	public String getResolutionCode(String resolution) {
+
+		String[] resolutionTemp = resolution.trim().split("\\*");
+
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+
+		paramMap.put("pixel1", resolutionTemp[0]);
+		paramMap.put("pixel2", resolutionTemp[1]);
+
+		return (String) this.commonDAO.queryForObject("Common.getResolutionCode", paramMap);
+	}
 }
