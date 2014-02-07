6ad41b1b0d8c7793a2521e09fda4e0cc2f063bc8
이태희 <zabara337@gmail.com>
2014-02-07 04:52:38 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@7839 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/display/feature/intimateMessage/service/IntimateMessageServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/feature/intimateMessage/service/IntimateMessageServiceImpl.java
@@ -25,9 +25,15 @@ import com.skplanet.storeplatform.framework.core.persistence.dao.CommonDAO;
 import com.skplanet.storeplatform.sac.client.display.vo.feature.intimateMessage.IntimateMessageSacReq;
 import com.skplanet.storeplatform.sac.client.display.vo.feature.intimateMessage.IntimateMessageSacRes;
 import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.CommonResponse;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.Date;
 import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.Identifier;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.Source;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.Title;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.Url;
 import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.IntimateMessage;
 import com.skplanet.storeplatform.sac.common.header.vo.SacRequestHeader;
+import com.skplanet.storeplatform.sac.display.common.DisplayCommonUtil;
+import com.skplanet.storeplatform.sac.display.common.constant.DisplayConstants;
 import com.skplanet.storeplatform.sac.display.feature.intimateMessage.vo.IntimateMessageDefault;
 
 /**
@@ -94,29 +100,92 @@ public class IntimateMessageServiceImpl implements IntimateMessageService {
 
 			IntimateMessage intimateMessage = null;
 			Identifier identifier = null;
+			Title title = null;
+			Title subTitle = null;
+			Url url = null;
+			Date date = null;
+			Source source = null;
 
-			List<IntimateMessage> intimateMessageList = null;
 			List<Identifier> identifierList = null;
+			List<Source> sourceList = null;
+			List<IntimateMessage> intimateMessageList = new ArrayList<IntimateMessage>();
+			;
 
 			for (int i = 0; i < resultList.size(); i++) {
 				messageDefault = resultList.get(i);
-
 				intimateMessage = new IntimateMessage();
-				intimateMessageList = new ArrayList<IntimateMessage>();
 
+				// ID 정보
 				identifier = new Identifier();
 				identifierList = new ArrayList<Identifier>();
-
 				identifier.setType(messageDefault.getMsgTypeCd());
 				identifier.setText(messageDefault.getMsgId());
 				identifierList.add(identifier);
-
 				intimateMessage.setIdentifierList(identifierList);
-				intimateMessageList.add(intimateMessage);
 
-				intimateMessageRes.setIntimateMessageList(intimateMessageList);
+				// 메인 제목
+				title = new Title();
+				title.setText(messageDefault.getMainMsg());
+				title.setColor(messageDefault.getMainColor());
+				intimateMessage.setTitle(title);
+
+				// 하위 제목
+				subTitle = new Title();
+				subTitle.setText(messageDefault.getInfrMsg());
+				subTitle.setColor(messageDefault.getInfrColor());
+				intimateMessage.setSubTitle(subTitle);
+
+				// 오퍼링 타입
+				if ("url".equals(messageDefault.getOfrTypeCd())) {
+					url = new Url();
+					date = new Date();
+
+					date.setType(DisplayConstants.DP_DATE_REG);
+					date.setText(messageDefault.getRegDt());
+					url.setDate(date);
+					url.setText(messageDefault.getOfrDesc());
+					intimateMessage.setUrl(url);
+				} else if ("themeRecomm".equals(messageDefault.getOfrTypeCd())) {
+					intimateMessage.setThemeRecommId(messageDefault.getOfrDesc());
+				} else if ("appCodi".equals(messageDefault.getOfrTypeCd())) {
+					intimateMessage.setAppCodi(messageDefault.getOfrDesc());
+				} else if ("purchaseHistory".equals(messageDefault.getOfrTypeCd())) {
+					intimateMessage.setPurchaseHistory("purchaseHistory");
+				}
+
+				// 배경 이미지
+				if (StringUtils.isNotEmpty(messageDefault.getGnbImgPath())) {
+					source = new Source();
+					sourceList = new ArrayList<Source>();
+					source.setMediaType(DisplayCommonUtil.getMimeType(messageDefault.getGnbImgPath()));
+					source.setType(DisplayConstants.DP_SOURCE_TYPE_GNB_BG);
+					source.setUrl(messageDefault.getGnbImgPath());
+					sourceList.add(source);
+				}
+
+				// 아이콘 이미지
+				if (StringUtils.isNotEmpty(messageDefault.getBiImgPath())) {
+					if (sourceList == null) {
+						sourceList = new ArrayList<Source>();
+					}
+					source = new Source();
+					source.setMediaType(DisplayCommonUtil.getMimeType(messageDefault.getBiImgPath()));
+					source.setType(DisplayConstants.DP_SOURCE_TYPE_GNB_ICON);
+					source.setUrl(messageDefault.getBiImgPath());
+					sourceList.add(source);
+				}
+
+				if (sourceList != null) {
+					intimateMessage.setSourceList(sourceList);
+					sourceList = null;
+				}
+
+				intimateMessageList.add(intimateMessage);
 			}
 
+			commonResponse.setTotalCount(messageDefault.getTotalCount());
+			intimateMessageRes.setCommonResponse(commonResponse);
+			intimateMessageRes.setIntimateMessageList(intimateMessageList);
 		} else {
 			intimateMessageRes.setCommonResponse(commonResponse);
 		}
