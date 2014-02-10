d46e56009e4419afa1223632be32f5b54d7f3519
심대진 <tlaeo00@gmail.com>
2014-02-10 09:26:07 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@8193 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/member/vo/user/DetailByDeviceIdSacRes.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/user/DetailByDeviceIdSacRes.java
@@ -19,39 +19,79 @@ public class DetailByDeviceIdSacRes extends CommonInfo {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * OMPD 여부 (Y or N)
+	 * OMPD 여부 (Y or N).
 	 */
 	private String isOpmd = "";
 
 	/**
-	 * OMPD 모번호
+	 * OMPD 모번호.
 	 */
 	private String msisdn = "";
 
 	/**
-	 * SKT 이용정지회원 여부 (Y or N)
+	 * SKT 이용정지회원 여부 (Y or N).
 	 */
 	private String isSktPause = "";
 
 	/**
-	 * 정책 리스트
+	 * 정책 리스트.
 	 */
 	private List<IndividualPolicyInfo> policyCodeList = null;
 
 	/**
-	 * 사용자 Key
+	 * 사용자 Key.
 	 */
 	private String userKey = "";
 
 	/**
-	 * 기기 Key
+	 * 사용자 구분 코드.
+	 */
+	private String userType = "";
+
+	/**
+	 * 사용자명.
+	 */
+	private String userName = "";
+
+	/**
+	 * 사용자 아이디.
+	 */
+	private String userId = "";
+
+	/**
+	 * 실명인증 여부.
+	 */
+	private String isRealNameYn = "";
+
+	/**
+	 * 사용자 생년월일.
+	 */
+	private String userBirthDay = "";
+
+	/**
+	 * 기기 Key.
 	 */
 	private String deviceKey = "";
 
 	/**
-	 * 이동 통신사
+	 * 기기 아이디.
+	 */
+	private String deviceId = "";
+
+	/**
+	 * 이동 통신사.
 	 */
 	private String deviceTelecom = "";
+
+	/**
+	 * 모델명.
+	 */
+	private String model = "";
+
+	/**
+	 * 선물수신가능 단말 여부.
+	 */
+	private String giftYn = "";
 
 	/**
 	 * @return String : isOpmd
@@ -129,6 +169,81 @@ public class DetailByDeviceIdSacRes extends CommonInfo {
 	}
 
 	/**
+	 * @return String : userType
+	 */
+	public String getUserType() {
+		return this.userType;
+	}
+
+	/**
+	 * @param userType
+	 *            String : the userType to set
+	 */
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	/**
+	 * @return String : userName
+	 */
+	public String getUserName() {
+		return this.userName;
+	}
+
+	/**
+	 * @param userName
+	 *            String : the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return String : userId
+	 */
+	public String getUserId() {
+		return this.userId;
+	}
+
+	/**
+	 * @param userId
+	 *            String : the userId to set
+	 */
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * @return String : isRealNameYn
+	 */
+	public String getIsRealNameYn() {
+		return this.isRealNameYn;
+	}
+
+	/**
+	 * @param isRealNameYn
+	 *            String : the isRealNameYn to set
+	 */
+	public void setIsRealNameYn(String isRealNameYn) {
+		this.isRealNameYn = isRealNameYn;
+	}
+
+	/**
+	 * @return String : userBirthDay
+	 */
+	public String getUserBirthDay() {
+		return this.userBirthDay;
+	}
+
+	/**
+	 * @param userBirthDay
+	 *            String : the userBirthDay to set
+	 */
+	public void setUserBirthDay(String userBirthDay) {
+		this.userBirthDay = userBirthDay;
+	}
+
+	/**
 	 * @return String : deviceKey
 	 */
 	public String getDeviceKey() {
@@ -144,6 +259,21 @@ public class DetailByDeviceIdSacRes extends CommonInfo {
 	}
 
 	/**
+	 * @return String : deviceId
+	 */
+	public String getDeviceId() {
+		return this.deviceId;
+	}
+
+	/**
+	 * @param deviceId
+	 *            String : the deviceId to set
+	 */
+	public void setDeviceId(String deviceId) {
+		this.deviceId = deviceId;
+	}
+
+	/**
 	 * @return String : deviceTelecom
 	 */
 	public String getDeviceTelecom() {
@@ -156,6 +286,36 @@ public class DetailByDeviceIdSacRes extends CommonInfo {
 	 */
 	public void setDeviceTelecom(String deviceTelecom) {
 		this.deviceTelecom = deviceTelecom;
+	}
+
+	/**
+	 * @return String : model
+	 */
+	public String getModel() {
+		return this.model;
+	}
+
+	/**
+	 * @param model
+	 *            String : the model to set
+	 */
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	/**
+	 * @return String : giftYn
+	 */
+	public String getGiftYn() {
+		return this.giftYn;
+	}
+
+	/**
+	 * @param giftYn
+	 *            String : the giftYn to set
+	 */
+	public void setGiftYn(String giftYn) {
+		this.giftYn = giftYn;
 	}
 
 }
