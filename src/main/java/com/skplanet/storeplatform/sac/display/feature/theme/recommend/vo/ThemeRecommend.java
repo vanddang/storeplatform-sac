0b15e568712efc7a615ad4812d987be8e6d4f7c4
윤주영 <jy1124yun@gmail.com>
2014-02-11 09:41:55 +0000

    isf - 테마 추천 API 생성
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@8473 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/display/feature/theme/recommend/vo/ThemeRecommend.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/feature/theme/recommend/vo/ThemeRecommend.java
@@ -2,6 +2,70 @@ package com.skplanet.storeplatform.sac.display.feature.theme.recommend.vo;
 
 public class ThemeRecommend {
 
-	private String totalCount;
+	private int totalCount;
+
+	private String prodId;
+	private String prodNm;
+	private String filePos;
+
+	/**
+	 * @return the totalCount
+	 */
+	public int getTotalCount() {
+		return this.totalCount;
+	}
+
+	/**
+	 * @param totalCount
+	 *            the totalCount to set
+	 */
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+	}
+
+	/**
+	 * @return the prodId
+	 */
+	public String getProdId() {
+		return this.prodId;
+	}
+
+	/**
+	 * @param prodId
+	 *            the prodId to set
+	 */
+	public void setProdId(String prodId) {
+		this.prodId = prodId;
+	}
+
+	/**
+	 * @return the prodNm
+	 */
+	public String getProdNm() {
+		return this.prodNm;
+	}
+
+	/**
+	 * @param prodNm
+	 *            the prodNm to set
+	 */
+	public void setProdNm(String prodNm) {
+		this.prodNm = prodNm;
+	}
+
+	/**
+	 * @return the filePos
+	 */
+	public String getFilePos() {
+		return this.filePos;
+	}
+
+	/**
+	 * @param filePos
+	 *            the filePos to set
+	 */
+	public void setFilePos(String filePos) {
+		this.filePos = filePos;
+	}
 
 }
