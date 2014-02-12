47bb04899ec5bcfdd1fa2d47111421644afa7fc9
양주원 <sden2720@gmail.com>
2014-02-12 09:19:14 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@8724 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/purchase/constant/PurchaseConstants.java
+++ src/main/java/com/skplanet/storeplatform/sac/purchase/constant/PurchaseConstants.java
@@ -60,6 +60,19 @@ public final class PurchaseConstants {
 	/** 구매상품타입_정액제상품. */
 	public static final String PRCHS_PROD_TYPE_FIX = "OR020203"; // 정액제상품
 
+	/** 테넌트 판매 정책. */
+	public static final String POLICY_ID_001 = "policy001"; // 일반고객쇼핑상품SKT후불결제한도금액
+	public static final String POLICY_ID_002 = "policy002"; // 우수고객쇼핑상품SKT후불결제한도금액
+	public static final String POLICY_ID_003 = "policy003"; // SKT후불결제금액제한
+	public static final String POLICY_ID_004 = "policy004"; // 선물수신한도설정
+	public static final String POLICY_ID_005 = "policy005"; // SKT/SKP법인폰제한
+	public static final String POLICY_ID_006 = "policy006"; // 서비스허용시험폰
+	public static final String POLICY_ID_007 = "policy007"; // 비과금단말등록(TestMdn)
+	public static final String POLICY_ID_008 = "policy008"; // Device기반 구매내역관리
+	public static final String POLICY_ID_009 = "policy009"; // 유료상품유료결제구매취소정책
+	public static final String POLICY_ID_010 = "policy010"; // 유료상품무료결제구매취소정책
+	public static final String POLICY_ID_011 = "policy011"; // 무료상품구매취소정책
+
 	/**
 	 * 자동 구매 해지 상태 코드
 	 */
