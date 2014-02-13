3e426af239ca5c9811ffbbd8294a859ab7413e33
심대진 <tlaeo00@gmail.com>
2014-02-13 07:59:47 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@8951 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/member/vo/user/CreateTermsAgreementReq.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/user/CreateTermsAgreementReq.java
@@ -1,6 +1,11 @@
 package com.skplanet.storeplatform.sac.client.member.vo.user;
 
+import java.util.List;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
 import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+import com.skplanet.storeplatform.sac.client.member.vo.common.AgreementInfo;
 
 /**
  * [REQUEST] Store 약관 동의 등록
@@ -10,5 +15,47 @@ import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
 public class CreateTermsAgreementReq extends CommonInfo {
 
 	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 사용자 Key.
+	 */
+	@NotEmpty
+	private String userKey = "";
+
+	/**
+	 * 약관 동의 리스트
+	 */
+	@NotEmpty
+	private List<AgreementInfo> agreementList = null;
+
+	/**
+	 * @return String : userKey
+	 */
+	public String getUserKey() {
+		return this.userKey;
+	}
+
+	/**
+	 * @param userKey
+	 *            String : the userKey to set
+	 */
+	public void setUserKey(String userKey) {
+		this.userKey = userKey;
+	}
+
+	/**
+	 * @return List<AgreementInfo> : agreementList
+	 */
+	public List<AgreementInfo> getAgreementList() {
+		return this.agreementList;
+	}
+
+	/**
+	 * @param agreementList
+	 *            List<AgreementInfo> : the agreementList to set
+	 */
+	public void setAgreementList(List<AgreementInfo> agreementList) {
+		this.agreementList = agreementList;
+	}
 
 }
