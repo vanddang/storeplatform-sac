76010996bae821d423902c658ef24071ac7fc1fd
서대영 <dale.seo@sk.com>
2014-02-13 04:55:20 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@8859 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/member/common/MemberCommonComponent.java
+++ src/main/java/com/skplanet/storeplatform/sac/member/common/MemberCommonComponent.java
@@ -21,7 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import com.skplanet.storeplatform.external.client.icas.sci.ICASSCI;
+import com.skplanet.storeplatform.external.client.icas.sci.IcasSci;
 import com.skplanet.storeplatform.external.client.icas.vo.GetCustomerCardEcReq;
 import com.skplanet.storeplatform.external.client.icas.vo.GetCustomerCardEcRes;
 import com.skplanet.storeplatform.external.client.icas.vo.GetCustomerEcReq;
@@ -80,7 +80,7 @@ public class MemberCommonComponent {
 	private UserSCI userSCI;
 
 	@Autowired
-	private ICASSCI icasSCI;
+	private IcasSci icasSCI;
 
 	@Autowired
 	private DeviceService deviceService;
