11d8949fd6f4ae2780d7ac62d6b3d490d5866a40
이승훈 <naly3512@naver.com>
2014-02-13 05:47:44 +0000

    StringUtils import 경로변경 com.skplanet.storeplatform.framework.core.util.StringUtils
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@8895 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/display/other/service/OtherServiceGroupServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/other/service/OtherServiceGroupServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
+import com.skplanet.storeplatform.framework.core.util.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
