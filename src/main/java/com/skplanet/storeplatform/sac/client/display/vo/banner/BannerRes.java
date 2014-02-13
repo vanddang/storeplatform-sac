85ea8c367f81ce8d71e4fdfa7f78bb3841963b4e
유시혁 <syugi1001@gmail.com>
2014-02-13 05:26:49 +0000

    배너 예외처리
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@8889 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/display/vo/banner/BannerRes.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/banner/BannerRes.java
@@ -2,11 +2,15 @@ package com.skplanet.storeplatform.sac.client.display.vo.banner;
 
 import java.util.List;
 
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
 import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
 import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.CommonResponse;
 import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Banner;
 
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
 public class BannerRes extends CommonInfo {
+	private static final long serialVersionUID = 1L;
 	private CommonResponse commonResponse;
 	private List<Banner> bannerList;
 
