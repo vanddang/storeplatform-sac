526d1773cb543fdef4dc17d38e5cca27da3d4744
김현일 <nahiyak@incross.com>
2014-02-14 05:18:42 +0000

    전시 테이블 사용자 ID, KEY 변경 SCI 테스트 작성
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@9212 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/test/java/com/skplanet/storeplatform/sac/display/localsci/sci/ChangeDisplayUserSCITest.java
@@ -0,0 +1,54 @@
+package com.skplanet.storeplatform.sac.display.localsci.sci;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.skplanet.storeplatform.sac.client.internal.display.localsci.sci.ChangeDisplayUserSCI;
+import com.skplanet.storeplatform.sac.client.internal.display.vo.ChangeDisplayUserSacReq;
+
+/**
+ * 
+ * ChangeDisplayUser SCI Test
+ * 
+ * 전시 테이블 사용자 ID, KEY 변경 테스트 작성
+ * 
+ * Updated on : 2014. 2. 14. Updated by : 김현일, 인크로스
+ */
+@ActiveProfiles(value = "local")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "classpath*:/spring-test/context-test.xml" })
+@WebAppConfiguration
+public class ChangeDisplayUserSCITest {
+
+	@Autowired
+	private ChangeDisplayUserSCI changeDisplayUserSCI;
+
+	/**
+	 * 
+	 * <pre>
+	 * 사용자 ID 변경.
+	 * </pre>
+	 */
+	@Test
+	public void testChangeUserId() {
+		ChangeDisplayUserSacReq changeDisplayUserSacReq = new ChangeDisplayUserSacReq();
+		this.changeDisplayUserSCI.changeUserId(changeDisplayUserSacReq);
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 사용자 KEY 변경.
+	 * </pre>
+	 */
+	@Test
+	public void testChangeUserKey() {
+		ChangeDisplayUserSacReq changeDisplayUserSacReq = new ChangeDisplayUserSacReq();
+		this.changeDisplayUserSCI.changeUserKey(changeDisplayUserSacReq);
+	}
+}
