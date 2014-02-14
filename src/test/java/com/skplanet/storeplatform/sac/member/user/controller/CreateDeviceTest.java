0c0542599cbd14eaab6fc69448ce12ff109edc6f
반범진 <vanddang@gtsoft.co.kr>
2014-02-14 09:57:48 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@9325 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/test/java/com/skplanet/storeplatform/sac/member/user/controller/CreateDeviceTest.java
+++ src/test/java/com/skplanet/storeplatform/sac/member/user/controller/CreateDeviceTest.java
@@ -14,7 +14,6 @@ import java.util.Date;
 import java.util.List;
 
 import org.codehaus.jackson.map.ObjectMapper;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -175,7 +174,7 @@ public class CreateDeviceTest {
 	 * 등록 후 삭제 처리.
 	 * </pre>
 	 */
-	@After
+	@Test
 	public void after() {
 		new TestCaseTemplate(this.mockMvc)
 				.url("/member/user/removeDevice/v1")
