2d56cd91f2c5cb6f8a80cdffd01e33b93ba45863
임근대 <kdlim@sk.com>
2014-02-17 17:52:03 +0000

    refs #6931 Tenant 인증 테스트 케이스 구현
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@9642 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/test/java/com/skplanet/storeplatform/sac/runtime/acl/service/authentication/AuthenticationServiceImplTest.java
+++ src/test/java/com/skplanet/storeplatform/sac/runtime/acl/service/authentication/AuthenticationServiceImplTest.java
@@ -1,6 +1,6 @@
 package com.skplanet.storeplatform.sac.runtime.acl.service.authentication;
 
-import static org.mockito.Mockito.verify;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 import org.junit.Test;
@@ -8,7 +8,10 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.skplanet.storeplatform.framework.core.exception.StorePlatformException;
 import com.skplanet.storeplatform.sac.runtime.acl.service.common.AclDataAccessService;
 import com.skplanet.storeplatform.sac.runtime.acl.vo.AuthKey;
 import com.skplanet.storeplatform.sac.runtime.acl.vo.AuthType;
@@ -17,6 +20,8 @@ import com.skplanet.storeplatform.sac.runtime.acl.vo.TenantStatus;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AuthenticationServiceImplTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(AuthenticationServiceImplTest.class);
 
 	@InjectMocks
 	private AuthenticationServiceImpl authenticationService;
@@ -39,28 +44,113 @@ public class AuthenticationServiceImplTest {
 		headers.setAuthKey(authKey);
 		headers.setRequestUrl("/member/user/createByMdn/v1");
 
-		this.authenticationService.authenticate(headers);
-
-		verify(this.dbAccessMock).selectTenantByAuthKey(authKey);
+		String errorCode = "";
+		try {
+			this.authenticationService.authenticate(headers);
+		} catch(StorePlatformException e) {
+			errorCode = e.getErrorInfo().getCode();
+		}
+		assertEquals("SAC_CMN_0031", errorCode);
 	}
 
 	@Test
-	public void authenticate_Tenant_Avaliable_AuthType_MAC() {
+	public void authenticate_Tenant_Avaliable_AuthType_MAC_성공() {
 		String authKey = "25f9aabf90acf38aa2e6d0da49e9eee75";
+		String secret = "25f9aabf90acf38aa2e6d0da49e9eee75";
+
+		String requestUri = "/member/user/createByMdn/v1";
+
+
 		AuthKey obj = new AuthKey();
 		obj.setTenantId("S01");
 		obj.setTenantStatusCd(TenantStatus.AVAILABLE.getCode());
 		obj.setAuthTypeCd(AuthType.KEY.name());
+		obj.setSecret(secret);
 
 		when(this.dbAccessMock.selectAuthKey(authKey)).thenReturn(obj);
 
 		HttpHeaders headers = new HttpHeaders();
 		headers.setInterfaceId("I01000001");
 		headers.setAuthKey(authKey);
-		headers.setRequestUrl("/member/user/createByMdn/v1");
+		headers.setRequestUrl(requestUri);
+		headers.setTimestamp("1392659265");
+		headers.setNonce("1392659265");
+		headers.setSignature("ispGcmR2Ngi3XKBKjUQLKhU47AM=");
 
 		this.authenticationService.authenticate(headers);
 
-		verify(this.dbAccessMock).selectTenantByAuthKey(authKey);
+		//verify(this.dbAccessMock).selectAuthKey(authKey);
+	}
+
+	@Test
+	public void authenticate_Tenant_Avaliable_AuthType_MAC_실패_Signature값_없음() {
+		String authKey = "25f9aabf90acf38aa2e6d0da49e9eee75";
+		String secret = "25f9aabf90acf38aa2e6d0da49e9eee75";
+
+		String requestUri = "/member/user/createByMdn/v1";
+
+
+		AuthKey obj = new AuthKey();
+		obj.setTenantId("S01");
+		obj.setTenantStatusCd(TenantStatus.AVAILABLE.getCode());
+		obj.setAuthTypeCd(AuthType.KEY.name());
+		obj.setSecret(secret);
+
+		when(this.dbAccessMock.selectAuthKey(authKey)).thenReturn(obj);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setInterfaceId("I01000001");
+		headers.setAuthKey(authKey);
+		headers.setRequestUrl(requestUri);
+		headers.setTimestamp("1392659261");
+		headers.setNonce("1392659261");
+		headers.setSignature("");
+
+
+		String errorCode = "";
+		try {
+			this.authenticationService.authenticate(headers);
+		} catch(StorePlatformException e) {
+			//Signature Exception
+			errorCode = e.getErrorInfo().getCode();
+		}
+		assertEquals("SAC_CMN_0032", errorCode);
+
+		//verify(this.dbAccessMock).selectAuthKey(authKey);
+	}
+	@Test
+	public void authenticate_Tenant_Avaliable_AuthType_MAC_실패1() {
+		String authKey = "25f9aabf90acf38aa2e6d0da49e9eee75";
+		String secret = "25f9aabf90acf38aa2e6d0da49e9eee75";
+
+		String requestUri = "/member/user/createByMdn/v1";
+
+
+		AuthKey obj = new AuthKey();
+		obj.setTenantId("S01");
+		obj.setTenantStatusCd(TenantStatus.AVAILABLE.getCode());
+		obj.setAuthTypeCd(AuthType.KEY.name());
+		obj.setSecret(secret);
+
+		when(this.dbAccessMock.selectAuthKey(authKey)).thenReturn(obj);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setInterfaceId("I01000001");
+		headers.setAuthKey(authKey);
+		headers.setRequestUrl(requestUri);
+		headers.setTimestamp("1392659261");
+		headers.setNonce("1392659261");
+		headers.setSignature("ispGcmR2Ngi3XKBKjUQLKhU47AM=");
+
+
+		String errorCode = "";
+		try {
+			this.authenticationService.authenticate(headers);
+		} catch(StorePlatformException e) {
+			errorCode = e.getErrorInfo().getCode();
+		}
+		assertEquals("SAC_CMN_0032", errorCode);
+
+		//verify(this.dbAccessMock).selectAuthKey(authKey);
 	}
 }
