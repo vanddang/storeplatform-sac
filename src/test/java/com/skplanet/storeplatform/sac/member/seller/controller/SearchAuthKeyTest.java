e58c7ade1c7a530157416670e3816d077df29ef8
한서구 <seoguman@nate.com>
2014-02-17 07:24:15 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@9518 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/test/java/com/skplanet/storeplatform/sac/member/seller/controller/SearchAuthKeyTest.java
+++ src/test/java/com/skplanet/storeplatform/sac/member/seller/controller/SearchAuthKeyTest.java
@@ -1,8 +1,10 @@
 package com.skplanet.storeplatform.sac.member.seller.controller;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,11 +23,15 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.skplanet.storeplatform.framework.test.RequestBodySetter;
 import com.skplanet.storeplatform.framework.test.SuccessCallback;
 import com.skplanet.storeplatform.framework.test.TestCaseTemplate;
 import com.skplanet.storeplatform.framework.test.TestCaseTemplate.RunMode;
+import com.skplanet.storeplatform.sac.client.member.vo.seller.AuthorizeReq;
+import com.skplanet.storeplatform.sac.client.member.vo.seller.AuthorizeRes;
 import com.skplanet.storeplatform.sac.client.member.vo.seller.DetailInformationRes;
 import com.skplanet.storeplatform.sac.member.common.constant.TestMemberConstant;
+import com.skplanet.storeplatform.sac.member.common.util.TestConvertMapperUtils;
 
 @ActiveProfiles(value = "local")
 @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
@@ -41,6 +47,12 @@ public class SearchAuthKeyTest {
 
 	private MockMvc mockMvc;
 
+	/** [REQUEST]. */
+	public static AuthorizeReq authorizeReq;
+
+	/** [RESPONSE]. */
+	public static AuthorizeRes authorizeRes;
+
 	/**
 	 * 
 	 * <pre>
@@ -50,6 +62,41 @@ public class SearchAuthKeyTest {
 	@Before
 	public void before() {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+
+		authorizeReq = new AuthorizeReq();
+	}
+
+	/**
+	 * <pre>
+	 * 판매자 회원 인증키 생성.
+	 * </pre>
+	 */
+	@Test
+	public void searchAuthKey() {
+
+		new TestCaseTemplate(this.mockMvc).url(TestMemberConstant.PREFIX_SELLER_PATH + "/authorize/v1")
+				.addHeaders("x-store-auth-info", "authKey=114127c7ef42667669819dad5df8d820c;ist=N")
+				.httpMethod(HttpMethod.POST).requestBody(new RequestBodySetter() {
+					@Override
+					public Object requestBody() {
+						authorizeReq.setSellerId("biz_7908");
+						authorizeReq.setSellerPW("hzImA3SQ");
+						authorizeReq.setReleaseLock("N");
+						authorizeReq.setExpireDate("3");
+						authorizeReq.setIpAddress("127.0.0.1");
+						LOGGER.debug("[REQUEST(SAC)-회원인증] : \n{}",
+								TestConvertMapperUtils.convertObjectToJson(authorizeReq));
+						return authorizeReq;
+					}
+				}).success(AuthorizeRes.class, new SuccessCallback() {
+					@Override
+					public void success(Object result, HttpStatus httpStatus, RunMode runMode) {
+						authorizeRes = (AuthorizeRes) result;
+						assertThat(authorizeRes.getSellerMbr(), notNullValue());
+						assertEquals("Y", authorizeRes.getIsLoginSuccess());
+					}
+				}, HttpStatus.OK, HttpStatus.ACCEPTED).run(RunMode.JSON);
+
 	}
 
 	/**
@@ -57,13 +104,12 @@ public class SearchAuthKeyTest {
 	 * 판매자 회원 인증키 조회.
 	 * </pre>
 	 */
-	@Test
-	public void searchAuthKey() {
-
+	@After
+	public void after() {
 		new TestCaseTemplate(this.mockMvc)
-				.url(TestMemberConstant.PREFIX_SELLER_PATH
-						+ "/detailInfomationByAuthorizationKey/v1?sessionKey=6188cc12179046148c506d979f38e8f5&extraDate=1")
-				.httpMethod(HttpMethod.GET).success(DetailInformationRes.class, new SuccessCallback() {
+				.url(TestMemberConstant.PREFIX_SELLER_PATH + "/detailInfomationByAuthorizationKey/v1?sessionKey="
+						+ authorizeRes.getSessionKey() + "&extraDate=1").httpMethod(HttpMethod.GET)
+				.success(DetailInformationRes.class, new SuccessCallback() {
 					@Override
 					public void success(Object result, HttpStatus httpStatus, RunMode runMode) {
 						DetailInformationRes res = (DetailInformationRes) result;
@@ -71,7 +117,6 @@ public class SearchAuthKeyTest {
 						LOGGER.debug("response param : {}", res.toString());
 					}
 				}, HttpStatus.OK, HttpStatus.ACCEPTED).run(RunMode.JSON);
-
 	}
 
 }
