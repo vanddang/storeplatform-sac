4f2089531806f9ca12f956bbdea39bd91d7ddf78
한서구 <seoguman@nate.com>
2014-02-17 08:08:50 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@9542 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/test/java/com/skplanet/storeplatform/sac/member/seller/controller/DetailSubsellerTest.java
+++ src/test/java/com/skplanet/storeplatform/sac/member/seller/controller/DetailSubsellerTest.java
@@ -3,6 +3,7 @@ package com.skplanet.storeplatform.sac.member.seller.controller;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,9 +22,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.skplanet.storeplatform.framework.test.RequestBodySetter;
 import com.skplanet.storeplatform.framework.test.SuccessCallback;
 import com.skplanet.storeplatform.framework.test.TestCaseTemplate;
 import com.skplanet.storeplatform.framework.test.TestCaseTemplate.RunMode;
+import com.skplanet.storeplatform.sac.client.member.vo.seller.CreateSubsellerReq;
+import com.skplanet.storeplatform.sac.client.member.vo.seller.CreateSubsellerRes;
 import com.skplanet.storeplatform.sac.client.member.vo.seller.DetailSubsellerRes;
 import com.skplanet.storeplatform.sac.member.common.constant.TestMemberConstant;
 
@@ -41,15 +45,63 @@ public class DetailSubsellerTest {
 
 	private MockMvc mockMvc;
 
+	/** [REQUEST]. */
+	public static CreateSubsellerReq createSubsellerReq;
+
+	/** [RESPONSE]. */
+	public static CreateSubsellerRes createSubsellerRes;
+
 	/**
 	 * 
 	 * <pre>
-	 * method 설명.
+	 * before method 설명.
 	 * </pre>
 	 */
 	@Before
 	public void before() {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+
+		createSubsellerReq = new CreateSubsellerReq();
+	}
+
+	/**
+	 * <pre>
+	 * 판매자 서브계정 등록.
+	 * </pre>
+	 */
+	@Test
+	public void createSubseller() {
+
+		new TestCaseTemplate(this.mockMvc).url(TestMemberConstant.PREFIX_SELLER_PATH + "/createSubseller/v1")
+				.httpMethod(HttpMethod.POST).requestBody(new RequestBodySetter() {
+					@Override
+					public Object requestBody() {
+
+						// 필수
+						createSubsellerReq.setSellerKey("IF1023501184720130823173955");
+						createSubsellerReq.setSubSellerID("011ZXCssss");
+						// createSubsellerReq.setIsNew("Y");
+
+						createSubsellerReq.setSubSellerMemo("test2");
+						createSubsellerReq.setSubSellerEmail("omc97asefd@hanmail.net");
+
+						// 새로 추가됨
+						// createSubsellerReq.setSubSellerKey("SS201402061427346800000640");
+						createSubsellerReq.setMemberPW("1234567999");
+						// createSubsellerReq.setOldPW("1234567999");
+
+						LOGGER.debug("request param : {}", createSubsellerReq.toString());
+						return createSubsellerReq;
+					}
+				}).success(CreateSubsellerRes.class, new SuccessCallback() {
+					@Override
+					public void success(Object result, HttpStatus httpStatus, RunMode runMode) {
+						createSubsellerRes = (CreateSubsellerRes) result;
+						// assertThat(res.getSubSellerKey(), notNullValue());
+						LOGGER.debug("response param : {}", createSubsellerRes.toString());
+					}
+				}, HttpStatus.OK, HttpStatus.ACCEPTED).run(RunMode.JSON);
+
 	}
 
 	/**
@@ -57,12 +109,12 @@ public class DetailSubsellerTest {
 	 * 서브계정 상세 조회.
 	 * </pre>
 	 */
-	@Test
-	public void detailSubseller() {
+	@After
+	public void after() {
 
 		new TestCaseTemplate(this.mockMvc)
-				.url(TestMemberConstant.PREFIX_SELLER_PATH
-						+ "/detailSubseller/v1?sellerKey=IF1023501184720130823173955").httpMethod(HttpMethod.GET)
+				.url(TestMemberConstant.PREFIX_SELLER_PATH + "/detailSubseller/v1?sellerKey="
+						+ createSubsellerRes.getSubSellerKey()).httpMethod(HttpMethod.GET)
 				.success(DetailSubsellerRes.class, new SuccessCallback() {
 					@Override
 					public void success(Object result, HttpStatus httpStatus, RunMode runMode) {
