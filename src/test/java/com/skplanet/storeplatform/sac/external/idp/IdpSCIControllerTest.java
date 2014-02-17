9b347b7c2562e62f027ffb30de58d4f0b01d0764
김경복 <kim.rejoice@gmail.com>
2014-02-17 04:20:07 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@9437 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/test/java/com/skplanet/storeplatform/sac/external/idp/IdpSCIControllerTest.java
@@ -0,0 +1,367 @@
+package com.skplanet.storeplatform.sac.external.idp;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.skplanet.storeplatform.external.client.idp.sci.IdpSCI;
+import com.skplanet.storeplatform.external.client.idp.vo.AlredyJoinCheckByEmailEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.AlredyJoinCheckByEmailEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.AuthForIdEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.AuthForIdEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.AuthForWapEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.AuthForWapEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.CheckDupIdEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.CommonRes;
+import com.skplanet.storeplatform.external.client.idp.vo.DeviceCompareEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.DeviceCompareEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.FindCommonProfileForServerEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.FindCommonProfileForServerEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.FindPasswdEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.FindPasswdEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.FindProfileForWapEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.FindProfileForWapEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.JoinForWapEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.JoinForWapEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.JoinSupServiceRequestEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.JoinSupServiceRequestEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.ModifyAuthInfoEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.ModifyAuthInfoEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.ModifyProfileEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.ModifyProfileEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.SecedeForWapEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.SecedeUserEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.SecedeUserEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.ServiceSubscriptionCheckEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.ServiceSubscriptionCheckEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.SimpleJoinEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.SimpleJoinEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.WaterMarkAuthEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.WaterMarkAuthImageEcRes;
+
+@ActiveProfiles(value = "local")
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({ "classpath*:/spring-test/context-test.xml" })
+public class IdpSCIControllerTest {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(IdpSCIControllerTest.class);
+
+	@Autowired
+	private IdpSCI idpSCI;
+
+	/**
+	 * <pre>
+	 * method 설명.
+	 * </pre>
+	 */
+	@Before
+	public void before() {
+	}
+
+	/**
+	 * <pre>
+	 * method 설명.
+	 * </pre>
+	 */
+	@After
+	public void after() {
+	}
+
+	/**
+	 * <pre>
+	 * 2.1.2. 서비스 중복 가입 체크. (이메일 기준).
+	 * </pre>
+	 */
+	@Test
+	public void testAlredyJoinCheckByEmail() {
+		AlredyJoinCheckByEmailEcReq req = new AlredyJoinCheckByEmailEcReq();
+		req.setKey("test@test.com");
+		AlredyJoinCheckByEmailEcRes res = this.idpSCI.alredyJoinCheckByEmail(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.1.13. 아이디 중복 체크.
+	 * </pre>
+	 */
+	@Test
+	public void testCheckDupID() {
+		CheckDupIdEcReq req = new CheckDupIdEcReq();
+		req.setUserId("test12312");
+		CommonRes res = this.idpSCI.checkDupId(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.1.8. 자동 가입 방지 Image 발급.
+	 * </pre>
+	 */
+	@Test
+	public void testWarterMarkImageUrl() {
+		WaterMarkAuthImageEcRes res = this.idpSCI.warterMarkImageUrl();
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.1.9. 자동 가입 방지 인증.
+	 * [POST]
+	 * </pre>
+	 */
+	@Test
+	public void testWarterMarkAuth() {
+		WaterMarkAuthEcReq req = new WaterMarkAuthEcReq();
+		CommonRes res = this.idpSCI.waterMarkAuth(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.1.10. 휴대폰 단말 기종 조회 및 업데이트.
+	 * </pre>
+	 */
+	@Test
+	public void testDeviceCompare() {
+		DeviceCompareEcReq req = new DeviceCompareEcReq();
+		DeviceCompareEcRes res = this.idpSCI.deviceCompare(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.2.7 간편 회원 가입.
+	 * </pre>
+	 */
+	@Test
+	public void testSimpleJoin() {
+		SimpleJoinEcReq req = new SimpleJoinEcReq();
+		SimpleJoinEcRes res = this.idpSCI.simpleJoin(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.3.1. 유선 회원의 로그인.
+	 * </pre>
+	 */
+	@Test
+	public void testUserAuthForId() {
+		AuthForIdEcReq req = new AuthForIdEcReq();
+		AuthForIdEcRes res = this.idpSCI.authForId(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.3.2. 유선 회원의 비밀번호 확인.
+	 * </pre>
+	 */
+	@Test
+	public void testAuthPwd() {
+		// try {
+		// this.idpSCI.authPwd("abc", "1234");
+		// } catch (StorePlatformException e) {
+		// LOGGER.debug("ErrorInfo : \n{}", e.getErrorInfo().toString());
+		// throw e;
+		// }
+	}
+
+	/**
+	 * <pre>
+	 * 2.4.2. 기본 Profile 조회 (For SP Server).
+	 * </pre>
+	 */
+	@Test
+	public void testSearchUserCommonInfo() {
+		FindCommonProfileForServerEcReq req = new FindCommonProfileForServerEcReq();
+		// 1=id, 2=Email, 3=User Key, default=1
+		FindCommonProfileForServerEcRes res = this.idpSCI.findCommonProfileForServer(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.4.3. 특정 Profile 조회 (For SO Server).
+	 * </pre>
+	 */
+	@Test
+	public void testSearchSpecialProfile() {
+		// try {
+		// // 1=id, 2=Email, 3=User Key, default=1
+		// this.idpSCI.searchSpecialProfile("3", "01088870008");
+		// } catch (StorePlatformException e) {
+		// LOGGER.debug("ErrorInfo : \n{}", e.getErrorInfo().toString());
+		// throw e;
+		// }
+	}
+
+	/**
+	 * <pre>
+	 * 2.4.5. 임시 Password 발급.
+	 * </pre>
+	 */
+	@Test
+	public void testFindPasswd() {
+		FindPasswdEcReq req = new FindPasswdEcReq();
+		FindPasswdEcRes res = this.idpSCI.findPasswd(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.5.1. 인증 정보 변경 API.
+	 * </pre>
+	 */
+	@Test
+	public void testModifyAuthInfo() {
+		ModifyAuthInfoEcReq req = new ModifyAuthInfoEcReq();
+		// 1=id, 2=Email, 3=User Key, default=1
+		ModifyAuthInfoEcRes res = this.idpSCI.modifyAuthInfo(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.5.2. 회원 정보 변경.
+	 * </pre>
+	 */
+	@Test
+	public void testModifyProfile() {
+		ModifyProfileEcReq req = new ModifyProfileEcReq();
+		ModifyProfileEcRes res = this.idpSCI.modifyProfile(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.6.1. 회원 해지.
+	 * </pre>
+	 */
+	@Test
+	public void testSecedeUser() {
+		SecedeUserEcReq req = new SecedeUserEcReq();
+		SecedeUserEcRes res = this.idpSCI.secedeUser(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * 3. MDN 기반서비스 API.
+	 */
+
+	/**
+	 * <pre>
+	 * 3.1.1. MDN/Password 중복 가입 체크.
+	 * </pre>
+	 */
+	@Test
+	public void testAleadyJoinCheckForMdn() {
+		// try {
+		// this.idpSCI.aleadyJoinCheckForMdn("01088870008");
+		// } catch (StorePlatformException e) {
+		// LOGGER.debug("ErrorInfo : \n{}", e.getErrorInfo().toString());
+		// throw e;
+		// }
+	}
+
+	/**
+	 * <pre>
+	 * 3.2.1. 모바일 회원가입.
+	 * </pre>
+	 */
+	@Test
+	public void testJoin4Wap() {
+		JoinForWapEcReq req = new JoinForWapEcReq();
+		JoinForWapEcRes res = this.idpSCI.joinForWap(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 3.3.1. 무선 회원 인증 (For SP Server).
+	 * </pre>
+	 */
+	@Test
+	public void testAuthForWap() {
+		AuthForWapEcReq req = new AuthForWapEcReq();
+		AuthForWapEcRes res = this.idpSCI.authForWap(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 3.4.1 무선 회원 Profile 조회 (For SP Server).
+	 * </pre>
+	 */
+	@Test
+	public void testFindProfileForWap() {
+		FindProfileForWapEcReq req = new FindProfileForWapEcReq();
+		FindProfileForWapEcRes res = this.idpSCI.findProfileForWap(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 3.6.1. 무선 회원 해지 (For SP Server).
+	 * </pre>
+	 */
+	@Test
+	public void testSecedeUser4Wap() {
+		SecedeForWapEcReq req = new SecedeForWapEcReq();
+		CommonRes res = this.idpSCI.secedeForWap(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * 4. 부가서비스 서비스 API.
+	 */
+
+	/**
+	 * <pre>
+	 * 4.1.1. 부가서비스 가입.
+	 * </pre>
+	 */
+	@Test
+	public void testJoinSupService() {
+		JoinSupServiceRequestEcReq req = new JoinSupServiceRequestEcReq();
+		JoinSupServiceRequestEcRes res = this.idpSCI.joinSupServiceRequest(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 4.2.1. 부가서비스 해지.
+	 * </pre>
+	 */
+	@Test
+	public void testSecedeSupService() {
+		// try {
+		// this.idpSCI.secedeSupService("", "", "");
+		// } catch (StorePlatformException e) {
+		// LOGGER.debug("ErrorInfo : \n{}", e.getErrorInfo().toString());
+		// throw e;
+		// }
+	}
+
+	/**
+	 * <pre>
+	 * 4.3.2. 부가서비스 가입 여부 조회.
+	 * </pre>
+	 */
+	@Test
+	public void testServiceSubscriptionCheck() {
+		ServiceSubscriptionCheckEcReq req = new ServiceSubscriptionCheckEcReq();
+		ServiceSubscriptionCheckEcRes res = this.idpSCI.serviceSubscriptionCheck(req);
+		LOGGER.debug(res.toString());
+	}
+}
---
+++ src/test/java/com/skplanet/storeplatform/sac/external/idp/ImIdpSCIControllerTest.java
@@ -0,0 +1,275 @@
+package com.skplanet.storeplatform.sac.external.idp;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.skplanet.storeplatform.external.client.idp.sci.ImIdpSCI;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.AgreeUserEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.AgreeUserEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.AuthForIdEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.AuthForIdEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.CheckIdStatusIdpImEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.CheckIdStatusIdpImEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.DiscardUserEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.DiscardUserEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.FindJoinServiceListIDPEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.FindJoinServiceListIDPEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.FindUserIdByMdnEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.FindUserIdByMdnEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.GetMdnInfoIdpEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.GetMdnInfoIdpEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.ModifyPwdEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.ModifyPwdEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.ResetUserPwdIdpEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.ResetUserPwdIdpEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.SetLoginStatusEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.SetLoginStatusEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.UpdateAdditionalInfoEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.UpdateAdditionalInfoEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.UpdateGuardianEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.UpdateGuardianEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.UpdateUserInfoEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.UpdateUserInfoEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.UpdateUserNameEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.UpdateUserNameEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.UserInfoIdpSearchServerEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.UserInfoIdpSearchServerEcRes;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.UserInfoSearchServerEcReq;
+import com.skplanet.storeplatform.external.client.idp.vo.imidp.UserInfoSearchServerEcRes;
+
+@ActiveProfiles(value = "local")
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({ "classpath*:/spring-test/context-test.xml" })
+public class ImIdpSCIControllerTest {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ImIdpSCIControllerTest.class);
+
+	@Autowired
+	private ImIdpSCI imIdpSCI;
+
+	/**
+	 * <pre>
+	 * method 설명.
+	 * </pre>
+	 */
+	@Before
+	public void before() {
+	}
+
+	/**
+	 * <pre>
+	 * method 설명.
+	 * </pre>
+	 */
+	@After
+	public void after() {
+	}
+
+	/**
+	 * <pre>
+	 * 2.1.2. 서비스 이용 동의.
+	 * </pre>
+	 */
+	@Test
+	public void testAgreeUser() {
+		AgreeUserEcReq req = new AgreeUserEcReq();
+		AgreeUserEcRes res = this.imIdpSCI.agreeUser(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.1.4. 개별약관해지요청.
+	 * </pre>
+	 */
+	@Test
+	public void testDiscardUser() {
+		DiscardUserEcReq req = new DiscardUserEcReq();
+		DiscardUserEcRes res = this.imIdpSCI.discardUser(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.1.6 공통프로파일조회요청 (For Server).
+	 * </pre>
+	 */
+	@Test
+	public void testUserInfoSearchServer() {
+		UserInfoSearchServerEcReq req = new UserInfoSearchServerEcReq();
+		UserInfoSearchServerEcRes res = this.imIdpSCI.userInfoSearchServer(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.1.7 공통프로파일정보수정요청.
+	 * </pre>
+	 */
+	@Test
+	public void testUpdateUserInfo() {
+		UpdateUserInfoEcReq req = new UpdateUserInfoEcReq();
+		UpdateUserInfoEcRes res = this.imIdpSCI.updateUserInfo(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.1.8 부가프로파일정보수정요청.
+	 * </pre>
+	 */
+	@Test
+	public void testUpdateAdditionalInfo() {
+		UpdateAdditionalInfoEcReq req = new UpdateAdditionalInfoEcReq();
+		UpdateAdditionalInfoEcRes res = this.imIdpSCI.updateAdditionalInfo(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.1.9 비밀번호변경요청.
+	 * </pre>
+	 */
+	@Test
+	public void testModifyPwd() {
+		ModifyPwdEcReq req = new ModifyPwdEcReq();
+		ModifyPwdEcRes res = this.imIdpSCI.modifyPwd(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.1.10. 비밀번호초기화및 임시비밀번호발급.
+	 * </pre>
+	 */
+	@Test
+	public void testResetUserPwdIdp() {
+		ResetUserPwdIdpEcReq req = new ResetUserPwdIdpEcReq();
+		ResetUserPwdIdpEcRes res = this.imIdpSCI.resetUserPwdIdp(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.1.20 로그인 상태 정보 변경 요청.
+	 * </pre>
+	 */
+	@Test
+	public void testSetLoginStatus() {
+		SetLoginStatusEcReq req = new SetLoginStatusEcReq();
+		SetLoginStatusEcRes res = this.imIdpSCI.setLoginStatus(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.1.24 실명변경 요청.
+	 * </pre>
+	 */
+	@Test
+	public void testUpdateUserName() {
+		UpdateUserNameEcReq req = new UpdateUserNameEcReq();
+		UpdateUserNameEcRes res = this.imIdpSCI.updateUserName(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.1.25 법정대리인 동의정보 변경 요청.
+	 * </pre>
+	 */
+	@Test
+	public void testUpdateGuardian() {
+		UpdateGuardianEcReq req = new UpdateGuardianEcReq();
+		UpdateGuardianEcRes res = this.imIdpSCI.updateGuardian(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/** 2.1.26 RX 배포 Retry 요청. */
+	/** 2.2.3 MDN 중복체크. */
+	/** 2.2.4 MDN 조회 (SKT, non-SKT 구분). */
+
+	/**
+	 * <pre>
+	 * 2.2.5 통합 ID 회원로그인.
+	 * </pre>
+	 */
+	@Test
+	public void testAuthForId() {
+		AuthForIdEcReq req = new AuthForIdEcReq();
+		AuthForIdEcRes res = this.imIdpSCI.authForId(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.2.6. 통합 ID 서비스 가입리스트 조회.
+	 * </pre>
+	 */
+	@Test
+	public void testFindJoinServiceListIDP() {
+		FindJoinServiceListIDPEcReq req = new FindJoinServiceListIDPEcReq();
+		FindJoinServiceListIDPEcRes res = this.imIdpSCI.findJoinServiceListIDP(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.2.9 ID 가입여부 체크.
+	 * </pre>
+	 */
+	@Test
+	public void testCheckIdStatusIdpIm() {
+		CheckIdStatusIdpImEcReq req = new CheckIdStatusIdpImEcReq();
+		CheckIdStatusIdpImEcRes res = this.imIdpSCI.checkIdStatusIdpIm(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.2.11 MDN 정보 조회 (SKT 가입자).
+	 * </pre>
+	 */
+	@Test
+	public void testGetMdnInfoIDP() {
+		GetMdnInfoIdpEcReq req = new GetMdnInfoIdpEcReq();
+		GetMdnInfoIdpEcRes res = this.imIdpSCI.getMdnInfoIDP(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/**
+	 * <pre>
+	 * 2.2.12 IM 통합회원 ID 찾기.
+	 * </pre>
+	 */
+	@Test
+	public void testFindUserIdByMdn() {
+		FindUserIdByMdnEcReq req = new FindUserIdByMdnEcReq();
+		FindUserIdByMdnEcRes res = this.imIdpSCI.findUserIdByMdn(req);
+		LOGGER.debug(res.toString());
+	}
+
+	/** 2.2.13 이용동의 가능여부 조회. */
+
+	/**
+	 * <pre>
+	 * 2.2.15. 기본 프로파일 조회(For Server).
+	 * </pre>
+	 */
+	@Test
+	public void testUserInfoIdpSearchServer() {
+		UserInfoIdpSearchServerEcReq req = new UserInfoIdpSearchServerEcReq();
+		UserInfoIdpSearchServerEcRes res = this.imIdpSCI.userInfoIdpSearchServer(req);
+		LOGGER.debug(res.toString());
+	}
+}
