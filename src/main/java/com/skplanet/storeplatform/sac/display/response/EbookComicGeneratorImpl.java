304f5a742bb6e115c400c4a9b516cae4138feb9b
홍지호 <twomine@gmail.com>
2014-02-18 00:55:13 +0000

    이북엔 그림작가 없고, 코믹에는 번역자 없음
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@9659 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/display/response/EbookComicGeneratorImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/response/EbookComicGeneratorImpl.java
@@ -36,8 +36,7 @@ public class EbookComicGeneratorImpl implements EbookComicGenerator {
 	@Override
 	public Contributor generateEbookContributor(MetaInfo metaInfo) {
 		Contributor contributor = new Contributor();
-		contributor.setName(metaInfo.getArtist1Nm()); // 글작가
-		contributor.setPainter(metaInfo.getArtist2Nm()); // 그림작가
+		contributor.setName(metaInfo.getArtist1Nm()); // 작가
 		contributor.setTranslator(metaInfo.getArtist3Nm()); // 번역자
 		contributor.setPublisher(metaInfo.getChnlCompNm()); // 출판사
 
@@ -53,7 +52,6 @@ public class EbookComicGeneratorImpl implements EbookComicGenerator {
 		Contributor contributor = new Contributor();
 		contributor.setName(metaInfo.getArtist1Nm()); // 글작가
 		contributor.setPainter(metaInfo.getArtist2Nm()); // 그림작가
-		contributor.setTranslator(metaInfo.getArtist2Nm()); // 그림작가
 		contributor.setPublisher(metaInfo.getChnlCompNm()); // 출판사
 
 		if (StringUtils.isNotEmpty(metaInfo.getIssueDay())) {
