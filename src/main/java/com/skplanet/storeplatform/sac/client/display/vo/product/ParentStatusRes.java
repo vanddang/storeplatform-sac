4d18564151c284f139d9558c4297927ffa06a5c0
정희원 <joyspring@sk.com>
2014-02-18 04:40:56 +0000

    ref #8970. 모상품 판매상태 조회 API 개발
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@9736 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/product/ParentStatusRes.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.display.vo.product;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+/**
+ * <p>
+ * ParentStatusRes
+ * </p>
+ * Updated on : 2014. 02. 18 Updated by : 정희원, SK 플래닛.
+ */
+public class ParentStatusRes extends CommonInfo {
+
+    private static final long serialVersionUID = 1L;
+
+    private String parentProdId;
+
+    private String parentStatus;
+
+    public String getParentProdId() {
+        return parentProdId;
+    }
+
+    public void setParentProdId(String parentProdId) {
+        this.parentProdId = parentProdId;
+    }
+
+    public String getParentStatus() {
+        return parentStatus;
+    }
+
+    public void setParentStatus(String parentStatus) {
+        this.parentStatus = parentStatus;
+    }
+}
