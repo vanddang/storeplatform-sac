cb35b0f9fb350c6ac348f4f45dc4d2fec1715078
이승훈 <naly3512@naver.com>
2014-02-19 05:46:38 +0000

    ThemeEpub vo 변경
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@9996 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/display/feature/theme/vo/ThemeEpubInfo.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.display.feature.theme.vo;
+
+/**
+ * 
+ * 
+ * Updated on : 2014. 2. 17. Updated by : 이승훈, 엔텔스.
+ */
+public class ThemeEpubInfo {
+	private int totalCount;
+	private String bnrMenuId;
+	private String bnrSeq;
+	private String bnrNm;
+	private String bnrDesc;
+	private String bnrInfo;
+	private String prodId;
+	private String imgPath;
+	private String regDt;
+	private String menuId;
+	private String bnrTitle;
+
+	public int getTotalCount() {
+		return this.totalCount;
+	}
+
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+	}
+
+	public String getBnrMenuId() {
+		return this.bnrMenuId;
+	}
+
+	public void setBnrMenuId(String bnrMenuId) {
+		this.bnrMenuId = bnrMenuId;
+	}
+
+	public String getBnrSeq() {
+		return this.bnrSeq;
+	}
+
+	public void setBnrSeq(String bnrSeq) {
+		this.bnrSeq = bnrSeq;
+	}
+
+	public String getBnrNm() {
+		return this.bnrNm;
+	}
+
+	public void setBnrNm(String bnrNm) {
+		this.bnrNm = bnrNm;
+	}
+
+	public String getBnrDesc() {
+		return this.bnrDesc;
+	}
+
+	public void setBnrDesc(String bnrDesc) {
+		this.bnrDesc = bnrDesc;
+	}
+
+	public String getBnrInfo() {
+		return this.bnrInfo;
+	}
+
+	public void setBnrInfo(String bnrInfo) {
+		this.bnrInfo = bnrInfo;
+	}
+
+	public String getProdId() {
+		return this.prodId;
+	}
+
+	public void setProdId(String prodId) {
+		this.prodId = prodId;
+	}
+
+	public String getImgPath() {
+		return this.imgPath;
+	}
+
+	public void setImgPath(String imgPath) {
+		this.imgPath = imgPath;
+	}
+
+	public String getRegDt() {
+		return this.regDt;
+	}
+
+	public void setRegDt(String regDt) {
+		this.regDt = regDt;
+	}
+
+	public String getMenuId() {
+		return this.menuId;
+	}
+
+	public void setMenuId(String menuId) {
+		this.menuId = menuId;
+	}
+
+	public String getBnrTitle() {
+		return this.bnrTitle;
+	}
+
+	public void setBnrTitle(String bnrTitle) {
+		this.bnrTitle = bnrTitle;
+	}
+
+}
