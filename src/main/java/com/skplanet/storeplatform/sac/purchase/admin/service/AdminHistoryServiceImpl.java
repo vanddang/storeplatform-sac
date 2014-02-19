86e6e4b147051672a7098c1fd0d9ae009712df1e
양주원 <sden2720@gmail.com>
2014-02-19 10:11:02 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@10110 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/purchase/admin/service/AdminHistoryServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/purchase/admin/service/AdminHistoryServiceImpl.java
@@ -9,19 +9,12 @@
  */
 package com.skplanet.storeplatform.sac.purchase.admin.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.skplanet.storeplatform.purchase.client.admin.sci.AdminHistorySCI;
-import com.skplanet.storeplatform.purchase.client.admin.vo.AdminHistorySc;
-import com.skplanet.storeplatform.purchase.client.admin.vo.AdminHistoryScReq;
-import com.skplanet.storeplatform.purchase.client.admin.vo.AdminHistoryScRes;
-import com.skplanet.storeplatform.sac.client.purchase.admin.vo.AdminHistorySac;
 import com.skplanet.storeplatform.sac.client.purchase.admin.vo.AdminHistorySacReq;
 import com.skplanet.storeplatform.sac.client.purchase.admin.vo.AdminHistorySacRes;
 
@@ -49,100 +42,100 @@ public class AdminHistoryServiceImpl implements AdminHistoryService {
 	public AdminHistorySacRes searchAdminHistoryList(AdminHistorySacReq request) {
 		this.LOGGER.debug("HistoryListSacRes : {}", request);
 
-		// SC request/response VO
-		AdminHistoryScReq scRequest = new AdminHistoryScReq();
-		AdminHistoryScRes scResponse = new AdminHistoryScRes();
-
-		// SAC Response VO
-		AdminHistorySacRes response = new AdminHistorySacRes();
-		List<AdminHistorySac> sacHistoryList = new ArrayList<AdminHistorySac>();
-		AdminHistorySac historySac = new AdminHistorySac();
-
-		/*************************************************
-		 * SC Request Setting Start
-		 *************************************************/
-		scRequest.setTenantId(request.getTenantId());
-		scRequest.setStartDt(request.getStartDt());
-		scRequest.setEndDt(request.getEndDt());
-		scRequest.setProdId(request.getProdId());
-		scRequest.setTenantProdGrpCd(request.getTenantProdGrpCd());
-		// scRequest.setOffset(request.getOffset());
-		// scRequest.setCount(request.getCount());
-		// pageInfo set
-		scRequest.getPage().setNo(request.getOffset());
-		scRequest.getPage().setRows(request.getCount());
-
-		/*************************************************
-		 * SC Request Setting End
-		 *************************************************/
-
-		// try {
-		// SC Call
-		scResponse = this.adminHistorySCI.searchAdminHistoryList(scRequest);
-		// } catch (Exception ex) {
-		// TODO : 추후 메세지 추가후 처리함
-		// throw new StorePlatformException("구매SC 호출중 오류발생", ex);
+		// // SC request/response VO
+		// AdminHistoryScReq scRequest = new AdminHistoryScReq();
+		// AdminHistoryScRes scResponse = new AdminHistoryScRes();
+		//
+		// // SAC Response VO
+		// AdminHistorySacRes response = new AdminHistorySacRes();
+		// List<AdminHistorySac> sacHistoryList = new ArrayList<AdminHistorySac>();
+		// AdminHistorySac historySac = new AdminHistorySac();
+		//
+		// /*************************************************
+		// * SC Request Setting Start
+		// *************************************************/
+		// scRequest.setTenantId(request.getTenantId());
+		// scRequest.setStartDt(request.getStartDt());
+		// scRequest.setEndDt(request.getEndDt());
+		// scRequest.setProdId(request.getProdId());
+		// scRequest.setTenantProdGrpCd(request.getTenantProdGrpCd());
+		// // scRequest.setOffset(request.getOffset());
+		// // scRequest.setCount(request.getCount());
+		// // pageInfo set
+		// scRequest.getPage().setNo(request.getOffset());
+		// scRequest.getPage().setRows(request.getCount());
+		//
+		// /*************************************************
+		// * SC Request Setting End
+		// *************************************************/
+		//
+		// // try {
+		// // SC Call
+		// scResponse = this.adminHistorySCI.searchAdminHistoryList(scRequest);
+		// // } catch (Exception ex) {
+		// // TODO : 추후 메세지 추가후 처리함
+		// // throw new StorePlatformException("구매SC 호출중 오류발생", ex);
+		// // }
+		//
+		// // SC객체를 SAC객체로 맵핑작업
+		//
+		// /*************************************************
+		// * SC -> SAC Response Setting Start
+		// *************************************************/
+		// for (AdminHistorySc obj : scResponse.getHistoryList()) {
+		//
+		// historySac = new AdminHistorySac();
+		//
+		// // 구매정보 set
+		// historySac.setTenantId(obj.getTenantId());
+		// // historySac.setSystemId(obj.getSystemId());
+		// historySac.setPrchsId(obj.getPrchsId());
+		// historySac.setPrchsDtlId(obj.getPrchsDtlId());
+		// historySac.setUseTenantId(obj.getUseTenantId());
+		// historySac.setUseUserKey(obj.getUseUserKey());
+		// historySac.setUseDeviceKey(obj.getUseDeviceKey());
+		// historySac.setPrchsDt(obj.getPrchsDt());
+		// historySac.setTotAmt(obj.getTotAmt());
+		// historySac.setSendUserKey(obj.getSendUserKey());
+		// historySac.setSendDeviceKey(obj.getSendDeviceKey());
+		// historySac.setRecvDt(obj.getRecvDt());
+		// historySac.setRecvConfPathCd(obj.getRecvConfPathCd());
+		// historySac.setTenantProdGrpCd(obj.getTenantProdGrpCd());
+		// historySac.setProdId(obj.getProdId());
+		// historySac.setProdAmt(obj.getProdAmt());
+		// historySac.setProdQty(obj.getProdQty());
+		// historySac.setStatusCd(obj.getStatusCd());
+		// historySac.setUseStartDt(obj.getUseStartDt());
+		// historySac.setUseExprDt(obj.getUseExprDt());
+		// historySac.setPrchsReqPathCd(obj.getPrchsReqPathCd());
+		// historySac.setHidingYn(obj.getHidingYn());
+		// historySac.setCancelReqPathCd(obj.getCancelReqPathCd());
+		// historySac.setCancelDt(obj.getCancelDt());
+		// historySac.setPrchsCaseCd(obj.getPrchsCaseCd());
+		// historySac.setRePrchsPmtYn(obj.getRePrchsPmtYn());
+		// historySac.setDwldStartDt(obj.getDwldStartDt());
+		// historySac.setDwldExprDt(obj.getDwldExprDt());
+		// historySac.setCpnPublishCd(obj.getCpnPublishCd());
+		// historySac.setCpnDlvUrl(obj.getCpnDlvUrl());
+		// historySac.setEtcSeq(obj.getEtcSeq());
+		// historySac.setUseFixrateProdId(obj.getUseFixrateProdId());
+		// historySac.setPrchsProdType(obj.getPrchsProdType());
+		// historySac.setResvCol01(obj.getResvCol01());
+		// historySac.setResvCol02(obj.getResvCol02());
+		// historySac.setResvCol03(obj.getResvCol03());
+		// historySac.setResvCol04(obj.getResvCol04());
+		// historySac.setResvCol05(obj.getResvCol05());
+		//
+		// sacHistoryList.add(historySac);
 		// }
-
-		// SC객체를 SAC객체로 맵핑작업
-
-		/*************************************************
-		 * SC -> SAC Response Setting Start
-		 *************************************************/
-		for (AdminHistorySc obj : scResponse.getHistoryList()) {
-
-			historySac = new AdminHistorySac();
-
-			// 구매정보 set
-			historySac.setTenantId(obj.getTenantId());
-			// historySac.setSystemId(obj.getSystemId());
-			historySac.setPrchsId(obj.getPrchsId());
-			historySac.setPrchsDtlId(obj.getPrchsDtlId());
-			historySac.setUseTenantId(obj.getUseTenantId());
-			historySac.setUseUserKey(obj.getUseUserKey());
-			historySac.setUseDeviceKey(obj.getUseDeviceKey());
-			historySac.setPrchsDt(obj.getPrchsDt());
-			historySac.setTotAmt(obj.getTotAmt());
-			historySac.setSendUserKey(obj.getSendUserKey());
-			historySac.setSendDeviceKey(obj.getSendDeviceKey());
-			historySac.setRecvDt(obj.getRecvDt());
-			historySac.setRecvConfPathCd(obj.getRecvConfPathCd());
-			historySac.setTenantProdGrpCd(obj.getTenantProdGrpCd());
-			historySac.setProdId(obj.getProdId());
-			historySac.setProdAmt(obj.getProdAmt());
-			historySac.setProdQty(obj.getProdQty());
-			historySac.setStatusCd(obj.getStatusCd());
-			historySac.setUseStartDt(obj.getUseStartDt());
-			historySac.setUseExprDt(obj.getUseExprDt());
-			historySac.setPrchsReqPathCd(obj.getPrchsReqPathCd());
-			historySac.setHidingYn(obj.getHidingYn());
-			historySac.setCancelReqPathCd(obj.getCancelReqPathCd());
-			historySac.setCancelDt(obj.getCancelDt());
-			historySac.setPrchsCaseCd(obj.getPrchsCaseCd());
-			historySac.setRePrchsPmtYn(obj.getRePrchsPmtYn());
-			historySac.setDwldStartDt(obj.getDwldStartDt());
-			historySac.setDwldExprDt(obj.getDwldExprDt());
-			historySac.setCpnPublishCd(obj.getCpnPublishCd());
-			historySac.setCpnDlvUrl(obj.getCpnDlvUrl());
-			historySac.setEtcSeq(obj.getEtcSeq());
-			historySac.setUseFixrateProdId(obj.getUseFixrateProdId());
-			historySac.setPrchsProdType(obj.getPrchsProdType());
-			historySac.setResvCol01(obj.getResvCol01());
-			historySac.setResvCol02(obj.getResvCol02());
-			historySac.setResvCol03(obj.getResvCol03());
-			historySac.setResvCol04(obj.getResvCol04());
-			historySac.setResvCol05(obj.getResvCol05());
-
-			sacHistoryList.add(historySac);
-		}
-
-		/*************************************************
-		 * SC -> SAC Response Setting End
-		 *************************************************/
-
-		response.setHistoryList(sacHistoryList);
-		response.setTotalCnt(scResponse.getTotalCnt());
-
+		//
+		// /*************************************************
+		// * SC -> SAC Response Setting End
+		// *************************************************/
+		//
+		// response.setHistoryList(sacHistoryList);
+		// response.setTotalCnt(scResponse.getTotalCnt());
+		AdminHistorySacRes response = new AdminHistorySacRes();
 		return response;
 	}
 
