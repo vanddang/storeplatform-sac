12a8416f4d5fef0d131966fc8608880ada393453
심대진 <tlaeo00@gmail.com>
2014-02-20 01:10:41 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@10142 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/test/java/com/skplanet/storeplatform/sac/member/common/constant/TestMemberConstant.java
+++ src/test/java/com/skplanet/storeplatform/sac/member/common/constant/TestMemberConstant.java
@@ -6,14 +6,19 @@ public final class TestMemberConstant {
 	public static final String PREFIX_SELLER_PATH = "/member/seller";
 
 	/**
-	 * 개발 미완료
+	 * 개발 미완료.
 	 */
 	public static final String PREFIX_USER_PATH_DEV = "/dev/member/user";
 
 	/**
-	 * 개발 완료
+	 * 개발 완료.
 	 */
 	public static final String PREFIX_USER_PATH_REAL = "/member/user";
+
+	/**
+	 * 개발서버로 테스트.
+	 */
+	// public static final String PREFIX_USER_PATH_REAL = "http://211.188.207.142/sp_sac/member/user";
 
 	public static final String USE_Y = "Y";
 	public static final String USE_N = "N";
