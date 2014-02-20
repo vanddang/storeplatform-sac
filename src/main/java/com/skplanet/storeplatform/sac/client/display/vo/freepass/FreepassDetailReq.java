fff9be5ef3759cec58f8f5bf6ecfecc1943dc268
서영배 <ra2coon@gmail.com>
2014-02-20 03:44:45 +0000

    정액제 상세 Req 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@10175 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/freepass/FreepassDetailReq.java
@@ -0,0 +1,278 @@
+package com.skplanet.storeplatform.sac.client.display.vo.freepass;
+
+public class FreepassDetailReq {
+
+	private static final long serialVersionUID = 1L;
+
+	private String topMenuId; // 최상위메뉴ID
+	private String kind; // 자유이용권 종류
+	private String productId; // 상품ID
+	private String menuId; // 메뉴ID
+	private String channelId; // 채널상품ID
+	private int offset; // offset
+	private int count; // count
+	private String bannerImageCd; // 배너이미지
+	private String thumbnailImageCd; // 썸네일이미지
+	private String prodStatusCd; // 상품상태
+	private String standardModelCd; // 상품상태
+	
+	
+	// common req 전까지 임시
+	private String tenantId; // 테넌트ID
+	private String langCd; // 언어
+	private String deviceModelCd; // 단말모델
+	
+	//구매내역 체크
+	private String userKey; // 사용자키
+	private String deviceKey; // 단말키
+
+	// Dummy Data용
+	private String dummy; // 더미체크
+
+	/**
+	 * @return the topMenuId
+	 */
+	public String getTopMenuId() {
+		return topMenuId;
+	}
+
+	/**
+	 * @param topMenuId the topMenuId to set
+	 */
+	public void setTopMenuId(String topMenuId) {
+		this.topMenuId = topMenuId;
+	}
+
+	/**
+	 * @return the kind
+	 */
+	public String getKind() {
+		return kind;
+	}
+
+	/**
+	 * @param kind the kind to set
+	 */
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
+
+	/**
+	 * @return the productId
+	 */
+	public String getProductId() {
+		return productId;
+	}
+
+	/**
+	 * @param productId the productId to set
+	 */
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+
+	/**
+	 * @return the menuId
+	 */
+	public String getMenuId() {
+		return menuId;
+	}
+
+	/**
+	 * @param menuId the menuId to set
+	 */
+	public void setMenuId(String menuId) {
+		this.menuId = menuId;
+	}
+
+	/**
+	 * @return the channelId
+	 */
+	public String getChannelId() {
+		return channelId;
+	}
+
+	/**
+	 * @param channelId the channelId to set
+	 */
+	public void setChannelId(String channelId) {
+		this.channelId = channelId;
+	}
+
+	/**
+	 * @return the offset
+	 */
+	public int getOffset() {
+		return offset;
+	}
+
+	/**
+	 * @param offset the offset to set
+	 */
+	public void setOffset(int offset) {
+		this.offset = offset;
+	}
+
+	/**
+	 * @return the count
+	 */
+	public int getCount() {
+		return count;
+	}
+
+	/**
+	 * @param count the count to set
+	 */
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	/**
+	 * @return the bannerImageCd
+	 */
+	public String getBannerImageCd() {
+		return bannerImageCd;
+	}
+
+	/**
+	 * @param bannerImageCd the bannerImageCd to set
+	 */
+	public void setBannerImageCd(String bannerImageCd) {
+		this.bannerImageCd = bannerImageCd;
+	}
+
+	/**
+	 * @return the thumbnailImageCd
+	 */
+	public String getThumbnailImageCd() {
+		return thumbnailImageCd;
+	}
+
+	/**
+	 * @param thumbnailImageCd the thumbnailImageCd to set
+	 */
+	public void setThumbnailImageCd(String thumbnailImageCd) {
+		this.thumbnailImageCd = thumbnailImageCd;
+	}
+
+	/**
+	 * @return the prodStatusCd
+	 */
+	public String getProdStatusCd() {
+		return prodStatusCd;
+	}
+
+	/**
+	 * @param prodStatusCd the prodStatusCd to set
+	 */
+	public void setProdStatusCd(String prodStatusCd) {
+		this.prodStatusCd = prodStatusCd;
+	}
+
+	/**
+	 * @return the standardModelCd
+	 */
+	public String getStandardModelCd() {
+		return standardModelCd;
+	}
+
+	/**
+	 * @param standardModelCd the standardModelCd to set
+	 */
+	public void setStandardModelCd(String standardModelCd) {
+		this.standardModelCd = standardModelCd;
+	}
+
+	/**
+	 * @return the tenantId
+	 */
+	public String getTenantId() {
+		return tenantId;
+	}
+
+	/**
+	 * @param tenantId the tenantId to set
+	 */
+	public void setTenantId(String tenantId) {
+		this.tenantId = tenantId;
+	}
+
+	/**
+	 * @return the langCd
+	 */
+	public String getLangCd() {
+		return langCd;
+	}
+
+	/**
+	 * @param langCd the langCd to set
+	 */
+	public void setLangCd(String langCd) {
+		this.langCd = langCd;
+	}
+
+	/**
+	 * @return the deviceModelCd
+	 */
+	public String getDeviceModelCd() {
+		return deviceModelCd;
+	}
+
+	/**
+	 * @param deviceModelCd the deviceModelCd to set
+	 */
+	public void setDeviceModelCd(String deviceModelCd) {
+		this.deviceModelCd = deviceModelCd;
+	}
+
+	/**
+	 * @return the dummy
+	 */
+	public String getDummy() {
+		return dummy;
+	}
+
+	/**
+	 * @param dummy the dummy to set
+	 */
+	public void setDummy(String dummy) {
+		this.dummy = dummy;
+	}
+
+	/**
+	 * @return the serialversionuid
+	 */
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	/**
+	 * @return the userKey
+	 */
+	public String getUserKey() {
+		return userKey;
+	}
+
+	/**
+	 * @param userKey the userKey to set
+	 */
+	public void setUserKey(String userKey) {
+		this.userKey = userKey;
+	}
+
+	/**
+	 * @return the deviceKey
+	 */
+	public String getDeviceKey() {
+		return deviceKey;
+	}
+
+	/**
+	 * @param deviceKey the deviceKey to set
+	 */
+	public void setDeviceKey(String deviceKey) {
+		this.deviceKey = deviceKey;
+	}
+	
+	
+}
