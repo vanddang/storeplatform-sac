e95495d8e6e034c6744652acf80816d36247b44f
오승민 <osm1021@sptek.co.kr>
2014-02-21 07:38:22 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@10514 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/display/vo/personal/PersonalUpdateAlarmRes.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/personal/PersonalUpdateAlarmRes.java
@@ -1,6 +1,10 @@
 package com.skplanet.storeplatform.sac.client.display.vo.personal;
 
+import java.util.List;
+
 import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.CommonResponse;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Product;
 
 /**
  * Application Update 알림 설정 Response Value Object.
@@ -9,4 +13,38 @@ import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
  */
 public class PersonalUpdateAlarmRes extends CommonInfo {
 	private static final long serialVersionUID = 1L;
+
+	private CommonResponse commonResponse;
+	private List<Product> productList;
+
+	/**
+	 * @return the commonResponse
+	 */
+	public CommonResponse getCommonResponse() {
+		return this.commonResponse;
+	}
+
+	/**
+	 * @param commonResponse
+	 *            the commonResponse to set
+	 */
+	public void setCommonResponse(CommonResponse commonResponse) {
+		this.commonResponse = commonResponse;
+	}
+
+	/**
+	 * @return the productList
+	 */
+	public List<Product> getProductList() {
+		return this.productList;
+	}
+
+	/**
+	 * @param productList
+	 *            the productList to set
+	 */
+	public void setProductList(List<Product> productList) {
+		this.productList = productList;
+	}
+
 }
