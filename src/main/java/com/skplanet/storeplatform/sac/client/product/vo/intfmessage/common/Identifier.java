4ecc072eca7af2ee9576969c7ee1fbbcb1e7c1d3
조준일 <takajunil.jo@gmail.com>
2014-02-21 04:40:31 +0000

    seller 주석 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@10400 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/product/vo/intfmessage/common/Identifier.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/product/vo/intfmessage/common/Identifier.java
@@ -28,17 +28,18 @@ public class Identifier extends CommonInfo implements Serializable {
 	 * ID 구분 정보 (catalog : 상품 카달로그, channel : 상품 채널 ("CSI 시즌"의 시즌별 Channel ID), content : 상품 Content (VOD에서 기간제상품,
 	 * 다운로드상품을 묶는 단위), episode : 상품 에피소드 ("CSI 시즌 1"의 1화 Episode ID), artist : 가수 또는 저자 ID, individual : 판매회원의 개발자ID,
 	 * privateOperator : 개인사업자의 개발자ID, corporation : 법인사업자의 개발자ID, foreigner : 외국인 개발자ID, brand : 브랜드ID, feedback :
-	 * 사용후기ID, banner : 배너ID, seedAppId : SeedApp식별자, downloadId : 다운로드ID, contributorId : 기여자 정보)
+	 * 사용후기ID, banner : 배너ID, seedAppId : SeedApp식별자, downloadId : 다운로드ID, contributorId : 기여자 정보), seller : 판매회원번호
 	 */
 	private String type;
 	private String text; // 상품ID
 
-    public Identifier() {}
+	public Identifier() {
+	}
 
-    public Identifier(String type, String text) {
-        this.type = type;
-        this.text = text;
-    }
+	public Identifier(String type, String text) {
+		this.type = type;
+		this.text = text;
+	}
 
 	public String getType() {
 		return this.type;
