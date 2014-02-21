e61b4639e60441fe08d62458bd509c4cbcf19ce6
이승훈 <naly3512@naver.com>
2014-02-21 01:15:14 +0000

    브렌드샵 테마 조회 작업
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@10333 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/display/vo/theme/ThemeBrandshopSacReq.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/theme/ThemeBrandshopSacReq.java
@@ -9,22 +9,97 @@
  */
 package com.skplanet.storeplatform.sac.client.display.vo.theme;
 
+import org.hibernate.validator.constraints.NotBlank;
+
 import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
 
 /**
- * 테마존 테마 상품 조회 Input Value Object.
+ * 브렌드샵 테마 상품 조회 Input Value Object.
  * 
  * Updated on : 2014. 2. 6. Updated by : 이승훈, 엔텔스.
  */
 public class ThemeBrandshopSacReq extends CommonInfo {
 
-	private String listId; // 리스트 Id
-	private String menuId; // 메뉴Id
-	private String themezoneId; // 테마존 Id
-	private String offset; // 시작점 ROW
-	private String count; // 페이지당 노출 ROW 수
+	private static final long serialVersionUID = 1L;
+
+	@NotBlank(message = "필수 파라미터 입니다.")
+	private String channelId;
+	private String menuId;
+	private String filteredBy; // 조회유형
+	private Integer offset = 1; // offset
+	private Integer count = 20; // count
+	private String tenantId; // 테넌트ID
+	private String langCd; // 언어코드
+	private String deviceModelCd; // 디바이스 모델 코드
+	private String topMenuId; // TOP 메뉴 아이디
+	private String bnrMenuId; // 배너 메뉴 아이디
+
 	// TODO osm1021 dummy data가 필요없어지면 삭제할것
 	private String dummy;
+
+	public String getChannelId() {
+		return this.channelId;
+	}
+
+	public void setChannelId(String channelId) {
+		this.channelId = channelId;
+	}
+
+	public String getMenuId() {
+		return this.menuId;
+	}
+
+	public void setMenuId(String menuId) {
+		this.menuId = menuId;
+	}
+
+	public String getFilteredBy() {
+		return this.filteredBy;
+	}
+
+	public void setFilteredBy(String filteredBy) {
+		this.filteredBy = filteredBy;
+	}
+
+	public String getTenantId() {
+		return this.tenantId;
+	}
+
+	public void setTenantId(String tenantId) {
+		this.tenantId = tenantId;
+	}
+
+	public String getLangCd() {
+		return this.langCd;
+	}
+
+	public void setLangCd(String langCd) {
+		this.langCd = langCd;
+	}
+
+	public String getDeviceModelCd() {
+		return this.deviceModelCd;
+	}
+
+	public void setDeviceModelCd(String deviceModelCd) {
+		this.deviceModelCd = deviceModelCd;
+	}
+
+	public String getTopMenuId() {
+		return this.topMenuId;
+	}
+
+	public void setTopMenuId(String topMenuId) {
+		this.topMenuId = topMenuId;
+	}
+
+	public String getBnrMenuId() {
+		return this.bnrMenuId;
+	}
+
+	public void setBnrMenuId(String bnrMenuId) {
+		this.bnrMenuId = bnrMenuId;
+	}
 
 	public String getDummy() {
 		return this.dummy;
@@ -34,128 +109,19 @@ public class ThemeBrandshopSacReq extends CommonInfo {
 		this.dummy = dummy;
 	}
 
-	/**
-	 * 
-	 * <pre>
-	 * 리스트 ID.
-	 * </pre>
-	 * 
-	 * @return String
-	 */
-	public String getListId() {
-		return this.listId;
-	}
-
-	/**
-	 * 
-	 * <pre>
-	 * 리스트 ID.
-	 * </pre>
-	 * 
-	 * @param listId
-	 *            listId
-	 */
-	public void setListId(String listId) {
-		this.listId = listId;
-	}
-
-	/**
-	 * 
-	 * <pre>
-	 * Menu Id.
-	 * </pre>
-	 * 
-	 * @return String
-	 */
-	public String getMenuId() {
-		return this.menuId;
-	}
-
-	/**
-	 * 
-	 * <pre>
-	 * Menu Id.
-	 * </pre>
-	 * 
-	 * @param menuId
-	 *            menuId
-	 */
-	public void setMenuId(String menuId) {
-		this.menuId = menuId;
-	}
-
-	/**
-	 * 
-	 * <pre>
-	 * 테마존 Id.
-	 * </pre>
-	 * 
-	 * @return String
-	 */
-	public String getThemezoneId() {
-		return this.themezoneId;
-	}
-
-	/**
-	 * 
-	 * <pre>
-	 * 테마존 Id.
-	 * </pre>
-	 * 
-	 * @param themezoneId
-	 *            themezoneId
-	 */
-	public void setThemezoneId(String themezoneId) {
-		this.themezoneId = themezoneId;
-	}
-
-	/**
-	 * 
-	 * <pre>
-	 * 시작점 ROW.
-	 * </pre>
-	 * 
-	 * @return String
-	 */
-	public String getOffset() {
+	public Integer getOffset() {
 		return this.offset;
 	}
 
-	/**
-	 * 
-	 * <pre>
-	 * 시작점 ROW.
-	 * </pre>
-	 * 
-	 * @param offset
-	 *            offset
-	 */
-	public void setOffset(String offset) {
+	public void setOffset(Integer offset) {
 		this.offset = offset;
 	}
 
-	/**
-	 * 
-	 * <pre>
-	 * 페이지당 노출될 ROW 개수.
-	 * </pre>
-	 * 
-	 * @return String
-	 */
-	public String getCount() {
+	public Integer getCount() {
 		return this.count;
 	}
 
-	/**
-	 * 
-	 * <pre>
-	 * 페이지당 노출될 ROW 개수.
-	 * </pre>
-	 * 
-	 * @param count
-	 *            count
-	 */
-	public void setCount(String count) {
+	public void setCount(Integer count) {
 		this.count = count;
 	}
 
