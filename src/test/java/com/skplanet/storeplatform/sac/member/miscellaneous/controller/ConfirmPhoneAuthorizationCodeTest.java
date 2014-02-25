ba4a8ca0f4cb0c184ebf468d2ec32f25acdc2e3f
김다슬 <daseul428@spteck.co.kr>
2014-02-25 05:44:42 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@10966 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/test/java/com/skplanet/storeplatform/sac/member/miscellaneous/controller/ConfirmPhoneAuthorizationCodeTest.java
+++ src/test/java/com/skplanet/storeplatform/sac/member/miscellaneous/controller/ConfirmPhoneAuthorizationCodeTest.java
@@ -15,9 +15,11 @@ import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.skplanet.storeplatform.framework.core.exception.StorePlatformException;
@@ -40,6 +42,8 @@ import com.skplanet.storeplatform.sac.member.miscellaneous.service.Miscellaneous
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration({ "classpath*:/spring-test/context-test.xml" })
+@TransactionConfiguration
+@Transactional
 public class ConfirmPhoneAuthorizationCodeTest {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ConfirmPhoneAuthorizationCodeTest.class);
@@ -75,14 +79,45 @@ public class ConfirmPhoneAuthorizationCodeTest {
 	 */
 	@Test
 	public void testConfirmPhoneAuthorizationCode() {
-		new TestCaseTemplate(this.mockMvc).url("/member/miscellaneous/ConfirmPhoneAuthorizationCode/v1")
+		new TestCaseTemplate(this.mockMvc).url("/member/miscellaneous/confirmPhoneAuthorizationCode/v1")
 				.httpMethod(HttpMethod.POST).requestBody(new RequestBodySetter() {
 					@Override
 					public Object requestBody() {
-						request.setUserPhone("01012344241");
-						request.setPhoneAuthCode("528934");
-						request.setPhoneSign("71a008abaa174b92aeb05bcc98403bee");
-						request.setTimeToLive("3");
+						request.setUserPhone("01020284280");
+						request.setPhoneAuthCode("474631");
+						request.setPhoneSign("9570dd977253469a8ad3c53439937f77");
+						request.setTimeToLive("10");
+						LOGGER.debug("[REQUEST(SAC)] JSON : \n{}", TestConvertMapperUtils.convertObjectToJson(request));
+						return request;
+					}
+				}).success(ConfirmPhoneAuthorizationCodeRes.class, new SuccessCallback() {
+
+					@Override
+					public void success(Object result, HttpStatus httpStatus, RunMode runMode) {
+						ConfirmPhoneAuthorizationCodeRes response = (ConfirmPhoneAuthorizationCodeRes) result;
+						assertEquals(response.getUserPhone(), request.getUserPhone());
+						LOGGER.debug("[RESPONSE(SAC)] : \n{}", TestConvertMapperUtils.convertObjectToJson(response));
+					}
+				}, HttpStatus.OK, HttpStatus.ACCEPTED).run(RunMode.JSON);
+
+	}
+
+	/**
+	 * <pre>
+	 * 휴대폰 인증 코드 확인.
+	 * - 인증시간 만료.
+	 * </pre>
+	 */
+	@Test
+	public void testExeptConfirmPhoneAuthorizationCode() {
+		new TestCaseTemplate(this.mockMvc).url("/member/miscellaneous/confirmPhoneAuthorizationCode/v1")
+				.httpMethod(HttpMethod.POST).requestBody(new RequestBodySetter() {
+					@Override
+					public Object requestBody() {
+						request.setUserPhone("01020284280");
+						request.setPhoneAuthCode("474631");
+						request.setPhoneSign("9570dd977253469a8ad3c53439937f77");
+						request.setTimeToLive("1");
 						LOGGER.debug("[REQUEST(SAC)] JSON : \n{}", TestConvertMapperUtils.convertObjectToJson(request));
 						return request;
 					}
@@ -107,14 +142,15 @@ public class ConfirmPhoneAuthorizationCodeTest {
 	@Test(expected = StorePlatformException.class)
 	public void testExceptConfirmPhoneAuthorizationCode() {
 
-		new TestCaseTemplate(this.mockMvc).url("/member/miscellaneous/ConfirmPhoneAuthorizationCode/v1")
+		new TestCaseTemplate(this.mockMvc).url("/member/miscellaneous/confirmPhoneAuthorizationCode/v1")
 				.httpMethod(HttpMethod.POST).requestBody(new RequestBodySetter() {
 
 					@Override
 					public Object requestBody() {
+
 						request.setUserPhone("01012344241");
-						request.setPhoneAuthCode("805531");
-						request.setPhoneSign("b3685c54bf7c491d8a97cc5211449864");
+						request.setPhoneAuthCode("528934");
+						request.setPhoneSign("71a008abaa174b92aeb05bcc98403bee");
 						request.setTimeToLive("3");
 						LOGGER.debug("[REQUEST(SAC)] JSON : \n{}", TestConvertMapperUtils.convertObjectToJson(request));
 						return request;
