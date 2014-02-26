d402d671c3084bf74cb84558ca72cfaa141af85e
유시혁 <syugi1001@gmail.com>
2014-02-26 10:09:22 +0000

    특정 작가별 상품 조회  - 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@11414 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/related/AuthorProductSacReq.java
@@ -0,0 +1,259 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.display.vo.related;
+
+import java.io.Serializable;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+/**
+ * 특정 작가별 상품 조회 Input Value Object.
+ * 
+ * Updated on : 2014. 02. 24. Updated by : 유시혁.
+ */
+public class AuthorProductSacReq extends CommonInfo implements Serializable {
+
+	private static final long serialVersionUID = 11123123143L;
+	private String authorName; // 작가 명
+	private String filteredBy; // 필터링 조건
+	private String exceptId; // 제외 상품 아이디
+	private Integer offset; // 시작점 ROW
+	private Integer count; // 페이지당 노출 ROW 수
+	private String tenantId; // 테넌트 ID
+	private String systemId; // 시스템Id
+	private String deviceModelCd; // 단말모델코드
+	private String langCd; // 언어코드
+
+	/**
+	 * 
+	 * <pre>
+	 * 작가 명.
+	 * </pre>
+	 * 
+	 * @return String
+	 */
+	public String getAuthorName() {
+		return this.authorName;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 작가 명.
+	 * </pre>
+	 * 
+	 * @param authorName
+	 *            String
+	 */
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 필터링 조건.
+	 * </pre>
+	 * 
+	 * @return String
+	 */
+	public String getFilteredBy() {
+		return this.filteredBy;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 필터링 조건.
+	 * </pre>
+	 * 
+	 * @param filteredBy
+	 *            String
+	 */
+	public void setFilteredBy(String filteredBy) {
+		this.filteredBy = filteredBy;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 제외 상품 아이디.
+	 * </pre>
+	 * 
+	 * @return String
+	 */
+	public String getExceptId() {
+		return this.exceptId;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 제외 상품 아이디.
+	 * </pre>
+	 * 
+	 * @param exceptId
+	 *            String
+	 */
+	public void setExceptId(String exceptId) {
+		this.exceptId = exceptId;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 시작점 ROW.
+	 * </pre>
+	 * 
+	 * @return Integer
+	 */
+	public Integer getOffset() {
+		return this.offset;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 시작점 ROW.
+	 * </pre>
+	 * 
+	 * @param offset
+	 *            Integer
+	 */
+	public void setOffset(Integer offset) {
+		this.offset = offset;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 페이지당 노출 ROW 수.
+	 * </pre>
+	 * 
+	 * @return Integer
+	 */
+	public Integer getCount() {
+		return this.count;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 페이지당 노출 ROW 수.
+	 * </pre>
+	 * 
+	 * @param count
+	 *            Integer
+	 */
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 테넌트 ID.
+	 * </pre>
+	 * 
+	 * @return String
+	 */
+	public String getTenantId() {
+		return this.tenantId;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 테넌트 ID.
+	 * </pre>
+	 * 
+	 * @param tenantId
+	 *            String
+	 */
+	public void setTenantId(String tenantId) {
+		this.tenantId = tenantId;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 시스템Id.
+	 * </pre>
+	 * 
+	 * @return String
+	 */
+	public String getSystemId() {
+		return this.systemId;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 시스템Id.
+	 * </pre>
+	 * 
+	 * @param systemId
+	 *            String
+	 */
+	public void setSystemId(String systemId) {
+		this.systemId = systemId;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 단말모델코드.
+	 * </pre>
+	 * 
+	 * @return String
+	 */
+	public String getDeviceModelCd() {
+		return this.deviceModelCd;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 단말모델코드.
+	 * </pre>
+	 * 
+	 * @param deviceModelCd
+	 *            String
+	 */
+	public void setDeviceModelCd(String deviceModelCd) {
+		this.deviceModelCd = deviceModelCd;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 언어코드.
+	 * </pre>
+	 * 
+	 * @return String
+	 */
+	public String getLangCd() {
+		return this.langCd;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 언어코드.
+	 * </pre>
+	 * 
+	 * @param langCd
+	 *            String
+	 */
+	public void setLangCd(String langCd) {
+		this.langCd = langCd;
+	}
+
+}
---
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/related/AuthorProductSacRes.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.display.vo.related;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.CommonResponse;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Product;
+
+/**
+ * 특정 작가별 상품 조회 Value Object.
+ * 
+ * Updated on : 2014. 02. 24 Updated by : 유시혁.
+ */
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+public class AuthorProductSacRes extends CommonInfo implements Serializable {
+
+	private static final long serialVersionUID = 11123123142L;
+
+	private CommonResponse commonResponse;
+
+	private List<Product> productList;
+
+	/**
+	 * 
+	 * <pre>
+	 * 공통 Response.
+	 * </pre>
+	 * 
+	 * @return CommonResponse
+	 */
+	public CommonResponse getCommonResponse() {
+		return this.commonResponse;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 공통 Response.
+	 * </pre>
+	 * 
+	 * @param commonResponse
+	 *            CommonResponse
+	 */
+	public void setCommonResponse(CommonResponse commonResponse) {
+		this.commonResponse = commonResponse;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 상품 리스트.
+	 * </pre>
+	 * 
+	 * @return List<Product>
+	 */
+	public List<Product> getProductList() {
+		return this.productList;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 상품 리스트.
+	 * </pre>
+	 * 
+	 * @param productList
+	 *            List<Product>
+	 */
+	public void setProductList(List<Product> productList) {
+		this.productList = productList;
+	}
+
+}
