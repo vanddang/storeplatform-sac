7898d4cfe91ec0fbc2e72e8ca1509c4076190a22
김경복 <kim.rejoice@gmail.com>
2014-02-27 06:05:41 +0000

    Complete - task : 2.3.6. 사용자별 정책 등록/수정
    Complete - task : 2.3.5. 사용자별 정책 조회 
    
    
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@11595 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/member/vo/miscellaneous/CreateIndividualPolicyReq.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/miscellaneous/CreateIndividualPolicyReq.java
@@ -30,6 +30,18 @@ public class CreateIndividualPolicyReq extends CommonInfo {
 	@NotBlank
 	private String value;
 
+	/** 정책_적용_값. */
+	private String limitAmount; // LIMT_AMT
+
+	/** 이전_정책_적용_값. */
+	private String preLimitAmount; // PRE_LIMT_AMT
+
+	/** 정책_적용_값. */
+	private String permissionType; // PMT_TYPE
+
+	/** 사용여부 (Y/N). */
+	private String isUsed; // USE_YN
+
 	/**
 	 * 등록자.
 	 */
@@ -94,6 +106,66 @@ public class CreateIndividualPolicyReq extends CommonInfo {
 	 */
 	public void setRegId(String regId) {
 		this.regId = regId;
+	}
+
+	/**
+	 * @return the limitAmount
+	 */
+	public String getLimitAmount() {
+		return this.limitAmount;
+	}
+
+	/**
+	 * @param limitAmount
+	 *            the limitAmount to set
+	 */
+	public void setLimitAmount(String limitAmount) {
+		this.limitAmount = limitAmount;
+	}
+
+	/**
+	 * @return the preLimitAmount
+	 */
+	public String getPreLimitAmount() {
+		return this.preLimitAmount;
+	}
+
+	/**
+	 * @param preLimitAmount
+	 *            the preLimitAmount to set
+	 */
+	public void setPreLimitAmount(String preLimitAmount) {
+		this.preLimitAmount = preLimitAmount;
+	}
+
+	/**
+	 * @return the permissionType
+	 */
+	public String getPermissionType() {
+		return this.permissionType;
+	}
+
+	/**
+	 * @param permissionType
+	 *            the permissionType to set
+	 */
+	public void setPermissionType(String permissionType) {
+		this.permissionType = permissionType;
+	}
+
+	/**
+	 * @return the isUsed
+	 */
+	public String getIsUsed() {
+		return this.isUsed;
+	}
+
+	/**
+	 * @param isUsed
+	 *            the isUsed to set
+	 */
+	public void setIsUsed(String isUsed) {
+		this.isUsed = isUsed;
 	}
 
 }
--- src/main/java/com/skplanet/storeplatform/sac/client/member/vo/miscellaneous/IndividualPolicyInfo.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/miscellaneous/IndividualPolicyInfo.java
@@ -1,5 +1,8 @@
 package com.skplanet.storeplatform.sac.client.member.vo.miscellaneous;
 
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+
 import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
 
 /**
@@ -8,6 +11,7 @@ import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
  * 
  * Updated on : 2014. 1. 6. Updated by : 김다슬, 인크로스.
  */
+@JsonSerialize(include = Inclusion.NON_NULL)
 public class IndividualPolicyInfo extends CommonInfo {
 
 	private static final long serialVersionUID = 1L;
@@ -23,6 +27,18 @@ public class IndividualPolicyInfo extends CommonInfo {
 	 * key값에 대한 value.
 	 */
 	private String value;
+
+	/** 정책_적용_값. */
+	private String limitAmount; // LIMT_AMT
+
+	/** 이전_정책_적용_값. */
+	private String preLimitAmount; // PRE_LIMT_AMT
+
+	/** 정책_적용_값. */
+	private String permissionType; // PMT_TYPE
+
+	/** 사용여부 (Y/N). */
+	private String isUsed; // USE_YN
 
 	/**
 	 * @return the policyCode
@@ -67,6 +83,66 @@ public class IndividualPolicyInfo extends CommonInfo {
 	 */
 	public void setValue(String value) {
 		this.value = value;
+	}
+
+	/**
+	 * @return the limitAmount
+	 */
+	public String getLimitAmount() {
+		return this.limitAmount;
+	}
+
+	/**
+	 * @param limitAmount
+	 *            the limitAmount to set
+	 */
+	public void setLimitAmount(String limitAmount) {
+		this.limitAmount = limitAmount;
+	}
+
+	/**
+	 * @return the preLimitAmount
+	 */
+	public String getPreLimitAmount() {
+		return this.preLimitAmount;
+	}
+
+	/**
+	 * @param preLimitAmount
+	 *            the preLimitAmount to set
+	 */
+	public void setPreLimitAmount(String preLimitAmount) {
+		this.preLimitAmount = preLimitAmount;
+	}
+
+	/**
+	 * @return the permissionType
+	 */
+	public String getPermissionType() {
+		return this.permissionType;
+	}
+
+	/**
+	 * @param permissionType
+	 *            the permissionType to set
+	 */
+	public void setPermissionType(String permissionType) {
+		this.permissionType = permissionType;
+	}
+
+	/**
+	 * @return the isUsed
+	 */
+	public String getIsUsed() {
+		return this.isUsed;
+	}
+
+	/**
+	 * @param isUsed
+	 *            the isUsed to set
+	 */
+	public void setIsUsed(String isUsed) {
+		this.isUsed = isUsed;
 	}
 
 }
