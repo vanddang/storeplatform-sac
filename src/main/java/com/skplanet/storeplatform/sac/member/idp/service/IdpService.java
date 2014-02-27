fd79ab910818009f7d1175563d7b5a540f709abc
반범진 <vanddang@gtsoft.co.kr>
2014-02-27 01:18:12 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@11458 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/member/idp/service/IdpService.java
+++ src/main/java/com/skplanet/storeplatform/sac/member/idp/service/IdpService.java
@@ -26,45 +26,6 @@ public interface IdpService {
 	/**
 	 * 
 	 * <pre>
-	 * 회선 변경 정보 Provisioning (번호변경).
-	 * - CMD : changeMobileNumber
-	 * </pre>
-	 * 
-	 * @param map
-	 *            Request로 받은 Parameter Map
-	 * @return IDP Provisioning 처리 결과
-	 */
-	public String changeMobileNumber(HashMap<String, String> map);
-
-	/**
-	 * 
-	 * <pre>
-	 * 회선 변경 정보 Provisioning (기기변경).
-	 * - CMD : changeMobileID
-	 * </pre>
-	 * 
-	 * @param map
-	 *            Request로 받은 Parameter Map
-	 * @return IDP Provisioning 처리 결과
-	 */
-	public String changeMobileID(HashMap<String, String> map);
-
-	/**
-	 * 
-	 * <pre>
-	 * 회선 변경 정보 Provisioning (번호해지).
-	 * - CMD : secedeMobileNumber
-	 * </pre>
-	 * 
-	 * @param map
-	 *            Request로 받은 Parameter Map
-	 * @return IDP Provisioning 처리 결과
-	 */
-	public String secedeMobileNumber(HashMap<String, String> map);
-
-	/**
-	 * 
-	 * <pre>
 	 * 휴대폰소유변경정보배포.
 	 * - CMD : RXInvalidUserTelNoIDP
 	 * </pre>
@@ -166,58 +127,6 @@ public interface IdpService {
 	 * @return
 	 */
 	public ImResult rXDeleteUserIdIDP(HashMap<String, String> map);
-
-	/**
-	 * 
-	 * <pre>
-	 * 가입 승인 만료 정보 Provisioning (유선, 통합 회원)
-	 * - CMD : joinComplete
-	 * </pre>
-	 * 
-	 * @param map
-	 *            Request로 받은 Parameter Map
-	 * @return IDP Provisioning 처리 결과
-	 */
-	public String joinComplete(HashMap<String, String> map);
-
-	/**
-	 * 
-	 * <pre>
-	 * 프로파일 변경 Provisioning (유선, 통합 회원)
-	 * - CMD : adjustWiredProfile
-	 * </pre>
-	 * 
-	 * @param map
-	 *            Request로 받은 Parameter Map
-	 * @return IDP Provisioning 처리 결과
-	 */
-	public String adjustWiredProfile(HashMap<String, String> map);
-
-	/**
-	 * 
-	 * <pre>
-	 * 부가서비스 가입 Provisioning
-	 * - CMD : ecgJoinedTStore
-	 * </pre>
-	 * 
-	 * @param map
-	 *            Request로 받은 Parameter Map
-	 * @return IDP Provisioning 처리 결과
-	 */
-	public String ecgJoinedTStore(HashMap<String, String> map);
-
-	/**
-	 * 
-	 * <pre>
-	 * 부가서비스 해지 Provisioning
-	 * - CMD : ecgScededTStore
-	 * </pre>
-	 * 
-	 * @param map
-	 *            Request로 받은 Parameter Map
-	 * @return IDP Provisioning 처리 결과
-	 */
-	public String ecgScededTStore(HashMap<String, String> map);
 
 	/**
 	 * 
