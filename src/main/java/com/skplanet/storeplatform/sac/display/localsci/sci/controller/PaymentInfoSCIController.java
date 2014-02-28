d0a52db1e629b99826cfac376285c6b33ad2d1be
홍지호 <twomine@gmail.com>
2014-02-28 09:25:31 +0000

    @Validated 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@11999 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/display/localsci/sci/controller/PaymentInfoSCIController.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/localsci/sci/controller/PaymentInfoSCIController.java
@@ -3,6 +3,7 @@ package com.skplanet.storeplatform.sac.display.localsci.sci.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 
 import com.skplanet.storeplatform.framework.integration.bean.LocalSCI;
 import com.skplanet.storeplatform.sac.client.internal.display.localsci.sci.PaymentInfoSCI;
@@ -36,7 +37,7 @@ public class PaymentInfoSCIController implements PaymentInfoSCI {
 	 * @return PaymentInfoSacRes 상품 메타 정보 리스트
 	 */
 	@Override
-	public PaymentInfoSacRes searchPaymentInfo(PaymentInfoSacReq req) {
+	public PaymentInfoSacRes searchPaymentInfo(@Validated PaymentInfoSacReq req) {
 		return this.paymentInfoService.searchPaymentInfo(req);
 	}
 
