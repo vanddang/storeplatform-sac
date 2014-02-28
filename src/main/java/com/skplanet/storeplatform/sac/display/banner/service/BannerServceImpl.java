bbdf4c8516283f787759775dbd3fc0c720df9e2a
이태희 <zabara337@gmail.com>
2014-02-28 04:22:18 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@11805 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/display/banner/service/BannerServceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/banner/service/BannerServceImpl.java
@@ -42,7 +42,7 @@ public class BannerServceImpl implements BannerService {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	private final int BANNER_MAX_COUNT = 20; // 요청 가능한 배너 최대 개수
-	private final int HOME_BANNER_COUNT = 2; // Home 배너 12개 (Mobile Web 정사각형 배너)
+	private final int HOME_BANNER_COUNT = 12; // Home 배너 12개 (Mobile Web 정사각형 배너)
 	private final int GAME_BANNER_COUNT = 2; // 게임 배너 2개 (Mobile Web 정사각형 배너)
 	private final int FUN_BANNER_COUNT = 2; // Fun 배너 2개 (Mobile Web 정사각형 배너)
 	private final int LIFE_BANNER_COUNT = 2; // 생활/위치 배너 2개 (Mobile Web 정사각형 배너)
