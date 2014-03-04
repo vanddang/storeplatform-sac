ae59964735438ad49a9ea33437a3bf7b686cfcdf
이태희 <zabara337@gmail.com>
2014-03-04 01:21:50 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@12339 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/display/download/service/DownloadEbookServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/download/service/DownloadEbookServiceImpl.java
@@ -140,6 +140,11 @@ public class DownloadEbookServiceImpl implements DownloadEbookService {
 		metaInfo = (MetaInfo) this.commonDAO.queryForObject("Download.selectDownloadEbookInfo", ebookReq);
 
 		if (metaInfo != null) {
+			if (DisplayConstants.DP_SERIAL_META_CLASS_CD.equals(metaInfo.getMetaClsfCd())) {
+				// 단품인 상품만 조회가능 합니다.
+				throw new StorePlatformException("SAC_DSP_0013");
+			}
+
 			Product product = new Product();
 
 			// 상품 ID 정보
