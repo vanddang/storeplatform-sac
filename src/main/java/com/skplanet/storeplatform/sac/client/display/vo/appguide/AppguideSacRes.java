68ec2fa679941deafea88f7c827448454679c57b
윤주영 <jy1124yun@gmail.com>
2014-03-06 06:03:02 +0000

    앱가이드 추가 수정 및 개발 - 테마 추천 목록, 테마추천별 상품 목록, 기존 모듈 수정
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@12879 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/display/vo/appguide/AppguideSacRes.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/appguide/AppguideSacRes.java
@@ -11,6 +11,8 @@ package com.skplanet.storeplatform.sac.client.display.vo.appguide;
 
 import java.util.List;
 
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
 import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
 import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.CommonResponse;
 import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Product;
@@ -20,6 +22,7 @@ import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Prod
  * 
  * Updated on : 2014. 02. 26. Updated by : 윤주영, SK 플래닛.
  */
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
 public class AppguideSacRes extends CommonInfo {
 
 	/**
@@ -31,19 +34,51 @@ public class AppguideSacRes extends CommonInfo {
 
 	private List<Product> productList;
 
+	private Product product;
+
+	/**
+	 * @return the commonResponse
+	 */
 	public CommonResponse getCommonResponse() {
 		return this.commonResponse;
 	}
 
-	public void setCommonRes(CommonResponse commonResponse) {
+	/**
+	 * @param commonResponse
+	 *            the commonResponse to set
+	 */
+	public void setCommonResponse(CommonResponse commonResponse) {
 		this.commonResponse = commonResponse;
 	}
 
+	/**
+	 * @return the product
+	 */
+	public Product getProduct() {
+		return this.product;
+	}
+
+	/**
+	 * @param product
+	 *            the product to set
+	 */
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	/**
+	 * @return the productList
+	 */
 	public List<Product> getProductList() {
 		return this.productList;
 	}
 
+	/**
+	 * @param productList
+	 *            the productList to set
+	 */
 	public void setProductList(List<Product> productList) {
 		this.productList = productList;
 	}
+
 }
