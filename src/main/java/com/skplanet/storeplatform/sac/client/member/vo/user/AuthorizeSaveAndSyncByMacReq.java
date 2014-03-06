eecaca01e7587ffab2817ddbf619654d6b7f1319
반범진 <vanddang@gtsoft.co.kr>
2014-03-06 10:57:34 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@13002 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/member/vo/user/AuthorizeSaveAndSyncByMacReq.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/user/AuthorizeSaveAndSyncByMacReq.java
@@ -19,10 +19,52 @@ public class AuthorizeSaveAndSyncByMacReq extends CommonInfo implements Serializ
 	private static final long serialVersionUID = 1L;
 
 	/**
+	 * 기기 ID.
+	 */
+	@NotEmpty(message = "파라미터가 존재하지 않습니다.")
+	private String deviceId;
+
+	/**
+	 * 이동 통신사.
+	 */
+	@NotEmpty(message = "파라미터가 존재하지 않습니다.")
+	private String deviceTelecom;
+
+	/**
 	 * mac 주소
 	 */
 	@NotEmpty(message = "파라미터가 존재하지 않습니다.")
 	private String macAddress;
+
+	/**
+	 * @return deviceId
+	 */
+	public String getDeviceId() {
+		return this.deviceId;
+	}
+
+	/**
+	 * @param deviceId
+	 *            String
+	 */
+	public void setDeviceId(String deviceId) {
+		this.deviceId = deviceId;
+	}
+
+	/**
+	 * @return deviceTelecom
+	 */
+	public String getDeviceTelecom() {
+		return this.deviceTelecom;
+	}
+
+	/**
+	 * @param deviceTelecom
+	 *            String
+	 */
+	public void setDeviceTelecom(String deviceTelecom) {
+		this.deviceTelecom = deviceTelecom;
+	}
 
 	/**
 	 * @return macAddress
