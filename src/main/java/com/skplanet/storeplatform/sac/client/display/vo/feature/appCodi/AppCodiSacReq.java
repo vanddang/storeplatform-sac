810316ee93172af9ff032553adc509e60897eb73
윤주영 <jy1124yun@gmail.com>
2014-03-07 07:55:52 +0000

    앱코디 annotaion validation 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@13279 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/display/vo/feature/appCodi/AppCodiSacReq.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/feature/appCodi/AppCodiSacReq.java
@@ -9,6 +9,11 @@
  */
 package com.skplanet.storeplatform.sac.client.display.vo.feature.appCodi;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.NotBlank;
+
 import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
 
 /**
@@ -20,12 +25,22 @@ public class AppCodiSacReq extends CommonInfo {
 
 	private static final long serialVersionUID = 11123123145L;
 
+	@NotNull
+	@NotBlank
+	@Pattern(regexp = "short|long|dummy")
 	private String filteredBy; // 서비스 구분 (short|long)
 
+	@NotNull
+	@NotBlank
 	private String userKey; // 사용자고유키
 
+	@NotNull
+	@NotBlank
+	@Pattern(regexp = "msisdn")
 	private String deviceIdType; // 기기ID유형
 
+	@NotNull
+	@NotBlank
 	private String deviceId; // 기기ID
 
 	private int offset = 1; // 시작점 ROW
