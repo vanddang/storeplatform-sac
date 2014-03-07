8b99c002a7a144f3ee52d753c83cf4069f97059e
한서구 <seoguman@nate.com>
2014-03-07 09:24:00 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@13347 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/member/seller/service/SellerServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/member/seller/service/SellerServiceImpl.java
@@ -4,12 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import javax.annotation.Resource;
+
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skplanet.icms.Message;
+import com.skplanet.icms.legacy.user.CancelAccountRequest;
+import com.skplanet.icms.operation.Command;
 import com.skplanet.storeplatform.framework.core.exception.StorePlatformException;
 import com.skplanet.storeplatform.member.client.common.vo.CommonRequest;
 import com.skplanet.storeplatform.member.client.common.vo.KeySearch;
@@ -106,6 +112,9 @@ public class SellerServiceImpl implements SellerService {
 	 */
 	@Autowired
 	private MemberCommonComponent component;
+
+	@Resource
+	private AmqpTemplate cmsAmqpTemplate;
 
 	/**
 	 * <pre>
@@ -1058,6 +1067,10 @@ public class SellerServiceImpl implements SellerService {
 
 		WithdrawRes response = new WithdrawRes();
 		response.setSellerKey(req.getSellerKey());
+
+		Message cancelAccountRequest = new Message(Command.CANCEL_ACCOUNT, new CancelAccountRequest(req.getSellerKey()));
+
+		this.cmsAmqpTemplate.convertSendAndReceive(cancelAccountRequest);
 
 		return response;
 	}
