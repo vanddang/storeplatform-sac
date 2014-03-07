0f83d3ea79b7e626028fe9b2341b200316df838f
서지한 <nadri7979@gmail.com>
2014-03-07 18:29:05 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@13397 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/display/product/service/CmsDeviceServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/product/service/CmsDeviceServiceImpl.java
@@ -3,8 +3,11 @@ package com.skplanet.storeplatform.sac.display.product.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.support.MessageSourceAccessor;
@@ -44,8 +47,8 @@ public class CmsDeviceServiceImpl implements CmsDeviceService {
 	@Qualifier("cmsApp")
 	private CommonDAO commonDAO;
 
-	// @Resource
-	// private AmqpTemplate deviceMappingRabbitTemplate;
+	@Resource
+	private AmqpTemplate deviceMappingRabbitTemplate;
 
 	/**
 	 * @description DEVICE-MAPPING 통합CMS > dev3.0 전문 처리
@@ -74,7 +77,7 @@ public class CmsDeviceServiceImpl implements CmsDeviceService {
 		String deviceModelCd = message.getModule();
 		List<DeviceMappingContentVO> contentList = message.getContentList();
 		if (contentList.size() <= 0) {
-		countFail = 9999;
+			countFail = 9999;
 		}
 
 		CmsVo cv = new CmsVo();
@@ -86,121 +89,121 @@ public class CmsDeviceServiceImpl implements CmsDeviceService {
 
 		for (DeviceMappingContentVO dmcv : contentList) {
 
-		try {
-		String prodId = dmcv.getCid();
-		String subContentsId = dmcv.getScid();
-		String lcdSize = dmcv.getLcdInfo();
-		String vmVer = dmcv.getOsVersion();
+			try {
+				String prodId = dmcv.getCid();
+				String subContentsId = dmcv.getScid();
+				String lcdSize = dmcv.getLcdInfo();
+				String vmVer = dmcv.getOsVersion();
 
-		// 데이터 유효성 체크
-		if (null == prodId || null == type) {
-		throw new StorePlatformException(IFConstants.CMS_RST_CODE_DP_DATA_INVALID_ERROR);
-		}
-		if (DeviceMappingCommandCds.CMD_DEVICE_REMAP_INS.getValue().equals(type)) {
-		if (null == subContentsId || null == deviceModelCd) { // 단말 추가
-		throw new StorePlatformException(IFConstants.CMS_RST_CODE_DP_DATA_INVALID_ERROR);
-		}
-		} else if (DeviceMappingCommandCds.CMD_DEVICE_REMAP_DEL.getValue().equals(type)) {
-		if (null == subContentsId || null == deviceModelCd) { // 단말 삭제
-		throw new StorePlatformException(IFConstants.CMS_RST_CODE_DP_DATA_INVALID_ERROR);
-		}
-		} else if (DeviceMappingCommandCds.CMD_OS_REMAP_INS.getValue().equals(type)) {
-		if (null == subContentsId || null == vmVer) { // OS 변경
-		throw new StorePlatformException(IFConstants.CMS_RST_CODE_DP_DATA_INVALID_ERROR);
-		}
-		} else if (DeviceMappingCommandCds.CMD_LCDSIZE_REMAP_INS.getValue().equals(type)) {
-		if (null == subContentsId || null == lcdSize) { // LCD Size 변경
-		throw new StorePlatformException(IFConstants.CMS_RST_CODE_DP_DATA_INVALID_ERROR);
-		}
-		}
+				// 데이터 유효성 체크
+				if (null == prodId || null == type) {
+					throw new StorePlatformException(IFConstants.CMS_RST_CODE_DP_DATA_INVALID_ERROR);
+				}
+				if (DeviceMappingCommandCds.CMD_DEVICE_REMAP_INS.getValue().equals(type)) {
+					if (null == subContentsId || null == deviceModelCd) { // 단말 추가
+						throw new StorePlatformException(IFConstants.CMS_RST_CODE_DP_DATA_INVALID_ERROR);
+					}
+				} else if (DeviceMappingCommandCds.CMD_DEVICE_REMAP_DEL.getValue().equals(type)) {
+					if (null == subContentsId || null == deviceModelCd) { // 단말 삭제
+						throw new StorePlatformException(IFConstants.CMS_RST_CODE_DP_DATA_INVALID_ERROR);
+					}
+				} else if (DeviceMappingCommandCds.CMD_OS_REMAP_INS.getValue().equals(type)) {
+					if (null == subContentsId || null == vmVer) { // OS 변경
+						throw new StorePlatformException(IFConstants.CMS_RST_CODE_DP_DATA_INVALID_ERROR);
+					}
+				} else if (DeviceMappingCommandCds.CMD_LCDSIZE_REMAP_INS.getValue().equals(type)) {
+					if (null == subContentsId || null == lcdSize) { // LCD Size 변경
+						throw new StorePlatformException(IFConstants.CMS_RST_CODE_DP_DATA_INVALID_ERROR);
+					}
+				}
 
-		this.log.info("Remapping Prod Info = " + prodId + " / " + subContentsId + " / " + deviceModelCd + " / "
-				+ lcdSize + " / " + vmVer);
+				this.log.info("Remapping Prod Info = " + prodId + " / " + subContentsId + " / " + deviceModelCd + " / "
+						+ lcdSize + " / " + vmVer);
 
-		// 상품 정보 체크
-		ProductVo dpProd = new ProductVo();
-		dpProd.setProdId(prodId);
-		int dpCount = this.commonDAO.queryForInt("Display_Device.selectDpProdCount", dpProd);
-		if (dpCount <= 0)
-			throw new StorePlatformException(IFConstants.CMS_RST_CODE_DP_DATA_INVALID_ERROR, "PROD_ID [ " + prodId
-					+ " ] 로 등록되어진 상품 정보가 없습니다.");
+				// 상품 정보 체크
+				ProductVo dpProd = new ProductVo();
+				dpProd.setProdId(prodId);
+				int dpCount = this.commonDAO.queryForInt("Display_Device.selectDpProdCount", dpProd);
+				if (dpCount <= 0)
+					throw new StorePlatformException(IFConstants.CMS_RST_CODE_DP_DATA_INVALID_ERROR, "PROD_ID [ "
+							+ prodId + " ] 로 등록되어진 상품 정보가 없습니다.");
 
-		if (DeviceMappingCommandCds.CMD_DEVICE_REMAP_INS.getValue().equals(type)) { // 단말 추가
-		DPSprtDeviceVO sh = new DPSprtDeviceVO();
-		sh.setProdId(prodId);
-		sh.setDeviceModelCd(deviceModelCd);
-		sh.setSubContentsId(subContentsId);
-		try {
-		this.commonDAO.delete("Display_Device.deleteDpSprtHp", sh);
-		} catch (Exception e) {
-		throw new StorePlatformException(IFConstants.CMS_RST_CODE_DB_ERROR, e);
-		}
+				if (DeviceMappingCommandCds.CMD_DEVICE_REMAP_INS.getValue().equals(type)) { // 단말 추가
+					DPSprtDeviceVO sh = new DPSprtDeviceVO();
+					sh.setProdId(prodId);
+					sh.setDeviceModelCd(deviceModelCd);
+					sh.setSubContentsId(subContentsId);
+					try {
+						this.commonDAO.delete("Display_Device.deleteDpSprtHp", sh);
+					} catch (Exception e) {
+						throw new StorePlatformException(IFConstants.CMS_RST_CODE_DB_ERROR, e);
+					}
 
-		try {
-		this.commonDAO.insert("Display_Device.insertDpSprtHp", sh);
-		} catch (Exception e) {
-		throw new StorePlatformException(IFConstants.CMS_RST_CODE_DB_ERROR, e);
-		}
-		} else if (DeviceMappingCommandCds.CMD_DEVICE_REMAP_DEL.getValue().equals(type)) { // 단말 삭제
-		DPSprtDeviceVO sh = new DPSprtDeviceVO();
-		sh.setProdId(prodId);
-		sh.setSubContentsId(subContentsId);
-		sh.setDeviceModelCd(deviceModelCd);
-		try {
-		this.commonDAO.delete("Display_Device.deleteDpSprtHp", sh);
-		} catch (Exception e) {
-		throw new StorePlatformException(IFConstants.CMS_RST_CODE_DB_ERROR, e);
-		}
-		} else if (DeviceMappingCommandCds.CMD_OS_REMAP_INS.getValue().equals(type)) { // OS 변경
-		DPProductSubContsVO sc = new DPProductSubContsVO();
-		sc.setProdId(prodId);
-		sc.setSubContentsId(subContentsId);
-		sc.setVmVer(vmVer);
-		try {
-		this.commonDAO.update("Display_Device.updateDpSubConts", sc);
-		} catch (Exception e) {
-		throw new StorePlatformException(IFConstants.CMS_RST_CODE_DB_ERROR, e);
-		}
-		} else if (DeviceMappingCommandCds.CMD_LCDSIZE_REMAP_INS.getValue().equals(type)) { // LCD Size 변경
-		DPProductSubContsVO sc = new DPProductSubContsVO();
-		sc.setProdId(prodId);
-		sc.setSubContentsId(subContentsId);
-		sc.setLcdSize(lcdSize);
-		try {
-		this.commonDAO.update("Display_Device.updateDpSubConts", sc);
-		} catch (Exception e) {
-		throw new StorePlatformException(IFConstants.CMS_RST_CODE_DB_ERROR, e);
-		}
-		}
-		// 결과 설정
-		cv.setResultCd(IFConstants.CMS_RST_CODE_SUCCESS);
-		cv.setResultMsg(this.messageSourceAccessor.getMessage("if.cms.msg.code." + cv.getResultCd()));
+					try {
+						this.commonDAO.insert("Display_Device.insertDpSprtHp", sh);
+					} catch (Exception e) {
+						throw new StorePlatformException(IFConstants.CMS_RST_CODE_DB_ERROR, e);
+					}
+				} else if (DeviceMappingCommandCds.CMD_DEVICE_REMAP_DEL.getValue().equals(type)) { // 단말 삭제
+					DPSprtDeviceVO sh = new DPSprtDeviceVO();
+					sh.setProdId(prodId);
+					sh.setSubContentsId(subContentsId);
+					sh.setDeviceModelCd(deviceModelCd);
+					try {
+						this.commonDAO.delete("Display_Device.deleteDpSprtHp", sh);
+					} catch (Exception e) {
+						throw new StorePlatformException(IFConstants.CMS_RST_CODE_DB_ERROR, e);
+					}
+				} else if (DeviceMappingCommandCds.CMD_OS_REMAP_INS.getValue().equals(type)) { // OS 변경
+					DPProductSubContsVO sc = new DPProductSubContsVO();
+					sc.setProdId(prodId);
+					sc.setSubContentsId(subContentsId);
+					sc.setVmVer(vmVer);
+					try {
+						this.commonDAO.update("Display_Device.updateDpSubConts", sc);
+					} catch (Exception e) {
+						throw new StorePlatformException(IFConstants.CMS_RST_CODE_DB_ERROR, e);
+					}
+				} else if (DeviceMappingCommandCds.CMD_LCDSIZE_REMAP_INS.getValue().equals(type)) { // LCD Size 변경
+					DPProductSubContsVO sc = new DPProductSubContsVO();
+					sc.setProdId(prodId);
+					sc.setSubContentsId(subContentsId);
+					sc.setLcdSize(lcdSize);
+					try {
+						this.commonDAO.update("Display_Device.updateDpSubConts", sc);
+					} catch (Exception e) {
+						throw new StorePlatformException(IFConstants.CMS_RST_CODE_DB_ERROR, e);
+					}
+				}
+				// 결과 설정
+				cv.setResultCd(IFConstants.CMS_RST_CODE_SUCCESS);
+				cv.setResultMsg(this.messageSourceAccessor.getMessage("if.cms.msg.code." + cv.getResultCd()));
 
-		} catch (StorePlatformException ie) {
-		cv.setResultCd(ie.getErrorInfo().getCode());
-		} catch (Exception e) {
-		throw new StorePlatformException(IFConstants.CMS_RST_CODE_UNKNOWN_ERROR, e);
-		}
+			} catch (StorePlatformException ie) {
+				cv.setResultCd(ie.getErrorInfo().getCode());
+			} catch (Exception e) {
+				throw new StorePlatformException(IFConstants.CMS_RST_CODE_UNKNOWN_ERROR, e);
+			}
 
-		if (cv.getResultCd().equals(IFConstants.CMS_RST_CODE_SUCCESS)) {
-		++countSuccess;
-		} else {
-		cv.setResultMsg(this.messageSourceAccessor.getMessage("if.cms.msg.code." + cv.getResultCd()));
-		dmcv.setMsg(cv.getResultMsg());
-		dmcv.setResult(cv.getResultCd());
-		resultFailList.add(dmcv);
-		++countFail;
-		}
+			if (cv.getResultCd().equals(IFConstants.CMS_RST_CODE_SUCCESS)) {
+				++countSuccess;
+			} else {
+				cv.setResultMsg(this.messageSourceAccessor.getMessage("if.cms.msg.code." + cv.getResultCd()));
+				dmcv.setMsg(cv.getResultMsg());
+				dmcv.setResult(cv.getResultCd());
+				resultFailList.add(dmcv);
+				++countFail;
+			}
 		}
 
 		this.log.debug(":::::countSuccess {} , countFail {} :::::::", countSuccess, countFail);
 
 		if (countFail == contentList.size() || countFail > 0) {
-		cv.setResultCd(IFConstants.CMS_RST_CODE_UNKNOWN_ERROR); // Result Code
-		cv.setResultMsg(this.messageSourceAccessor.getMessage("if.cms.msg.code." + cv.getResultCd()));
+			cv.setResultCd(IFConstants.CMS_RST_CODE_UNKNOWN_ERROR); // Result Code
+			cv.setResultMsg(this.messageSourceAccessor.getMessage("if.cms.msg.code." + cv.getResultCd()));
 		} else {
-		cv.setResultCd(IFConstants.CMS_RST_CODE_SUCCESS);
-		cv.setResultMsg(this.messageSourceAccessor.getMessage("if.cms.msg.code." + cv.getResultCd()));
+			cv.setResultCd(IFConstants.CMS_RST_CODE_SUCCESS);
+			cv.setResultMsg(this.messageSourceAccessor.getMessage("if.cms.msg.code." + cv.getResultCd()));
 		}
 
 		// 결과 전송
@@ -210,7 +213,7 @@ public class CmsDeviceServiceImpl implements CmsDeviceService {
 		dmr.setMsg(cv.getResultMsg()); // Result Message
 		dmr.setContentList(resultFailList);
 
-		// this.deviceMappingRabbitTemplate.convertAndSend(dmr);
+		this.deviceMappingRabbitTemplate.convertAndSend(dmr);
 
 		this.log.debug("*********************************************************");
 		this.log.debug("***** dmr.getResult         :::: {}", dmr.getResult());
