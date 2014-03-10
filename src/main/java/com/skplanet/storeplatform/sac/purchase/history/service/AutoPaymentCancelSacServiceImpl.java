2d5eda30001a91ee7ec2a4fbac2e8cf004b64c0f
조용진 <kama624@naver.com>
2014-03-10 05:07:26 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@13608 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/purchase/history/service/AutoPaymentCancelSacServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/purchase/history/service/AutoPaymentCancelSacServiceImpl.java
@@ -9,16 +9,13 @@
  */
 package com.skplanet.storeplatform.sac.purchase.history.service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.skplanet.storeplatform.external.client.tstore.sci.TStoreNotiSCI;
-import com.skplanet.storeplatform.framework.core.exception.StorePlatformException;
+import com.skplanet.storeplatform.external.client.tstore.vo.TStoreNotiEcReq;
 import com.skplanet.storeplatform.purchase.client.history.sci.AutoPaymentCancelSCI;
 import com.skplanet.storeplatform.purchase.client.history.vo.AutoPaymentCancelScReq;
 import com.skplanet.storeplatform.purchase.client.history.vo.AutoPaymentCancelScRes;
@@ -53,23 +50,17 @@ public class AutoPaymentCancelSacServiceImpl implements AutoPaymentCancelSacServ
 		// ResultYn이 'Y'일 경우에만 이메일 발송
 		if (autoPaymentCancelRes.getResultYn().equals("Y")) {
 			this.logger.debug("@@@@@@자동결재해지예약/예약취소/해지 이메일 발송 처리@@@@@@");
-			Map<String, Object> reqMap = new HashMap<String, Object>();
-
-			reqMap.put("userKey", autoPaymentCancelReq.getUserKey());
-			reqMap.put("deviceKey", autoPaymentCancelReq.getDeviceKey());
-			reqMap.put("prchsId", autoPaymentCancelReq.getPrchsId());
-			// 구매일시는 조회하여 가저옴
-			reqMap.put("prchsDt", autoPaymentCancelRes.getPrchsDt());
-			// type code = 01:일반결제, 02:자동결제, 03:자동결제해지예약, 04:자동결제 해지완료
-			reqMap.put("type", "03");
 			// tstore 이메일 발송
-			try {
-				// TODO TENANT(Tstore 구현X) 주석처리
-				// this.tStoreNotiSCI.postTStoreNoti(reqMap);
-			} catch (Exception ex) {
-				// 공통에러코드 정의 필요
-				throw new StorePlatformException("", ex);
-			}
+			// try {
+			// TODO TENANT(Tstore 구현X) 주석처리
+			TStoreNotiEcReq tStoreNotiEcReq = new TStoreNotiEcReq();
+			tStoreNotiEcReq.setUserKey(autoPaymentCancelReq.getUserKey());
+			tStoreNotiEcReq.setDeviceKey(autoPaymentCancelReq.getDeviceKey());
+			tStoreNotiEcReq.setPrchsId(autoPaymentCancelReq.getPrchsId());
+			tStoreNotiEcReq.setPrchsDt(autoPaymentCancelRes.getPrchsDt());
+			// type code = 01:일반결제, 02:자동결제, 03:자동결제해지예약, 04:자동결제 해지완료
+			tStoreNotiEcReq.setType("03");
+			// this.tStoreNotiSCI.postTStoreNoti(tStoreNotiEcReq);
 		}
 		return autoPaymentCancelRes;
 	}
