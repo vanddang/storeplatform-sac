d8747a691cd51eb11a4f6046f7e57995b157a545
이태희 <zabara337@gmail.com>
2014-03-11 05:43:34 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@13978 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/device/DeviceChangeSacRes.java
@@ -0,0 +1,50 @@
+package com.skplanet.storeplatform.sac.client.display.vo.device;
+
+import java.util.List;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.CommonResponse;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Device;
+
+/**
+ * 단말 UserAgent 관리 정보 조회 Response Value Object.
+ * 
+ * Updated on : 2014. 03. 11. Updated by : 이태희.
+ */
+public class DeviceChangeSacRes extends CommonInfo {
+	private static final long serialVersionUID = 1L;
+
+	private CommonResponse commonResponse;
+
+	private List<Device> deviceList;
+
+	/**
+	 * @return the commonResponse
+	 */
+	public CommonResponse getCommonResponse() {
+		return this.commonResponse;
+	}
+
+	/**
+	 * @param commonResponse
+	 *            the commonResponse to set
+	 */
+	public void setCommonResponse(CommonResponse commonResponse) {
+		this.commonResponse = commonResponse;
+	}
+
+	/**
+	 * @return the deviceList
+	 */
+	public List<Device> getDeviceList() {
+		return this.deviceList;
+	}
+
+	/**
+	 * @param deviceList
+	 *            the deviceList to set
+	 */
+	public void setDeviceList(List<Device> deviceList) {
+		this.deviceList = deviceList;
+	}
+}
