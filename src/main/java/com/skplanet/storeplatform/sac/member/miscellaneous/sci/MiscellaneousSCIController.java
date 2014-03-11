76c2aa0020b79691584cf3d02c86de941de5096f
강신완 <@gmail.com>
2014-03-11 07:58:11 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@14064 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/member/miscellaneous/sci/MiscellaneousSCIController.java
@@ -0,0 +1,101 @@
+/**
+ * 
+ */
+package com.skplanet.storeplatform.sac.member.miscellaneous.sci;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.skplanet.storeplatform.framework.integration.bean.LocalSCI;
+import com.skplanet.storeplatform.sac.client.internal.member.miscellaneous.sci.MiscellaneousSCI;
+import com.skplanet.storeplatform.sac.client.internal.member.miscellaneous.vo.GetIndividualPolicySacReq;
+import com.skplanet.storeplatform.sac.client.internal.member.miscellaneous.vo.GetIndividualPolicySacRes;
+import com.skplanet.storeplatform.sac.client.internal.member.miscellaneous.vo.IndividualPolicyInfoSac;
+import com.skplanet.storeplatform.sac.client.member.vo.miscellaneous.GetIndividualPolicyReq;
+import com.skplanet.storeplatform.sac.client.member.vo.miscellaneous.GetIndividualPolicyReq.PolicyCode;
+import com.skplanet.storeplatform.sac.client.member.vo.miscellaneous.GetIndividualPolicyRes;
+import com.skplanet.storeplatform.sac.client.member.vo.miscellaneous.IndividualPolicyInfo;
+import com.skplanet.storeplatform.sac.common.header.vo.SacRequestHeader;
+import com.skplanet.storeplatform.sac.common.util.SacRequestHeaderHolder;
+import com.skplanet.storeplatform.sac.member.miscellaneous.service.MiscellaneousService;
+
+/**
+ * 기타 기능 내부메소드 호출 Controller.
+ * 
+ * Updated on : 2014. 3. 11. Updated by : 강신완, 부르칸.
+ */
+@LocalSCI
+@RequestMapping(value = "/member/miscellaneous/sci")
+public class MiscellaneousSCIController implements MiscellaneousSCI {
+	private static final Logger LOGGER = LoggerFactory.getLogger(MiscellaneousSCIController.class);
+
+	@Autowired
+	private MiscellaneousService miscellaneousService;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.skplanet.storeplatform.sac.client.internal.member.miscellaneous.sci
+	 * .MiscellaneousSCI
+	 * #getIndividualPolicy(com.skplanet.storeplatform.sac.client
+	 * .internal.member.miscellaneous.vo.GetIndividualPolicySacReq)
+	 */
+	@Override
+	@RequestMapping(value = "/getIndividualPolicy", method = RequestMethod.POST)
+	@ResponseBody
+	public GetIndividualPolicySacRes getIndividualPolicy(@RequestBody @Validated GetIndividualPolicySacReq req) {
+
+		// 헤더 정보 셋팅
+		SacRequestHeader requestHeader = SacRequestHeaderHolder.getValue();
+		LOGGER.info("[MiscellaneousSCIController.getIndividualPolicy] RequestHeader : {}, \nRequestParameter : {}", requestHeader, req);
+
+		List<PolicyCode> codeList = new ArrayList<PolicyCode>();
+		for (int i = 0; i < req.getPolicyCodeList().size(); i++) {
+			PolicyCode code = new PolicyCode();
+			code.setPolicyCode(req.getPolicyCodeList().get(i).getPolicyCode());
+			codeList.add(code);
+		}
+
+		System.out.println("================ codeList : " + codeList.toString());
+		System.out.println("================ key : " + req.getKey());
+
+		GetIndividualPolicyReq getIndividualPolicyReq = new GetIndividualPolicyReq();
+		getIndividualPolicyReq.setKey(req.getKey());
+		getIndividualPolicyReq.setPolicyCodeList(codeList);
+
+		GetIndividualPolicyRes getIndividualPolicyRes = this.miscellaneousService.getIndividualPolicy(requestHeader, getIndividualPolicyReq);
+
+		GetIndividualPolicySacRes sacRes = new GetIndividualPolicySacRes();
+		List<IndividualPolicyInfoSac> infoSacList = new ArrayList<IndividualPolicyInfoSac>();
+
+		for (IndividualPolicyInfo policyList : getIndividualPolicyRes.getPolicyList()) {
+			IndividualPolicyInfoSac infoSac = new IndividualPolicyInfoSac();
+			infoSac.setIsUsed(policyList.getIsUsed());
+			infoSac.setKey(policyList.getKey());
+			infoSac.setLimitAmount(policyList.getLimitAmount());
+			infoSac.setPermissionType(policyList.getPermissionType());
+			infoSac.setPolicyCode(policyList.getPolicyCode());
+			infoSac.setPreLimitAmount(policyList.getPreLimitAmount());
+			infoSac.setValue(policyList.getValue());
+
+			infoSacList.add(infoSac);
+		}
+		sacRes.setPolicyList(infoSacList);
+
+		LOGGER.info("[MiscellaneousSCIController.getIndividualPolicy] ResponseParameter : {}", sacRes.toString());
+
+		return sacRes;
+
+	}
+
+}
