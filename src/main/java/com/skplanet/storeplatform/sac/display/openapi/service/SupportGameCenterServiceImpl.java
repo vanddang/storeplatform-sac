d44c2b61de3095c65c92adab0075a3008450f909
백승현 <bback@sptek.co.kr>
2014-03-13 05:43:11 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@14662 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/display/openapi/service/SupportGameCenterServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/openapi/service/SupportGameCenterServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Service;
 
 import com.skplanet.storeplatform.framework.core.exception.StorePlatformException;
 import com.skplanet.storeplatform.framework.core.persistence.dao.CommonDAO;
+import com.skplanet.storeplatform.framework.core.util.StringUtils;
 import com.skplanet.storeplatform.sac.client.display.vo.openapi.SupportGameCenterSacReq;
 import com.skplanet.storeplatform.sac.client.display.vo.openapi.SupportGameCenterSacRes;
 import com.skplanet.storeplatform.sac.client.internal.purchase.history.sci.HistoryInternalSCI;
@@ -96,8 +97,6 @@ public class SupportGameCenterServiceImpl implements SupportGameCenterService {
 		this.log.debug("####### userKey : " + supportGameCenterSacReq.getUserKey());
 		this.log.debug("####### deviceKey : " + supportGameCenterSacReq.getDeviceKey());
 
-		// 상품ID
-
 		/*
 		 * AID로 상품ID 가져오기
 		 */
@@ -105,51 +104,55 @@ public class SupportGameCenterServiceImpl implements SupportGameCenterService {
 		prodIdList = this.commonDAO.queryForList("OpenApi.getProductIdByAid", supportGameCenterSacReq, MetaInfo.class);
 
 		if (prodIdList.size() != 0) {
-			/*****************************/
-			// 구매내역 조회를 위한 생성자
-			ProductListSacIn productListSacIn = null;
-			List<ProductListSacIn> ProductListSacInList = null;
-			try {
-				productListSacIn = new ProductListSacIn();
-				ProductListSacInList = new ArrayList<ProductListSacIn>();
 
-				int k = 0;
-				String[] arrayProductId = new String[prodIdList.size()];
-				; // 상품ID LIST
-				Iterator<MetaInfo> iterator = prodIdList.iterator();
-				while (iterator.hasNext()) {
-					MetaInfo metaInfo = iterator.next();
-					this.log.debug("####### prodId : " + metaInfo.getProdId());
-					// 조회할 상품ID SET
-					arrayProductId[k] = metaInfo.getProdId();
-					productListSacIn.setProdId(metaInfo.getProdId());
-					ProductListSacInList.add(productListSacIn);
-					k++;
+			// userKey와 deviceKey가 존재하는 경우 구매내역 조회
+			if (StringUtils.isNotEmpty(userKey) && StringUtils.isNotEmpty(deviceKey)) {
+				// 구매내역 조회를 위한 생성자
+				ProductListSacIn productListSacIn = null;
+				List<ProductListSacIn> ProductListSacInList = null;
+				try {
+					productListSacIn = new ProductListSacIn();
+					ProductListSacInList = new ArrayList<ProductListSacIn>();
+
+					int k = 0;
+					String[] arrayProductId = new String[prodIdList.size()];
+					; // 상품ID LIST
+					Iterator<MetaInfo> iterator = prodIdList.iterator();
+					while (iterator.hasNext()) {
+						MetaInfo metaInfo = iterator.next();
+						this.log.debug("####### prodId : " + metaInfo.getProdId());
+						// 조회할 상품ID SET
+						arrayProductId[k] = metaInfo.getProdId();
+						productListSacIn.setProdId(metaInfo.getProdId());
+						ProductListSacInList.add(productListSacIn);
+						k++;
+					}
+					supportGameCenterSacReq.setArrayProductId(arrayProductId);
+
+					historyReq = new HistoryListSacInReq();
+					historyReq.setTenantId(supportGameCenterSacReq.getTenantId());
+					historyReq.setUserKey(userKey);
+					historyReq.setDeviceKey(deviceKey);
+					historyReq.setPrchsProdHaveYn(DisplayConstants.PRCHS_PROD_HAVE_YES);
+					historyReq.setPrchsProdType(DisplayConstants.PRCHS_PROD_TYPE_UNIT);
+					historyReq.setStartDt(DisplayConstants.PRCHS_START_DATE);
+					historyReq.setEndDt(sysDate);
+					historyReq.setOffset(1);
+					historyReq.setCount(prodIdList.size());
+					historyReq.setProductList(ProductListSacInList);
+					historyReq.setPrchsStatusCd("OR000301"); // 구매완료
+
+					// 구매내역 조회 실행
+					historyRes = this.historyInternalSCI.searchHistoryList(historyReq);
+
+				} catch (Exception ex) {
+					purchaseFlag = false;
+					this.log.error("구매내역 조회 연동 중 오류가 발생하였습니다. \n{}", ex);
+					// throw new StorePlatformException("SAC_DSP_2001", ex);
 				}
-				supportGameCenterSacReq.setArrayProductId(arrayProductId);
-
-				historyReq = new HistoryListSacInReq();
-				historyReq.setTenantId(supportGameCenterSacReq.getTenantId());
-				historyReq.setUserKey(userKey);
-				historyReq.setDeviceKey(deviceKey);
-				historyReq.setPrchsProdHaveYn(DisplayConstants.PRCHS_PROD_HAVE_YES);
-				historyReq.setPrchsProdType(DisplayConstants.PRCHS_PROD_TYPE_UNIT);
-				historyReq.setStartDt(DisplayConstants.PRCHS_START_DATE);
-				historyReq.setEndDt(sysDate);
-				historyReq.setOffset(1);
-				historyReq.setCount(prodIdList.size());
-				historyReq.setProductList(ProductListSacInList);
-				historyReq.setPrchsStatusCd("OR000301"); // 구매완료
-
-				// 구매내역 조회 실행
-				historyRes = this.historyInternalSCI.searchHistoryList(historyReq);
-
-			} catch (Exception ex) {
+			} else {
 				purchaseFlag = false;
-				this.log.error("구매내역 조회 연동 중 오류가 발생하였습니다. \n{}", ex);
-				// throw new StorePlatformException("SAC_DSP_2001", ex);
 			}
-			/*****************************/
 		} else {
 			// 요청한 AID 상품이 존재하지 않는다.
 			throw new StorePlatformException("SAC_DSP_0005", supportGameCenterSacReq.getAidList());
@@ -200,11 +203,10 @@ public class SupportGameCenterServiceImpl implements SupportGameCenterService {
 					String prchsState = null; // 구매상태
 					String prchsProdId = null; // 구매 상품ID
 					String puchsPrice = null; // 구매 상품금액
-					String drmYn = null; // 구매상품 Drm여부
 
 					this.log.debug("############# historyRes.getTotalCnt() : " + historyRes.getTotalCnt());
 					if (historyRes.getTotalCnt() > 0) {
-						// List<Purchase> purchaseList = new ArrayList<Purchase>();
+						// 구매 내역이 존재하는 경우
 
 						for (int i = 0; i < historyRes.getTotalCnt(); i++) {
 							prchsId = historyRes.getHistoryList().get(i).getPrchsId();
@@ -214,7 +216,6 @@ public class SupportGameCenterServiceImpl implements SupportGameCenterService {
 							prchsCaseCd = historyRes.getHistoryList().get(i).getPrchsCaseCd();
 							prchsProdId = historyRes.getHistoryList().get(i).getProdId();
 							puchsPrice = historyRes.getHistoryList().get(i).getProdAmt();
-							drmYn = historyRes.getHistoryList().get(i).getDrmYn();
 
 							this.log.debug("############# prchsId : " + prchsId + " ### prchsProdId : " + prchsProdId
 									+ " #### prchsCaseCd : " + prchsCaseCd + " #### dwldExprDt : " + dwldExprDt);
@@ -241,6 +242,11 @@ public class SupportGameCenterServiceImpl implements SupportGameCenterService {
 							}
 						}
 					}
+				} else {
+					// 구매내역이 존재하지 않는 경우
+					purchase = new Purchase();
+					purchase.setState("none");
+					product.setPurchase(purchase);
 				}
 
 				Support support = new Support();
