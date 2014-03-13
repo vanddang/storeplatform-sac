ed4c2e5498152c8c420f021075add625c35b8bc4
서대영 <dale.seo@sk.com>
2014-03-13 10:02:07 +0000

    refs #10703: ACL 통합 테스트 작성 https://project.itopping.co.kr:82/issues/10703
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@14796 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/test/java/com/skplanet/storeplatform/sac/runtime/acl/service/AclServiceTobeImplTest.java
+++ src/test/java/com/skplanet/storeplatform/sac/runtime/acl/service/AclServiceTobeImplTest.java
@@ -1,6 +1,7 @@
 package com.skplanet.storeplatform.sac.runtime.acl.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 
@@ -12,6 +13,8 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.skplanet.storeplatform.framework.core.exception.StorePlatformException;
+import com.skplanet.storeplatform.sac.runtime.acl.service.authentication.AuthenticateService;
+import com.skplanet.storeplatform.sac.runtime.acl.service.authorization.AuthorizeService;
 import com.skplanet.storeplatform.sac.runtime.acl.service.verification.VerifyService;
 import com.skplanet.storeplatform.sac.runtime.acl.util.AclUtils;
 import com.skplanet.storeplatform.sac.runtime.acl.vo.HttpHeaders;
@@ -23,27 +26,74 @@ public class AclServiceTobeImplTest {
 	private AclServiceTobeImpl aclService;
 
 	@Mock
-	private VerifyService requestValidateMock;
+	private VerifyService verifyServiceMock;
+
+	@Mock
+	private AuthenticateService authenticateServiceMock;
+
+	@Mock
+	private AuthorizeService authorizeServiceMock;
 
 	@Before
 	public void setUp() throws Exception {
 	}
 
 	@Test(expected=StorePlatformException.class)
-	public void test() {
+	public void testValidate() {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setInterfaceId("I01000001");
 		headers.setRequestUrl("/member/user/createByMdn/v1");
 		headers.setTimestamp(AclUtils.getTimestamp() + "");
 
-		doThrow(new StorePlatformException("SAC_CMN_001")).when(this.requestValidateMock).verifyHeaders(headers);
+		doThrow(new StorePlatformException("SAC_CMN_0001")).when(this.verifyServiceMock).verifyHeaders(headers);
 		try {
 			this.aclService.validate(headers);
 		} catch (StorePlatformException e) {
-			assertEquals("SAC_CMN_001", e.getErrorInfo().getCode());
+			assertEquals("SAC_CMN_0001", e.getErrorInfo().getCode());
 			throw e;
 		} finally {
-			verify(this.requestValidateMock).verifyHeaders(headers);
+			verify(this.verifyServiceMock).verifyHeaders(headers);
+		}
+	}
+
+	@Test(expected=StorePlatformException.class)
+	public void testAuthenticate() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setInterfaceId("IS01000001");
+		headers.setAuthKey("S01c61c0cad59adc6e03069e71614a5a0a3");
+		headers.setTimestamp("1392659261");
+		headers.setNonce("1392659261");
+		headers.setSignature("");
+
+		doThrow(new StorePlatformException("SAC_CMN_0036")).when(this.authenticateServiceMock).authenticate(headers);
+		try {
+			this.aclService.authenticate(headers);
+		} catch (StorePlatformException e) {
+			assertEquals("SAC_CMN_0036", e.getErrorInfo().getCode());
+			throw e;
+		} finally {
+			verify(this.authenticateServiceMock).authenticate(headers);
+		}
+	}
+
+
+	@Test(expected=StorePlatformException.class)
+	public void testAuthorize() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAuthKey("S01c61c0cad59adc6e03069e71614a5a0a3");
+		headers.setInterfaceId("IS01000001");
+		headers.setRequestUrl("/member/user/createByMdn/v1");
+
+		doNothing().when(this.authorizeServiceMock).checkInterface(headers);
+		doThrow(new StorePlatformException("SAC_CMN_0064")).when(this.authorizeServiceMock).checkMapping(headers);
+		try {
+			this.aclService.authorize(headers);
+		} catch (StorePlatformException e) {
+			assertEquals("SAC_CMN_0064", e.getErrorInfo().getCode());
+			throw e;
+		} finally {
+			verify(this.authorizeServiceMock).checkInterface(headers);
+			verify(this.authorizeServiceMock).checkMapping(headers);
 		}
 	}
 
