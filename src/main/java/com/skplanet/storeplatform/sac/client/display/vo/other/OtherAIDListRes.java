710cba9fcedc6cc05f520ada2cbd8144a877d985
백승현 <bback@sptek.co.kr>
2014-03-14 05:18:30 +0000

    상품 ID 조회(By AID)
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@14961 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/other/OtherAIDListRes.java
@@ -0,0 +1,54 @@
+package com.skplanet.storeplatform.sac.client.display.vo.other;
+
+import java.util.List;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.CommonResponse;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Product;
+
+/**
+ * AID로 상품ID 조회 Request Value Object.
+ * 
+ * Updated on : 2014. 3. 14. Updated by : 백승현, 인크로스.
+ */
+public class OtherAIDListRes extends CommonInfo {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Common Response.
+	 */
+	private CommonResponse commonResponse;
+
+	private List<Product> productList;
+
+	/**
+	 * @return the commonResponse
+	 */
+	public CommonResponse getCommonResponse() {
+		return this.commonResponse;
+	}
+
+	/**
+	 * @param commonResponse
+	 *            the commonResponse to set
+	 */
+	public void setCommonResponse(CommonResponse commonResponse) {
+		this.commonResponse = commonResponse;
+	}
+
+	/**
+	 * @return the productList
+	 */
+	public List<Product> getProductList() {
+		return this.productList;
+	}
+
+	/**
+	 * @param productList
+	 *            the productList to set
+	 */
+	public void setProductList(List<Product> productList) {
+		this.productList = productList;
+	}
+
+}
