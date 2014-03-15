023e51d1394e06837932f69ffbaddd3464d09749
반범진 <vanddang@gtsoft.co.kr>
2014-03-15 08:10:14 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@15226 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/member/vo/user/AuthorizeSaveAndSyncByMacReq.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/user/AuthorizeSaveAndSyncByMacReq.java
@@ -1,10 +1,12 @@
 package com.skplanet.storeplatform.sac.client.member.vo.user;
 
 import java.io.Serializable;
+import java.util.List;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+import com.skplanet.storeplatform.sac.client.member.vo.common.DeviceExtraInfo;
 
 /**
  * [REQUEST] Save&Sync 인증.
@@ -35,6 +37,21 @@ public class AuthorizeSaveAndSyncByMacReq extends CommonInfo implements Serializ
 	 */
 	@NotEmpty(message = "파라미터가 존재하지 않습니다.")
 	private String macAddress;
+
+	/**
+	 * 기기 고유 번호.
+	 */
+	private String nativeId;
+
+	/**
+	 * 기기 계정(Gmail 주소).
+	 */
+	private String deviceAccount;
+
+	/**
+	 * 휴대기기 부가정보 리스트.
+	 */
+	private List<DeviceExtraInfo> deviceExtraInfoList;
 
 	/**
 	 * @return deviceId
@@ -79,6 +96,51 @@ public class AuthorizeSaveAndSyncByMacReq extends CommonInfo implements Serializ
 	 */
 	public void setMacAddress(String macAddress) {
 		this.macAddress = macAddress;
+	}
+
+	/**
+	 * @return nativeId
+	 */
+	public String getNativeId() {
+		return this.nativeId;
+	}
+
+	/**
+	 * @param nativeId
+	 *            String
+	 */
+	public void setNativeId(String nativeId) {
+		this.nativeId = nativeId;
+	}
+
+	/**
+	 * @return String
+	 */
+	public String getDeviceAccount() {
+		return this.deviceAccount;
+	}
+
+	/**
+	 * @param deviceAccount
+	 *            String
+	 */
+	public void setDeviceAccount(String deviceAccount) {
+		this.deviceAccount = deviceAccount;
+	}
+
+	/**
+	 * @return deviceExtraInfoList
+	 */
+	public List<DeviceExtraInfo> getDeviceExtraInfoList() {
+		return this.deviceExtraInfoList;
+	}
+
+	/**
+	 * @param deviceExtraInfoList
+	 *            List<DeviceExtraInfo>
+	 */
+	public void setDeviceExtraInfoList(List<DeviceExtraInfo> deviceExtraInfoList) {
+		this.deviceExtraInfoList = deviceExtraInfoList;
 	}
 
 }
