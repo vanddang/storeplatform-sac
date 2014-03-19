cdfc125f40519e1fbc831bf3df449f40292ed29a
이승택 <stakwork@gmail.com>
2014-03-19 11:21:26 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@16154 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/purchase/vo/order/CreateBizPurchaseSacRes.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.purchase.vo.order;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+/**
+ * 
+ * Biz 쿠폰 구매이력 생성 & 쿠폰 발급 요청 응답 VO
+ * 
+ * Updated on : 2014. 3. 19. Updated by : 이승택, nTels.
+ */
+public class CreateBizPurchaseSacRes extends CommonInfo {
+	private static final long serialVersionUID = 1L;
+
+	private String prchsId; // 구매 ID
+	private int count; // 생성된 구매이력 건수
+
+	/**
+	 * @return the prchsId
+	 */
+	public String getPrchsId() {
+		return this.prchsId;
+	}
+
+	/**
+	 * @param prchsId
+	 *            the prchsId to set
+	 */
+	public void setPrchsId(String prchsId) {
+		this.prchsId = prchsId;
+	}
+
+	/**
+	 * @return the count
+	 */
+	public int getCount() {
+		return this.count;
+	}
+
+	/**
+	 * @param count
+	 *            the count to set
+	 */
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+}
---
+++ src/main/java/com/skplanet/storeplatform/sac/client/purchase/vo/order/PurchaseUserKeyInfo.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.purchase.vo.order;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+import com.skplanet.storeplatform.sac.client.purchase.vo.order.CreatePurchaseSacReq.GroupCreateBizPurchase;
+
+/**
+ * 
+ * 구매요청 시, 구매자(혹은 수신자) 의 회원/디바이스 Key 정보 VO
+ * 
+ * Updated on : 2014. 1. 16. Updated by : 이승택, nTels.
+ */
+public class PurchaseUserKeyInfo extends CommonInfo {
+	private static final long serialVersionUID = 1L;
+
+	@NotBlank(groups = { GroupCreateBizPurchase.class })
+	private String userKey;
+	@NotBlank(groups = { GroupCreateBizPurchase.class })
+	private String deviceKey;
+
+	/**
+	 * @return the userKey
+	 */
+	public String getUserKey() {
+		return this.userKey;
+	}
+
+	/**
+	 * @param userKey
+	 *            the userKey to set
+	 */
+	public void setUserKey(String userKey) {
+		this.userKey = userKey;
+	}
+
+	/**
+	 * @return the deviceKey
+	 */
+	public String getDeviceKey() {
+		return this.deviceKey;
+	}
+
+	/**
+	 * @param deviceKey
+	 *            the deviceKey to set
+	 */
+	public void setDeviceKey(String deviceKey) {
+		this.deviceKey = deviceKey;
+	}
+
+}
