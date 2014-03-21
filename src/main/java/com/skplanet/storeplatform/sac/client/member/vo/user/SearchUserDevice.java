f88dc799ec937dde77708f72b7d0e414b4f4abb6
강신완 <@gmail.com>
2014-03-21 08:26:12 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@16801 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/member/vo/user/SearchUserDevice.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/user/SearchUserDevice.java
@@ -3,8 +3,6 @@
  */
 package com.skplanet.storeplatform.sac.client.member.vo.user;
 
-import java.util.List;
-
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
 
@@ -16,20 +14,30 @@ import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
  * Updated on : 2014. 3. 07. Updated by : 강신완, 부르칸.
  */
 @JsonSerialize(include = Inclusion.NON_NULL)
-public class SearchUserDeviceReq extends CommonInfo {
+public class SearchUserDevice extends CommonInfo {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * 디바이스 키.
 	 */
-	private List<SearchUserDevice> searchUserDeviceReqList;
+	private String deviceKey;
 
-	public List<SearchUserDevice> getSearchUserDeviceReqList() {
-		return this.searchUserDeviceReqList;
+	private String userKey;
+
+	public String getDeviceKey() {
+		return this.deviceKey;
 	}
 
-	public void setSearchUserDeviceReqList(List<SearchUserDevice> searchUserDeviceReqList) {
-		this.searchUserDeviceReqList = searchUserDeviceReqList;
+	public void setDeviceKey(String deviceKey) {
+		this.deviceKey = deviceKey;
+	}
+
+	public String getUserKey() {
+		return this.userKey;
+	}
+
+	public void setUserKey(String userKey) {
+		this.userKey = userKey;
 	}
 
 	public static long getSerialversionuid() {
