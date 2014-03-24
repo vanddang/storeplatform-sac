e2b83871623c92c5f10720c97220444f3213d593
이석희 <winterer81@naver.com>
2014-03-24 08:11:07 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@17084 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/display/localsci/sci/service/UpdatePurchaseCountServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/localsci/sci/service/UpdatePurchaseCountServiceImpl.java
@@ -22,6 +22,8 @@ import org.springframework.stereotype.Service;
 import com.skplanet.storeplatform.framework.core.exception.StorePlatformException;
 import com.skplanet.storeplatform.framework.core.persistence.dao.CommonDAO;
 import com.skplanet.storeplatform.sac.client.internal.display.localsci.vo.UpdatePurchaseCountSacReq;
+import com.skplanet.storeplatform.sac.display.common.constant.DisplayConstants;
+import com.skplanet.storeplatform.sac.display.meta.vo.ProductBasicInfo;
 
 /**
  * UpdatePurchaseCount Service 인터페이스(CoreStoreBusiness) 구현체
@@ -48,26 +50,90 @@ public class UpdatePurchaseCountServiceImpl implements UpdatePurchaseCountServic
 	public void updatePurchaseCount(List<UpdatePurchaseCountSacReq> reqList) {
 		Map<String, String> map = null;
 		List<Map> productList = null;
-
 		int prchsCnt = 0;
+		int prodCnt = 0;
+
 		for (int i = 0; i < reqList.size(); i++) {
 			map = new HashMap<String, String>();
-			if (i > 99) {
-				throw new StorePlatformException("SAC_DSP_0007", "100");
+			if (i > 9) {
+				throw new StorePlatformException("SAC_DSP_0007", "10");
 			}
 
 			map.put("tenantId", reqList.get(i).getTenantId());
 			map.put("productId", reqList.get(i).getProductId());
-			map.put("purchaseCount", reqList.get(i).getPurchaseCount().toString());
 
-			prchsCnt = (Integer) this.commonDAO.queryForObject("LocalSci.getPurchaseCount", map);
+			// 상품 기본 정보(상품군) 조회
+			ProductBasicInfo productBasicInfo = this.commonDAO.queryForObject("LocalSci.selectProductInfo", map,
+					ProductBasicInfo.class);
 
-			// 해당 상품의 구매수가 0이면 업데이트 할 구매건수도 0으로 SET
-			if (prchsCnt > 0) {
-				if (this.commonDAO.update("LocalSci.updatePurchaseCount", map) <= 0) {
+			if (productBasicInfo != null) {
+				this.log.debug("----------------------------------------------------------");
+				this.log.debug("svcGrpCd : {}", productBasicInfo.getSvcGrpCd());
+				this.log.debug("getContentsTypeCd : {}", productBasicInfo.getContentsTypeCd());
+				this.log.debug("partProdId : {}", productBasicInfo.getPartProdId());
+				this.log.debug("catalogId : {}", productBasicInfo.getCatalogId());
+				this.log.debug("prodId : {}", productBasicInfo.getProdId());
+				this.log.debug("----------------------------------------------------------");
+
+				map.put("productId", productBasicInfo.getPartProdId());
+
+				// TB_DP_TENANT_PROD_STATS 상품 존재 유무 확인
+				prodCnt = (Integer) this.commonDAO.queryForObject("LocalSci.selectTenantProdStats", map);
+
+				if (prodCnt < 1) {
+					// 대상 상품이 없으면 해당 상품 insert 실행 Insert 시 구매수는 Default 0으로 Set
+					map.put("purchaseCount", "0");
 					this.commonDAO.update("LocalSci.insertPurchaseProd", map);
 				}
+
+				map.put("purchaseCount", reqList.get(i).getPurchaseCount().toString());
+
+				prchsCnt = (Integer) this.commonDAO.queryForObject("LocalSci.getPurchaseCount", map);
+
+				// 해당상품의 구매수 + 업데이트 구매수가 0보다 작으면 현재 상품의 구매 수만큼 -해서 상품구매수를 0으로 되게 SET
+				if (prchsCnt + reqList.get(i).getPurchaseCount() < 0) {
+					Integer resetCount = 0 - prchsCnt; // 상품의 현재 구매수만큼 -해주기 위한 변수
+					map.put("purchaseCount", resetCount.toString());
+				}
+
+				// episode 상품에 대한 update 실행
+				this.commonDAO.update("LocalSci.updatePurchaseCount", map);
+
+				// APP 상품은 Episode 한번만 update
+				if (!DisplayConstants.DP_APP_PROD_SVC_GRP_CD.equals((productBasicInfo.getSvcGrpCd()))) {
+					if (DisplayConstants.DP_MULTIMEDIA_PROD_SVC_GRP_CD.equals((productBasicInfo.getSvcGrpCd()))) {
+						map.put("productId", productBasicInfo.getProdId()); // 멀티미디어 상품일 때 채널 ID를 SET
+					} else if (DisplayConstants.DP_TSTORE_SHOPPING_PROD_SVC_GRP_CD.equals((productBasicInfo
+							.getSvcGrpCd()))) {
+						map.put("productId", productBasicInfo.getCatalogId());// 쇼핑 상품 일때 카탈로그 ID를 SET
+					}
+
+					// TB_DP_TENANT_PROD_STATS 상품 존재 유무 확인
+					prodCnt = (Integer) this.commonDAO.queryForObject("LocalSci.selectTenantProdStats", map);
+
+					if (prodCnt < 1) {
+						// 대상 상품이 없으면 해당 상품 insert 실행 Insert 시 구매수는 Default 0으로 Set
+						map.put("purchaseCount", "0");
+						this.commonDAO.update("LocalSci.insertPurchaseProd", map);
+					}
+
+					map.put("purchaseCount", reqList.get(i).getPurchaseCount().toString());
+
+					prchsCnt = (Integer) this.commonDAO.queryForObject("LocalSci.getPurchaseCount", map);
+
+					// 해당상품의 구매수 + 업데이트 구매수가 0보다 작으면 현재 상품의 구매 수만큼 -해서 상품구매수를 0으로 되게 SET
+					if (prchsCnt + reqList.get(i).getPurchaseCount() < 0) {
+						Integer resetCount = 0 - prchsCnt; // 상품의 현재 구매수만큼 -해주기 위한 변수
+						map.put("purchaseCount", resetCount.toString());
+					}
+
+					// channel 및 catalog Id에 대한 update 실행
+					this.commonDAO.update("LocalSci.updatePurchaseCount", map);
+
+				}
+
 			}
+
 		}
 	}
 }
