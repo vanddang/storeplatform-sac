50067e5141390c764769a6b4058ce10c8d2f416f
양주원 <sden2720@gmail.com>
2014-03-24 04:09:25 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@16973 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/purchase/shopping/service/ShoppingAsyncService.java
+++ src/main/java/com/skplanet/storeplatform/sac/purchase/shopping/service/ShoppingAsyncService.java
@@ -9,6 +9,9 @@
  */
 package com.skplanet.storeplatform.sac.purchase.shopping.service;
 
+import com.skplanet.storeplatform.external.client.shopping.vo.BizCouponReq;
+import com.skplanet.storeplatform.external.client.shopping.vo.BizCouponRes;
+
 //import com.skplanet.storeplatform.external.client.shopping.vo.BizCouponReq;
 //import com.skplanet.storeplatform.external.client.shopping.vo.BizCouponRes;
 
@@ -29,6 +32,6 @@ public interface ShoppingAsyncService {
 	 *            couponUseStatusSacParam
 	 * @return CouponUseStatusSacResult
 	 */
-	// public BizCouponRes getShoppingAsync(BizCouponReq request);
+	public BizCouponRes getShoppingAsync(BizCouponReq request);
 
 }
--- src/main/java/com/skplanet/storeplatform/sac/purchase/shopping/service/ShoppingAsyncServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/purchase/shopping/service/ShoppingAsyncServiceImpl.java
@@ -9,10 +9,21 @@
  */
 package com.skplanet.storeplatform.sac.purchase.shopping.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skplanet.storeplatform.external.client.shopping.vo.BizCouponItem;
+import com.skplanet.storeplatform.external.client.shopping.vo.BizCouponPublish;
+import com.skplanet.storeplatform.external.client.shopping.vo.BizCouponReq;
+import com.skplanet.storeplatform.external.client.shopping.vo.BizCouponRes;
 import com.skplanet.storeplatform.purchase.client.shopping.sci.ShoppingAsyncSCI;
+import com.skplanet.storeplatform.purchase.client.shopping.vo.ShoppingAsyncItemSc;
+import com.skplanet.storeplatform.purchase.client.shopping.vo.ShoppingAsyncListSc;
+import com.skplanet.storeplatform.purchase.client.shopping.vo.ShoppingAsyncReq;
+import com.skplanet.storeplatform.purchase.client.shopping.vo.ShoppingAsyncRes;
 //import com.skplanet.storeplatform.external.client.shopping.vo.BizCouponItem;
 //import com.skplanet.storeplatform.external.client.shopping.vo.BizCouponPublish;
 //import com.skplanet.storeplatform.external.client.shopping.vo.BizCouponReq;
@@ -28,44 +39,57 @@ public class ShoppingAsyncServiceImpl implements ShoppingAsyncService {
 
 	@Autowired
 	private ShoppingAsyncSCI shoppingAsyncSCI;
-	/*
-	 * @Override public BizCouponRes getShoppingAsync(BizCouponReq request) {
-	 * 
-	 * ShoppingAsyncReq reqSc = new ShoppingAsyncReq(); ShoppingAsyncRes resSc = new ShoppingAsyncRes();
-	 * 
-	 * ShoppingAsyncListSc listSc; List<ShoppingAsyncListSc> prchsList = new ArrayList<ShoppingAsyncListSc>();
-	 * 
-	 * ShoppingAsyncItemSc item; List<ShoppingAsyncItemSc> itemList = new ArrayList<ShoppingAsyncItemSc>();
-	 * 
-	 * System.out.println("" + request.getXmlData().getPublish().size()); System.out.println("" +
-	 * request.getXmlData().getPublish().get(0).getItems().size());
-	 * 
-	 * for (BizCouponPublish obj : request.getXmlData().getPublish()) { listSc = new ShoppingAsyncListSc(); itemList =
-	 * new ArrayList<ShoppingAsyncItemSc>();
-	 * 
-	 * listSc.setPrchsId(obj.getPrchsId()); listSc.setAvail_startdate(obj.getAvail_startdate());
-	 * listSc.setAvail_enddate(obj.getAvail_enddate()); listSc.setMdn(obj.getMdn()); listSc.setMdn2(obj.getMdn2());
-	 * 
-	 * for (BizCouponItem objItem : obj.getItems()) { item = new ShoppingAsyncItemSc();
-	 * 
-	 * item.setItemCode(objItem.getItemCode()); item.setPublishCode(objItem.getPublishCode());
-	 * item.setShippingUrl(objItem.getShippingUrl()); item.setExtraData(objItem.getExtraData());
-	 * 
-	 * itemList.add(item); } listSc.setItems(itemList);
-	 * 
-	 * prchsList.add(listSc);
-	 * 
-	 * }
-	 * 
-	 * reqSc.setPublish(prchsList);
-	 * 
-	 * System.out.println("###" + reqSc.getPublish().size()); System.out.println("###" +
-	 * reqSc.getPublish().get(0).getItems().size());
-	 * 
-	 * resSc = this.shoppingAsyncSCI.updateShoppingAsync(reqSc);
-	 * 
-	 * return new BizCouponRes();
-	 * 
-	 * }
-	 */
+
+	@Override
+	public BizCouponRes getShoppingAsync(BizCouponReq request) {
+
+		ShoppingAsyncReq reqSc = new ShoppingAsyncReq();
+		ShoppingAsyncRes resSc = new ShoppingAsyncRes();
+
+		ShoppingAsyncListSc listSc;
+		List<ShoppingAsyncListSc> prchsList = new ArrayList<ShoppingAsyncListSc>();
+
+		ShoppingAsyncItemSc item;
+		List<ShoppingAsyncItemSc> itemList = new ArrayList<ShoppingAsyncItemSc>();
+
+		System.out.println("" + request.getXmlData().getPublish().size());
+		System.out.println("" + request.getXmlData().getPublish().get(0).getItems().size());
+
+		for (BizCouponPublish obj : request.getXmlData().getPublish()) {
+			listSc = new ShoppingAsyncListSc();
+			itemList = new ArrayList<ShoppingAsyncItemSc>();
+
+			listSc.setPrchsId(obj.getPrchsId());
+			listSc.setAvail_startdate(obj.getAvail_startdate());
+			listSc.setAvail_enddate(obj.getAvail_enddate());
+			listSc.setMdn(obj.getMdn());
+			listSc.setMdn2(obj.getMdn2());
+
+			for (BizCouponItem objItem : obj.getItems()) {
+				item = new ShoppingAsyncItemSc();
+
+				item.setItemCode(objItem.getItemCode());
+				item.setPublishCode(objItem.getPublishCode());
+				item.setShippingUrl(objItem.getShippingUrl());
+				item.setExtraData(objItem.getExtraData());
+
+				itemList.add(item);
+			}
+			listSc.setItems(itemList);
+
+			prchsList.add(listSc);
+
+		}
+
+		reqSc.setPublish(prchsList);
+
+		System.out.println("###" + reqSc.getPublish().size());
+		System.out.println("###" + reqSc.getPublish().get(0).getItems().size());
+
+		resSc = this.shoppingAsyncSCI.updateShoppingAsync(reqSc);
+
+		return new BizCouponRes();
+
+	}
+
 }
