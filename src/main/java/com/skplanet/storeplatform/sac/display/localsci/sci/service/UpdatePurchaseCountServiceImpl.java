e7d321425a93d0373156255bc3eaad4519dc509d
김형식 <jade.kimhs@gmail.com>
2014-03-24 09:48:28 +0000

    특가 상품 구매건수 SCI
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@17141 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/display/localsci/sci/service/UpdatePurchaseCountServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/localsci/sci/service/UpdatePurchaseCountServiceImpl.java
@@ -13,6 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,45 +56,91 @@ public class UpdatePurchaseCountServiceImpl implements UpdatePurchaseCountServic
 			if (i > 9) {
 				throw new StorePlatformException("SAC_DSP_0007", "10");
 			}
+			if (!StringUtils.equals(reqList.get(i).getSpcYn(), "Y")) { // 특가 상품 여부 가 아닌경우
+				map.put("tenantId", reqList.get(i).getTenantId());
+				map.put("productId", reqList.get(i).getProductId());
 
-			map.put("tenantId", reqList.get(i).getTenantId());
-			map.put("productId", reqList.get(i).getProductId());
+				// 상품 기본 정보(상품군) 조회
+				ProductBasicInfo productBasicInfo = this.commonDAO.queryForObject("LocalSci.selectProductInfo", map,
+						ProductBasicInfo.class);
 
-			// 상품 기본 정보(상품군) 조회
-			ProductBasicInfo productBasicInfo = this.commonDAO.queryForObject("LocalSci.selectProductInfo", map,
-					ProductBasicInfo.class);
+				if (productBasicInfo != null) {
+					this.log.debug("----------------------------------------------------------");
+					this.log.debug("svcGrpCd : {}", productBasicInfo.getSvcGrpCd());
+					this.log.debug("getContentsTypeCd : {}", productBasicInfo.getContentsTypeCd());
+					this.log.debug("partProdId : {}", productBasicInfo.getPartProdId());
+					this.log.debug("catalogId : {}", productBasicInfo.getCatalogId());
+					this.log.debug("prodId : {}", productBasicInfo.getProdId());
+					this.log.debug("----------------------------------------------------------");
 
-			if (productBasicInfo != null) {
-				this.log.debug("----------------------------------------------------------");
-				this.log.debug("svcGrpCd : {}", productBasicInfo.getSvcGrpCd());
-				this.log.debug("getContentsTypeCd : {}", productBasicInfo.getContentsTypeCd());
-				this.log.debug("partProdId : {}", productBasicInfo.getPartProdId());
-				this.log.debug("catalogId : {}", productBasicInfo.getCatalogId());
-				this.log.debug("prodId : {}", productBasicInfo.getProdId());
-				this.log.debug("----------------------------------------------------------");
+					map.put("productId", productBasicInfo.getPartProdId());
+					this.updatePurchaseCount(map, reqList.get(i).getPurchaseCount()); // Episode에 대한 Update 로직 실행
 
-				map.put("productId", productBasicInfo.getPartProdId());
-				this.updatePurchaseCount(map, reqList.get(i).getPurchaseCount()); // Episode에 대한 Update 로직 실행
+					// APP 상품은 Episode 한번만 update
+					if (!DisplayConstants.DP_APP_PROD_SVC_GRP_CD.equals((productBasicInfo.getSvcGrpCd()))) {
+						if (DisplayConstants.DP_MULTIMEDIA_PROD_SVC_GRP_CD.equals((productBasicInfo.getSvcGrpCd()))) {
+							map.put("productId", productBasicInfo.getProdId()); // 멀티미디어 상품일 때 채널 ID를 SET
+							this.updatePurchaseCount(map, reqList.get(i).getPurchaseCount()); // 채널에 대한 Update 로직 실행
+						} else if (DisplayConstants.DP_TSTORE_SHOPPING_PROD_SVC_GRP_CD.equals((productBasicInfo
+								.getSvcGrpCd()))) {
+							map.put("productId", productBasicInfo.getProdId()); // 쇼핑 상품 일때 Channel ID를 SET
+							this.updatePurchaseCount(map, reqList.get(i).getPurchaseCount()); // 채널에 대한 Update 로직 실행
+							map.put("productId", productBasicInfo.getCatalogId());// 쇼핑 상품 일때 카탈로그 ID를 SET
+							this.updatePurchaseCount(map, reqList.get(i).getPurchaseCount()); // 카탈로그에 대한 Update 로직 실행
+						}
+					}
 
-				// APP 상품은 Episode 한번만 update
-				if (!DisplayConstants.DP_APP_PROD_SVC_GRP_CD.equals((productBasicInfo.getSvcGrpCd()))) {
-					if (DisplayConstants.DP_MULTIMEDIA_PROD_SVC_GRP_CD.equals((productBasicInfo.getSvcGrpCd()))) {
-						map.put("productId", productBasicInfo.getProdId()); // 멀티미디어 상품일 때 채널 ID를 SET
-						this.updatePurchaseCount(map, reqList.get(i).getPurchaseCount()); // 채널에 대한 Update 로직 실행
-					} else if (DisplayConstants.DP_TSTORE_SHOPPING_PROD_SVC_GRP_CD.equals((productBasicInfo
-							.getSvcGrpCd()))) {
-						map.put("productId", productBasicInfo.getProdId()); // 쇼핑 상품 일때 Channel ID를 SET
-						this.updatePurchaseCount(map, reqList.get(i).getPurchaseCount()); // 채널에 대한 Update 로직 실행
-						map.put("productId", productBasicInfo.getCatalogId());// 쇼핑 상품 일때 카탈로그 ID를 SET
-						this.updatePurchaseCount(map, reqList.get(i).getPurchaseCount()); // 카탈로그에 대한 Update 로직 실행
+				}
+			} else { // 특가 상품 여부 가 아닌경우
+				map.put("tenantId", reqList.get(i).getTenantId());
+				map.put("productId", reqList.get(i).getProductId());
+				map.put("purchaseDate", reqList.get(i).getPurchaseDate());
+				int specialProdSize = 3;
+				// 상품 기본 정보(상품군) 조회
+				ProductBasicInfo productBasicInfo = this.commonDAO.queryForObject("LocalSci.selectProductInfo", map,
+						ProductBasicInfo.class);
+
+				if (productBasicInfo != null) {
+					this.log.debug("----------------------------------------------------------");
+					this.log.debug("svcGrpCd : {}", productBasicInfo.getSvcGrpCd());
+					this.log.debug("getContentsTypeCd : {}", productBasicInfo.getContentsTypeCd());
+					this.log.debug("partProdId : {}", productBasicInfo.getPartProdId());
+					this.log.debug("catalogId : {}", productBasicInfo.getCatalogId());
+					this.log.debug("prodId : {}", productBasicInfo.getProdId());
+					this.log.debug("----------------------------------------------------------");
+					if (StringUtils.isEmpty(productBasicInfo.getPartProdId())) {
+						throw new StorePlatformException("SAC_DSP_0002", "partProdId", productBasicInfo.getPartProdId());
+					}
+					if (StringUtils.isEmpty(productBasicInfo.getProdId())) {
+						throw new StorePlatformException("SAC_DSP_0002", "prodId", productBasicInfo.getProdId());
+					}
+					if (StringUtils.isEmpty(productBasicInfo.getCatalogId())) {
+						throw new StorePlatformException("SAC_DSP_0002", "catalogId", productBasicInfo.getCatalogId());
+					}
+
+					for (int kk = 0; kk < specialProdSize; kk++) {
+						if (kk == 0) {
+							map.put("productId", productBasicInfo.getPartProdId());
+						} else if (kk == 1) {
+							map.put("productId", productBasicInfo.getProdId());
+						} else if (kk == 2) {
+							map.put("productId", productBasicInfo.getCatalogId());
+						}
+						this.updateSpecialPurchaseCount(map, reqList.get(i).getPurchaseCount()); // 특가상품 Update 로직 실행
 					}
 				}
-
 			}
-
 		}
 	}
 
+	/**
+	 * 상품 구매 건수 업데이트.
+	 * 
+	 * @param map
+	 *            map
+	 * @param reqPrrchsCnt
+	 *            reqPrrchsCnt
+	 */
 	public void updatePurchaseCount(Map<String, String> map, int reqPrrchsCnt) {
 		int prodCnt = 0;
 		int prchsCnt = 0;
@@ -118,5 +165,39 @@ public class UpdatePurchaseCountServiceImpl implements UpdatePurchaseCountServic
 
 		// channel 및 catalog Id에 대한 update 실행
 		this.commonDAO.update("LocalSci.updatePurchaseCount", map);
+	}
+
+	/**
+	 * 특가 상품 구매 건수 업데이트.
+	 * 
+	 * @param map
+	 *            map
+	 * @param reqPrrchsCnt
+	 *            reqPrrchsCnt
+	 */
+	private void updateSpecialPurchaseCount(Map<String, String> map, int reqPrrchsCnt) {
+		int prodCnt = 0;
+		int prchsCnt = 0;
+		// TB_DP_TENANT_PROD_STATS 상품 존재 유무 확인
+		prodCnt = (Integer) this.commonDAO.queryForObject("LocalSci.selectSpecialProdCount", map);
+
+		if (prodCnt < 1) {
+			// 대상 상품이 없으면 해당 상품 insert 실행 Insert 시 구매수는 Default 0으로 Set
+			map.put("purchaseCount", "0");
+			this.commonDAO.update("LocalSci.insertSpecialPurchaseProd", map);
+		}
+
+		map.put("purchaseCount", Integer.toString(reqPrrchsCnt));
+
+		prchsCnt = (Integer) this.commonDAO.queryForObject("LocalSci.getSpecialPurchaseCount", map);
+
+		// 해당상품의 구매수 + 업데이트 구매수가 0보다 작으면 현재 상품의 구매 수만큼 -해서 상품구매수를 0으로 되게 SET
+		if (prchsCnt + reqPrrchsCnt < 0) {
+			Integer resetCount = 0 - prchsCnt; // 상품의 현재 구매수만큼 -해주기 위한 변수
+			map.put("purchaseCount", resetCount.toString());
+		}
+
+		// channel 및 catalog Id에 대한 update 실행
+		this.commonDAO.update("LocalSci.updateSpecialPurchaseCount", map);
 	}
 }
