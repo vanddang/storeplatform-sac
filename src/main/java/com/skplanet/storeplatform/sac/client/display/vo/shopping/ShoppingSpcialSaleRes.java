3f5d9dd76deba4ac23b4e1724e9bd35325fd08cf
김형식 <jade.kimhs@gmail.com>
2014-03-26 02:51:41 +0000

    쇼핑 상품 API
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@17733 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/shopping/ShoppingSpcialSaleRes.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ import java.util.List;
+
+import com.skplanet.storeplatform.framework.core.annotation.ProtobufMapping;
+lose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.display.vo.shopping;
+
+import java.util.List;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.CommonResponse;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Promotion;
+
+/**
+ * <pre>
+ * 쇼핑 Value Object.
+ * </pre>
+ * 
+ * Created on : 2014-01-02 Created by : 김형식, SK플래닛 Last Updated on : 2014-01-02 Last Updated by : 김형식, SK플래닛
+ */
+public class ShoppingSpcialSaleRes extends CommonInfo {
+	/**
+	 * 
+	 */
+	private CommonResponse commonResponse;
+	private static final long serialVersionUID = 1L;
+	private List<Promotion> promotionList;
+
+	/**
+	 * @return the commonResponse
+	 */
+	public CommonResponse getCommonResponse() {
+		return this.commonResponse;
+	}
+
+	/**
+	 * @param commonResponse
+	 *            the commonResponse to set
+	 */
+	public void setCommonResponse(CommonResponse commonResponse) {
+		this.commonResponse = commonResponse;
+	}
+
+	/**
+	 * @return the promotionList
+	 */
+	public List<Promotion> getPromotionList() {
+		return this.promotionList;
+	}
+
+	/**
+	 * @param promotionList
+	 *            the promotionList to set
+	 */
+	public void setPromotionList(List<Promotion> promotionList) {
+		this.promotionList = promotionList;
+	}
+
+}
