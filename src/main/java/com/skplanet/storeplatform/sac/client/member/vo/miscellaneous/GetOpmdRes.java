0aac944a18e995061a312293fad9ef4372b40e12
김다슬 <daseul428@spteck.co.kr>
2014-03-27 07:00:40 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@18266 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/member/vo/miscellaneous/GetOpmdRes.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/miscellaneous/GetOpmdRes.java
@@ -20,20 +20,20 @@ public class GetOpmdRes extends CommonInfo {
 	 */
 	private String msisdn;
 
-	// EC에서 내려주는 정보 - 공통모듈에 제공하기 위한 파라미터
-	/**
-	 * 고객의 자번호(OPMD MDN).
-	 */
-	private String opmdMdn;
-	/**
-	 * 고객 자번호(OPMD MND) 일시정지 상태.
-	 */
-	private String pauseYN;
-
-	/**
-	 * 고객의 모번호 서비스관리번호.
-	 */
-	private String mobileSvcMngNum;
+	// // EC에서 내려주는 정보 - 공통모듈에 제공하기 위한 파라미터
+	// /**
+	// * 고객의 자번호(OPMD MDN).
+	// */
+	// private String opmdMdn;
+	// /**
+	// * 고객 자번호(OPMD MND) 일시정지 상태.
+	// */
+	// private String pauseYN;
+	//
+	// /**
+	// * 고객의 모번호 서비스관리번호.
+	// */
+	// private String mobileSvcMngNum;
 
 	/**
 	 * @return the msisdn
@@ -50,49 +50,49 @@ public class GetOpmdRes extends CommonInfo {
 		this.msisdn = msisdn;
 	}
 
-	/**
-	 * @return the opmdMdn
-	 */
-	public String getOpmdMdn() {
-		return this.opmdMdn;
-	}
-
-	/**
-	 * @param opmdMdn
-	 *            the opmdMdn to set
-	 */
-	public void setOpmdMdn(String opmdMdn) {
-		this.opmdMdn = opmdMdn;
-	}
-
-	/**
-	 * @return the pauseYN
-	 */
-	public String getPauseYN() {
-		return this.pauseYN;
-	}
-
-	/**
-	 * @param pauseYN
-	 *            the pauseYN to set
-	 */
-	public void setPauseYN(String pauseYN) {
-		this.pauseYN = pauseYN;
-	}
-
-	/**
-	 * @return the mobileSvcMngNum
-	 */
-	public String getMobileSvcMngNum() {
-		return this.mobileSvcMngNum;
-	}
-
-	/**
-	 * @param mobileSvcMngNum
-	 *            the mobileSvcMngNum to set
-	 */
-	public void setMobileSvcMngNum(String mobileSvcMngNum) {
-		this.mobileSvcMngNum = mobileSvcMngNum;
-	}
+	// /**
+	// * @return the opmdMdn
+	// */
+	// public String getOpmdMdn() {
+	// return this.opmdMdn;
+	// }
+	//
+	// /**
+	// * @param opmdMdn
+	// * the opmdMdn to set
+	// */
+	// public void setOpmdMdn(String opmdMdn) {
+	// this.opmdMdn = opmdMdn;
+	// }
+	//
+	// /**
+	// * @return the pauseYN
+	// */
+	// public String getPauseYN() {
+	// return this.pauseYN;
+	// }
+	//
+	// /**
+	// * @param pauseYN
+	// * the pauseYN to set
+	// */
+	// public void setPauseYN(String pauseYN) {
+	// this.pauseYN = pauseYN;
+	// }
+	//
+	// /**
+	// * @return the mobileSvcMngNum
+	// */
+	// public String getMobileSvcMngNum() {
+	// return this.mobileSvcMngNum;
+	// }
+	//
+	// /**
+	// * @param mobileSvcMngNum
+	// * the mobileSvcMngNum to set
+	// */
+	// public void setMobileSvcMngNum(String mobileSvcMngNum) {
+	// this.mobileSvcMngNum = mobileSvcMngNum;
+	// }
 
 }
