5c411c3ad65aa9aaef4b43ae199d44c941d73604
정희원 <joyspring@sk.com>
2014-03-27 05:23:51 +0000

    refs #11660 IAP용 request_product_info API 구현
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@18174 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/display/other/service/OtherPartProductService.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.display.other.service;
+
+import com.skplanet.storeplatform.sac.display.other.vo.PartProduct;
+
+import java.util.List;
+
+/**
+ * <p>
+ * DESC
+ * </p>
+ * Updated on : 2014. 03. 25 Updated by : 정희원, SK 플래닛.
+ */
+public interface OtherPartProductService {
+
+    List<PartProduct> getPartProductList(String aid, String tenantId, String langCd);
+}
