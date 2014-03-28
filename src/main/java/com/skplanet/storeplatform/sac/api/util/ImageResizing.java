e59e7b307ed6e2b2681f8df90edf354fb2524404
김형식 <jade.kimhs@gmail.com>
2014-03-28 03:40:06 +0000

    쇼핑 상품 전처리 기능
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@18502 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/api/util/ImageResizing.java
+++ src/main/java/com/skplanet/storeplatform/sac/api/util/ImageResizing.java
@@ -14,6 +14,8 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.awt.image.PixelGrabber;
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -43,65 +45,76 @@ public class ImageResizing {
 	 * @return boolean
 	 */
 	public boolean resize(File src, File dest, int width, int height) {
-		try {
-			Image srcImg = null;
-			String suffix = src.getName().substring(src.getName().lastIndexOf('.') + 1).toLowerCase();
-			this.log.info("log8881");
-			if (suffix.equals("bmp") || suffix.equals("png") || suffix.equals("gif")) {
+		Image srcImg = null;
+		String suffix = src.getName().substring(src.getName().lastIndexOf('.') + 1).toLowerCase();
+		this.log.info("log8881");
+		if (suffix.equals("bmp") || suffix.equals("png") || suffix.equals("gif")) {
+			try {
 				srcImg = ImageIO.read(src);
-			} else {
-				// BMP가 아닌 경우 ImageIcon을 활용해서 Image 생성
-				// 이렇게 하는 이유는 getScaledInstance를 통해 구한 이미지를
-				// PixelGrabber.grabPixels로 리사이즈 할때
-				// 빠르게 처리하기 위함이다.
+			} catch (IOException e) {
+				return false;
+			}
+		} else {
+			// BMP가 아닌 경우 ImageIcon을 활용해서 Image 생성
+			// 이렇게 하는 이유는 getScaledInstance를 통해 구한 이미지를
+			// PixelGrabber.grabPixels로 리사이즈 할때
+			// 빠르게 처리하기 위함이다.
+			try {
 				srcImg = new ImageIcon(src.toURL()).getImage();
+			} catch (MalformedURLException e) {
+				return false;
 			}
-			this.log.info("log8882");
+		}
+		this.log.info("log8882");
 
-			int srcWidth = srcImg.getWidth(null);
-			int srcHeight = srcImg.getHeight(null);
+		int srcWidth = srcImg.getWidth(null);
+		int srcHeight = srcImg.getHeight(null);
 
-			int destWidth = -1, destHeight = -1;
-			this.log.info("log8883");
-			if (width == SAME) {
-				destWidth = srcWidth;
-			} else if (width > 0) {
-				destWidth = width;
-			}
+		int destWidth = -1, destHeight = -1;
+		this.log.info("log8883");
+		if (width == SAME) {
+			destWidth = srcWidth;
+		} else if (width > 0) {
+			destWidth = width;
+		}
 
-			if (height == SAME) {
-				destHeight = srcHeight;
-			} else if (height > 0) {
-				destHeight = height;
-			}
-			this.log.info("log8884");
-			if (width == RATIO && height == RATIO) {
-				destWidth = srcWidth;
-				destHeight = srcHeight;
-			} else if (width == RATIO) {
-				double ratio = ((double) destHeight) / ((double) srcHeight);
-				destWidth = (int) (srcWidth * ratio);
-			} else if (height == RATIO) {
-				double ratio = ((double) destWidth) / ((double) srcWidth);
-				destHeight = (int) (srcHeight * ratio);
-			}
-			this.log.info("log8885");
-			Image imgTarget = srcImg.getScaledInstance(destWidth, destHeight, Image.SCALE_SMOOTH);
-			int pixels[] = new int[destWidth * destHeight];
-			PixelGrabber pg = new PixelGrabber(imgTarget, 0, 0, destWidth, destHeight, pixels, 0, destWidth);
-			this.log.info("log8886");
+		if (height == SAME) {
+			destHeight = srcHeight;
+		} else if (height > 0) {
+			destHeight = height;
+		}
+		this.log.info("log8884");
+		if (width == RATIO && height == RATIO) {
+			destWidth = srcWidth;
+			destHeight = srcHeight;
+		} else if (width == RATIO) {
+			double ratio = ((double) destHeight) / ((double) srcHeight);
+			destWidth = (int) (srcWidth * ratio);
+		} else if (height == RATIO) {
+			double ratio = ((double) destWidth) / ((double) srcWidth);
+			destHeight = (int) (srcHeight * ratio);
+		}
+		this.log.info("log8885");
+		Image imgTarget = srcImg.getScaledInstance(destWidth, destHeight, Image.SCALE_SMOOTH);
+		int pixels[] = new int[destWidth * destHeight];
+		PixelGrabber pg = new PixelGrabber(imgTarget, 0, 0, destWidth, destHeight, pixels, 0, destWidth);
+		this.log.info("log8886");
+		try {
 			pg.grabPixels();
-			this.log.info("log8887");
-			BufferedImage destImg = new BufferedImage(destWidth, destHeight, BufferedImage.TYPE_INT_RGB);
-			destImg.setRGB(0, 0, destWidth, destHeight, pixels, 0, destWidth);
-			this.log.info("log8888");
-			ImageIO.write(destImg, "jpg", dest);
-			this.log.info("log8889");
-			return true;
-		} catch (Exception e) {
-			this.log.info(e.getMessage());
+		} catch (InterruptedException e) {
 			return false;
 		}
+		this.log.info("log8887");
+		BufferedImage destImg = new BufferedImage(destWidth, destHeight, BufferedImage.TYPE_INT_RGB);
+		destImg.setRGB(0, 0, destWidth, destHeight, pixels, 0, destWidth);
+		this.log.info("log8888");
+		try {
+			ImageIO.write(destImg, "jpg", dest);
+		} catch (IOException e) {
+			return false;
+		}
+		this.log.info("log8889");
+		return true;
 	}
 
 }
