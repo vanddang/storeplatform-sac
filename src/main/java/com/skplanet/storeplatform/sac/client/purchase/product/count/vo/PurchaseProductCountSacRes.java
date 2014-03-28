f91ff05a17859dcf5b5c39caeb9039ce738b69c5
최성우 <cswoo8101@gmail.com>
2014-03-28 04:39:17 +0000

    구매 상품 건수 업데이트 관련 작업
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@18520 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/purchase/product/count/vo/PurchaseProductCountSacRes.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/purchase/product/count/vo/PurchaseProductCountSacRes.java
@@ -20,4 +20,53 @@ public class PurchaseProductCountSacRes extends CommonInfo {
 
 	private static final long serialVersionUID = 1L;
 
+	private Integer totCnt;
+	private Integer successCnt;
+	private Integer failCnt;
+
+	/**
+	 * @return the totCnt
+	 */
+	public Integer getTotCnt() {
+		return this.totCnt;
+	}
+
+	/**
+	 * @param totCnt
+	 *            the totCnt to set
+	 */
+	public void setTotCnt(Integer totCnt) {
+		this.totCnt = totCnt;
+	}
+
+	/**
+	 * @return the successCnt
+	 */
+	public Integer getSuccessCnt() {
+		return this.successCnt;
+	}
+
+	/**
+	 * @param successCnt
+	 *            the successCnt to set
+	 */
+	public void setSuccessCnt(Integer successCnt) {
+		this.successCnt = successCnt;
+	}
+
+	/**
+	 * @return the failCnt
+	 */
+	public Integer getFailCnt() {
+		return this.failCnt;
+	}
+
+	/**
+	 * @param failCnt
+	 *            the failCnt to set
+	 */
+	public void setFailCnt(Integer failCnt) {
+		this.failCnt = failCnt;
+	}
+
 }
