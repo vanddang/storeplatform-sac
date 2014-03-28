06a28fbf4d38d53d19c15b2bb5a4846f20b556b2
이승훈 <naly3512@naver.com>
2014-03-28 08:57:44 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@18692 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/display/category/service/CategorySpecificSongServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/category/service/CategorySpecificSongServiceImpl.java
@@ -163,8 +163,6 @@ public class CategorySpecificSongServiceImpl implements CategorySpecificSongServ
 			List<ProductBasicInfo> productBasicInfoList = this.commonDAO.queryForList(
 					"CategorySpecificProduct.selectProductInfoList", prodIdList, ProductBasicInfo.class);
 
-			this.log.debug("##### parameter cnt : {}", prodIdList.size());
-			this.log.debug("##### selected product basic info cnt : {}", productBasicInfoList.size());
 			if (productBasicInfoList != null) {
 				Map<String, Object> paramMap = new HashMap<String, Object>();
 				paramMap.put("tenantHeader", header.getTenantHeader());
