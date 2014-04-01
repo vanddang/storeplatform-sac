142aa2a0fa6215d07936c092ac344079327430e2
이석희 <winterer81@naver.com>
2014-04-01 09:10:14 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@19402 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/display/vo/personal/RecommendNewMemberProductReq.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/personal/RecommendNewMemberProductReq.java
@@ -22,6 +22,12 @@ public class RecommendNewMemberProductReq extends CommonInfo {
 	private Integer offset; // 시작점 ROW
 	private Integer count; // 페이지당 노출 ROW 수
 
+	private String ebookSprtYn; // eBook 상품 지원여부
+	private String comicSprtYn; // Comic 상품 지원여부
+	private String musicSprtYn; // 음악 상품 지원여부
+	private String videoDrmSprtYn; // VOD 상품 DRM 지원 여부
+	private String sdVideoSprtYn; // VOD 상품 SD 지원 여부
+
 	/**
 	 * @return the tenantId
 	 */
@@ -140,6 +146,81 @@ public class RecommendNewMemberProductReq extends CommonInfo {
 	 */
 	public void setCount(Integer count) {
 		this.count = count;
+	}
+
+	/**
+	 * @return the ebookSprtYn
+	 */
+	public String getEbookSprtYn() {
+		return this.ebookSprtYn;
+	}
+
+	/**
+	 * @param ebookSprtYn
+	 *            the ebookSprtYn to set
+	 */
+	public void setEbookSprtYn(String ebookSprtYn) {
+		this.ebookSprtYn = ebookSprtYn;
+	}
+
+	/**
+	 * @return the comicSprtYn
+	 */
+	public String getComicSprtYn() {
+		return this.comicSprtYn;
+	}
+
+	/**
+	 * @param comicSprtYn
+	 *            the comicSprtYn to set
+	 */
+	public void setComicSprtYn(String comicSprtYn) {
+		this.comicSprtYn = comicSprtYn;
+	}
+
+	/**
+	 * @return the musicSprtYn
+	 */
+	public String getMusicSprtYn() {
+		return this.musicSprtYn;
+	}
+
+	/**
+	 * @param musicSprtYn
+	 *            the musicSprtYn to set
+	 */
+	public void setMusicSprtYn(String musicSprtYn) {
+		this.musicSprtYn = musicSprtYn;
+	}
+
+	/**
+	 * @return the videoDrmSprtYn
+	 */
+	public String getVideoDrmSprtYn() {
+		return this.videoDrmSprtYn;
+	}
+
+	/**
+	 * @param videoDrmSprtYn
+	 *            the videoDrmSprtYn to set
+	 */
+	public void setVideoDrmSprtYn(String videoDrmSprtYn) {
+		this.videoDrmSprtYn = videoDrmSprtYn;
+	}
+
+	/**
+	 * @return the sdVideoSprtYn
+	 */
+	public String getSdVideoSprtYn() {
+		return this.sdVideoSprtYn;
+	}
+
+	/**
+	 * @param sdVideoSprtYn
+	 *            the sdVideoSprtYn to set
+	 */
+	public void setSdVideoSprtYn(String sdVideoSprtYn) {
+		this.sdVideoSprtYn = sdVideoSprtYn;
 	}
 
 }
