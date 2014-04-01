05ad00b1e7b1233d1bd5fa9b379878e881e67beb
유시혁 <syugi1001@gmail.com>
2014-04-01 07:55:46 +0000

    프로비저닝 보완
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@19372 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/display/menu/service/CategoryServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/menu/service/CategoryServiceImpl.java
@@ -148,7 +148,7 @@ public class CategoryServiceImpl implements CategoryService {
 		this.log.debug("카테고리 조회");
 		if (requestVO.getTopMenuId().equals(DisplayConstants.DP_TV_TOP_MENU_ID)
 				|| requestVO.getTopMenuId().equals(DisplayConstants.DP_EBOOK_TOP_MENU_ID)
-				|| requestVO.getTopMenuId().equals(DisplayConstants.DP_COMIC_TOP_MENU_ID)) { // 멀티미디어 카테고리 조회
+				|| requestVO.getTopMenuId().equals(DisplayConstants.DP_COMIC_TOP_MENU_ID)) { // 이북, 코믹, 방송 카테고리 조회
 			this.log.debug("멀티미디어 카테고리 조회");
 			resultList = this.commonDAO.queryForList("MenuCategory.selectMultiSubCategoryList", requestVO, Menu.class);
 		} else if (requestVO.getTopMenuId().equals(DisplayConstants.DP_MOVIE_TOP_MENU_ID)) { // 영화 카테고리 조회
