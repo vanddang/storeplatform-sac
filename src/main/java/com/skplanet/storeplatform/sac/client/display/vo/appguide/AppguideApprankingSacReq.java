3f5f99bd709058066154eeed9bf9fda400eaf5e0
이승훈 <naly3512@naver.com>
2014-04-02 02:48:08 +0000

    프로비저닝 적용
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@19555 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/display/vo/appguide/AppguideApprankingSacReq.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/appguide/AppguideApprankingSacReq.java
@@ -53,6 +53,14 @@ public class AppguideApprankingSacReq extends CommonInfo {
 
 	private String prodCharge; // 유/무료 여부
 
+	private String anyDeviceModelCd; // 가상단말명
+	// TB_CM_DEVICE 가능여부
+	private String ebookSprtYn; // eBook 상품 지원여부
+	private String comicSprtYn; // Comic 상품 지원여부
+	private String musicSprtYn; // 음악 상품 지원여부
+	private String videoDrmSprtYn; // VOD 상품 DRM 지원 여부
+	private String sdVideoSprtYn; // VOD 상품 SD 지원 여부
+
 	/**
 	 * 
 	 * <pre>
@@ -374,6 +382,96 @@ public class AppguideApprankingSacReq extends CommonInfo {
 	 */
 	public void setProdCharge(String prodCharge) {
 		this.prodCharge = prodCharge;
+	}
+
+	/**
+	 * @return the anyDeviceModelCd
+	 */
+	public String getAnyDeviceModelCd() {
+		return this.anyDeviceModelCd;
+	}
+
+	/**
+	 * @param anyDeviceModelCd
+	 *            the anyDeviceModelCd to set
+	 */
+	public void setAnyDeviceModelCd(String anyDeviceModelCd) {
+		this.anyDeviceModelCd = anyDeviceModelCd;
+	}
+
+	/**
+	 * @return the ebookSprtYn
+	 */
+	public String getEbookSprtYn() {
+		return this.ebookSprtYn;
+	}
+
+	/**
+	 * @param ebookSprtYn
+	 *            the ebookSprtYn to set
+	 */
+	public void setEbookSprtYn(String ebookSprtYn) {
+		this.ebookSprtYn = ebookSprtYn;
+	}
+
+	/**
+	 * @return the comicSprtYn
+	 */
+	public String getComicSprtYn() {
+		return this.comicSprtYn;
+	}
+
+	/**
+	 * @param comicSprtYn
+	 *            the comicSprtYn to set
+	 */
+	public void setComicSprtYn(String comicSprtYn) {
+		this.comicSprtYn = comicSprtYn;
+	}
+
+	/**
+	 * @return the musicSprtYn
+	 */
+	public String getMusicSprtYn() {
+		return this.musicSprtYn;
+	}
+
+	/**
+	 * @param musicSprtYn
+	 *            the musicSprtYn to set
+	 */
+	public void setMusicSprtYn(String musicSprtYn) {
+		this.musicSprtYn = musicSprtYn;
+	}
+
+	/**
+	 * @return the videoDrmSprtYn
+	 */
+	public String getVideoDrmSprtYn() {
+		return this.videoDrmSprtYn;
+	}
+
+	/**
+	 * @param videoDrmSprtYn
+	 *            the videoDrmSprtYn to set
+	 */
+	public void setVideoDrmSprtYn(String videoDrmSprtYn) {
+		this.videoDrmSprtYn = videoDrmSprtYn;
+	}
+
+	/**
+	 * @return the sdVideoSprtYn
+	 */
+	public String getSdVideoSprtYn() {
+		return this.sdVideoSprtYn;
+	}
+
+	/**
+	 * @param sdVideoSprtYn
+	 *            the sdVideoSprtYn to set
+	 */
+	public void setSdVideoSprtYn(String sdVideoSprtYn) {
+		this.sdVideoSprtYn = sdVideoSprtYn;
 	}
 
 }
