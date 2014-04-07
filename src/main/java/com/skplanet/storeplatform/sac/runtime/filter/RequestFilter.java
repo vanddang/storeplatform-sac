2991c28b3dd686b63c30d5f4023a7d36e97c10b4
김상호 <lovelykeyplayer@gmail.com>
2014-04-07 07:21:46 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@20566 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/runtime/filter/RequestFilter.java
+++ src/main/java/com/skplanet/storeplatform/sac/runtime/filter/RequestFilter.java
@@ -1,10 +1,16 @@
 package com.skplanet.storeplatform.sac.runtime.filter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import com.skplanet.storeplatform.framework.core.cache.process.GlobalCacheProcessor;
 import com.skplanet.storeplatform.framework.core.util.log.TLogThreadHolder;
 import com.skplanet.storeplatform.framework.integration.filter.AbstractStorePlatformRequestFilter;
 
@@ -26,14 +32,24 @@ public class RequestFilter extends AbstractStorePlatformRequestFilter {
 		TLogThreadHolder.setShuttleLogger(LoggerFactory.getLogger("TLOG_SAC_LOGGER"));
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.skplanet.storeplatform.framework.integration.filter.AbstractStorePlatformRequestFilter#getGUID()
-	 */
 	@Override
-	protected String getGUID(HttpServletRequest request) {
-		return null;
+	protected Map<String, String> getReqeustHeader(HttpServletRequest request) {
+
+		ApplicationContext ac = WebApplicationContextUtils.getWebApplicationContext(this.filterConfig
+				.getServletContext());
+
+		Map<String, String> headerMap = new HashMap<String, String>();
+
+		/* GlobalCacheProcessor useCache 가 false 일 경우 request header 내 'x-sac-use-cache' 에 false 를 셋팅 한다. */
+		GlobalCacheProcessor globalCacheProcessor = (GlobalCacheProcessor) ac.getBean("globalCacheProcessor");
+
+		if (globalCacheProcessor != null) {
+			if (!globalCacheProcessor.isUseCache()) {
+				headerMap.put("x-sac-use-cache", "false");
+			}
+		}
+
+		return headerMap;
 	}
 
 }
