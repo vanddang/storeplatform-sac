2377c32afb9f9ca20ef8af3309eee89842754866
반범진 <vanddang@gtsoft.co.kr>
2014-04-09 04:16:03 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@21067 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/member/vo/user/AuthorizeSaveAndSyncByMacRes.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/user/AuthorizeSaveAndSyncByMacRes.java
@@ -2,6 +2,9 @@ package com.skplanet.storeplatform.sac.client.member.vo.user;
 
 import java.io.Serializable;
 
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+
 import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
 
 /**
@@ -9,6 +12,7 @@ import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
  * 
  * Updated on : 2014. 3. 5. Updated by : 반범진. 지티소프트.
  */
+@JsonSerialize(include = Inclusion.NON_NULL)
 public class AuthorizeSaveAndSyncByMacRes extends CommonInfo implements Serializable {
 
 	/**
