cf4bb436eb168d7d6da57a0898f033b7c9b3cceb
김다슬 <daseul428@spteck.co.kr>
2014-04-10 09:23:48 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@21590 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/member/vo/seller/DetailInformationForProductReq.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/seller/DetailInformationForProductReq.java
@@ -2,14 +2,13 @@ package com.skplanet.storeplatform.sac.client.member.vo.seller;
 
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
-import org.hibernate.validator.constraints.NotBlank;
 
 import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
 
 /**
- * 판매자회원 기본 정보 조회
+ * [REQUEST] 상품상세의 판매자 정보.
  * 
- * Updated on : 2014. 1. 7. Updated by : 한서구, 부르칸.
+ * Updated on : 2014. 4. 10. Updated by : 김다슬, 인크로스.
  */
 @JsonSerialize(include = Inclusion.NON_NULL)
 public class DetailInformationForProductReq extends CommonInfo {
@@ -17,8 +16,9 @@ public class DetailInformationForProductReq extends CommonInfo {
 	private static final long serialVersionUID = 1L;
 
 	/** 판매자 key. */
-	@NotBlank
 	private String sellerKey;
+	/** App ID. */
+	private String aid;
 
 	/**
 	 * @return the sellerKey
@@ -35,4 +35,18 @@ public class DetailInformationForProductReq extends CommonInfo {
 		this.sellerKey = sellerKey;
 	}
 
+	/**
+	 * @return the aid
+	 */
+	public String getAid() {
+		return this.aid;
+	}
+
+	/**
+	 * @param aid
+	 *            the aid to set
+	 */
+	public void setAid(String aid) {
+		this.aid = aid;
+	}
 }
