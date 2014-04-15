4da02be22888618deeb6ea09c1ab45ff9d70c74e
최현식 <designtker@gmail.com>
2014-04-15 08:45:39 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@22735 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/runtime/interceptor/InternalControllerCallCheckingInterceptor.java
+++ src/main/java/com/skplanet/storeplatform/sac/runtime/interceptor/InternalControllerCallCheckingInterceptor.java
@@ -10,34 +10,29 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import com.skplanet.storeplatform.framework.core.exception.StorePlatformException;
 import com.skplanet.storeplatform.framework.core.util.StringUtils;
 
 /**
- * Calss 설명
+ * 내부 컨트롤러콜 체커 인터셉터
  * 
- * Updated on : 2014. 4. 14. Updated by : 홍占썸동, SK 占시뤄옙占쏙옙.
+ * Updated on : 2013. 11. 26. Updated by : 최현식과장, ANB
  */
 public class InternalControllerCallCheckingInterceptor extends HandlerInterceptorAdapter {
 	private static Logger LOGGER = LoggerFactory.getLogger(InternalControllerCallCheckingInterceptor.class);
-
-	public InternalControllerCallCheckingInterceptor() {
-
-	}
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
 		if (StringUtils.equalsIgnoreCase(request.getServerName(), "LOCALHOST")) {
 			LOGGER.info("VAILED REQUEST URL : [url : {}]", request.getRequestURL().toString());
-			// return true;
+			return true;
 
 		} else {
 			LOGGER.info("INVAILED REQUEST URL: [url : {}]", request.getRequestURL().toString());
-			// TODO : 현재 테스트 진행 중
-			// throw new StorePlatformException("SAC_CMN_0003", request.getRequestURL().toString());
+			throw new StorePlatformException("SYS_ERROR_INCORRECT_CALL", request.getRequestURL().toString());
 		}
 
-		return true;
 	}
 
 }
