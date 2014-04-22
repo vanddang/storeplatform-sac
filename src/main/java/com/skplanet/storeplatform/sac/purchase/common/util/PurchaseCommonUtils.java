03456c27305d320650fa16a6d7738fd7ec123020
양주원 <sden2720@gmail.com>
2014-04-22 08:27:01 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@25126 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/purchase/common/util/PurchaseCommonUtils.java
+++ src/main/java/com/skplanet/storeplatform/sac/purchase/common/util/PurchaseCommonUtils.java
@@ -9,15 +9,10 @@
  */
 package com.skplanet.storeplatform.sac.purchase.common.util;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 
-import com.skplanet.storeplatform.framework.core.exception.StorePlatformException;
 import com.skplanet.storeplatform.sac.client.purchase.common.vo.PurchaseHeaderSacReq;
 import com.skplanet.storeplatform.sac.common.header.vo.SacRequestHeader;
 
@@ -30,45 +25,6 @@ import com.skplanet.storeplatform.sac.common.header.vo.SacRequestHeader;
 public class PurchaseCommonUtils {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(PurchaseCommonUtils.class);
-
-	/**
-	 * BindingValidation
-	 * 
-	 * @param bindingResult
-	 *            바인딩결과
-	 * @return void
-	 */
-	public void getBindingValid(BindingResult bindingResult) {
-
-		// 필수값 체크 추후 삭제할거임~~~
-		if (bindingResult.hasErrors()) {
-			List<FieldError> errors = bindingResult.getFieldErrors();
-			for (FieldError error : errors) {
-
-				LOGGER.debug(error.getCode());
-
-				if (error.getCode().equals("NotBlank") || error.getCode().equals("NotEmpty")
-						|| error.getCode().equals("NotNull")) {
-					throw new StorePlatformException("SAC_PUR_0001", error.getField());
-
-				} else if (error.getCode().equals("Pattern")) {
-					// @Pattern 처리시 pattern의 message 셋팅
-					throw new StorePlatformException("SAC_PUR_0003", error.getField(), error.getRejectedValue(),
-							error.getDefaultMessage());
-
-				} else if (error.getCode().equals("Max")) {
-					throw new StorePlatformException("SAC_PUR_0004", error.getField(), error.getRejectedValue(), 100);
-
-				} else if (error.getCode().equals("Min")) {
-					throw new StorePlatformException("SAC_PUR_0005", error.getField(), error.getRejectedValue(), 1);
-
-				} else {
-					throw new StorePlatformException("SAC_PUR_9999");
-				}
-
-			}
-		}
-	}
 
 	/**
 	 * <pre>
