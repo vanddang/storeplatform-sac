53bbfee86bb7fcad9c9fd360b68e7b4b55532380
김경복 <kim.rejoice@gmail.com>
2014-04-28 01:53:59 +0000

    Value - Masking 처리 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@27224 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/member/common/util/ConvertMapperUtils.java
+++ src/main/java/com/skplanet/storeplatform/sac/member/common/util/ConvertMapperUtils.java
@@ -2,10 +2,14 @@ package com.skplanet.storeplatform.sac.member.common.util;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import com.skplanet.storeplatform.framework.core.exception.StorePlatformException;
+import com.skplanet.storeplatform.sac.client.member.vo.seller.CreateReq;
 
 /**
  * Convert Mapper Util
@@ -13,6 +17,10 @@ import com.skplanet.storeplatform.framework.core.exception.StorePlatformExceptio
  * Updated on : 2014. 1. 21. Updated by : 김경복, 부르칸.
  */
 public class ConvertMapperUtils {
+
+	private static final String[] MASKING_PARAMS = { "userPw", "oldPassword", "newPassword", "sellerPw", "subSellerPw",
+			"oldPw", "newPw" };
+	private static final String MASKING_VALUE = "*";
 
 	/**
 	 * <pre>
@@ -53,6 +61,7 @@ public class ConvertMapperUtils {
 	/**
 	 * <pre>
 	 * Object To JSON.
+	 * MASKING_PARAMS : 마스킹이 필요한 필드에 MASKING_VALUE 로 치환
 	 * </pre>
 	 * 
 	 * @param obj
@@ -61,12 +70,62 @@ public class ConvertMapperUtils {
 	 */
 	public static String convertObjectToJson(Object obj) {
 		String returnStr = "";
+		String setMethodPrefix = "set";
+		String methodString = null;
+		String keyAttribute = null;
+		// return value
+		StringBuffer rv = null;
+		List<String> reflectionFields = Arrays.asList(MASKING_PARAMS);
 		try {
+			// Fields
+			Field[] fields = obj.getClass().getDeclaredFields();
+			// Methods
+			Method[] methods = obj.getClass().getMethods();
+
+			// Fields [for - Start]
+			for (int i = 0; i < fields.length; i++) {
+				// 접근 허용
+				fields[i].setAccessible(true);
+				// Field Name
+				keyAttribute = fields[i].getName();
+				methodString = setMethodPrefix + keyAttribute.substring(0, 1).toUpperCase() + keyAttribute.substring(1);
+
+				// Methods [for - Start]
+				for (int j = 0; j < methods.length; j++) {
+					// Set Method
+					if (StringUtils.equals(methodString, methods[j].getName())) {
+
+						// ReflectionFields [for - Start]
+						for (String reflectionField : reflectionFields) {
+
+							// reflectionField
+							if (StringUtils.equals(keyAttribute, reflectionField)) {
+								// masking value
+								if (fields[i].get(obj) instanceof String) {
+									rv = new StringBuffer();
+									String value = (String) fields[i].get(obj);
+									for (int k = 0; k < value.length(); k++) {
+										rv.append(MASKING_VALUE);
+									}
+								}
+								// MASKING_VALUE => invoke
+								obj.getClass().getMethod(methodString, String.class).invoke(obj, rv.toString());
+							}
+
+						}// ReflectionFields [for - End]
+					}
+
+				}// Methods [for - End]
+
+			}// Fields [for - End]
+
 			ObjectMapper mapper = new ObjectMapper();
 			returnStr = mapper.writeValueAsString(obj);
+
 		} catch (Exception e) {
 			throw new StorePlatformException("SAC_MEM_0099", e);
 		}
 		return returnStr;
 	}
+
 }
