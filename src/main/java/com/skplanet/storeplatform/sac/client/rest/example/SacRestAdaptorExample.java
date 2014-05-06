96bf2978a6241d806dc22d982aa860852fbd58d4
서대영 <dale.seo@sk.com>
2014-05-06 09:08:19 +0000

    refs #13961: 상용키로도 SAC Rest Client 사용이 가능하도록 변경 https://project.itopping.co.kr:82/issues/13961
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@30555 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/rest/example/SacRestAdaptorExample.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/rest/example/SacRestAdaptorExample.java
@@ -8,34 +8,34 @@ import com.skplanet.storeplatform.sac.client.rest.error.SacRestClientException;
  * 아래와 같이 어댑터 패턴을 사용하여 SAC 호출 부분을 모듈화하면,
  * 추후 SacRestClient이 변경되었을 때 영향을 최소화 시킬수 있다.
  *
- * Updated on : 2014. 3. 7.
- * Updated by : 서대영, SK 플래닛.
+ * Created on 2014. 3. 7. Created by 서대영, SK 플래닛.
+ * Updated on 2014. 5. 5. Updated by 서대영, SK 플래닛. : 상용키로 변경
  */
+/*
+##### SAC 환경별 Host 정보 (2014-05-06 기준) #####
+DEV => dev-store.sungsu.skplanet.com
+QA => qa-store.store.sungsu.skplanet.com
+REAL => store.sungsu.skplanet.com
+################################################
+*/
 public class SacRestAdaptorExample {
 
 	// 추후 구현체 바꿀 수 있도록 인터페이스 사용 권장
 	private final SacRestClient restClient;
 
 	public SacRestAdaptorExample() {
-		/*
-		 ##### SAC 환경별 Host 정보 (2014-03-07 기준) #####
-		 DEV => devspweb1.sungsu.skplanet.com/sp_sac
-		 QA => qa-store.sungsu.store.skplanet.com/sp_sac
-	     REAL => store.sungsu.store.skplanet.com
-	     ################################################
-		 */
-		String host = "devspweb1.sungsu.skplanet.com/sp_sac";
-		// 테스트키 (유효기간 : 2014-03-24 ~ 2014-04-23)
-		String authKey = "S010c629d2e2fb303ce5664c1ab3bc40a2e";
-		// 인증키 (유효 기간 : 2014-03-24 ~ 2015-03-23)
-		// String authKey = "S01464ce656b142258a5f7441c133fed9ca";
-		// 비밀키
-		// String secret = "c3b578a3d086e45dec6bb1d1cf970e45";
-		// 시스템 아이디 참조 (https://project.itopping.co.kr:82/projects/api_center/wiki/TenantSystem)
+		// 호스트 (프로토콜, 포트 제거할 것!)
+		String host = "localhost:8010";
+		// 인증키 (SAC에 발급 신청, 유효 기간 확인)
+		String authKey = "S018aff2d1ada07fbc284cd3e0b243af437";
+		// 비밀키 (SAC에 발급 신청, 유효 기간 확인)
+		String secret = "6d121baf2a4ae3b74e2b61a3b888c250";
+		// 테넌트 아이디 (https://project.itopping.co.kr:82/projects/api_center/wiki/TenantSystem)
+		String tenantId = "S01";
+		// 시스템 아이디 (https://project.itopping.co.kr:82/projects/api_center/wiki/TenantSystem)
 		String systemId = "S01-06001";
 		// 호스트 및 인증 정보 입력하여 템플릿 생성
-		this.restClient= new SacRestClientApache(host, authKey, systemId);
-		// this.restClient= new SacRestClientApache(host, authKey, secret, systemId);
+		this.restClient= new SacRestClientApache(host, authKey, secret, tenantId, systemId);
 	}
 
 	public <T> T get(String interfaceId, String path, Class<T> responseType, Object param) throws SacRestClientException {
