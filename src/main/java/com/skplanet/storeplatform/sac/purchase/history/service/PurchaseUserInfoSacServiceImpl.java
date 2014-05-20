2bae8e68848345dda63dff82c84d4692695279f4
조용진 <kama624@naver.com>
2014-05-20 04:41:04 +0000

    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@36326 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/purchase/history/service/PurchaseUserInfoSacServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/purchase/history/service/PurchaseUserInfoSacServiceImpl.java
@@ -3,14 +3,25 @@
  */
 package com.skplanet.storeplatform.sac.purchase.history.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skplanet.storeplatform.framework.core.exception.StorePlatformException;
 import com.skplanet.storeplatform.purchase.client.history.sci.PurchaseUserInfoSCI;
 import com.skplanet.storeplatform.purchase.client.history.vo.UserInfoScReq;
 import com.skplanet.storeplatform.purchase.client.history.vo.UserInfoScRes;
+import com.skplanet.storeplatform.sac.client.internal.member.user.sci.SearchUserSCI;
+import com.skplanet.storeplatform.sac.client.internal.member.user.vo.SearchUserDeviceSac;
+import com.skplanet.storeplatform.sac.client.internal.member.user.vo.SearchUserDeviceSacReq;
+import com.skplanet.storeplatform.sac.client.internal.member.user.vo.SearchUserDeviceSacRes;
+import com.skplanet.storeplatform.sac.client.internal.member.user.vo.UserDeviceInfoSac;
 
 /**
  * 구매내역 회원정보 변경.
@@ -26,8 +37,8 @@ public class PurchaseUserInfoSacServiceImpl implements PurchaseUserInfoSacServic
 
 	// @Autowired
 	// private PurchaseMemberRepository purchaseMemberRepository;
-	// @Autowired
-	// private SearchUserSCI searchUserSCI;
+	@Autowired
+	private SearchUserSCI searchUserSCI;
 
 	/**
 	 * 구매내역 회원정보 변경.
@@ -40,34 +51,34 @@ public class PurchaseUserInfoSacServiceImpl implements PurchaseUserInfoSacServic
 	public UserInfoScRes updateUserDevice(UserInfoScReq userInfoScReq) {
 
 		// String tenantId = userInfoScReq.getTenantId();
-		// String newUserKey = userInfoScReq.getNewUserKey();
-		// String newDeviceKey = "";
-		//
-		// if (StringUtils.isBlank(userInfoScReq.getDeviceKey())) {
-		// newDeviceKey = this.purchaseUserInfoSCI.searchDeviceKey(userInfoScReq);
-		// } else {
-		// newDeviceKey = userInfoScReq.getNewDeviceKey();
-		// }
-		// SearchUserDeviceSac searchUserDeviceSac = new SearchUserDeviceSac();
-		// searchUserDeviceSac.setUserKey(newUserKey);
-		// searchUserDeviceSac.setDeviceKey(newDeviceKey);
-		//
-		// List<SearchUserDeviceSac> searchUserDeviceSacList = new ArrayList<SearchUserDeviceSac>();
-		// searchUserDeviceSacList.add(searchUserDeviceSac);
-		//
-		// SearchUserDeviceSacReq searchUserDeviceSacReq = new SearchUserDeviceSacReq();
-		// searchUserDeviceSacReq.setSearchUserDeviceReqList(searchUserDeviceSacList);
-		//
-		// SearchUserDeviceSacRes searchUserDeviceSacRes = null;
-		// try {
-		// searchUserDeviceSacRes = this.searchUserSCI.searchUserByDeviceKey(searchUserDeviceSacReq);
-		// Map<String, UserDeviceInfoSac> userDeviceInfoMap = searchUserDeviceSacRes.getUserDeviceInfo();
-		// if (userDeviceInfoMap == null || (userDeviceInfoMap.containsKey(newDeviceKey) == false)) {
-		// throw new StorePlatformException("SAC_PUR_4106", newUserKey, newDeviceKey);
-		// }
-		// } catch (StorePlatformException e) {
-		// throw new StorePlatformException("SAC_PUR_4106", newUserKey, newDeviceKey);
-		// }
+		String newUserKey = userInfoScReq.getNewUserKey();
+		String newDeviceKey = "";
+
+		if (StringUtils.isBlank(userInfoScReq.getDeviceKey())) {
+			newDeviceKey = this.purchaseUserInfoSCI.searchDeviceKey(userInfoScReq);
+		} else {
+			newDeviceKey = userInfoScReq.getNewDeviceKey();
+		}
+		SearchUserDeviceSac searchUserDeviceSac = new SearchUserDeviceSac();
+		searchUserDeviceSac.setUserKey(newUserKey);
+		searchUserDeviceSac.setDeviceKey(newDeviceKey);
+
+		List<SearchUserDeviceSac> searchUserDeviceSacList = new ArrayList<SearchUserDeviceSac>();
+		searchUserDeviceSacList.add(searchUserDeviceSac);
+
+		SearchUserDeviceSacReq searchUserDeviceSacReq = new SearchUserDeviceSacReq();
+		searchUserDeviceSacReq.setSearchUserDeviceReqList(searchUserDeviceSacList);
+
+		SearchUserDeviceSacRes searchUserDeviceSacRes = null;
+		try {
+			searchUserDeviceSacRes = this.searchUserSCI.searchUserByDeviceKey(searchUserDeviceSacReq);
+			Map<String, UserDeviceInfoSac> userDeviceInfoMap = searchUserDeviceSacRes.getUserDeviceInfo();
+			if (userDeviceInfoMap == null || (userDeviceInfoMap.containsKey(newDeviceKey) == false)) {
+				throw new StorePlatformException("SAC_PUR_4106", newUserKey, newDeviceKey);
+			}
+		} catch (StorePlatformException e) {
+			throw new StorePlatformException("SAC_PUR_4106", newUserKey, newDeviceKey);
+		}
 
 		return this.purchaseUserInfoSCI.updateUserDevice(userInfoScReq);
 	}
