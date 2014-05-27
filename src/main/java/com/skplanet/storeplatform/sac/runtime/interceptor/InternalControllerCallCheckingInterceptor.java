e739cbf47038909010d23f3603e6fe190bf9ce2c
김상호 <lovelykeyplayer@gmail.com>
2014-05-27 07:53:46 +0000

    INTERNAL CALL 허용 IP 로직 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@40801 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/runtime/interceptor/InternalControllerCallCheckingInterceptor.java
+++ src/main/java/com/skplanet/storeplatform/sac/runtime/interceptor/InternalControllerCallCheckingInterceptor.java
@@ -16,15 +16,17 @@ import com.skplanet.storeplatform.framework.core.util.StringUtils;
 /**
  * 내부 컨트롤러콜 체커 인터셉터
  * 
- * Updated on : 2013. 11. 26. Updated by : 최현식과장, ANB
+ * Updated on : 2013. 11. 26. Updated by : 최현식과장, ANB Updated on : 2014. 05. 27. Updated by : 김상호, ANB
  */
 public class InternalControllerCallCheckingInterceptor extends HandlerInterceptorAdapter {
 	private static Logger LOGGER = LoggerFactory.getLogger(InternalControllerCallCheckingInterceptor.class);
 
+	private String acceptRemoteAddr;
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-		if (StringUtils.equalsIgnoreCase(request.getServerName(), "LOCALHOST")) {
+		if (this.isAccept(request)) {
 			LOGGER.info("VAILED REQUEST URL : [url : {}]", request.getRequestURL().toString());
 			return true;
 
@@ -33,6 +35,49 @@ public class InternalControllerCallCheckingInterceptor extends HandlerIntercepto
 			throw new StorePlatformException("SYS_ERROR_INCORRECT_CALL", request.getRequestURL().toString());
 		}
 
+	}
+
+	/**
+	 * <pre>
+	 * Internal call 을 통과 시킬 서버 목록을 셋팅.
+	 * </pre>
+	 * 
+	 * @param acceptRemoteAddr
+	 *            acceptRemoteAddr
+	 */
+	public void setAcceptRemoteAddr(String acceptRemoteAddr) {
+		this.acceptRemoteAddr = acceptRemoteAddr;
+	}
+
+	/**
+	 * <pre>
+	 * Internal call 통과 여부.
+	 * </pre>
+	 * 
+	 * @param request
+	 *            HttpServletRequest
+	 * @return boolean boolean
+	 */
+	private boolean isAccept(HttpServletRequest request) {
+
+		String callServerName = request.getServerName();
+		String remoteAddr = request.getRemoteAddr();
+
+		if (StringUtils.equalsIgnoreCase(callServerName, "LOCALHOST")) {
+			return true;
+		}
+
+		if (this.acceptRemoteAddr != null) {
+			for (String serverName : this.acceptRemoteAddr.split(",")) {
+				if (StringUtils.equalsIgnoreCase(remoteAddr, serverName)) {
+					LOGGER.info("VAILED REMOTE ADDRESS [{}]", remoteAddr);
+					return true;
+				}
+			}
+			LOGGER.info("INVAILED REMOTE ADDRESS [{}]", remoteAddr);
+		}
+
+		return false;
 	}
 
 }
