53e9e30463cbb7e665f7d4426d530cfd6efb5b16
임근대 <kdlim@sk.com>
2014-05-28 22:47:43 +0000

    VM 버전 정보 조회 Util - 방어 코드 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@41807 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/test/java/com/skplanet/storeplatform/sac/display/common/DisplayCommonUtilTest.java
+++ src/test/java/com/skplanet/storeplatform/sac/display/common/DisplayCommonUtilTest.java
@@ -19,5 +19,33 @@ public class DisplayCommonUtilTest {
 		System.out.println("mimeType="+mimeType);
 		assertEquals("video/mp4", mimeType);
 	}
+	@Test
+	public void getOsVer() {
+		String os = "Android/4.0.4";
+		//String result = "4.0";
+		String osVer = DisplayCommonUtil.getOsVer(os);
+		System.out.println("os="+os+", osVer="+osVer);
+		assertEquals(osVer, "4.0");
+	}
 	
+	@Test
+	public void getOsVer_nullOrEmptyCheck() {
+		String os = "";
+		String osVer = DisplayCommonUtil.getOsVer(os);
+		System.out.println("os="+os+", osVer="+osVer);
+		assertEquals(osVer, osVer);
+		
+		os = null;
+		osVer = DisplayCommonUtil.getOsVer(os);
+		System.out.println("os="+os+", osVer="+osVer);
+		assertEquals(osVer, os);
+	}
+
+	@Test
+	public void getOsVer_2() {
+		String os = "Android/";
+		//String result = "4.0";
+		String osVer = DisplayCommonUtil.getOsVer(os);
+		assertEquals(osVer, os);
+	}
 }
