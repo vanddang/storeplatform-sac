bd4751cac68ca62fff0ec8b366599d4925fe9f1b
조용진 <kama624@naver.com>
2014-06-10 02:43:38 +0000

    회원정보 업데이트 방어로직 제거
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@45204 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/purchase/history/service/PurchaseUserInfoSacServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/purchase/history/service/PurchaseUserInfoSacServiceImpl.java
@@ -3,26 +3,15 @@
  */
 package com.skplanet.storeplatform.sac.purchase.history.service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.skplanet.storeplatform.framework.core.exception.StorePlatformException;
 import com.skplanet.storeplatform.purchase.client.history.sci.PurchaseUserInfoSCI;
 import com.skplanet.storeplatform.purchase.client.history.vo.UserInfoScReq;
 import com.skplanet.storeplatform.purchase.client.history.vo.UserInfoScRes;
 import com.skplanet.storeplatform.sac.client.internal.member.user.sci.SearchUserSCI;
-import com.skplanet.storeplatform.sac.client.internal.member.user.vo.SearchUserDeviceSac;
-import com.skplanet.storeplatform.sac.client.internal.member.user.vo.SearchUserDeviceSacReq;
-import com.skplanet.storeplatform.sac.client.internal.member.user.vo.SearchUserDeviceSacRes;
-import com.skplanet.storeplatform.sac.client.internal.member.user.vo.UserDeviceInfoSac;
-import com.skplanet.storeplatform.sac.purchase.constant.PurchaseConstants;
 
 /**
  * 구매내역 회원정보 변경.
@@ -48,46 +37,46 @@ public class PurchaseUserInfoSacServiceImpl implements PurchaseUserInfoSacServic
 	 */
 	@Override
 	public UserInfoScRes updateUserDevice(UserInfoScReq userInfoScReq) {
-
+		// 20140609 과부하 문제로 주석처리
 		// String tenantId = userInfoScReq.getTenantId();
-		String newUserKey = userInfoScReq.getNewUserKey();
-		String newDeviceKey = "";
+		// String newUserKey = userInfoScReq.getNewUserKey();
+		// String newDeviceKey = "";
 
-		if (StringUtils.isBlank(userInfoScReq.getDeviceKey())) {
-			newDeviceKey = this.purchaseUserInfoSCI.searchDeviceKey(userInfoScReq);
-
-			if (StringUtils.isBlank(newDeviceKey)) {
-				UserInfoScRes userInfoScRes = new UserInfoScRes();
-				userInfoScRes.setCount(0);
-				return userInfoScRes;
-			}
-		} else {
-			newDeviceKey = userInfoScReq.getNewDeviceKey();
-		}
-		SearchUserDeviceSac searchUserDeviceSac = new SearchUserDeviceSac();
-		searchUserDeviceSac.setUserKey(newUserKey);
-		searchUserDeviceSac.setDeviceKey(newDeviceKey);
-
-		List<SearchUserDeviceSac> searchUserDeviceSacList = new ArrayList<SearchUserDeviceSac>();
-		searchUserDeviceSacList.add(searchUserDeviceSac);
-
-		SearchUserDeviceSacReq searchUserDeviceSacReq = new SearchUserDeviceSacReq();
-		searchUserDeviceSacReq.setSearchUserDeviceReqList(searchUserDeviceSacList);
-
-		SearchUserDeviceSacRes searchUserDeviceSacRes = null;
-		try {
-			searchUserDeviceSacRes = this.searchUserSCI.searchUserByDeviceKey(searchUserDeviceSacReq);
-			Map<String, UserDeviceInfoSac> userDeviceInfoMap = searchUserDeviceSacRes.getUserDeviceInfo();
-			if (userDeviceInfoMap == null || (userDeviceInfoMap.containsKey(newDeviceKey) == false)) {
-				throw new StorePlatformException("SAC_PUR_4106", newUserKey, newDeviceKey);
-			}
-		} catch (StorePlatformException e) {
-			if (StringUtils.equals(e.getCode(), PurchaseConstants.SACINNER_MEMBER_RESULT_NOTFOUND)) {
-				throw new StorePlatformException("SAC_PUR_4106", newUserKey, newDeviceKey);
-			} else {
-				throw e;
-			}
-		}
+		// if (StringUtils.isBlank(userInfoScReq.getDeviceKey())) {
+		// newDeviceKey = this.purchaseUserInfoSCI.searchDeviceKey(userInfoScReq);
+		//
+		// if (StringUtils.isBlank(newDeviceKey)) {
+		// UserInfoScRes userInfoScRes = new UserInfoScRes();
+		// userInfoScRes.setCount(0);
+		// return userInfoScRes;
+		// }
+		// } else {
+		// newDeviceKey = userInfoScReq.getNewDeviceKey();
+		// }
+		// SearchUserDeviceSac searchUserDeviceSac = new SearchUserDeviceSac();
+		// searchUserDeviceSac.setUserKey(newUserKey);
+		// searchUserDeviceSac.setDeviceKey(newDeviceKey);
+		//
+		// List<SearchUserDeviceSac> searchUserDeviceSacList = new ArrayList<SearchUserDeviceSac>();
+		// searchUserDeviceSacList.add(searchUserDeviceSac);
+		//
+		// SearchUserDeviceSacReq searchUserDeviceSacReq = new SearchUserDeviceSacReq();
+		// searchUserDeviceSacReq.setSearchUserDeviceReqList(searchUserDeviceSacList);
+		//
+		// SearchUserDeviceSacRes searchUserDeviceSacRes = null;
+		// try {
+		// searchUserDeviceSacRes = this.searchUserSCI.searchUserByDeviceKey(searchUserDeviceSacReq);
+		// Map<String, UserDeviceInfoSac> userDeviceInfoMap = searchUserDeviceSacRes.getUserDeviceInfo();
+		// if (userDeviceInfoMap == null || (userDeviceInfoMap.containsKey(newDeviceKey) == false)) {
+		// throw new StorePlatformException("SAC_PUR_4106", newUserKey, newDeviceKey);
+		// }
+		// } catch (StorePlatformException e) {
+		// if (StringUtils.equals(e.getCode(), PurchaseConstants.SACINNER_MEMBER_RESULT_NOTFOUND)) {
+		// throw new StorePlatformException("SAC_PUR_4106", newUserKey, newDeviceKey);
+		// } else {
+		// throw e;
+		// }
+		// }
 
 		return this.purchaseUserInfoSCI.updateUserDevice(userInfoScReq);
 	}
