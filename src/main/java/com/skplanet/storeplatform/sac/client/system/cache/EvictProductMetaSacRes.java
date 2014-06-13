b6fff0717ae4433476865b8143797eca417b5231
정희원 <joyspring@sk.com>
2014-06-13 09:07:06 +0000

    CacheEvict용 Client 생성.
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@46648 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/system/cache/EvictProductMetaSacRes.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.system.cache;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+/**
+ * <p>
+ * DESC
+ * </p>
+ * Updated on : 2014. 06. 13 Updated by : 정희원, SK 플래닛.
+ */
+public class EvictProductMetaSacRes extends CommonInfo {
+
+    private static final long serialVersionUID = 1L;
+
+    String prodType;
+
+    String prodId;
+
+    public String getProdType() {
+        return prodType;
+    }
+
+    public void setProdType(String prodType) {
+        this.prodType = prodType;
+    }
+
+    public String getProdId() {
+        return prodId;
+    }
+
+    public void setProdId(String prodId) {
+        this.prodId = prodId;
+    }
+}
