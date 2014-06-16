9a1e210fdc8935c0c5904c3e9b2719a12ec1ec9b
서대영 <dale.seo@sk.com>
2014-06-16 07:29:48 +0000

    refs #17141: Routing 전처리 보완 (Properties 하드코딩 -> DB연동) https://project.itopping.co.kr:82/issues/17141
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@46960 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/test/java/com/skplanet/storeplatform/sac/runtime/extend/url/SacExternalUrlBuilderTest.java
@@ -0,0 +1,49 @@
+package com.skplanet.storeplatform.sac.runtime.extend.url;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.skplanet.storeplatform.sac.runtime.common.vo.Bypass;
+import com.skplanet.storeplatform.sac.runtime.common.vo.Component;
+
+public class SacExternalUrlBuilderTest {
+
+	@Test
+	public void testBuildLocal() {
+		Bypass bypass = new Bypass("0050001");
+		bypass.setPath("/nate/search");
+
+		Component component = new Component("005");
+		component.setProtocolNm("http");
+		component.setDomain("localhost");
+		component.setPort(8210);
+		bypass.setComponent(component);
+
+		UriComponentsBuilder builder = SacExternalUrlBuilder.buildUrl(bypass);
+		String actual = builder.build().toString();
+		System.out.println("# testBuildLocal() :" + actual);
+		String expected = "http://localhost:8210/nate/search";
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testBuildProd() {
+		Bypass bypass = new Bypass("0050001");
+		bypass.setPath("/nate/search");
+
+		Component component = new Component("005");
+		component.setProtocolNm("http");
+		component.setDomain("ec.store.sungsu.skplanet.com");
+		component.setPort(80);
+		bypass.setComponent(component);
+
+		UriComponentsBuilder builder = SacExternalUrlBuilder.buildUrl(bypass);
+		String actual = builder.build().toString();
+		System.out.println("# testBuildProd() :" + actual);
+		String expected = "http://ec.store.sungsu.skplanet.com:80/nate/search";
+		assertEquals(expected, actual);
+	}
+
+}
