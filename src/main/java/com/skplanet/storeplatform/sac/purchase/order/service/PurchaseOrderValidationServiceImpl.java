5420aa7ff8de76cf117c08dadd65f223f5e7bb57
이승택 <stakwork@gmail.com>
2014-06-26 08:48:51 +0000

    ref #18034 : T-Freemium 19금 상품에 대한 실명(성인)인증 없이 처리  - T혜택 이벤트 유입경로도 T프리미엄으로 처리
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@50411 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/purchase/order/service/PurchaseOrderValidationServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/purchase/order/service/PurchaseOrderValidationServiceImpl.java
@@ -648,8 +648,11 @@ public class PurchaseOrderValidationServiceImpl implements PurchaseOrderValidati
 					continue;
 				} else {
 					// T-Freemium 19금 상품에 대한 실명(성인)인증 없이 처리 : 2014.06.25 반영
+					// T-Freemium 은 OR000420, OR000413 2개의 요청경로로 유입됨.
 					if (StringUtils.equals(purchaseOrderInfo.getPrchsReqPathCd(),
-							PurchaseConstants.PRCHS_REQ_PATH_T_FREEMIUM) == false) {
+							PurchaseConstants.PRCHS_REQ_PATH_T_FREEMIUM) == false
+							&& StringUtils.equals(purchaseOrderInfo.getPrchsReqPathCd(),
+									PurchaseConstants.PRCHS_REQ_PATH_T_BENEFIT_EVENT) == false) {
 						throw new StorePlatformException("SAC_PUR_4105");
 					}
 					// // T-Freemium 19금 상품에 대한 실명(성인)인증 없이 처리 : 2014.06.25 반영
