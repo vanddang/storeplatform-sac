b2029ca99b9fd3c25be44116a2a702000e097413
이승택 <stakwork@gmail.com>
2014-07-17 09:20:53 +0000

    구매완료Noti연동방식변경: 결제처리 결과에 구매완료Noti 항목들 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@53910 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/purchase/vo/order/NotifyPaymentSacRes.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/purchase/vo/order/NotifyPaymentSacRes.java
@@ -22,11 +22,11 @@ public class NotifyPaymentSacRes extends CommonInfo {
 
 	private String prchsId; // 구매 ID
 	private int count; // 처리된 결제수단 갯수
-	// private String userKey; // 상품 보유자 회원 Key
-	// private String deviceKey; // 상품 보유자 디바이스 Key
-	// private String prchsDt; // 구매일시
-	// private String type; // 구매완료 Noti 타입: 01-일반결제
-	// private String publishType; // 쇼핑쿠폰 발행 처리 타입 : 01-동기 (또는 쇼핑 이외 상품), 02-비동기
+	private String userKey; // 상품 보유자 회원 Key
+	private String deviceKey; // 상품 보유자 디바이스 Key
+	private String prchsDt; // 구매일시
+	private String type; // 구매완료 Noti 타입: 01-일반결제
+	private String publishType; // 쇼핑쿠폰 발행 처리 타입 : 01-동기 (또는 쇼핑 이외 상품), 02-비동기
 
 	/**
 	 */
@@ -72,6 +72,81 @@ public class NotifyPaymentSacRes extends CommonInfo {
 	 */
 	public void setCount(int count) {
 		this.count = count;
+	}
+
+	/**
+	 * @return the userKey
+	 */
+	public String getUserKey() {
+		return this.userKey;
+	}
+
+	/**
+	 * @param userKey
+	 *            the userKey to set
+	 */
+	public void setUserKey(String userKey) {
+		this.userKey = userKey;
+	}
+
+	/**
+	 * @return the deviceKey
+	 */
+	public String getDeviceKey() {
+		return this.deviceKey;
+	}
+
+	/**
+	 * @param deviceKey
+	 *            the deviceKey to set
+	 */
+	public void setDeviceKey(String deviceKey) {
+		this.deviceKey = deviceKey;
+	}
+
+	/**
+	 * @return the prchsDt
+	 */
+	public String getPrchsDt() {
+		return this.prchsDt;
+	}
+
+	/**
+	 * @param prchsDt
+	 *            the prchsDt to set
+	 */
+	public void setPrchsDt(String prchsDt) {
+		this.prchsDt = prchsDt;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return this.type;
+	}
+
+	/**
+	 * @param type
+	 *            the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/**
+	 * @return the publishType
+	 */
+	public String getPublishType() {
+		return this.publishType;
+	}
+
+	/**
+	 * @param publishType
+	 *            the publishType to set
+	 */
+	public void setPublishType(String publishType) {
+		this.publishType = publishType;
 	}
 
 }
