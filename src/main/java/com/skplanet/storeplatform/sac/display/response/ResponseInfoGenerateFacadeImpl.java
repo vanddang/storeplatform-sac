69561ae1c5ea900cfed58ae59cb4d8bc98ba1bea
임근대 <kdlim@sk.com>
2014-08-15 04:55:04 +0000

    Tstore 멤버쉽 캐쉬 무료 InApp 상품 적립율 정보 노출
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@58265 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/display/response/ResponseInfoGenerateFacadeImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/response/ResponseInfoGenerateFacadeImpl.java
@@ -1172,7 +1172,7 @@ public class ResponseInfoGenerateFacadeImpl implements ResponseInfoGenerateFacad
         	// 앱상품 && 인앱상품이 존재하면 노출
         	// 인앱 지원 여부 참조. AppInfoGeneratorImpl.generateSupportList
          	if(StringUtils.isEmpty(metaInfo.getPartParentClsfCd())) {
-         		mileageInfo = null;
+         		mileageInfo = new MileageInfo();
          	} 
          }
         
