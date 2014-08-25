03f6e897a778e6a55e18083245825dfa73a58115
반범진 <vanddang@gtsoft.co.kr>
2014-08-25 09:05:55 +0000

    refs #21176DCD 가입 API 추가 - EC UAPS연동 에러 스킵처리
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@59877 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/member/miscellaneous/service/MiscellaneousServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/member/miscellaneous/service/MiscellaneousServiceImpl.java
@@ -916,37 +916,37 @@ public class MiscellaneousServiceImpl implements MiscellaneousService {
 			uapsReq.setDeviceId(devideId);
 			uapsReq.setType("mdn");
 
+			UserEcRes uapsRes = null;
 			try {
-
-				UserEcRes uapsRes = this.uapsSCI.getMappingInfo(uapsReq);
-				if (uapsRes != null) {
-					if (StringUtils.isNotBlank(uapsRes.getSvcMngNum())) {
-						svcMngNum = uapsRes.getSvcMngNum();
-
-						DCDInfo dcdInfo = new DCDInfo();
-						dcdInfo.setRegChannel(systemId);
-						dcdInfo.setTenantID(tenantId);
-						dcdInfo.setEntryClass(regCd);
-						dcdInfo.setServiceNumber(svcMngNum);
-						dcdInfo.setDeviceID(devideId);
-						dcdInfo.setRegDeviceID(null);
-						dcdInfo.setPriorityClass("0");
-						dcdInfo.setProductID(prodId);
-
-						CreateDCDRequest createDcdReq = new CreateDCDRequest();
-						createDcdReq.setCommonRequest(commonRequest);
-						createDcdReq.setDCDInfo(dcdInfo);
-						this.userSCI.createDCD(createDcdReq);
-					} else {
-						// else svcMngNum 존재하지 않을 경우
-						LOGGER.info("## svcMngNum is not exists : " + devideId);
-					}
-				}
-
+				uapsRes = this.uapsSCI.getMappingInfo(uapsReq);
 			} catch (StorePlatformException e) {
-				// ignore
+				LOGGER.info("## {} createDCD uaps error : {} {}", devideId, e.getErrorInfo().getCode(), e
+						.getErrorInfo().getMessage());
 			}
 
+			if (uapsRes != null) {
+				if (StringUtils.isNotBlank(uapsRes.getSvcMngNum())) {
+					svcMngNum = uapsRes.getSvcMngNum();
+
+					DCDInfo dcdInfo = new DCDInfo();
+					dcdInfo.setRegChannel(systemId);
+					dcdInfo.setTenantID(tenantId);
+					dcdInfo.setEntryClass(regCd);
+					dcdInfo.setServiceNumber(svcMngNum);
+					dcdInfo.setDeviceID(devideId);
+					dcdInfo.setRegDeviceID(null);
+					dcdInfo.setPriorityClass("0");
+					dcdInfo.setProductID(prodId);
+
+					CreateDCDRequest createDcdReq = new CreateDCDRequest();
+					createDcdReq.setCommonRequest(commonRequest);
+					createDcdReq.setDCDInfo(dcdInfo);
+					this.userSCI.createDCD(createDcdReq);
+				} else {
+					// else svcMngNum 존재하지 않을 경우
+					LOGGER.info("## {} createDCD svcMngNum is not exists : ", devideId);
+				}
+			}
 		}
 
 		CreateDCDRes res = new CreateDCDRes();
