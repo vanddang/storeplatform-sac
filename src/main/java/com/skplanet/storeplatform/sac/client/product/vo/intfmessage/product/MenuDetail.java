44ffe6313ab7842d06efba8ed2bf4fa9f3a511cb
서대영 <dale.seo@sk.com>
2014-10-08 12:01:58 +0000

    refs #24702: 메뉴 카테고리 조회 API 응답에 일반/장르 구분 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@65471 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/product/vo/intfmessage/product/MenuDetail.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/product/vo/intfmessage/product/MenuDetail.java
@@ -56,6 +56,7 @@ public class MenuDetail extends CommonInfo {
 	private Integer lnbFileSize;
 	private String preCategoryInfo;
 	private String categoryMenuYn;
+	private String section;
 
 	/**
 	 * 
@@ -830,6 +831,32 @@ public class MenuDetail extends CommonInfo {
 	 */
 	public void setCategoryMenuYn(String categoryMenuYn) {
 		this.categoryMenuYn = categoryMenuYn;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 일반 장르 구분 (장르 : genre, 일반 : null) - 이북에서만 의미
+	 * </pre>
+	 * 
+	 * @param section
+	 *            String
+	 */
+	public String getSection() {
+		return section;
+	}
+
+	/**
+	 * 
+	 * <pre>
+	 * 일반 장르 구분 (장르 : genre, 일반 : null) - 이북에서만 의미
+	 * </pre>
+	 * 
+	 * @param section
+	 *            String
+	 */
+	public void setSection(String section) {
+		this.section = section;
 	}
 
 }
