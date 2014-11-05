184f6e7e99ae4623270b2003f15896b13fcbe461
김경복 <kim.rejoice@gmail.com>
2014-11-05 06:57:13 +0000

    회원 정보 조회 v2 응답 추가 - 만나이 (적용일 : 11월 26일)
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@70075 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/member/vo/common/UserInfo.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/common/UserInfo.java
@@ -208,323 +208,610 @@ public class UserInfo extends CommonInfo {
 	 */
 	private String prodExpoLevl;
 
+	/** 회원의 만나이. */
+	private String realAge;
+
 	/* 사용자 부가정보 */
 	private List<UserExtraInfo> userExtraInfoList;
 
+	/**
+	 * @return the deviceCount
+	 */
 	public String getDeviceCount() {
 		return this.deviceCount;
 	}
 
+	/**
+	 * @param deviceCount
+	 *            the deviceCount to set
+	 */
 	public void setDeviceCount(String deviceCount) {
 		this.deviceCount = deviceCount;
 	}
 
-	public String getImMbrNo() {
-		return this.imMbrNo;
-	}
-
-	public void setImMbrNo(String imMbrNo) {
-		this.imMbrNo = imMbrNo;
-	}
-
-	public String getImRegDate() {
-		return this.imRegDate;
-	}
-
-	public void setImRegDate(String imRegDate) {
-		this.imRegDate = imRegDate;
-	}
-
-	public String getIsMemberPoint() {
-		return this.isMemberPoint;
-	}
-
-	public void setIsMemberPoint(String isMemberPoint) {
-		this.isMemberPoint = isMemberPoint;
-	}
-
-	public String getLoginStatusCode() {
-		return this.loginStatusCode;
-	}
-
-	public void setLoginStatusCode(String loginStatusCode) {
-		this.loginStatusCode = loginStatusCode;
-	}
-
-	public String getRegDate() {
-		return this.regDate;
-	}
-
-	public void setRegDate(String regDate) {
-		this.regDate = regDate;
-	}
-
-	public String getSecedeDate() {
-		return this.secedeDate;
-	}
-
-	public void setSecedeDate(String secedeDate) {
-		this.secedeDate = secedeDate;
-	}
-
-	public String getSecedeReasonCode() {
-		return this.secedeReasonCode;
-	}
-
-	public void setSecedeReasonCode(String secedeReasonCode) {
-		this.secedeReasonCode = secedeReasonCode;
-	}
-
-	public String getSecedeReasonMessage() {
-		return this.secedeReasonMessage;
-	}
-
-	public void setSecedeReasonMessage(String secedeReasonMessage) {
-		this.secedeReasonMessage = secedeReasonMessage;
-	}
-
-	public String getStopStatusCode() {
-		return this.stopStatusCode;
-	}
-
-	public void setStopStatusCode(String stopStatusCode) {
-		this.stopStatusCode = stopStatusCode;
-	}
-
-	public String getUserKey() {
-		return this.userKey;
-	}
-
-	public void setUserKey(String userKey) {
-		this.userKey = userKey;
-	}
-
-	public String getUserType() {
-		return this.userType;
-	}
-
-	public void setUserType(String userType) {
-		this.userType = userType;
-	}
-
-	public String getUserMainStatus() {
-		return this.userMainStatus;
-	}
-
-	public void setUserMainStatus(String userMainStatus) {
-		this.userMainStatus = userMainStatus;
-	}
-
-	public String getUserSubStatus() {
-		return this.userSubStatus;
-	}
-
-	public void setUserSubStatus(String userSubStatus) {
-		this.userSubStatus = userSubStatus;
-	}
-
-	public String getImSvcNo() {
-		return this.imSvcNo;
-	}
-
-	public void setImSvcNo(String imSvcNo) {
-		this.imSvcNo = imSvcNo;
-	}
-
-	public String getIsImChanged() {
-		return this.isImChanged;
-	}
-
-	public void setIsImChanged(String isImChanged) {
-		this.isImChanged = isImChanged;
-	}
-
-	public String getUserId() {
-		return this.userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-	public String getUserTelecom() {
-		return this.userTelecom;
-	}
-
-	public void setUserTelecom(String userTelecom) {
-		this.userTelecom = userTelecom;
-	}
-
-	public String getUserPhoneCountry() {
-		return this.userPhoneCountry;
-	}
-
-	public void setUserPhoneCountry(String userPhoneCountry) {
-		this.userPhoneCountry = userPhoneCountry;
-	}
-
-	public String getUserPhone() {
-		return this.userPhone;
-	}
-
-	public void setUserPhone(String userPhone) {
-		this.userPhone = userPhone;
-	}
-
-	public String getIsRecvSMS() {
-		return this.isRecvSMS;
-	}
-
-	public void setIsRecvSMS(String isRecvSMS) {
-		this.isRecvSMS = isRecvSMS;
-	}
-
-	public String getUserEmail() {
-		return this.userEmail;
-	}
-
-	public void setUserEmail(String userEmail) {
-		this.userEmail = userEmail;
-	}
-
-	public String getIsRecvEmail() {
-		return this.isRecvEmail;
-	}
-
-	public void setIsRecvEmail(String isRecvEmail) {
-		this.isRecvEmail = isRecvEmail;
-	}
-
-	public String getUserName() {
-		return this.userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getUserSex() {
-		return this.userSex;
-	}
-
-	public void setUserSex(String userSex) {
-		this.userSex = userSex;
-	}
-
-	public String getUserBirthDay() {
-		return this.userBirthDay;
-	}
-
-	public void setUserBirthDay(String userBirthDay) {
-		this.userBirthDay = userBirthDay;
-	}
-
-	public String getUserZip() {
-		return this.userZip;
-	}
-
-	public void setUserZip(String userZip) {
-		this.userZip = userZip;
-	}
-
-	public String getUserAddress() {
-		return this.userAddress;
-	}
-
-	public void setUserAddress(String userAddress) {
-		this.userAddress = userAddress;
-	}
-
-	public String getUserDetailAddress() {
-		return this.userDetailAddress;
-	}
-
-	public void setUserDetailAddress(String userDetailAddress) {
-		this.userDetailAddress = userDetailAddress;
-	}
-
-	public String getUserCity() {
-		return this.userCity;
-	}
-
-	public void setUserCity(String userCity) {
-		this.userCity = userCity;
-	}
-
-	public String getUserState() {
-		return this.userState;
-	}
-
-	public void setUserState(String userState) {
-		this.userState = userState;
-	}
-
-	public String getUserCountry() {
-		return this.userCountry;
-	}
-
-	public void setUserCountry(String userCountry) {
-		this.userCountry = userCountry;
-	}
-
-	public String getUserLanguage() {
-		return this.userLanguage;
-	}
-
-	public void setUserLanguage(String userLanguage) {
-		this.userLanguage = userLanguage;
-	}
-
-	public String getIsParent() {
-		return this.isParent;
-	}
-
-	public void setIsParent(String isParent) {
-		this.isParent = isParent;
-	}
-
-	public String getIsRealName() {
-		return this.isRealName;
-	}
-
-	public void setIsRealName(String isRealName) {
-		this.isRealName = isRealName;
-	}
-
-	public String getImSiteCode() {
-		return this.imSiteCode;
-	}
-
-	public void setImSiteCode(String imSiteCode) {
-		this.imSiteCode = imSiteCode;
-	}
-
-	public List<UserExtraInfo> getUserExtraInfoList() {
-		return this.userExtraInfoList;
-	}
-
-	public void setUserExtraInfoList(List<UserExtraInfo> userExtraInfoList) {
-		this.userExtraInfoList = userExtraInfoList;
-	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-
+	/**
+	 * @return the totalDeviceCount
+	 */
 	public String getTotalDeviceCount() {
 		return this.totalDeviceCount;
 	}
 
+	/**
+	 * @param totalDeviceCount
+	 *            the totalDeviceCount to set
+	 */
 	public void setTotalDeviceCount(String totalDeviceCount) {
 		this.totalDeviceCount = totalDeviceCount;
 	}
 
+	/**
+	 * @return the imMbrNo
+	 */
+	public String getImMbrNo() {
+		return this.imMbrNo;
+	}
+
+	/**
+	 * @param imMbrNo
+	 *            the imMbrNo to set
+	 */
+	public void setImMbrNo(String imMbrNo) {
+		this.imMbrNo = imMbrNo;
+	}
+
+	/**
+	 * @return the imRegDate
+	 */
+	public String getImRegDate() {
+		return this.imRegDate;
+	}
+
+	/**
+	 * @param imRegDate
+	 *            the imRegDate to set
+	 */
+	public void setImRegDate(String imRegDate) {
+		this.imRegDate = imRegDate;
+	}
+
+	/**
+	 * @return the isMemberPoint
+	 */
+	public String getIsMemberPoint() {
+		return this.isMemberPoint;
+	}
+
+	/**
+	 * @param isMemberPoint
+	 *            the isMemberPoint to set
+	 */
+	public void setIsMemberPoint(String isMemberPoint) {
+		this.isMemberPoint = isMemberPoint;
+	}
+
+	/**
+	 * @return the loginStatusCode
+	 */
+	public String getLoginStatusCode() {
+		return this.loginStatusCode;
+	}
+
+	/**
+	 * @param loginStatusCode
+	 *            the loginStatusCode to set
+	 */
+	public void setLoginStatusCode(String loginStatusCode) {
+		this.loginStatusCode = loginStatusCode;
+	}
+
+	/**
+	 * @return the regDate
+	 */
+	public String getRegDate() {
+		return this.regDate;
+	}
+
+	/**
+	 * @param regDate
+	 *            the regDate to set
+	 */
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+
+	/**
+	 * @return the secedeDate
+	 */
+	public String getSecedeDate() {
+		return this.secedeDate;
+	}
+
+	/**
+	 * @param secedeDate
+	 *            the secedeDate to set
+	 */
+	public void setSecedeDate(String secedeDate) {
+		this.secedeDate = secedeDate;
+	}
+
+	/**
+	 * @return the secedeReasonCode
+	 */
+	public String getSecedeReasonCode() {
+		return this.secedeReasonCode;
+	}
+
+	/**
+	 * @param secedeReasonCode
+	 *            the secedeReasonCode to set
+	 */
+	public void setSecedeReasonCode(String secedeReasonCode) {
+		this.secedeReasonCode = secedeReasonCode;
+	}
+
+	/**
+	 * @return the secedeReasonMessage
+	 */
+	public String getSecedeReasonMessage() {
+		return this.secedeReasonMessage;
+	}
+
+	/**
+	 * @param secedeReasonMessage
+	 *            the secedeReasonMessage to set
+	 */
+	public void setSecedeReasonMessage(String secedeReasonMessage) {
+		this.secedeReasonMessage = secedeReasonMessage;
+	}
+
+	/**
+	 * @return the stopStatusCode
+	 */
+	public String getStopStatusCode() {
+		return this.stopStatusCode;
+	}
+
+	/**
+	 * @param stopStatusCode
+	 *            the stopStatusCode to set
+	 */
+	public void setStopStatusCode(String stopStatusCode) {
+		this.stopStatusCode = stopStatusCode;
+	}
+
+	/**
+	 * @return the userKey
+	 */
+	public String getUserKey() {
+		return this.userKey;
+	}
+
+	/**
+	 * @param userKey
+	 *            the userKey to set
+	 */
+	public void setUserKey(String userKey) {
+		this.userKey = userKey;
+	}
+
+	/**
+	 * @return the userType
+	 */
+	public String getUserType() {
+		return this.userType;
+	}
+
+	/**
+	 * @param userType
+	 *            the userType to set
+	 */
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	/**
+	 * @return the userMainStatus
+	 */
+	public String getUserMainStatus() {
+		return this.userMainStatus;
+	}
+
+	/**
+	 * @param userMainStatus
+	 *            the userMainStatus to set
+	 */
+	public void setUserMainStatus(String userMainStatus) {
+		this.userMainStatus = userMainStatus;
+	}
+
+	/**
+	 * @return the userSubStatus
+	 */
+	public String getUserSubStatus() {
+		return this.userSubStatus;
+	}
+
+	/**
+	 * @param userSubStatus
+	 *            the userSubStatus to set
+	 */
+	public void setUserSubStatus(String userSubStatus) {
+		this.userSubStatus = userSubStatus;
+	}
+
+	/**
+	 * @return the imSvcNo
+	 */
+	public String getImSvcNo() {
+		return this.imSvcNo;
+	}
+
+	/**
+	 * @param imSvcNo
+	 *            the imSvcNo to set
+	 */
+	public void setImSvcNo(String imSvcNo) {
+		this.imSvcNo = imSvcNo;
+	}
+
+	/**
+	 * @return the isImChanged
+	 */
+	public String getIsImChanged() {
+		return this.isImChanged;
+	}
+
+	/**
+	 * @param isImChanged
+	 *            the isImChanged to set
+	 */
+	public void setIsImChanged(String isImChanged) {
+		this.isImChanged = isImChanged;
+	}
+
+	/**
+	 * @return the userId
+	 */
+	public String getUserId() {
+		return this.userId;
+	}
+
+	/**
+	 * @param userId
+	 *            the userId to set
+	 */
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * @return the userTelecom
+	 */
+	public String getUserTelecom() {
+		return this.userTelecom;
+	}
+
+	/**
+	 * @param userTelecom
+	 *            the userTelecom to set
+	 */
+	public void setUserTelecom(String userTelecom) {
+		this.userTelecom = userTelecom;
+	}
+
+	/**
+	 * @return the userPhoneCountry
+	 */
+	public String getUserPhoneCountry() {
+		return this.userPhoneCountry;
+	}
+
+	/**
+	 * @param userPhoneCountry
+	 *            the userPhoneCountry to set
+	 */
+	public void setUserPhoneCountry(String userPhoneCountry) {
+		this.userPhoneCountry = userPhoneCountry;
+	}
+
+	/**
+	 * @return the userPhone
+	 */
+	public String getUserPhone() {
+		return this.userPhone;
+	}
+
+	/**
+	 * @param userPhone
+	 *            the userPhone to set
+	 */
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+
+	/**
+	 * @return the isRecvSMS
+	 */
+	public String getIsRecvSMS() {
+		return this.isRecvSMS;
+	}
+
+	/**
+	 * @param isRecvSMS
+	 *            the isRecvSMS to set
+	 */
+	public void setIsRecvSMS(String isRecvSMS) {
+		this.isRecvSMS = isRecvSMS;
+	}
+
+	/**
+	 * @return the userEmail
+	 */
+	public String getUserEmail() {
+		return this.userEmail;
+	}
+
+	/**
+	 * @param userEmail
+	 *            the userEmail to set
+	 */
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	/**
+	 * @return the isRecvEmail
+	 */
+	public String getIsRecvEmail() {
+		return this.isRecvEmail;
+	}
+
+	/**
+	 * @param isRecvEmail
+	 *            the isRecvEmail to set
+	 */
+	public void setIsRecvEmail(String isRecvEmail) {
+		this.isRecvEmail = isRecvEmail;
+	}
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return this.userName;
+	}
+
+	/**
+	 * @param userName
+	 *            the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return the userSex
+	 */
+	public String getUserSex() {
+		return this.userSex;
+	}
+
+	/**
+	 * @param userSex
+	 *            the userSex to set
+	 */
+	public void setUserSex(String userSex) {
+		this.userSex = userSex;
+	}
+
+	/**
+	 * @return the userBirthDay
+	 */
+	public String getUserBirthDay() {
+		return this.userBirthDay;
+	}
+
+	/**
+	 * @param userBirthDay
+	 *            the userBirthDay to set
+	 */
+	public void setUserBirthDay(String userBirthDay) {
+		this.userBirthDay = userBirthDay;
+	}
+
+	/**
+	 * @return the userZip
+	 */
+	public String getUserZip() {
+		return this.userZip;
+	}
+
+	/**
+	 * @param userZip
+	 *            the userZip to set
+	 */
+	public void setUserZip(String userZip) {
+		this.userZip = userZip;
+	}
+
+	/**
+	 * @return the userAddress
+	 */
+	public String getUserAddress() {
+		return this.userAddress;
+	}
+
+	/**
+	 * @param userAddress
+	 *            the userAddress to set
+	 */
+	public void setUserAddress(String userAddress) {
+		this.userAddress = userAddress;
+	}
+
+	/**
+	 * @return the userDetailAddress
+	 */
+	public String getUserDetailAddress() {
+		return this.userDetailAddress;
+	}
+
+	/**
+	 * @param userDetailAddress
+	 *            the userDetailAddress to set
+	 */
+	public void setUserDetailAddress(String userDetailAddress) {
+		this.userDetailAddress = userDetailAddress;
+	}
+
+	/**
+	 * @return the userCity
+	 */
+	public String getUserCity() {
+		return this.userCity;
+	}
+
+	/**
+	 * @param userCity
+	 *            the userCity to set
+	 */
+	public void setUserCity(String userCity) {
+		this.userCity = userCity;
+	}
+
+	/**
+	 * @return the userState
+	 */
+	public String getUserState() {
+		return this.userState;
+	}
+
+	/**
+	 * @param userState
+	 *            the userState to set
+	 */
+	public void setUserState(String userState) {
+		this.userState = userState;
+	}
+
+	/**
+	 * @return the userCountry
+	 */
+	public String getUserCountry() {
+		return this.userCountry;
+	}
+
+	/**
+	 * @param userCountry
+	 *            the userCountry to set
+	 */
+	public void setUserCountry(String userCountry) {
+		this.userCountry = userCountry;
+	}
+
+	/**
+	 * @return the userLanguage
+	 */
+	public String getUserLanguage() {
+		return this.userLanguage;
+	}
+
+	/**
+	 * @param userLanguage
+	 *            the userLanguage to set
+	 */
+	public void setUserLanguage(String userLanguage) {
+		this.userLanguage = userLanguage;
+	}
+
+	/**
+	 * @return the isParent
+	 */
+	public String getIsParent() {
+		return this.isParent;
+	}
+
+	/**
+	 * @param isParent
+	 *            the isParent to set
+	 */
+	public void setIsParent(String isParent) {
+		this.isParent = isParent;
+	}
+
+	/**
+	 * @return the isRealName
+	 */
+	public String getIsRealName() {
+		return this.isRealName;
+	}
+
+	/**
+	 * @param isRealName
+	 *            the isRealName to set
+	 */
+	public void setIsRealName(String isRealName) {
+		this.isRealName = isRealName;
+	}
+
+	/**
+	 * @return the imSiteCode
+	 */
+	public String getImSiteCode() {
+		return this.imSiteCode;
+	}
+
+	/**
+	 * @param imSiteCode
+	 *            the imSiteCode to set
+	 */
+	public void setImSiteCode(String imSiteCode) {
+		this.imSiteCode = imSiteCode;
+	}
+
+	/**
+	 * @return the prodExpoLevl
+	 */
 	public String getProdExpoLevl() {
 		return this.prodExpoLevl;
 	}
 
+	/**
+	 * @param prodExpoLevl
+	 *            the prodExpoLevl to set
+	 */
 	public void setProdExpoLevl(String prodExpoLevl) {
 		this.prodExpoLevl = prodExpoLevl;
+	}
+
+	/**
+	 * @return the realAge
+	 */
+	public String getRealAge() {
+		return this.realAge;
+	}
+
+	/**
+	 * @param realAge
+	 *            the realAge to set
+	 */
+	public void setRealAge(String realAge) {
+		this.realAge = realAge;
+	}
+
+	/**
+	 * @return the userExtraInfoList
+	 */
+	public List<UserExtraInfo> getUserExtraInfoList() {
+		return this.userExtraInfoList;
+	}
+
+	/**
+	 * @param userExtraInfoList
+	 *            the userExtraInfoList to set
+	 */
+	public void setUserExtraInfoList(List<UserExtraInfo> userExtraInfoList) {
+		this.userExtraInfoList = userExtraInfoList;
 	}
 
 }
