fe8c656b2dbbf02ab70c1be0b3f54e9b750679fc
김경복 <kim.rejoice@gmail.com>
2014-11-06 09:04:04 +0000

    휴대기기 설정정보 등록/수정/조회 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@70545 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/user/SearchDeviceSetInfoSacRes.java
@@ -0,0 +1,155 @@
+package com.skplanet.storeplatform.sac.client.member.vo.user;
+
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+/**
+ * 2.1.48. 휴대기기 설정 정보 조회. [RESPONSE]
+ * 
+ * Updated on : 2014. 10. 31. Updated by : Rejoice, Burkhan
+ */
+@JsonSerialize(include = Inclusion.NON_NULL)
+public class SearchDeviceSetInfoSacRes extends CommonInfo {
+
+	private static final long serialVersionUID = 1L;
+
+	/** 휴대기기 PIN 등록 유무. */
+	private String isPin;
+	/** 자동 업데이트 유무. */
+	private String isAutoUpdate;
+	/** 자동 업데이트 설정 구분. */
+	private String autoUpdateSet;
+	/** Wi-Fi 자동 업데이트 유무. */
+	private String isAutoUpdateWifi;
+	/** 로그인 잠금 유무. */
+	private String isLoginLock;
+	/** 결제 PIN 재입력 여부. */
+	private String isPinRetry;
+	/** 성인 콘테츠 잠금 유무. */
+	private String isAdult;
+	/** 결제 PIN 잠김 여부. */
+	private String isPinClosed;
+
+	/**
+	 * @return the isPin
+	 */
+	public String getIsPin() {
+		return this.isPin;
+	}
+
+	/**
+	 * @param isPin
+	 *            the isPin to set
+	 */
+	public void setIsPin(String isPin) {
+		this.isPin = isPin;
+	}
+
+	/**
+	 * @return the isAutoUpdate
+	 */
+	public String getIsAutoUpdate() {
+		return this.isAutoUpdate;
+	}
+
+	/**
+	 * @param isAutoUpdate
+	 *            the isAutoUpdate to set
+	 */
+	public void setIsAutoUpdate(String isAutoUpdate) {
+		this.isAutoUpdate = isAutoUpdate;
+	}
+
+	/**
+	 * @return the autoUpdateSet
+	 */
+	public String getAutoUpdateSet() {
+		return this.autoUpdateSet;
+	}
+
+	/**
+	 * @param autoUpdateSet
+	 *            the autoUpdateSet to set
+	 */
+	public void setAutoUpdateSet(String autoUpdateSet) {
+		this.autoUpdateSet = autoUpdateSet;
+	}
+
+	/**
+	 * @return the isAutoUpdateWifi
+	 */
+	public String getIsAutoUpdateWifi() {
+		return this.isAutoUpdateWifi;
+	}
+
+	/**
+	 * @param isAutoUpdateWifi
+	 *            the isAutoUpdateWifi to set
+	 */
+	public void setIsAutoUpdateWifi(String isAutoUpdateWifi) {
+		this.isAutoUpdateWifi = isAutoUpdateWifi;
+	}
+
+	/**
+	 * @return the isLoginLock
+	 */
+	public String getIsLoginLock() {
+		return this.isLoginLock;
+	}
+
+	/**
+	 * @param isLoginLock
+	 *            the isLoginLock to set
+	 */
+	public void setIsLoginLock(String isLoginLock) {
+		this.isLoginLock = isLoginLock;
+	}
+
+	/**
+	 * @return the isPinRetry
+	 */
+	public String getIsPinRetry() {
+		return this.isPinRetry;
+	}
+
+	/**
+	 * @param isPinRetry
+	 *            the isPinRetry to set
+	 */
+	public void setIsPinRetry(String isPinRetry) {
+		this.isPinRetry = isPinRetry;
+	}
+
+	/**
+	 * @return the isAdult
+	 */
+	public String getIsAdult() {
+		return this.isAdult;
+	}
+
+	/**
+	 * @param isAdult
+	 *            the isAdult to set
+	 */
+	public void setIsAdult(String isAdult) {
+		this.isAdult = isAdult;
+	}
+
+	/**
+	 * @return the isPinClosed
+	 */
+	public String getIsPinClosed() {
+		return this.isPinClosed;
+	}
+
+	/**
+	 * @param isPinClosed
+	 *            the isPinClosed to set
+	 */
+	public void setIsPinClosed(String isPinClosed) {
+		this.isPinClosed = isPinClosed;
+	}
+
+}
