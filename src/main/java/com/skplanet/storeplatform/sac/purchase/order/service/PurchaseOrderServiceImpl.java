bf49abdc9f9252d7cf837df40668d196a8f7b6e1
이승택 <stakwork@gmail.com>
2014-11-13 06:47:03 +0000

    구매인증 - 프로모션/배너 정보 : 상용은 일단 세팅X
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@72363 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/purchase/order/service/PurchaseOrderServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/purchase/order/service/PurchaseOrderServiceImpl.java
@@ -792,10 +792,13 @@ public class PurchaseOrderServiceImpl implements PurchaseOrderService {
 			res.setIapProdInfo(iapProdInfo);
 		}
 
-		// 프로모션 정보
+		// 프로모션/배너 정보
 		if (StringUtils.startsWith(verifyOrderInfo.getSystemId(), "S00")) {
-			res.setPromotionList(this.searchPromotionList(prchsDtlMore));
-			res.setBannerList(this.searchBannerList(prchsDtlMore));
+			// 상용은 세팅X
+			if (StringUtils.equalsIgnoreCase(this.envServerLevel, PurchaseConstants.ENV_SERVER_LEVEL_REAL) == false) {
+				res.setPromotionList(this.searchPromotionList(prchsDtlMore));
+				res.setBannerList(this.searchBannerList(prchsDtlMore));
+			}
 		}
 
 		// 판매자 정보 세팅
