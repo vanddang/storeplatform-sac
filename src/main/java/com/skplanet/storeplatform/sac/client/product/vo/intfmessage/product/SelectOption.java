f4ff9d1e604808c18c437b3344a40dc3b745f265
김형식 <jade.kimhs@gmail.com>
2014-12-09 05:52:13 +0000

    쇼핑 특가 상품에 대한 품절 여부(Sold Out) 등록
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@78271 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/product/vo/intfmessage/product/SelectOption.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/product/vo/intfmessage/product/SelectOption.java
@@ -34,6 +34,15 @@ public class SelectOption extends CommonInfo implements Serializable {
 	private String itemCode; // 아이템 코드
 	private String salesStatus; // 판매 상태
 	private String subYn; // 서브 여부
+	private String status; // 상품 상태 (soldout : 매진상품)
+	private Integer maxCount; // 최대 판매가능 수량
+	private Integer maxMonthlySale; // 월간 최대 판매가능 수량
+	private Integer maxDailySale; // 일간 최대 판매가능 수량
+	private Integer maxMonthlyBuy; // 1인 월간 최대 구매가능 수량
+	private Integer maxDailyBuy; // 1인 일간 최대 구매가능 수량
+	private Integer maxOnceBuy; // 1회 최대 구매가능 수량
+	private String specialCouponId; // 특가상품 쿠폰 ID
+
 	private List<SubSelectOption> subSelectOptionList; // 두번째 옵션 리스트
 
 	/**
@@ -139,6 +148,126 @@ public class SelectOption extends CommonInfo implements Serializable {
 	 */
 	public void setSubYn(String subYn) {
 		this.subYn = subYn;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public String getStatus() {
+		return this.status;
+	}
+
+	/**
+	 * @param status
+	 *            the status to set
+	 */
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	/**
+	 * @return the maxCount
+	 */
+	public Integer getMaxCount() {
+		return this.maxCount;
+	}
+
+	/**
+	 * @param maxCount
+	 *            the maxCount to set
+	 */
+	public void setMaxCount(Integer maxCount) {
+		this.maxCount = maxCount;
+	}
+
+	/**
+	 * @return the maxMonthlySale
+	 */
+	public Integer getMaxMonthlySale() {
+		return this.maxMonthlySale;
+	}
+
+	/**
+	 * @param maxMonthlySale
+	 *            the maxMonthlySale to set
+	 */
+	public void setMaxMonthlySale(Integer maxMonthlySale) {
+		this.maxMonthlySale = maxMonthlySale;
+	}
+
+	/**
+	 * @return the maxDailySale
+	 */
+	public Integer getMaxDailySale() {
+		return this.maxDailySale;
+	}
+
+	/**
+	 * @param maxDailySale
+	 *            the maxDailySale to set
+	 */
+	public void setMaxDailySale(Integer maxDailySale) {
+		this.maxDailySale = maxDailySale;
+	}
+
+	/**
+	 * @return the maxMonthlyBuy
+	 */
+	public Integer getMaxMonthlyBuy() {
+		return this.maxMonthlyBuy;
+	}
+
+	/**
+	 * @param maxMonthlyBuy
+	 *            the maxMonthlyBuy to set
+	 */
+	public void setMaxMonthlyBuy(Integer maxMonthlyBuy) {
+		this.maxMonthlyBuy = maxMonthlyBuy;
+	}
+
+	/**
+	 * @return the maxDailyBuy
+	 */
+	public Integer getMaxDailyBuy() {
+		return this.maxDailyBuy;
+	}
+
+	/**
+	 * @param maxDailyBuy
+	 *            the maxDailyBuy to set
+	 */
+	public void setMaxDailyBuy(Integer maxDailyBuy) {
+		this.maxDailyBuy = maxDailyBuy;
+	}
+
+	/**
+	 * @return the maxOnceBuy
+	 */
+	public Integer getMaxOnceBuy() {
+		return this.maxOnceBuy;
+	}
+
+	/**
+	 * @param maxOnceBuy
+	 *            the maxOnceBuy to set
+	 */
+	public void setMaxOnceBuy(Integer maxOnceBuy) {
+		this.maxOnceBuy = maxOnceBuy;
+	}
+
+	/**
+	 * @return the specialCouponId
+	 */
+	public String getSpecialCouponId() {
+		return this.specialCouponId;
+	}
+
+	/**
+	 * @param specialCouponId
+	 *            the specialCouponId to set
+	 */
+	public void setSpecialCouponId(String specialCouponId) {
+		this.specialCouponId = specialCouponId;
 	}
 
 	/**
