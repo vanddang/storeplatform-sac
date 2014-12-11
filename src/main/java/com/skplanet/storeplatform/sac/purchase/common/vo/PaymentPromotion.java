e9ed53352bd321cf1726fe22f7bb9dc2b3728b5a
이승택 <stakwork@gmail.com>
2014-12-11 07:43:01 +0000

    결제수단 프로모션 조회
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@79134 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/purchase/common/vo/PaymentPromotion.java
+++ src/main/java/com/skplanet/storeplatform/sac/purchase/common/vo/PaymentPromotion.java
@@ -19,6 +19,8 @@ import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
 public class PaymentPromotion extends CommonInfo {
 	private static final long serialVersionUID = 1L;
 
+	private String tenantId;
+
 	private Integer promSeq;
 	private String paymentMtdCd;
 	private String promNm;
@@ -30,6 +32,21 @@ public class PaymentPromotion extends CommonInfo {
 	private String regDt;
 	private String updId;
 	private String updDt;
+
+	/**
+	 * @return the tenantId
+	 */
+	public String getTenantId() {
+		return this.tenantId;
+	}
+
+	/**
+	 * @param tenantId
+	 *            the tenantId to set
+	 */
+	public void setTenantId(String tenantId) {
+		this.tenantId = tenantId;
+	}
 
 	/**
 	 * @return the promSeq
