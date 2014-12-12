ea9b2400a5d8719ec8139705d36221ac1ed57556
정희원 <joyspring@sk.com>
2014-12-12 07:55:07 +0000

    로컬 환경에서 글로벌 캐시가 초기화 되지 않은 경우 runtime에서 오류가 나지 않도록 수정.
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@79422 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/runtime/filter/RequestFilter.java
+++ src/main/java/com/skplanet/storeplatform/sac/runtime/filter/RequestFilter.java
@@ -3,11 +3,17 @@ package com.skplanet.storeplatform.sac.runtime.filter;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.common.collect.Collections2;
+import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import com.skplanet.storeplatform.framework.core.cache.process.GlobalCacheProcessor;
@@ -18,10 +24,24 @@ import com.skplanet.storeplatform.framework.integration.filter.AbstractStorePlat
  * Thread Context Filter
  * 
  * Updated on : 2014-01-06 Updated by : 김상호, 에이엔비.
+ * Updated on : 2014-12-12 Updated by : 정희원, SKP.
  */
 public class RequestFilter extends AbstractStorePlatformRequestFilter {
 
-	@Override
+    private boolean checkGlobalCache = true;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        super.init(filterConfig);
+
+        ApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(filterConfig.getServletContext());
+        // 실행 환경의 프로파일이 local로 지정된 경우 globalCacheProcessor가 정의되지 않은 경우 plandas의 상태값을 헤더에 포함시키지 않도록 처리한다.
+        if(ArrayUtils.contains(ctx.getEnvironment().getActiveProfiles(), "local")) {
+            checkGlobalCache = ctx.containsBeanDefinition("globalCacheProcessor");
+        }
+    }
+
+    @Override
 	protected String getContextId() {
 		return "SAC";
 	}
@@ -35,19 +55,18 @@ public class RequestFilter extends AbstractStorePlatformRequestFilter {
 	@Override
 	protected Map<String, String> getReqeustHeader(HttpServletRequest request) {
 
-		ApplicationContext ac = WebApplicationContextUtils.getWebApplicationContext(this.filterConfig
-				.getServletContext());
+        Map<String, String> headerMap = new HashMap<String, String>();
 
-		Map<String, String> headerMap = new HashMap<String, String>();
+        if (checkGlobalCache) {
+            ApplicationContext ac = WebApplicationContextUtils.getWebApplicationContext(this.filterConfig
+                    .getServletContext());
 
-		/* GlobalCacheProcessor useCache 가 false 일 경우 request header 내 'x-sac-use-cache' 에 false 를 셋팅 한다. */
-		GlobalCacheProcessor globalCacheProcessor = (GlobalCacheProcessor) ac.getBean("globalCacheProcessor");
-
-		if (globalCacheProcessor != null) {
-			if (!globalCacheProcessor.isUseCache()) {
-				headerMap.put("x-sac-use-cache", "false");
-			}
-		}
+		    /* GlobalCacheProcessor useCache 가 false 일 경우 request header 내 'x-sac-use-cache' 에 false 를 셋팅 한다. */
+            GlobalCacheProcessor globalCacheProcessor = (GlobalCacheProcessor) ac.getBean("globalCacheProcessor");
+            if (!globalCacheProcessor.isUseCache()) {
+                headerMap.put("x-sac-use-cache", "false");
+            }
+        }
 
 		return headerMap;
 	}
