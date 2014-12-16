f5e75bb0702be48581abc0ee2413ffe89ce28c7e
임근대 <kdlim@sk.com>
2014-12-16 01:52:58 +0000

    book type 원복
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@79865 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/display/response/EbookComicGeneratorImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/response/EbookComicGeneratorImpl.java
@@ -118,6 +118,7 @@ public class EbookComicGeneratorImpl implements EbookComicGenerator {
 		book.setTotalCount(metaInfo.getBookCount());
 
 		//이북 type
+		/*
         if(StringUtils.equals(metaInfo.getBookClsfCd(), DisplayConstants.DP_BOOK_BOOK)) {
         	book.setType(DisplayConstants.DP_BOOK_TYPE_BOOK);
         } else if(StringUtils.equals(metaInfo.getBookClsfCd(), DisplayConstants.DP_BOOK_SERIAL)) {
@@ -125,6 +126,7 @@ public class EbookComicGeneratorImpl implements EbookComicGenerator {
         } else if(StringUtils.equals(metaInfo.getBookClsfCd(), DisplayConstants.DP_BOOK_MAGAZINE)) {
         	book.setType(DisplayConstants.DP_BOOK_TYPE_MAGAZINE);
         }
+        */
 		
 		if (DisplayConstants.DP_SERIAL_META_CLASS_CD.equals(metaInfo.getMetaClsfCd())
 				|| DisplayConstants.DP_INTERACTIVE_WEBTOON_META_CLASS_CD.equals(metaInfo.getMetaClsfCd())
@@ -132,7 +134,7 @@ public class EbookComicGeneratorImpl implements EbookComicGenerator {
 				|| DisplayConstants.DP_MAGAZINE_COMIC_META_CLASS_CD.equals(metaInfo.getMetaClsfCd())
 				|| DisplayConstants.DP_WEBTOON_COMIC_META_CLASS_CD.equals(metaInfo.getMetaClsfCd())) {
 			// eBook 연재물인 경우에만 Type과 status를 적용한다.
-			//book.setType(DisplayConstants.DP_EBOOK_SERIAL_NM);
+			book.setType(DisplayConstants.DP_EBOOK_SERIAL_NM);
 			if ("Y".equals(metaInfo.getComptYn())) {
 				book.setStatus(DisplayConstants.DP_EBOOK_COMPLETED_NM);
 			} else {
