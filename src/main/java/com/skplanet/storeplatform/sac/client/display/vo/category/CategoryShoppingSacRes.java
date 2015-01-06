efba3d70c14651d24b5714c986c767776c659322
김형식 <jade.kimhs@gmail.com>
2015-01-06 02:35:21 +0000

    commonResponse 추가 수정
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@83043 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/display/vo/category/CategoryShoppingSacRes.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/category/CategoryShoppingSacRes.java
@@ -3,13 +3,28 @@ package com.skplanet.storeplatform.sac.client.display.vo.category;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 
 import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.CommonResponse;
 import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Product;
 
 @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
 public class CategoryShoppingSacRes extends CommonInfo {
 	private static final long serialVersionUID = 1L;
-
+	private CommonResponse commonResponse;
 	private Product product;
+
+	/**
+	 * @return the commonResponse
+	 */
+	public CommonResponse getCommonResponse() {
+		return commonResponse;
+	}
+
+	/**
+	 * @param commonResponse the commonResponse to set
+	 */
+	public void setCommonResponse(CommonResponse commonResponse) {
+		this.commonResponse = commonResponse;
+	}
 
 	/**
 	 * @return the product
