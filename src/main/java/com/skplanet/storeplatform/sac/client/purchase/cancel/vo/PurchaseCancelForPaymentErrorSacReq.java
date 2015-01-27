b59c025fa9a7445257bb9652eab08111b7c0eecb
양주원 <sden2720@gmail.com>
2015-01-27 10:15:47 +0000

    IAP자동결제해지 코드값 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@86117 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/purchase/cancel/vo/PurchaseCancelForPaymentErrorSacReq.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/purchase/cancel/vo/PurchaseCancelForPaymentErrorSacReq.java
@@ -28,7 +28,7 @@ public class PurchaseCancelForPaymentErrorSacReq extends PurchaseCommonSacReq {
 	private String reqUserId;
 
 	@NotBlank
-	@Pattern(regexp = "^OR000440|^OR000443")
+	@Pattern(regexp = "^OR000440|^OR000443|^OR000447")
 	private String cancelReqPathCd;
 
 	@NotBlank
