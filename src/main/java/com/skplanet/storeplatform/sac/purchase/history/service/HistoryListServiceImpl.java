2b0034fef25800a4e6a4afdb6e8b93839a6811b5
양주원 <sden2720@gmail.com>
2015-01-30 06:52:55 +0000

    구매내역조회v1에 deviceId 조회되도록 수정
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@86733 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/purchase/history/service/HistoryListServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/purchase/history/service/HistoryListServiceImpl.java
@@ -293,6 +293,7 @@ public class HistoryListServiceImpl implements HistoryListService {
 			if (!StringUtils.isEmpty(historySac.getUseUserKey()) && !StringUtils.isEmpty(historySac.getUseDeviceKey())
 					&& !PurchaseConstants.NON_MEMBER.equals(historySac.getUseUserKey())) {
 				deviceInfo = new SearchUserDeviceSac();
+				deviceInfo.setTenantId(historySac.getTenantId());
 				deviceInfo.setUserKey(historySac.getUseUserKey());
 				deviceInfo.setDeviceKey(historySac.getUseDeviceKey());
 				deviceList.add(deviceInfo);
@@ -307,6 +308,7 @@ public class HistoryListServiceImpl implements HistoryListService {
 					&& !StringUtils.isEmpty(historySac.getSendDeviceKey())
 					&& !PurchaseConstants.NON_MEMBER.equals(historySac.getSendUserKey())) {
 				sendDeviceInfo = new SearchUserDeviceSac();
+				sendDeviceInfo.setTenantId(historySac.getTenantId());
 				sendDeviceInfo.setUserKey(historySac.getSendUserKey());
 				sendDeviceInfo.setDeviceKey(historySac.getSendDeviceKey());
 				sendDeviceList.add(sendDeviceInfo);
