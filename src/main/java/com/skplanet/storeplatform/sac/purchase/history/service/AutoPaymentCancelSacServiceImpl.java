a215d2a4bb43c70b7db94129ce2552425bd14392
양주원 <sden2720@gmail.com>
2015-03-05 02:08:50 +0000

    tstore noti 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@92265 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/purchase/history/service/AutoPaymentCancelSacServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/purchase/history/service/AutoPaymentCancelSacServiceImpl.java
@@ -14,9 +14,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skplanet.storeplatform.external.client.tstore.sci.TStoreNotiSCI;
+import com.skplanet.storeplatform.external.client.tstore.vo.TStoreNotiV2EcReq;
+import com.skplanet.storeplatform.external.client.tstore.vo.TStoreNotiV2EcRes;
+import com.skplanet.storeplatform.framework.core.util.StringUtils;
 import com.skplanet.storeplatform.purchase.client.history.sci.AutoPaymentCancelSCI;
 import com.skplanet.storeplatform.purchase.client.history.vo.AutoPaymentCancelScReq;
 import com.skplanet.storeplatform.purchase.client.history.vo.AutoPaymentCancelScRes;
+import com.skplanet.storeplatform.sac.purchase.constant.PurchaseConstants;
 
 /**
  * 기구매 SAC Service 인터페이스 구현체
@@ -31,8 +36,8 @@ public class AutoPaymentCancelSacServiceImpl implements AutoPaymentCancelSacServ
 	@Autowired
 	private AutoPaymentCancelSCI autoPaymentCancelSCI;
 
-	// @Autowired
-	// private TStoreNotiSCI tStoreNotiSCI;
+	@Autowired
+	private TStoreNotiSCI tStoreNotiSCI;
 
 	/**
 	 * 자동결재해지예약/예약취소/해지 SAC Service.
@@ -44,41 +49,39 @@ public class AutoPaymentCancelSacServiceImpl implements AutoPaymentCancelSacServ
 	@Override
 	public AutoPaymentCancelScRes updateReservation(AutoPaymentCancelScReq autoPaymentCancelscReq) {
 
+		// 자동결제해지요청
 		AutoPaymentCancelScRes autoPaymentCancelRes = this.autoPaymentCancelSCI
 				.updateReservation(autoPaymentCancelscReq);
-		// 이메일발송을 tenant(Tstore)에서 처리함
-		// // 부분유료화자경결재해지가 아닐시에는 메일전송처리를 한다.
-		// if (!autoPaymentCancelscReq.getAutoPaymentStatusCd().equals(PurchaseConstants.AUTO_PRCHS_STATUS_IAP_CLOSED))
-		// {
-		// // ResultYn이 'Y'일 경우에만 이메일 발송
-		// if (autoPaymentCancelRes.getResultYn().equals("Y")) {
-		// this.logger.debug("@@@@@@자동결재해지예약/예약취소/해지 이메일 발송 처리@@@@@@");
-		// // tstore 이메일 발송
-		// // try {
-		// // TODO TENANT(Tstore 구현X) 주석처리
-		// TStoreNotiEcReq tStoreNotiEcReq = new TStoreNotiEcReq();
-		// tStoreNotiEcReq.setUserKey(autoPaymentCancelscReq.getUserKey());
-		// tStoreNotiEcReq.setDeviceKey(autoPaymentCancelscReq.getDeviceKey());
-		// tStoreNotiEcReq.setPrchsId(autoPaymentCancelscReq.getPrchsId());
-		// tStoreNotiEcReq.setPrchsDt(autoPaymentCancelRes.getPrchsDt());
-		// // type code = 01:일반결제, 02:자동결제, 03:자동결제해지예약, 04:자동결제 해지완료
-		// tStoreNotiEcReq.setType(PurchaseConstants.TSTORE_NOTI_TYPE_AUTOPAY_RESERVED);
-		// // 2014-03-18 쇼핑의 동기와 비동기 수용을 위해 추가됨
-		// // publishType code = (01:동기, 02:비동기, 03:비동기완료)
-		// // - 동기 : 쇼핑 상품의 비동기 방식을 제외한 전 상품 구매시 사용, E-Mail/MMS/SMS 발송
-		// // - 비동기 : 쇼핑 상품의 비동기 방식을 경우, E-Mail만 발송
-		// // - 비동기완료 : 비동기로 응답이 되었을 경우, MMS만 발송
-		// tStoreNotiEcReq.setPublishType(PurchaseConstants.TSTORE_NOTI_PUBLISH_TYPE_SYNC);
-		// try {
-		// TStoreNotiEcRes tStoreNotiEcRes = this.tStoreNotiSCI.postTStoreNoti(tStoreNotiEcReq);
-		// this.logger.debug("TStoreNotiEcRes Code    : {}", tStoreNotiEcRes.getCode());
-		// this.logger.debug("TStoreNotiEcRes Message : {}", tStoreNotiEcRes.getMessage());
-		//
-		// } catch (Exception ex) {
-		// this.logger.debug("AutoPaymentCancel {}", ex);
-		// }
-		// }
-		// }
+
+		// 해지시 SMS 발송을 위한 결제노티 요청
+		if (StringUtils.equals(PurchaseConstants.AUTO_PRCHS_STATUS_CLOSED,
+				autoPaymentCancelscReq.getAutoPaymentStatusCd())) {
+
+			if (autoPaymentCancelRes != null && StringUtils.equals("Y", autoPaymentCancelRes.getResultYn())) {
+
+				TStoreNotiV2EcReq tStoreNotiV2EcReq = new TStoreNotiV2EcReq();
+
+				tStoreNotiV2EcReq.setUserKey(autoPaymentCancelscReq.getUserKey());
+				tStoreNotiV2EcReq.setDeviceKey(autoPaymentCancelscReq.getDeviceKey());
+				tStoreNotiV2EcReq.setPrchsId(autoPaymentCancelscReq.getPrchsId());
+				tStoreNotiV2EcReq.setPrchsDt(autoPaymentCancelRes.getPrchsDt());
+				tStoreNotiV2EcReq.setType(PurchaseConstants.TSTORE_NOTI_TYPE_AUTOPAY_CLOSED); // 04 : 자동결제 해지완료
+				tStoreNotiV2EcReq.setReason(PurchaseConstants.TSTORE_NOTI_REASON_AUTOPAY_CLOSED); // 01:VOD 월정액 해지 요청시
+				tStoreNotiV2EcReq.setProdId(autoPaymentCancelRes.getProdId());
+
+				tStoreNotiV2EcReq.setPublishType("01");
+				tStoreNotiV2EcReq.setGiftYn("N");
+
+				try {
+					TStoreNotiV2EcRes tStoreNotiV2EcRes = this.tStoreNotiSCI.postTStoreNotiV2(tStoreNotiV2EcReq);
+					this.logger.info("PRCHS,AUTOPAYMENTCANCEL,SAC,NOTI,RES,{}", tStoreNotiV2EcRes);
+				} catch (Exception e) {
+					this.logger.info("PRCHS,AUTOPAYMENTCANCEL,SAC,ERROR,{},{}", autoPaymentCancelscReq.getPrchsId(),
+							e.getMessage());
+				}
+
+			}
+		}
 		return autoPaymentCancelRes;
 	}
 }
