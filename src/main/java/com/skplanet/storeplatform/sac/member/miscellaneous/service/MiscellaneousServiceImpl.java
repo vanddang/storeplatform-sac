9368ce8410d46fd9104aed357ad704c74b7f1992
윤보영 <bottangs@gmail.com>
2015-03-06 05:48:26 +0000

    휴대폰 인증 여부 확인 추가 - confirmPhoneAutorizationCheck
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@92672 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/member/miscellaneous/service/MiscellaneousServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/member/miscellaneous/service/MiscellaneousServiceImpl.java
@@ -62,6 +62,8 @@ import com.skplanet.storeplatform.sac.client.member.vo.miscellaneous.ConfirmCapt
 import com.skplanet.storeplatform.sac.client.member.vo.miscellaneous.ConfirmCaptchaRes;
 import com.skplanet.storeplatform.sac.client.member.vo.miscellaneous.ConfirmEmailAuthorizationCodeReq;
 import com.skplanet.storeplatform.sac.client.member.vo.miscellaneous.ConfirmEmailAuthorizationCodeRes;
+import com.skplanet.storeplatform.sac.client.member.vo.miscellaneous.ConfirmPhoneAuthorizationCheckReq;
+import com.skplanet.storeplatform.sac.client.member.vo.miscellaneous.ConfirmPhoneAuthorizationCheckRes;
 import com.skplanet.storeplatform.sac.client.member.vo.miscellaneous.ConfirmPhoneAuthorizationCodeReq;
 import com.skplanet.storeplatform.sac.client.member.vo.miscellaneous.ConfirmPhoneAuthorizationCodeRes;
 import com.skplanet.storeplatform.sac.client.member.vo.miscellaneous.CreateAdditionalServiceReq;
@@ -953,6 +955,46 @@ public class MiscellaneousServiceImpl implements MiscellaneousService {
 
 		CreateDCDRes res = new CreateDCDRes();
 		res.setDeviceId(devideId);
+		return res;
+	}
+
+	/**
+	 * <pre>
+	 * 2.3.17.	휴대폰 인증 여부 확인.
+	 * </pre>
+	 * 
+	 * @param header
+	 *            SacRequestHeader
+	 * @param request
+	 *            ConfirmPhoneAuthorizationCheckReq
+	 * @return ConfirmPhoneAuthorizationCheckRes
+	 */
+	@Override
+	public ConfirmPhoneAuthorizationCheckRes confirmPhoneAutorizationCheck(SacRequestHeader header,
+			ConfirmPhoneAuthorizationCheckReq req) {
+		String tenantId = header.getTenantHeader().getTenantId();
+		String systemId = header.getTenantHeader().getSystemId();
+		String userPhone = req.getUserPhone();
+
+		ServiceAuth serviceAuthInfo = new ServiceAuth();
+		serviceAuthInfo.setTenantId(tenantId);
+		serviceAuthInfo.setSystemId(systemId);
+		serviceAuthInfo.setAuthTypeCd(MemberConstants.AUTH_TYPE_CD_SMS);
+		serviceAuthInfo.setAuthMdn(userPhone);
+		serviceAuthInfo.setAuthSign(req.getPhoneSign());
+
+		ServiceAuth resultInfo = this.commonDao.queryForObject("Miscellaneous.searchPhoneAuthCheck", serviceAuthInfo,
+				ServiceAuth.class);
+
+		if (resultInfo == null) {
+			throw new StorePlatformException("SAC_MEM_0002", "휴대폰인증");
+		}
+
+		ConfirmPhoneAuthorizationCheckRes res = new ConfirmPhoneAuthorizationCheckRes();
+		res.setUserPhone(userPhone);
+		res.setComptYn(resultInfo.getAuthComptYn());
+		res.setRegDt(resultInfo.getRegDt());
+		res.setUpdDt(resultInfo.getUpdDt());
 		return res;
 	}
 }
