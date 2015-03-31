ca5d6b2a441e24e5f1bdd89208cc9e0520800761
반범진 <vanddang@gtsoft.co.kr>
2015-03-31 00:57:48 +0000

    타사 심플인증 성능테스트 로직 제거
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@97659 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/member/user/controller/LoginController.java
+++ src/main/java/com/skplanet/storeplatform/sac/member/user/controller/LoginController.java
@@ -397,12 +397,7 @@ public class LoginController {
 
 		LOGGER.info("Request : {}", ConvertMapperUtils.convertObjectToJson(req));
 
-		AuthorizeForOllehMarketSacRes res = null;
-		if (StringUtils.equals(this.envServerLevel, "real") || StringUtils.equals(this.envServerLevel, "local")) {
-			res = this.loginService.authorizeForOllehMarketTest(requestHeader, req);
-		} else {
-			res = this.loginService.authorizeForOllehMarket(requestHeader, req);
-		}
+		AuthorizeForOllehMarketSacRes res = this.loginService.authorizeForOllehMarket(requestHeader, req);
 
 		LOGGER.info("Response : {}, {}, {}", res.getDeviceId(), res.getUserInfo().getUserKey(), res.getUserStatus());
 
@@ -435,12 +430,7 @@ public class LoginController {
 
 		LOGGER.info("Request : {}", ConvertMapperUtils.convertObjectToJson(req));
 
-		AuthorizeForUplusStoreSacRes res = null;
-		if (StringUtils.equals(this.envServerLevel, "real") || StringUtils.equals(this.envServerLevel, "local")) {
-			res = this.loginService.authorizeForUplusStoreTest(requestHeader, req);
-		} else {
-			res = this.loginService.authorizeForUplusStore(requestHeader, req);
-		}
+		AuthorizeForUplusStoreSacRes res = this.loginService.authorizeForUplusStore(requestHeader, req);
 
 		LOGGER.info("Response : {}, {}, {}", res.getDeviceId(), res.getUserInfo().getUserKey(), res.getUserStatus());
 
--- src/main/java/com/skplanet/storeplatform/sac/member/user/service/LoginService.java
+++ src/main/java/com/skplanet/storeplatform/sac/member/user/service/LoginService.java
@@ -176,20 +176,6 @@ public interface LoginService {
 
 	/**
 	 * <pre>
-	 * Olleh Market 회원의 회원인증 성능테스트용.
-	 * </pre>
-	 * 
-	 * @param requestHeader
-	 *            SacRequestHeader
-	 * @param req
-	 *            AuthorizeForOllehMarketSacReq
-	 * @return AuthorizeForOllehMarketSacRes
-	 */
-	public AuthorizeForOllehMarketSacRes authorizeForOllehMarketTest(SacRequestHeader requestHeader,
-			@Valid @RequestBody AuthorizeForOllehMarketSacReq req);
-
-	/**
-	 * <pre>
 	 * Uplus Store 회원의 회원인증.
 	 * </pre>
 	 * 
@@ -200,20 +186,6 @@ public interface LoginService {
 	 * @return AuthorizeForUplusStoreSacRes
 	 */
 	public AuthorizeForUplusStoreSacRes authorizeForUplusStore(SacRequestHeader requestHeader,
-			@Valid @RequestBody AuthorizeForUplusStoreSacReq req);
-
-	/**
-	 * <pre>
-	 * Uplus Store 회원의 회원인증 성능테스트용.
-	 * </pre>
-	 * 
-	 * @param requestHeader
-	 *            SacRequestHeader
-	 * @param req
-	 *            AuthorizeForUplusStoreSacReq
-	 * @return AuthorizeForUplusStoreSacRes
-	 */
-	public AuthorizeForUplusStoreSacRes authorizeForUplusStoreTest(SacRequestHeader requestHeader,
 			@Valid @RequestBody AuthorizeForUplusStoreSacReq req);
 
 	/**
