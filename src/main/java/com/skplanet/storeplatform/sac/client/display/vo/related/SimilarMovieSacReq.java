cfb3eb92ecd8fbb5adc5d7d0182eb2057f7cc257
문동선 <dongsun.moon@sk.com>
2015-04-01 02:44:05 +0000

    ref #34842 : [비슷한영화] 비슷한 영화 목록 조회 API 개발 - parameter 추가: count, prodGradeCd - response 추가: count, hasNext
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@98183 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/display/vo/related/SimilarMovieSacReq.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/related/SimilarMovieSacReq.java
@@ -2,11 +2,27 @@ package com.skplanet.storeplatform.sac.client.display.vo.related;
 
 public class SimilarMovieSacReq {
 	private String prodId;
+	private String prodGradeCd;
+	private Integer count;
 
 	public String getProdId() {
 		return prodId;
 	}
 	public void setProdId(String prodId) {
 		this.prodId = prodId;
+	}
+	public String getProdGradeCd() {
+		return prodGradeCd;
+	}
+	public void setProdGradeCd(String prodGradeCd) {
+		this.prodGradeCd = prodGradeCd;
+	}
+	public Integer getCount() {
+		if(count==null)
+			return 20;
+		return count;
+	}
+	public void setCount(Integer count) {
+		this.count = count;
 	}
 }
--- src/main/java/com/skplanet/storeplatform/sac/client/display/vo/related/SimilarMovieSacRes.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/related/SimilarMovieSacRes.java
@@ -5,8 +5,22 @@ import java.util.List;
 import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Product;
 
 public class SimilarMovieSacRes {
+	private Integer count;
+	private String hasNext;
 	private List<Product> productList;
 
+	public Integer getCount() {
+		return count;
+	}
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+	public String getHasNext() {
+		return hasNext;
+	}
+	public void setHasNext(String hasNext) {
+		this.hasNext = hasNext;
+	}
 	public List<Product> getProductList() {
 		return productList;
 	}
