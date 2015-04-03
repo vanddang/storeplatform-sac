de53c8f8c557da738a94d320ee4659aa3e4dda76
서대영 <dale.seo@sk.com>
2015-04-03 08:09:15 +0000

    [공통] Inbound(EC->SAC) 호출 모듈 보완 (https://project.itopping.co.kr/issues/37184)
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@98666 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/rest/apache/RestInvoker.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/rest/apache/RestInvoker.java
@@ -6,13 +6,9 @@ import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class RestInvoker {
 	
-	private static final Logger LOGGER = LoggerFactory.getLogger(RestInvoker.class);
-
 	private static HttpClient client;
 
 	static {
@@ -21,7 +17,7 @@ public class RestInvoker {
 		cm.setDefaultMaxPerRoute(200); // concurrent connections per given route
 		
 		RequestConfig config = RequestConfig.custom()
-				.setSocketTimeout(30 * 1000) // socket timeout = 30sec
+				.setSocketTimeout(20 * 1000) // socket timeout = 30sec
 				.setConnectTimeout(2 * 1000) // connect timeout = 5sec
 			    .build();
 		
@@ -34,7 +30,6 @@ public class RestInvoker {
 	public static HttpResponse invoke(HttpUriRequest request) {
 		HttpResponse response;
 		try {
-			// LOGGER.debug(client.getConnectionManager());
 			response = client.execute(request);
 		} catch (Exception e) {
 			throw new RuntimeException("Failed to execute HttpClient.", e);
