d795c5a67f18d0832208774d63823b48a82ace33
황민규 <minkyu_hwang@sk.com>
2015-04-09 04:43:46 +0000

    ref #37351. PP연동구조 개선(결제 처리 결과 알림 SAC이관)
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@99480 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/purchase/vo/order/PPNotifyPaymentSacRes.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.purchase.vo.order;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+/**
+ * 
+ * 결제 결과 Noti 응답 VO
+ * 
+ * Updated on : 2014. 2. 28. Updated by : 이승택, nTels.
+ */
+public class PPNotifyPaymentSacRes extends CommonInfo {
+	private static final long serialVersionUID = 201402281L;
+
+	private String prchsId; // 구매 ID
+	private int count; // 처리된 결제수단 갯수
+	private String shippingUrl; // 상품 배송지 입력 URL
+
+	/**
+	 */
+	public PPNotifyPaymentSacRes() {
+	}
+
+	/**
+	 * @param prchsId
+	 *            구매 ID
+	 * @param count
+	 *            처리된 결제수단 갯수
+	 */
+	public PPNotifyPaymentSacRes(String prchsId, int count) {
+		this.prchsId = prchsId;
+		this.count = count;
+	}
+
+	/**
+	 * @return the prchsId
+	 */
+	public String getPrchsId() {
+		return this.prchsId;
+	}
+
+	/**
+	 * @param prchsId
+	 *            the prchsId to set
+	 */
+	public void setPrchsId(String prchsId) {
+		this.prchsId = prchsId;
+	}
+
+	/**
+	 * @return the count
+	 */
+	public int getCount() {
+		return this.count;
+	}
+
+	/**
+	 * @param count
+	 *            the count to set
+	 */
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	/**
+	 * @return the shippingUrl
+	 */
+	public String getShippingUrl() {
+		return this.shippingUrl;
+	}
+
+	/**
+	 * @param shippingUrl
+	 *            the shippingUrl to set
+	 */
+	public void setShippingUrl(String shippingUrl) {
+		this.shippingUrl = shippingUrl;
+	}
+
+}
