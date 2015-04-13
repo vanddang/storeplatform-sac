c06760f7bc0f81d662b1d387df4f8ce06a272934
문동선 <dongsun.moon@sk.com>
2015-04-13 08:43:30 +0000

    ref #34842 : [비슷한영화] 비슷한 영화 목록 조회 API 개발 - parameter 변경: prodId -> productId - parameter 추가: exceptId
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@100028 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/display/vo/related/SimilarMovieSacReq.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/related/SimilarMovieSacReq.java
@@ -1,21 +1,28 @@
 package com.skplanet.storeplatform.sac.client.display.vo.related;
 
 public class SimilarMovieSacReq {
-	private String prodId;
+	private String productId;
 	private String prodGradeCd;
+	private String exceptId;
 	private Integer count;
 
-	public String getProdId() {
-		return prodId;
+	public String getProductId() {
+		return productId;
 	}
-	public void setProdId(String prodId) {
-		this.prodId = prodId;
+	public void setProductId(String prodId) {
+		this.productId = prodId;
 	}
 	public String getProdGradeCd() {
 		return prodGradeCd;
 	}
 	public void setProdGradeCd(String prodGradeCd) {
 		this.prodGradeCd = prodGradeCd;
+	}
+	public String getExceptId() {
+		return exceptId;
+	}
+	public void setExceptId(String exceptId) {
+		this.exceptId = exceptId;
 	}
 	public Integer getCount() {
 		if(count==null)
