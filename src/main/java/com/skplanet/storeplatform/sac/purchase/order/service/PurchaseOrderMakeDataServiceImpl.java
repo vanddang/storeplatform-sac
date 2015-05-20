41ec16259e350c8259c750c887bd2b0f255be680
황민규 <minkyu_hwang@sk.com>
2015-05-20 00:27:52 +0000

    ref #40586. 정액제 고도화 요건 개발
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@107294 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/purchase/order/service/PurchaseOrderMakeDataServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/purchase/order/service/PurchaseOrderMakeDataServiceImpl.java
@@ -54,8 +54,8 @@ public class PurchaseOrderMakeDataServiceImpl implements PurchaseOrderMakeDataSe
 
 	@Value("#{systemProperties['spring.profiles.active']}")
 	private String envServerLevel;
-	@Value("#{propertiesForSac['purchase.order.drm.applicable']}")
-	private boolean drmApplicable;
+	@Value("#{propertiesForSac['purchase.order.drm.redate']}")
+	private boolean drmRedate;
 
 	@Autowired
 	private PurchaseOrderAssistService purchaseOrderAssistService;
@@ -212,7 +212,7 @@ public class PurchaseOrderMakeDataServiceImpl implements PurchaseOrderMakeDataSe
 					// - 초기 재다운로드 종료일시는 무제한 (다운로드 비대상인 정액제상품 & 정액제로 이용하는 에피소드 상품은 제외)
 					// - DRM_YN 값 변경
 					// - 외부구매경로(T프리미엄 등 비과금 구매요청 경로), 정액제 상품, 정액제 이용 에피소드 상품 경우 제외
-					if (this.drmApplicable) {
+					if (this.drmRedate) {
 						if (StringUtils.equals("Y", product.getDrmYn())
 								&& (purchaseOrderInfo.isVod() || purchaseOrderInfo.isEbookcomic())
 								&& purchaseOrderInfo.isFlat() == false
