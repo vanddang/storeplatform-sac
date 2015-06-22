88fcfef4a42fd71cd3fcfb1c7295d4be2da98f96
서대영 <dale.seo@sk.com>
2015-06-22 05:48:33 +0000

    테넌트에 따라 서비스 On/Off 안 되는 오류 (https://project.itopping.co.kr/issues/45598)
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@114776 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/other/sacservice/service/SacServiceTypeServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/other/sacservice/service/SacServiceTypeServiceImpl.java
@@ -34,7 +34,7 @@ public class SacServiceTypeServiceImpl implements SacServiceTypeService {
 	@Override
 	public SacService fromGetReq(GetActiveReq req, SacRequestHeader header) {
 		String serviceCd = req.getServiceCd();
-		String tenantId = chooseValueFromBodyOrHeader(req.getServiceCd(), header.getTenantHeader().getTenantId());
+		String tenantId = chooseValueFromBodyOrHeader(req.getTenantId(), header.getTenantHeader().getTenantId());
 		String simOperator = chooseValueFromBodyOrHeader(req.getSimOperator(), header.getNetworkHeader().getSimOperator());
 		String model = chooseValueFromBodyOrHeader(req.getModel(), header.getDeviceHeader().getModel());
 		
