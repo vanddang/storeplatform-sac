46caf4979ce1595e77cd9cc9068293d950990d50
양해엽 <haeyup.yang@sk.com>
2015-07-02 03:31:39 +0000

    배너 리스트 조회 V2 추가  - next로 조회 가능 하도록 기능 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@116571 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/banner/BannerListSacReq.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+
+package com.skplanet.storeplatform.sac.client.display.vo.banner;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+import org.hibernate.validator.constraints.NotBlank;
+
+/**
+ * Updated on : 2015-06-30. Updated by : 양해엽, SK Planet.
+ */
+public class BannerListSacReq extends CommonInfo {
+    private static final long serialVersionUID = 1L;
+
+    @NotBlank
+    private String bnrMenuId; // 배너 메뉴 ID
+
+    @NotBlank
+    private String bnrExpoMenuId; // 배너 노출 메뉴 ID
+
+    @NotBlank
+    private String imgSizeCd; // 배너 이미지 코드
+
+    private String startKey;
+    private Integer count;
+
+    public String getBnrMenuId() {
+        return bnrMenuId;
+    }
+
+    public void setBnrMenuId(String bnrMenuId) {
+        this.bnrMenuId = bnrMenuId;
+    }
+
+    public String getBnrExpoMenuId() {
+        return bnrExpoMenuId;
+    }
+
+    public void setBnrExpoMenuId(String bnrExpoMenuId) {
+        this.bnrExpoMenuId = bnrExpoMenuId;
+    }
+
+    public String getImgSizeCd() {
+        return imgSizeCd;
+    }
+
+    public void setImgSizeCd(String imgSizeCd) {
+        this.imgSizeCd = imgSizeCd;
+    }
+
+    public String getStartKey() {
+        return startKey;
+    }
+
+    public void setStartKey(String startKey) {
+        this.startKey = startKey;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+}
---
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/banner/BannerListSacRes.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+
+package com.skplanet.storeplatform.sac.client.display.vo.banner;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.common.CommonResponse;
+import com.skplanet.storeplatform.sac.client.product.vo.intfmessage.product.Banner;
+
+import java.util.List;
+
+/**
+ * Updated on : 2015-06-30. Updated by : 양해엽, SK Planet.
+ */
+public class BannerListSacRes extends CommonInfo {
+    private static final long serialVersionUID = 1L;
+
+    private String startKey;
+    private String hasNext; // Y/N
+
+    private CommonResponse commonResponse;
+    private List<Banner> bannerList;
+
+    public String getStartKey() {
+        return startKey;
+    }
+
+    public void setStartKey(String startKey) {
+        this.startKey = startKey;
+    }
+
+    public String getHasNext() {
+        return hasNext;
+    }
+
+    public void setHasNext(String hasNext) {
+        this.hasNext = hasNext;
+    }
+
+    public CommonResponse getCommonResponse() {
+        return commonResponse;
+    }
+
+    public void setCommonResponse(CommonResponse commonResponse) {
+        this.commonResponse = commonResponse;
+    }
+
+    public List<Banner> getBannerList() {
+        return bannerList;
+    }
+
+    public void setBannerList(List<Banner> bannerList) {
+        this.bannerList = bannerList;
+    }
+}
