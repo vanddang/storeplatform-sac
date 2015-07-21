01eb0b21a7b6325e559e126859a82496d59fb782
김희민 <heemin.kim@sk.com>
2015-07-21 01:14:44 +0000

    비슷한 상품 조회 API 수정
    페이징 처리 옵션 추가 구현
    
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@118174 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/display/vo/related/RelatedProductSacReq.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/related/RelatedProductSacReq.java
@@ -2,6 +2,7 @@ package com.skplanet.storeplatform.sac.client.display.vo.related;
 
 import org.hibernate.validator.constraints.NotBlank;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
@@ -14,6 +15,17 @@ public class RelatedProductSacReq {
     private String relatedType;
 
     private String prodGradeCd;
+
+    @Min(0)
+    private Integer offset;
+
+    @Min(1)
+    private Integer count;
+
+    public RelatedProductSacReq() {
+        this.offset = 0;
+        this.count = 20;
+    }
 
     public String getProductId() {
         return productId;
@@ -37,5 +49,21 @@ public class RelatedProductSacReq {
 
     public void setProdGradeCd(String prodGradeCd) {
         this.prodGradeCd = prodGradeCd;
+    }
+
+    public Integer getOffset() {
+        return offset;
+    }
+
+    public void setOffset(Integer offset) {
+        this.offset = offset;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
     }
 }
--- src/main/java/com/skplanet/storeplatform/sac/client/display/vo/related/RelatedProductSacRes.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/related/RelatedProductSacRes.java
@@ -7,7 +7,34 @@ import java.util.List;
 
 @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
 public class RelatedProductSacRes {
+    private Boolean hasNext;
+    private Integer nextOffset;
+    private Integer count;
     private List<Product> productList;
+
+    public Boolean getHasNext() {
+        return hasNext;
+    }
+
+    public void setHasNext(Boolean hasNext) {
+        this.hasNext = hasNext;
+    }
+
+    public Integer getNextOffset() {
+        return nextOffset;
+    }
+
+    public void setNextOffset(Integer nextOffset) {
+        this.nextOffset = nextOffset;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
 
     public List<Product> getProductList() {
         return productList;
