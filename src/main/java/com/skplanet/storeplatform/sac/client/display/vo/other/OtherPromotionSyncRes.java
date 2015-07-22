72e597a697f3c8b54312e05b4ba029ceef54627e
정희원 <joyspring@sk.com>
2015-07-22 06:22:52 +0000

    프로모션 이벤트 동기화 API 더미 버전
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@118369 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/other/OtherPromotionSyncRes.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2013 SK planet.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK planet.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK planet.
+ */
+package com.skplanet.storeplatform.sac.client.display.vo.other;
+
+import com.skplanet.storeplatform.framework.core.common.vo.CommonInfo;
+
+/**
+ * <p>
+ * OtherPromotionSyncRes
+ * </p>
+ * Updated on : 2015. 07. 22 Updated by : 정희원, SK 플래닛.
+ */
+public class OtherPromotionSyncRes extends CommonInfo {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer updtCnt;
+
+    public OtherPromotionSyncRes(Integer updtCnt) {
+        this.updtCnt = updtCnt;
+    }
+
+    public Integer getUpdtCnt() {
+        return updtCnt;
+    }
+
+    public void setUpdtCnt(Integer updtCnt) {
+        this.updtCnt = updtCnt;
+    }
+}
