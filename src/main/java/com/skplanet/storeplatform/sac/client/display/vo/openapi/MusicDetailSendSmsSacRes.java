8866db7d1aebb7488f53b4a153eb27dbf06c2ea6
김희민 <heemin.kim@sk.com>
2015-08-07 05:54:09 +0000

    음악 상품 상세 URL SMS 전송 OPEN API 구현
    음악 상품 상세 URL을 SMS로 전송할 수 있는 OPEN API 구현
    
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@119724 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/display/vo/openapi/MusicDetailSendSmsSacRes.java
@@ -0,0 +1,20 @@
+package com.skplanet.storeplatform.sac.client.display.vo.openapi;
+
+
+public class MusicDetailSendSmsSacRes {
+
+    private String result;
+
+    public MusicDetailSendSmsSacRes() {
+        result = "success";
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+}
