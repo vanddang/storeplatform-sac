a27d4f26ad6e4b48f66d063f2ff856c7b946f0b4
정희원 <joyspring@sk.com>
2015-08-25 04:04:45 +0000

    버그픽스
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@120959 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/display/localsci/sci/service/PaymentInfoServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/display/localsci/sci/service/PaymentInfoServiceImpl.java
@@ -320,7 +320,7 @@ public class PaymentInfoServiceImpl implements PaymentInfoService {
         String chnlId = "", menuId = "";
 
         // 이용권 상품이면서 "시리즈 전회차 상품"인 경우 매핑된 채널ID의 상품 정보를 조회하여 프로모션 정보를 조회한다.
-        if(paymentInfo.getCmpxProdClsfCd().equals(FIXRATE_PROD_TYPE_VOD_SERIESPASS)) {
+        if(FIXRATE_PROD_TYPE_VOD_SERIESPASS.equals(paymentInfo.getCmpxProdClsfCd())) {
             MappedProduct mappedProduct = commonDAO.queryForObject("PaymentInfo.getMappedProdByVoucher", paymentInfo.getProdId(), MappedProduct.class);
             if(mappedProduct != null) {
                 chnlId = mappedProduct.getProdId();
