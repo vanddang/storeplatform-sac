54987ea9dc2f1b617450f3215cfc0f74e6057603
김형식 <jade.kimhs@gmail.com>
2015-08-27 04:13:56 +0000

    이미지 상세 처리 추가로 인한수정
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@121205 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/api/service/ShoppingCouponServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/api/service/ShoppingCouponServiceImpl.java
@@ -525,33 +525,23 @@ public class ShoppingCouponServiceImpl implements ShoppingCouponService {
 			int totalCnt = 1;
 			int seq = 1;
 			log.info("_>>>>>>>>>>catalogDtlSeq>>>>>>>>>>>>"+catalogDtlSeq);
+			
 			// 카탈로그 상세이미지 리사이즈
 			for (int i = 0; i < catalogDtlSeq; i++) {
-
+				boolean flagYn = false;
 				
 				if(i==1){
     				// 카탈로그 상세이미지 변수 (두번째 상세)
     				catalogImgPath1 = dpCatalogInfo.getDtlImgPath1(); // 이미지경로 + 이미지NAME
-    				uploadDir1 = catalogImgPath1.substring(0, catalogImgPath1.lastIndexOf(File.separator) + 1); // 이미지 경로
-    				srcFileName1 = catalogImgPath1.substring(catalogImgPath1.lastIndexOf(File.separator)).replace(File.separator,
-    						""); // 이미지NAME
-    				tmpFileName1 = srcFileName1.substring(0, srcFileName1.lastIndexOf(".")); // 파일명
-    				fileExt1 = srcFileName1.substring(srcFileName1.lastIndexOf(".") + 1); // 확장자
-    
-    				img = new ImageIcon(uploadDir1 + srcFileName1).getImage(); // 이미지 사이즈를 구하기 위해 이미지를 가져옴.
-    
-    				oWidth = img.getWidth(null); // 이미지 가로사이즈
-    				oHeight = img.getHeight(null); // 이미지 세로사이즈
-    				nHeightSize = oHeight;
-    				this.log.info("■■■■■oWidth■■■■■ ::" + oWidth);
-    				this.log.info("■■■■■oHeight■■■■■ :: " + oHeight);
-    				
-    							
+    				flagYn =true;
 				}
-				
 				if(i==2){
     				// 카탈로그 상세이미지 변수 (세번째 상세)
     				catalogImgPath1 = dpCatalogInfo.getDtlImgPath2(); // 이미지경로 + 이미지NAME
+    				flagYn =true;
+				}					
+				
+				if(flagYn){
     				uploadDir1 = catalogImgPath1.substring(0, catalogImgPath1.lastIndexOf(File.separator) + 1); // 이미지 경로
     				srcFileName1 = catalogImgPath1.substring(catalogImgPath1.lastIndexOf(File.separator)).replace(File.separator,
     						""); // 이미지NAME
@@ -564,8 +554,9 @@ public class ShoppingCouponServiceImpl implements ShoppingCouponService {
     				oHeight = img.getHeight(null); // 이미지 세로사이즈
     				nHeightSize = oHeight;
     				this.log.info("■■■■■oWidth■■■■■ ::" + oWidth);
-    				this.log.info("■■■■■oHeight■■■■■ :: " + oHeight);
-				}				
+    				this.log.info("■■■■■oHeight■■■■■ :: " + oHeight);					
+				}
+			
 				
 				targetFileName1 = tmpFileName1 + "." + fileExt1;
 				int width = oWidth;
