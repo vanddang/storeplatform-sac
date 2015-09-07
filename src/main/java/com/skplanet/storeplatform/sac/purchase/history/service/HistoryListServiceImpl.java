154de7ed71a154c75313c58a632a9fa4428f4da4
양주원 <sden2720@gmail.com>
2015-09-07 08:58:05 +0000

    디바이스정책비교 equal -> startWith로 변경
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@121648 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/purchase/history/service/HistoryListServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/purchase/history/service/HistoryListServiceImpl.java
@@ -9,31 +9,51 @@
  */
 package com.skplanet.storeplatform.sac.purchase.history.service;
 
-import com.skplanet.storeplatform.framework.core.exception.StorePlatformException;
-import com.skplanet.storeplatform.framework.core.util.StringUtils;
-import com.skplanet.storeplatform.purchase.client.common.vo.TenantSalePolicy;
-import com.skplanet.storeplatform.purchase.client.history.sci.HistorySCI;
-import com.skplanet.storeplatform.purchase.client.history.vo.*;
-import com.skplanet.storeplatform.sac.client.internal.display.localsci.sci.CmpxInfoSCI;
-import com.skplanet.storeplatform.sac.client.internal.display.localsci.sci.ProductInfoSCI;
-import com.skplanet.storeplatform.sac.client.internal.display.localsci.vo.*;
-import com.skplanet.storeplatform.sac.client.internal.member.user.sci.SearchUserSCI;
-import com.skplanet.storeplatform.sac.client.internal.member.user.vo.SearchUserDeviceSac;
-import com.skplanet.storeplatform.sac.client.internal.member.user.vo.SearchUserDeviceSacReq;
-import com.skplanet.storeplatform.sac.client.internal.member.user.vo.SearchUserDeviceSacRes;
-import com.skplanet.storeplatform.sac.client.internal.member.user.vo.UserDeviceInfoSac;
-import com.skplanet.storeplatform.sac.client.purchase.history.vo.*;
-import com.skplanet.storeplatform.sac.purchase.common.service.PurchaseTenantPolicyService;
-import com.skplanet.storeplatform.sac.purchase.constant.PurchaseConstants;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.skplanet.storeplatform.framework.core.exception.StorePlatformException;
+import com.skplanet.storeplatform.framework.core.util.StringUtils;
+import com.skplanet.storeplatform.purchase.client.common.vo.TenantSalePolicy;
+import com.skplanet.storeplatform.purchase.client.history.sci.HistorySCI;
+import com.skplanet.storeplatform.purchase.client.history.vo.HistoryCountScReq;
+import com.skplanet.storeplatform.purchase.client.history.vo.HistoryCountScRes;
+import com.skplanet.storeplatform.purchase.client.history.vo.HistoryListScReq;
+import com.skplanet.storeplatform.purchase.client.history.vo.HistoryListScRes;
+import com.skplanet.storeplatform.purchase.client.history.vo.HistorySc;
+import com.skplanet.storeplatform.purchase.client.history.vo.ProductCountSc;
+import com.skplanet.storeplatform.sac.client.internal.display.localsci.sci.CmpxInfoSCI;
+import com.skplanet.storeplatform.sac.client.internal.display.localsci.sci.ProductInfoSCI;
+import com.skplanet.storeplatform.sac.client.internal.display.localsci.vo.CmpxBasicInfo;
+import com.skplanet.storeplatform.sac.client.internal.display.localsci.vo.CmpxBasicInfoSacReq;
+import com.skplanet.storeplatform.sac.client.internal.display.localsci.vo.CmpxBasicInfoSacRes;
+import com.skplanet.storeplatform.sac.client.internal.display.localsci.vo.ProductInfo;
+import com.skplanet.storeplatform.sac.client.internal.display.localsci.vo.ProductInfoSacReq;
+import com.skplanet.storeplatform.sac.client.internal.display.localsci.vo.ProductInfoSacRes;
+import com.skplanet.storeplatform.sac.client.internal.member.user.sci.SearchUserSCI;
+import com.skplanet.storeplatform.sac.client.internal.member.user.vo.SearchUserDeviceSac;
+import com.skplanet.storeplatform.sac.client.internal.member.user.vo.SearchUserDeviceSacReq;
+import com.skplanet.storeplatform.sac.client.internal.member.user.vo.SearchUserDeviceSacRes;
+import com.skplanet.storeplatform.sac.client.internal.member.user.vo.UserDeviceInfoSac;
+import com.skplanet.storeplatform.sac.client.purchase.history.vo.HistoryCountSacReq;
+import com.skplanet.storeplatform.sac.client.purchase.history.vo.HistoryCountSacRes;
+import com.skplanet.storeplatform.sac.client.purchase.history.vo.HistoryListSacReq;
+import com.skplanet.storeplatform.sac.client.purchase.history.vo.HistoryListSacRes;
+import com.skplanet.storeplatform.sac.client.purchase.history.vo.HistoryListSacV2Req;
+import com.skplanet.storeplatform.sac.client.purchase.history.vo.HistoryListSacV2Res;
+import com.skplanet.storeplatform.sac.client.purchase.history.vo.HistorySac;
+import com.skplanet.storeplatform.sac.client.purchase.history.vo.HistorySacV2;
+import com.skplanet.storeplatform.sac.client.purchase.history.vo.ProductCountSac;
+import com.skplanet.storeplatform.sac.client.purchase.history.vo.ProductListSac;
+import com.skplanet.storeplatform.sac.purchase.common.service.PurchaseTenantPolicyService;
+import com.skplanet.storeplatform.sac.purchase.constant.PurchaseConstants;
 
 /**
  * 구매내역 Implements
@@ -160,7 +180,7 @@ public class HistoryListServiceImpl implements HistoryListService {
 			mdnCategoryList.add(obj.getTenantProdGrpCd());
 
 			if (!StringUtils.isBlank(request.getTenantProdGrpCd())
-					&& obj.getTenantProdGrpCd().equals(request.getTenantProdGrpCd())) {
+					&& StringUtils.startsWith(request.getTenantProdGrpCd(), obj.getTenantProdGrpCd())) {
 				selectDeviceYn = "Y"; // 디바이스정책 카테고리 조회일 경우 조회 조건으로 DeviceKey를 넣는다.
 			}
 		}
