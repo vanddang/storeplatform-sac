27b861868844cfd00e956174c7ee9402ff22bc91
양주원 <sden2720@gmail.com>
2015-10-02 06:32:48 +0000

    조회쿼리 없이 업데이트쿼리로 처리
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac@122857 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/purchase/history/service/PurchaseDrmInfoServiceImpl.java
+++ src/main/java/com/skplanet/storeplatform/sac/purchase/history/service/PurchaseDrmInfoServiceImpl.java
@@ -14,14 +14,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.skplanet.storeplatform.framework.core.util.StringUtils;
 import com.skplanet.storeplatform.purchase.client.history.sci.PurchaseDrmInfoSCI;
-import com.skplanet.storeplatform.purchase.client.history.vo.PurchaseDrmInfoSc;
 import com.skplanet.storeplatform.purchase.client.history.vo.PurchaseDrmInfoScReq;
 import com.skplanet.storeplatform.purchase.client.history.vo.PurchaseDrmInfoScRes;
 import com.skplanet.storeplatform.sac.client.internal.purchase.history.vo.PurchaseDrmInfoSacInReq;
 import com.skplanet.storeplatform.sac.client.internal.purchase.history.vo.PurchaseDrmInfoSacInRes;
-import com.skplanet.storeplatform.sac.purchase.constant.PurchaseConstants;
 
 /**
  * 구매DRM정보 Implements
@@ -65,40 +62,14 @@ public class PurchaseDrmInfoServiceImpl implements PurchaseDrmInfoService {
 		 * SC Request Setting End
 		 *************************************************/
 
-		PurchaseDrmInfoSc purchaseDrmInfoSc = this.purchaseDrmInfoSCI.selectPrchsDtl(scRequest);
-
-		this.logger.info("####PurchaseDrmInfoServiceImpl.purchaseDrmInfoSc {}", purchaseDrmInfoSc);
-
-		// 구매내역이 존재하며 쇼핑상품이 아닌경우
-		if (purchaseDrmInfoSc != null
-				&& !StringUtils.startsWith(purchaseDrmInfoSc.getTenantProdGrpCd(),
-						PurchaseConstants.TENANT_PRODUCT_GROUP_SHOPPING)) {
-
-			// 처리대기 상태인 경우만 처리
-			if (StringUtils.equals(PurchaseConstants.PROCESSING_STATUS_STANDBY,
-					purchaseDrmInfoSc.getUsePeriodRedateCd())) {
-
-				// 선물건이고 수신확인이 된 경우에는 처리안함
-				if (StringUtils.equals(PurchaseConstants.PRCHS_CASE_GIFT_CD, purchaseDrmInfoSc.getPrchsCaseCd())
-						&& !StringUtils.isBlank(purchaseDrmInfoSc.getRecvDt())) {
-
-					// OR021101 : 처리대기
-					// OR021102 : 처리완료
-
-				} else {
-					/**
-					 * SC Call
-					 */
-					scResponse = this.purchaseDrmInfoSCI.updatePrchaseDrm(scRequest);
-				}
-			}
-		}
+		/** SC Call */
+		scResponse = this.purchaseDrmInfoSCI.updatePrchaseDrm(scRequest);
 
 		/*************************************************
 		 * SC -> SAC Response Setting Start
 		 *************************************************/
 		response.setPrchsId(request.getPrchsId());
-		response.setResultYn(!StringUtils.equals("Y", scResponse.getResultYn()) ? "N" : scResponse.getResultYn());
+		response.setResultYn(scResponse.getResultYn());
 		response.setProdId(request.getProdId());
 		response.setUseStartDt(scResponse.getUseStartDt());
 		response.setUseExprDt(scResponse.getUseExprDt());
