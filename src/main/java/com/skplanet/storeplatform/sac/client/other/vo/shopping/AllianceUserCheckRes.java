448ed185bec35fb077a5273ec52b7aacd9ab600d
심대진 <tlaeo00@gmail.com>
2015-10-14 04:22:47 +0000

    신규 상품권 제휴사 회원 인증 조회 API 개발. 내용 : 신규 API 개발.
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@123444 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

---
+++ src/main/java/com/skplanet/storeplatform/sac/client/other/vo/shopping/AllianceUserCheckRes.java
@@ -0,0 +1,77 @@
+package com.skplanet.storeplatform.sac.client.other.vo.shopping;
+
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+/**
+ * 제휴사 회원 유효성 체크 Value Object
+ *
+ * Updated on : 2015. 10. 07. Updated by : 심대진, 다모아 솔루션.
+ */
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+public class AllianceUserCheckRes {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 쇼핑 브랜드ID.
+     */
+    private String brandId;
+    /**
+     * 쇼핑 CMD 브랜드ID.
+     */
+    private String cmsBrandId;
+    /**
+     * 제휴사 회원 ID.
+     */
+    private String chargeId;
+    /**
+     * 제휴사 회원명.
+     */
+    private String chargeNm;
+    /**
+     * 제휴사 회원 여부.
+     * - Y : 제휴사 회원
+     * - N : 제휴사 회원 아님
+     */
+    private String allianceUserYn;
+
+    public String getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(String brandId) {
+        this.brandId = brandId;
+    }
+
+    public String getCmsBrandId() {
+        return cmsBrandId;
+    }
+
+    public void setCmsBrandId(String cmsBrandId) {
+        this.cmsBrandId = cmsBrandId;
+    }
+
+    public String getChargeId() {
+        return chargeId;
+    }
+
+    public void setChargeId(String chargeId) {
+        this.chargeId = chargeId;
+    }
+
+    public String getChargeNm() {
+        return chargeNm;
+    }
+
+    public void setChargeNm(String chargeNm) {
+        this.chargeNm = chargeNm;
+    }
+
+    public String getAllianceUserYn() {
+        return allianceUserYn;
+    }
+
+    public void setAllianceUserYn(String allianceUserYn) {
+        this.allianceUserYn = allianceUserYn;
+    }
+}
