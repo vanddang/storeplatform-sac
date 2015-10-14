98b2e49ec442651ca7fe55492e322d98bd582138
최진호 <jhchoi@bogogt.com>
2015-10-14 01:03:46 +0000

    2015.10.28 배송지 관리 API 추가 - 기본 배송지 2개 > 1개로 수정, 조회시 배송지타입 optional 기능 추가
    git-svn-id: https://skvalley.com/svn/repos/storeplatform/trunk/sac/storeplatform-sac-client@123411 01f5c84a-aead-4b29-b0f1-7c54dbeec05f

--- src/main/java/com/skplanet/storeplatform/sac/client/member/vo/user/SearchDeliveryInfoSacReq.java
+++ src/main/java/com/skplanet/storeplatform/sac/client/member/vo/user/SearchDeliveryInfoSacReq.java
@@ -14,6 +14,9 @@ public class SearchDeliveryInfoSacReq extends CommonInfo {
 	/** 사용자 키 */
 	private String userKey;
 
+	/** 배송지 타입 */
+	private String deliveryTypeCd;
+
 	/**
 	 * 사용자 키를 리턴한다.
 	 * 
@@ -31,6 +34,25 @@ public class SearchDeliveryInfoSacReq extends CommonInfo {
 	 */
 	public void setUserKey(String userKey) {
 		this.userKey = userKey;
+	}
+
+	/**
+	 * 배송지 타입을(를) 리턴한다.
+	 * 
+	 * @return deliveryTypeCd - 배송지 타입
+	 */
+	public String getDeliveryTypeCd() {
+		return this.deliveryTypeCd;
+	}
+
+	/**
+	 * 배송지 타입을(를) 셋팅한다.
+	 * 
+	 * @param deliveryTypeCd
+	 *            배송지 타입
+	 */
+	public void setDeliveryTypeCd(String deliveryTypeCd) {
+		this.deliveryTypeCd = deliveryTypeCd;
 	}
 
 	/**
