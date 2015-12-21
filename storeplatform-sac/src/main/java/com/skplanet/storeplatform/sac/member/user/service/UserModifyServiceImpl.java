8da60d139f6d48d1edd31b58a305aed065667b94
bottangs <bottangs@gmail.com>
2015-12-21 14:58:46 +0900

    2.1.17.  성인 인증 정보 등록 변경 : IDP 연동 제거, TENANT_ID 제거

--- storeplatform-sac/src/main/java/com/skplanet/storeplatform/sac/member/user/service/UserModifyServiceImpl.java
+++ storeplatform-sac/src/main/java/com/skplanet/storeplatform/sac/member/user/service/UserModifyServiceImpl.java
@@ -339,154 +339,18 @@ public class UserModifyServiceImpl implements UserModifyService {
 	@Override
 	public CreateRealNameRes regRealName(SacRequestHeader sacHeader, CreateRealNameReq req) {
 
-		/**
-		 * userAuthKey 가 정의된 값과 같은지 비교하여 연동 여부를 판단한다.
-		 */
-		if (this.mcc.isIdpConnect(req.getUserAuthKey())) {
-
-			/**
-			 * 회원 정보 조회.
-			 */
-			UserInfo userInfo = this.mcc.getUserBaseInfo("userKey", req.getUserKey(), sacHeader);
-
-			/**
-			 * 통합서비스번호 존재 유무로 통합회원인지 기존회원인지 판단한다. (UserType보다 더 신뢰함.) 회원 타입에 따라서 [통합IDP, 기존IDP] 연동처리 한다.
-			 * 
-			 * 통합회원일경우만 처리한다.
-			 */
-			LOGGER.debug("## 사용자 타입  : {}", userInfo.getUserType());
-			LOGGER.debug("## 통합회원번호 : {}", StringUtils.isNotEmpty(userInfo.getImSvcNo()));
-			if (StringUtils.isNotEmpty(userInfo.getImSvcNo())) {
-
-				LOGGER.debug("## ====================================================");
-				LOGGER.debug("## One ID 통합회원일 경우만 통합 IDP 연동을 한다.");
-				LOGGER.debug("## ====================================================");
-
-				/**
-				 * 실명인증 대상 여부에 따라 분기 처리. (OWN=본인, PARENT=법정대리인)
-				 */
-				if (StringUtils.equals(req.getIsOwn(), MemberConstants.AUTH_TYPE_OWN)) { // 본인
-
-					/**
-					 * 통합IDP 회원정보 조회 연동 (cmd - findCommonProfileForServerIDP))
-					 */
-					UserInfoIdpSearchServerEcReq userInfoIdpSearchServerEcReq = new UserInfoIdpSearchServerEcReq();
-					userInfoIdpSearchServerEcReq.setKey(userInfo.getImSvcNo()); // 통합서비스 관리번호
-					UserInfoIdpSearchServerEcRes userInfoIdpSearchServerEcRes = this.imIdpSCI
-							.userInfoIdpSearchServer(userInfoIdpSearchServerEcReq);
-
-					/**
-					 * OnedID 조회 정보와 Request 로 받은 정보와 비교 로직 수행.
-					 */
-					String oneIdRealNameType = this.compareRealName(sacHeader, req, userInfoIdpSearchServerEcRes);
-
-					if (StringUtils.isNotEmpty(oneIdRealNameType)) {
-
-						/**
-						 * is_rname_auth, user_birthday, user_ci 가 동일해야만 업데이트
-						 * 
-						 * 실명인증을 받았다 하더라도 개명 등의 이유로 이름은 변경될 수 있다.
-						 * 
-						 * 통합IDP 실명인증 본인 연동 (cmd = TXUpdateUserNameIDP)
-						 */
-						UpdateUserNameEcReq updateUserNameEcReq = new UpdateUserNameEcReq();
-						updateUserNameEcReq.setIsRnameAuth(MemberConstants.USE_Y); // 실명인증 유무
-						updateUserNameEcReq.setKey(userInfo.getImSvcNo());
-						updateUserNameEcReq.setUserAuthKey(req.getUserAuthKey());
-						updateUserNameEcReq.setUserName(req.getUserName()); // 이름
-						updateUserNameEcReq.setUserBirthday(req.getUserBirthDay()); // 생년월일
-						updateUserNameEcReq.setUserSex(req.getUserSex()); // 성별 (M=남자, F=여자, N:미확인)
-						// 실명인증 수단 코드 1: 휴대폰, 2: 아이핀, 9:기타
-						updateUserNameEcReq.setRnameAuthMnsCode(this.convertRealNameMethod(req.getRealNameMethod(),
-								req.getIsOwn()));
-						// 실명인증 회원 코드 10 : 내국인, 20 : 외국인
-						updateUserNameEcReq.setRnameAuthMbrCode(this.convertResident(req.getResident()));
-						updateUserNameEcReq.setRnameAuthTypeCd(oneIdRealNameType); // 실명 인증 유형 코드 R=회원 개명 E=CI 기보유
-						updateUserNameEcReq.setUserCi(req.getUserCi());
-						updateUserNameEcReq.setUserDi(req.getUserDi());
-						updateUserNameEcReq.setRnameAuthDate(req.getRealNameDate());
-						this.imIdpSCI.updateUserName(updateUserNameEcReq);
-
-					}
-
-				} else if (StringUtils.equals(req.getIsOwn(), MemberConstants.AUTH_TYPE_PARENT)) { // 법정대리인
-
-					if (StringUtils.isBlank(req.getUserBirthDay())) {
-						throw new StorePlatformException("SAC_MEM_0002", "userBirthDay");
-					}
-
-					if (StringUtils.isBlank(req.getParentBirthDay())) {
-						throw new StorePlatformException("SAC_MEM_0002", "parentBirthDay");
-					}
-
-					/**
-					 * 법정대리인 나이 유효성 체크.
-					 */
-					this.mcc.checkParentBirth(req.getUserBirthDay(), req.getParentBirthDay());
-
-					try {
-
-						/**
-						 * 통합IDP 실명인증 법정대리인 연동 (cmd = TXUpdateGuardianInfoIDP)
-						 */
-						UpdateGuardianEcReq updateGuardianEcReq = new UpdateGuardianEcReq();
-						updateGuardianEcReq.setKey(userInfo.getImSvcNo());
-						updateGuardianEcReq.setUserAuthKey(req.getUserAuthKey());
-						// 법정대리인관계코드 (0:부, 1:모, 2:기타)
-						updateGuardianEcReq.setParentType(this.convertParentType(req.getParentType()));
-						updateGuardianEcReq.setParentRnameAuthKey(req.getUserCi()); // 법정대리인 실명인증 값 (CI) [외국인은 null
-																					// 로....]
-						// 법정대리인실명인증수단코드 1:휴대폰 본인인증, , 3:IPIN, 6:이메일 (외국인 법정대리인 인증)
-						updateGuardianEcReq.setParentRnameAuthType(this.convertRealNameMethod(req.getRealNameMethod(),
-								req.getIsOwn()));
-						// 법정대리인동의여부 Y=동의, N=미동의 (Y만 가능)
-						updateGuardianEcReq.setIsParentApprove(MemberConstants.USE_Y);
-						updateGuardianEcReq.setParentName(req.getUserName());
-						updateGuardianEcReq.setParentBirthday(req.getParentBirthDay()); // 법정대리인 생년월일
-						updateGuardianEcReq.setParentEmail(req.getParentEmail());
-						if (req.getRealNameDate().length() == 14) {
-							// 법정대리인동의일자(YYYYMMDD)
-							updateGuardianEcReq.setParentApproveDate(DateUtil.changeDateStringtoEight(req
-									.getRealNameDate()));
-						} else {
-							updateGuardianEcReq.setParentApproveDate(req.getRealNameDate());
-						}
-						this.imIdpSCI.updateGuardian(updateGuardianEcReq);
-
-					} catch (StorePlatformException spe) {
-
-						if (StringUtils.equals(spe.getErrorInfo().getCode(), MemberConstants.EC_IDP_ERROR_CODE_TYPE
-								+ "2402X000")) {
-
-							/**
-							 * IDP 에러 [[ 2402X000 : 법정대리인 동의된 회원입니다.]] ==> Skip 처리 한다.
-							 * 
-							 * 2014.02.21 임재호K 협의. (IDP의 실명인증과 SAC의 실명인증은 별개로 본다.)
-							 */
-							LOGGER.debug("## >> Skip 처리 한다.");
-							LOGGER.debug("## >> Error Code : {}", spe.getErrorInfo().getCode());
-							LOGGER.debug("## >> Error Msg  : {}", spe.getErrorInfo().getMessage());
-
-						} else {
-
-							throw spe;
-
-						}
-
-					}
-
-				} else { // 법인
-
-					/**
-					 * 법인 단말일 경우 Skip.
-					 */
-					LOGGER.debug("## >> 법인 단말일 경우는 Skip.");
-
-				}
-
-			} // 통합회원 비교
-
-		} // userAuthKey 비교
+		/** 회원 정보 조회. */
+		List<KeySearch> keySearchList = new ArrayList<KeySearch>();
+		KeySearch keySchUserKey = new KeySearch();
+		keySchUserKey.setKeyType(MemberConstants.KEY_TYPE_INSD_USERMBR_NO);
+		keySchUserKey.setKeyString(req.getUserKey());
+		keySearchList.add(keySchUserKey);
+		SearchExtentUserRequest searchExtentUserRequest = new SearchExtentUserRequest();
+		CommonRequest commonRequest = new CommonRequest();
+		searchExtentUserRequest.setCommonRequest(commonRequest);
+		searchExtentUserRequest.setKeySearchList(keySearchList);
+		searchExtentUserRequest.setUserInfoYn(MemberConstants.USE_Y);
+		this.userSCI.searchExtentUser(searchExtentUserRequest);
 
 		/**
 		 * SC 실명인증 연동.
@@ -582,7 +446,6 @@ public class UserModifyServiceImpl implements UserModifyService {
 			 */
 			MbrAuth mbrAuth = new MbrAuth();
 			mbrAuth.setIsRealName(MemberConstants.USE_Y); // 실명인증 여부
-			mbrAuth.setTenantID(sacHeader.getTenantHeader().getTenantId()); // 테넌트 아이디
 			mbrAuth.setBirthDay(req.getUserBirthDay()); // 생년월일
 			mbrAuth.setTelecom(req.getDeviceTelecom()); // 이동 통신사
 			mbrAuth.setPhone(req.getUserPhone()); // 사용자 휴대폰
@@ -701,114 +564,6 @@ public class UserModifyServiceImpl implements UserModifyService {
 
 	/**
 	 * <pre>
-	 * 실명인증여부, 생년월일, CI 정보 IDP 결과또는 DB 와 정보가 동일한지 비교함.
-	 * </pre>
-	 * 
-	 * @param sacHeader
-	 *            공통 헤더
-	 * @param req
-	 *            Request Value Object
-	 * @param idpResult
-	 *            (통합IDP 회원 정보 조회 응답 결과)
-	 * @return String 결과 타입
-	 */
-	private String compareRealName(SacRequestHeader sacHeader, CreateRealNameReq req,
-			UserInfoIdpSearchServerEcRes idpResult) {
-
-		String oneIdRealNameType = "";
-
-		/**
-		 * 통합IDP 조회결과 is_rname_auth 여부가 Y or N 에 따라 분기 처리.
-		 */
-		if (StringUtils.equals(idpResult.getIsRnameAuth(), MemberConstants.USE_Y)) {
-
-			LOGGER.debug("####### IDP 실명인증 CI    : {}", idpResult.getUserCi());
-			LOGGER.debug("####### IDP 실명인증 Birth : {}", req.getUserBirthDay());
-
-			// One ID CI, birthday 비교
-			if (StringUtils.isNotEmpty(idpResult.getUserCi())) { // 기 등록 된 CI가 존재하는 경우는 CI + 생년월일(social_date) 비교
-				if (!StringUtils.equals(req.getUserCi(), idpResult.getUserCi())
-						&& !StringUtils.equals(req.getUserBirthDay(), idpResult.getUserBirthday())) {
-					throw new StorePlatformException("SAC_MEM_1400");
-				}
-			} else { // 생년월일(social_date) 비교
-				if (!StringUtils.equals(req.getUserBirthDay(), idpResult.getUserBirthday())) {
-					throw new StorePlatformException("SAC_MEM_1401");
-				}
-			}
-
-			if (StringUtils.equals(req.getUserName(), idpResult.getUserName())) {
-				if (StringUtils.isEmpty(idpResult.getUserCi())) { // CI 기보유
-					oneIdRealNameType = "E";
-				}
-			} else { // 개명
-				LOGGER.debug("## 개명..........");
-				oneIdRealNameType = "R";
-			}
-
-			// /**
-			// * 타사이트에서 실명인증을 하고 t-Store 에서 개명 신청을 시도할 경우.
-			// */
-			// if (StringUtils.equals(oneIdRealNameType, "R")) {
-			// if (!StringUtils.equals(idpResult.getModifySstCode(), MemberConstants.SSO_SST_CD_TSTORE)) {
-			// throw new StorePlatformException("SAC_MEM_1402", idpResult.getModifySstCode());
-			// }
-			// }
-
-		} else { // 최초 인증
-
-			SearchUserRequest searchUserRequest = new SearchUserRequest();
-			searchUserRequest.setCommonRequest(this.mcc.getSCCommonRequest(sacHeader));
-
-			/**
-			 * 검색 조건 setting
-			 */
-			List<KeySearch> keySearchList = new ArrayList<KeySearch>();
-			KeySearch keySchUserKey = new KeySearch();
-			keySchUserKey.setKeyType(MemberConstants.KEY_TYPE_INSD_USERMBR_NO);
-			keySchUserKey.setKeyString(req.getUserKey());
-			keySearchList.add(keySchUserKey);
-			searchUserRequest.setKeySearchList(keySearchList);
-
-			/**
-			 * 회원 정보조회 (실명 인증정보)
-			 */
-			SearchUserResponse schUserRes = this.userSCI.searchUser(searchUserRequest);
-
-			/**
-			 * 인증 정보가 존재하면....
-			 */
-			if (schUserRes.getMbrAuth().getSequence() != null
-					|| StringUtils.equals(schUserRes.getMbrAuth().getSequence(), "")) {
-				LOGGER.debug("####### DB 실명인증 CI    : {}", schUserRes.getMbrAuth().getCi());
-				LOGGER.debug("####### DB 실명인증 Birth : {}", schUserRes.getMbrAuth().getBirthDay());
-
-				// One ID CI, birthday 비교
-				if (StringUtils.isNotEmpty(schUserRes.getMbrAuth().getCi())) { // 기 등록 된 CI가 존재하는 경우는 CI +
-																			   // 생년월일(social_date)
-					// 비교
-					if (!StringUtils.equals(req.getUserCi(), schUserRes.getMbrAuth().getCi())
-							&& !StringUtils.equals(req.getUserBirthDay(), schUserRes.getMbrAuth().getBirthDay())) {
-						throw new StorePlatformException("SAC_MEM_1400");
-					}
-				} else { // 생년월일(social_date) 비교
-					if (!StringUtils.equals(req.getUserBirthDay(), schUserRes.getMbrAuth().getBirthDay())) {
-						throw new StorePlatformException("SAC_MEM_1401");
-					}
-				}
-			}
-
-			oneIdRealNameType = "R";
-
-		}
-
-		LOGGER.debug("### oneIdRealNameType : {}", oneIdRealNameType);
-
-		return oneIdRealNameType;
-	}
-
-	/**
-	 * <pre>
 	 * 약관동의 정보를 등록 또는 수정하는 기능을 제공한다.
 	 * </pre>
 	 * 
@@ -923,63 +678,6 @@ public class UserModifyServiceImpl implements UserModifyService {
 
 	/**
 	 * <pre>
-	 * 실명인증 수단코드를 통합 IDP 규격에 맞게 Converting.
-	 * </pre>
-	 * 
-	 * @param realNameMethod
-	 *            실명인증 수단 코드 (US011101 : 휴대폰 인증, US011102 : IPIN 인증)
-	 * @param isOwn
-	 *            실명인증 대상 여부. (OWN=본인, PARENT=법정대리인)
-	 * @return 통합 IDP 코드[본인] (1 : 휴대폰, 2 : 아이핀, 9 : 기타)
-	 * 
-	 *         통합 IDP 코드[법정대리인] (1 : 휴대폰, 3 : 아이핀, 6 : 이메일(외국인 법정대리인 인증))
-	 */
-	private String convertRealNameMethod(String realNameMethod, String isOwn) {
-
-		if (StringUtils.equals(isOwn, MemberConstants.AUTH_TYPE_OWN)) { // 본인
-
-			if (StringUtils.equals(realNameMethod, MemberConstants.REAL_NAME_AUTH_MOBILE)) {
-				return "1"; // 휴대폰 인증
-			} else if (StringUtils.equals(realNameMethod, MemberConstants.REAL_NAME_AUTH_IPIN)) {
-				return "2"; // IPIN 인증
-			} else {
-				return "9"; // 기타
-			}
-
-		} else { // 법정대리인
-
-			if (StringUtils.equals(realNameMethod, MemberConstants.REAL_NAME_AUTH_MOBILE)) {
-				return "1"; // 휴대폰 인증
-			} else if (StringUtils.equals(realNameMethod, MemberConstants.REAL_NAME_AUTH_IPIN)) {
-				return "3"; // IPIN 인증
-			} else {
-				return "6"; // 이메일(외국인 법정대리인 인증)
-			}
-
-		}
-	}
-
-	/**
-	 * <pre>
-	 * 실명인증 회원코드를 통합 IDP 규격에 맞게 Converting.
-	 * </pre>
-	 * 
-	 * @param resident
-	 *            실명인증 회원코드 (local : 내국인, foreign : 외국인)
-	 * @return 통합 IDP 코드 (10 : 내국인, 20 : 외국인)
-	 */
-	private String convertResident(String resident) {
-		if (StringUtils.equals(resident, "local")) {
-			return "10"; // 내국인
-		} else if (StringUtils.equals(resident, "foreign")) {
-			return "20"; // 외국인
-		} else {
-			return "";
-		}
-	}
-
-	/**
-	 * <pre>
 	 * SC에 사용하는 정보로 내외국인 여부 Converting.
 	 * </pre>
 	 * 
@@ -994,25 +692,6 @@ public class UserModifyServiceImpl implements UserModifyService {
 			return MemberConstants.USE_N; // 외국인
 		} else {
 			return "";
-		}
-	}
-
-	/**
-	 * <pre>
-	 * 법정대리인관계코드를 통합 IDP 규격에 맞게 Converting.
-	 * </pre>
-	 * 
-	 * @param parentType
-	 *            법정대리인관계코드 (US011801 : 아버지, US011802 : 어머니, US011803 : 기타)
-	 * @return 통합 IDP 코드 (0 : 아버지, 1 : 어머니, 2 : 기타)
-	 */
-	private String convertParentType(String parentType) {
-		if (StringUtils.equals(parentType, MemberConstants.PARENT_TYPE_FATHER)) {
-			return "0"; // 아버지
-		} else if (StringUtils.equals(parentType, MemberConstants.PARENT_TYPE_MOTHER)) {
-			return "1"; // 어머니
-		} else {
-			return "2"; // 기타
 		}
 	}
 
