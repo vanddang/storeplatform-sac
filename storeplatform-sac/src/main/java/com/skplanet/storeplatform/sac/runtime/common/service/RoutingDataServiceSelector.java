9de60651d3f899c1fe97223bc9e2db9056b2af89
1001910 <kdlim@sk.com>
2016-02-02 18:33:43 +0900

    Onestore QA 서버 SAC -> EC 라우팅 로직 예외처리
    - 고도화QA, Onestore QA 서버 병행 운영을 위해
    - Onestore QA 서버일 경우 Onestore EC 도메인을 호출하도록  정보하드 코딩
    - 환경 변수 -DROUTING_ENV=onestore 설정

---
+++ storeplatform-sac/src/main/java/com/skplanet/storeplatform/sac/runtime/common/service/RoutingDataServiceSelector.java
@@ -0,0 +1,33 @@
+package com.skplanet.storeplatform.sac.runtime.common.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.Map;
+
+@Service
+public class RoutingDataServiceSelector {
+
+    // TODO : spring 4.x 버전 사용시 @Conditional 로 변경
+    @Value("#{systemProperties['QA_ENV'] != null && systemProperties['QA_ENV'].equals('onestore') ? " +
+            "'com.skplanet.storeplatform.sac.runtime.common.service.RoutingDataOnestoreQaServiceImpl' : " +
+            "'com.skplanet.storeplatform.sac.runtime.common.service.RoutingDataServiceImpl'}")
+    String routingDataServiceBeanName;
+
+    @Autowired
+    Map<String, RoutingDataService> routingDataServiceMap;
+
+    @PostConstruct
+    public void initRoutingDataServiceBean() {
+        routingDataService = routingDataServiceMap.get(routingDataServiceBeanName);
+    }
+
+    RoutingDataService routingDataService;
+
+    public RoutingDataService getRoutingDataService() {
+
+        return this.routingDataService;
+    }
+}
