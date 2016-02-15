39bb2e2154c8a25aa1c4c192c0a52f1d0d43416c
1001910 <kdlim@sk.com>
2016-02-15 13:27:29 +0900

    SACMEM-126 Captcha 생성 - Captcha 생성 Library 추가

---
+++ storeplatform-sac/src/main/java/com/skplanet/storeplatform/sac/member/miscellaneous/service/CaptchaGenerator.java
@@ -0,0 +1,101 @@
+package com.skplanet.storeplatform.sac.member.miscellaneous.service;
+
+import nl.captcha.Captcha;
+import nl.captcha.backgrounds.BackgroundProducer;
+import nl.captcha.backgrounds.TransparentBackgroundProducer;
+import nl.captcha.gimpy.GimpyRenderer;
+import nl.captcha.gimpy.RippleGimpyRenderer;
+import nl.captcha.noise.CurvedLineNoiseProducer;
+import nl.captcha.noise.NoiseProducer;
+import nl.captcha.text.producer.DefaultTextProducer;
+import nl.captcha.text.producer.NumbersAnswerProducer;
+import nl.captcha.text.producer.TextProducer;
+import nl.captcha.text.renderer.DefaultWordRenderer;
+import nl.captcha.text.renderer.WordRenderer;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CaptchaGenerator implements InitializingBean {
+
+    private BackgroundProducer backgroundProducer;
+    private NumbersAnswerProducer numbersAnswerProducer;
+    private TextProducer textProducer;
+    private WordRenderer wordRenderer;
+    private NoiseProducer noiseProducer;
+    private GimpyRenderer gimpyRenderer;
+    boolean isBorder;
+
+    public Captcha createCaptcha(int width, int height) {
+        Captcha.Builder builder = new Captcha.Builder(width, height)
+                .addBackground(backgroundProducer)
+                        //.addText(textProducer, wordRenderer)
+                .addText(numbersAnswerProducer)
+                .addNoise(noiseProducer);
+
+        if (isBorder) {
+            builder.addBorder();
+        }
+
+        return builder.build();
+    }
+
+    public void setBackgroundProducer(BackgroundProducer backgroundProducer) {
+        this.backgroundProducer = backgroundProducer;
+    }
+
+    public void setTextProducer(TextProducer textProducer) {
+        this.textProducer = textProducer;
+    }
+
+    public void setWordRenderer(WordRenderer wordRenderer) {
+        this.wordRenderer = wordRenderer;
+    }
+
+    public void setNoiseProducer(NoiseProducer noiseProducer) {
+        this.noiseProducer = noiseProducer;
+    }
+
+    public void setGimpyRenderer(GimpyRenderer gimpyRenderer) {
+        this.gimpyRenderer = gimpyRenderer;
+    }
+
+    public void setBorder(boolean isBorder) {
+        this.isBorder = isBorder;
+    }
+
+    public NumbersAnswerProducer getNumbersAnswerProducer() {
+        return numbersAnswerProducer;
+    }
+
+    public void setNumbersAnswerProducer(NumbersAnswerProducer numbersAnswerProducer) {
+        this.numbersAnswerProducer = numbersAnswerProducer;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        if (this.backgroundProducer == null) {
+            this.backgroundProducer = new TransparentBackgroundProducer();
+        }
+
+        if (this.textProducer == null) {
+            this.textProducer = new DefaultTextProducer();
+        }
+
+        if (this.wordRenderer == null) {
+            this.wordRenderer = new DefaultWordRenderer();
+        }
+
+        if (this.numbersAnswerProducer == null) {
+            this.numbersAnswerProducer = new NumbersAnswerProducer();
+        }
+
+        if (this.noiseProducer == null) {
+            this.noiseProducer = new CurvedLineNoiseProducer();
+        }
+
+        if (this.gimpyRenderer == null) {
+            this.gimpyRenderer = new RippleGimpyRenderer();
+        }
+    }
+}
