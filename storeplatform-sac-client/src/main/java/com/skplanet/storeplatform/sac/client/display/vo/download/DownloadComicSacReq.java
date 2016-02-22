b3a236d8c18e8a76ba3f373d655e253fdad80d65
이유진(Youjin Lee)/Store기술개발팀/SKP <jinypty@sk.com>
2016-02-22 15:33:33 +0900

    refs #61901 이북 for D/L DRM 키 변경 개발 - Wi-Fi 전용 단말을 위한 DRM키 정책 추가 - userType이 null or 모바일회원(US011501) 인 경우 DRM키가 MDN / 그 외 회원타입은 userKey

--- storeplatform-sac-client/src/main/java/com/skplanet/storeplatform/sac/client/display/vo/download/DownloadComicSacReq.java
+++ storeplatform-sac-client/src/main/java/com/skplanet/storeplatform/sac/client/display/vo/download/DownloadComicSacReq.java
@@ -39,6 +39,7 @@ public class DownloadComicSacReq extends CommonInfo {
 	private String imageCd; // 이미지코드
 	private String anyDeviceModelCd; // 가상 프로비저닝 단말모델코드
 
+	private String userType; // 회원 타입. (US011501 : 모바일회원, US011502 : IDP회원, US011503 : OneID회원, 기타......등등....)
 
 	/**
 	 * @return the productId
@@ -221,7 +222,12 @@ public class DownloadComicSacReq extends CommonInfo {
 		this.additionalMsisdn = additionalMsisdn;
 	}
 
+	public String getUserType() {
+		return userType;
+	}
 
-
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
 
 }
